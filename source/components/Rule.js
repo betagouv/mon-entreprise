47f4b7c5ce981e133508573424323fb2397c1943
Mael Thomas <mael.thomas@data.gouv.fr>
2016-12-16 17:09:21 +0100

    Visualisation intéreactive des règles

--- source/components/Rule.js
+++ source/components/Rule.js
@@ -1,15 +1,17 @@
 import React, { Component } from 'react'
 import {findRuleByName} from '../model.js'
-import {safeDump} from 'js-yaml'
 import './Rule.css'
+import JSONTree from 'react-json-tree'
 
 export default class Rule extends Component {
 	render() {
+		let json = findRuleByName(this.props.params.name)[2]
+
 		return (
 			<div id="variable">
 				<pre>
 					<code className="yaml">
-						{safeDump(findRuleByName(this.props.params.name)[2])}
+						<JSONTree hideRoot="true" shouldExpandNode={() => true} data={json} />
 					</code>
 				</pre>
 			</div>
