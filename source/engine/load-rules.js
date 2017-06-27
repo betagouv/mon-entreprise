81bfb9b22de36ced993ee5adbc4e1938f19e3ce7
Laurent Bossavit <laurent.git@bossavit.com>
2017-06-27 13:36:31 +0200

    :gear: Clarifie et documente le chargement des règles

--- source/engine/load-rules.js
+++ source/engine/load-rules.js
@@ -1,30 +1,28 @@
 import R from 'ramda'
 
-/*
-TODO sélection temporaire de dossier, tant que toute la base de règles n'est pas vérifiée
-*/
-
-let requireAll = requireContext =>
-  requireContext.keys().map(requireContext)
-
-let rules= R.pipe(
-  R.chain(
-    requireAll,
-  ),
-  R.unnest,
-  R.reject(R.isNil)
-)( // This array can't be generated, as the arguments to require.context must be literals :-|
+// This array can't be generated, as the arguments to require.context must be literals :-|
+let directoryLoaders = 
   [
-  require.context(
-    '../../règles/rémunération-travail/cdd',
-    true, /([A-Za-z\u00C0-\u017F]|\.|-|_)+.yaml$/),
-  require.context(
-    '../../règles/rémunération-travail/entités/ok',
-    true, /([A-Za-z\u00C0-\u017F]|\.|-|_)+.yaml$/),
-  require.context(
-    '../../règles/rémunération-travail/cotisations/ok',
-    true, /([A-Za-z\u00C0-\u017F]|\.|-|_)+.yaml$/),
-])
+    require.context('../../règles/rémunération-travail/cdd',
+                    true, /([A-Za-z\u00C0-\u017F]|\.|-|_)+.yaml$/),
+    require.context('../../règles/rémunération-travail/entités/ok',
+                    true, /([A-Za-z\u00C0-\u017F]|\.|-|_)+.yaml$/),
+    require.context('../../règles/rémunération-travail/cotisations/ok',
+                    true, /([A-Za-z\u00C0-\u017F]|\.|-|_)+.yaml$/),
+  ]
 
+// require.context returns an object which
+// a) is a function behaving like 'requires', taking a filename and returning a module and
+// b) has additional properties, some function-valued; keys() returns the files matched
+// A "module" is simply the contents of a file according to a Webpack loader; this can be JS, JSON, etc.
+// Thus, this weird loadAll returns an array, each item of which is the contents of each file in a directory
+let loadAll = directoryLoaderFunction =>
+  directoryLoaderFunction.keys().map(directoryLoaderFunction)
+
+let rules = R.pipe(
+  R.map(loadAll),
+  R.flatten,
+  R.reject(R.isNil)
+)(directoryLoaders)
 
 export default rules
