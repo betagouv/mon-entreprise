1c330558df97ad354a6d3745c2b3fe67e0372105
Laurent Bossavit <laurent.git@bossavit.com>
2017-06-27 15:14:30 +0200

    :white_check_mark: Ajoute chai et quelques tests moins triviaux

---
+++ __tests__/rules.test.js
@@ -0,0 +1,20 @@
+import {expect} from 'chai'
+import {enrichRule} from '../source/engine/rules'
+
+describe('enrichRule', function() {
+
+  it('should extract the type of the rule', function() {
+    let rule = {cotisation:{}}
+    expect(enrichRule(rule)).to.have.property('type','cotisation')
+  });
+
+  it('should extract the dotted name of the rule', function() {
+    let rule = {espace:"contrat salarié", nom: "CDD"}
+    expect(enrichRule(rule)).to.have.property('dottedName','contrat salarié . CDD')
+  });
+
+  it('should render Markdown in sub-questions', function() {
+    let rule = {"sous-question":"**wut**"}
+    expect(enrichRule(rule)).to.have.property('subquestion','<p><strong>wut</strong></p>\n')
+  });
+});
