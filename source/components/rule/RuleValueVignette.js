8ff141eff98f8374eae30e30eec228b1d0391f7f
mama <mael.thomas@data.gouv.fr>
2017-10-19 14:45:39 +0200

    Ajout d'un flash au changement d'un résultat

--- source/components/rule/RuleValueVignette.js
+++ source/components/rule/RuleValueVignette.js
@@ -1,11 +1,13 @@
 import React from "react"
-import {Link} from 'react-router-dom'
-import {encodeRuleName} from 'Engine/rules'
-import classNames from 'classnames'
-import {capitalise0} from '../../utils'
-let fmt = new Intl.NumberFormat('fr-FR').format
-export let humanFigure = decimalDigits => value => fmt(value.toFixed(decimalDigits))
-import './RuleValueVignette2.css'
+import { Link } from "react-router-dom"
+import { encodeRuleName } from "Engine/rules"
+import classNames from "classnames"
+import { capitalise0 } from "../../utils"
+let fmt = new Intl.NumberFormat("fr-FR").format
+export let humanFigure = decimalDigits => value =>
+	fmt(value.toFixed(decimalDigits))
+import "./RuleValueVignette2.css"
+import ReactCSSTransitionGroup from "react-addons-css-transition-group"
 
 export default ({
 	name,
@@ -15,36 +17,49 @@ export default ({
 	nodeValue: ruleValue
 }) =>
 	do {
-		let
-			unsatisfied = ruleValue == null,
+		let unsatisfied = ruleValue == null,
 			irrelevant = ruleValue == 0,
-			number = typeof ruleValue == 'number' && ruleValue > 0
-
+			number = typeof ruleValue == "number" && ruleValue > 0
 		;<span
 			key={name}
-			className={classNames('RuleValueVignette', { unsatisfied, irrelevant, number })}
+			className={classNames("RuleValueVignette", {
+				unsatisfied,
+				irrelevant,
+				number
+			})}
 		>
 			<Link to={"/regle/" + encodeRuleName(name)}>
-				<div className="rule-type">
-					{type}
-				</div>
+				<div className="rule-type">{type}</div>
 				<div className="rule-box">
-					<span className="rule-name">
-						{titre || capitalise0(name)}
-					</span>
-					<span>{' '}
-						{conversationStarted &&
-							(irrelevant
-								? "Vous n'êtes pas concerné"
-								: unsatisfied
-									? <em>En attente de vos réponses...</em>
-									: <div><span className="figure">
-										<b>{humanFigure(2)(ruleValue) + " €"}</b>
-									</span>
-									{/*<p><i className="fa fa-lightbulb-o" aria-hidden="true"></i><em>Pourquoi ?</em></p> */}
-									</div>)}
-					</span>
+					<span className="rule-name">{titre || capitalise0(name)}</span>
+					<RuleValue
+						{...{ unsatisfied, irrelevant, conversationStarted, ruleValue }}
+					/>
 				</div>
 			</Link>
 		</span>
+	}
+
+let RuleValue = ({ unsatisfied, irrelevant, conversationStarted, ruleValue }) =>
+	do {
+		let [className, text] = irrelevant
+			? ["irrelevant", "Vous n'êtes pas concerné"]
+			: unsatisfied
+				? ["unsatisfied", "En attente de vos réponses..."]
+				: ["figure", humanFigure(2)(ruleValue) + " €"]
+
+		{
+			/*<p><i className="fa fa-lightbulb-o" aria-hidden="true"></i><em>Pourquoi ?</em></p> */
+		}
+
+		<ReactCSSTransitionGroup
+			transitionName="flash"
+			transitionEnterTimeout={100}
+			transitionLeaveTimeout={100}
+		>
+			<span key={text} className="rule-value">
+				{" "}
+				{conversationStarted && <span className={className}>{text}</span>}
+			</span>
+		</ReactCSSTransitionGroup>
 	}
