2253385ff9680bbf313a2c7ecb58dbbaeb4be3df
mama <mael.thomas@data.gouv.fr>
2017-11-29 10:30:06 +0100

    Affichage du nombre de tests par mécanisme

--- source/components/Mecanisms.js
+++ source/components/Mecanisms.js
@@ -2,13 +2,31 @@
 import React, { Component } from 'react'
 import knownMecanims from 'Engine/known-mecanisms.yaml'
 import R from 'ramda'
+import './Mecanisms.css'
+
+let directoryLoader = require.context('../../test/mécanismes/', true, /.yaml$/),
+	tests = R.fromPairs(
+		directoryLoader
+			.keys()
+			.map(key => [
+				key.replace(/\/|\.|(yaml)/g, '').replace(/-/g, ' '),
+				directoryLoader(key)
+			])
+	)
 
 export default class Mecanisms extends Component {
 	render() {
 		return (
-			<ul>
+			<ul id="mecanisms">
 				{R.toPairs(knownMecanims).map(([name, data]) => (
-					<li key={name}>{name}</li>
+					<li key={name}>
+						{name}
+						{tests[name] == null ? (
+							<p className="warning">Pas de tests !</p>
+						) : (
+							<p>nombre de tests {tests[name].length}</p>
+						)}
+					</li>
 				))}
 			</ul>
 		)
