f86903c929a9af9e745c3fb91ad16ac1d583a556
Laurent Bossavit <laurent.git@bossavit.com>
2017-11-29 20:37:24 +0100

    :gear: Affiche le détail des branches

--- test/results.grid.test.js
+++ test/results.grid.test.js
@@ -4,7 +4,7 @@ import {rules as realRules, enrichRule} from '../source/engine/rules'
 import {analyse, parseAll} from '../source/engine/traverse'
 import {reduceSteps} from '../source/reducers'
 
-import {byBranch, byName, cell} from '../source/components/ResultsGrid.js'
+import {byBranch, byName, cell, subCell} from '../source/components/ResultsGrid.js'
 
 let tracker = {push: array => null}
 
@@ -72,9 +72,10 @@ describe('results grid', function() {
 		maladie = byName(result.maladie),
 		names = R.keys(maladie)
 
-	expect(names).to.have.lengthOf(3)
+	expect(names).to.have.lengthOf(4)
 	expect(names).to.include("contrat salarié . maladie")
 	expect(names).to.include("contrat salarié . ATMP")
+	expect(names).to.include("contrat salarié . complémentaire santé")
 	expect(names).to.include("contrat salarié . cotisation pénibilité")
   });
 
@@ -92,7 +93,26 @@ describe('results grid', function() {
 	let analysis = step2.analysis
 
 	expect(cell("retraite","salarié",analysis)).to.be.closeTo(257, 5)
-	expect(cell("autre","salarié",analysis)).to.be.closeTo(200, 5)
+	expect(cell("autre","salarié",analysis)).to.be.closeTo(180, 5)
+  });
+
+  it('should access cell values', function() {
+    let fakeState = {}
+    let stateSelector = state => name => fakeState[name]
+
+    let rules = realRules.map(enrichRule),
+        reducer = reduceSteps(tracker, rules, stateSelector)
+
+    var step1 = reducer({foldedSteps: []},{type:'START_CONVERSATION', targetNames: ['salaire net', 'salaire total']})
+	fakeState['contrat salarié . salaire de base'] = 2300
+	var step2 = reducer(step1,{type:'STEP_ACTION', name: 'fold', step: 'contrat salarié . salaire de base'})
+
+	let analysis = step2.analysis,
+		result = byBranch(analysis),
+		maladie = byName(result.maladie)
+
+	expect(subCell(maladie,"contrat salarié . ATMP","salarié")).to.be.closeTo(0, 0.1)
+	expect(subCell(maladie,"contrat salarié . ATMP","employeur")).to.be.closeTo(54, 1)
   });
 
 });
