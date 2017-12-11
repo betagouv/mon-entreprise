b0a01b61f57bb5d82f36290748f0f58c3f75e35c
mama <mael.thomas@data.gouv.fr>
2017-12-11 14:38:49 +0100

    Ajout d'un menu mobile

--- source/components/Home.js
+++ source/components/Home.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import './Home.css'
 import ReactPiwik from 'Components/Tracker' //TODO réintégrer Piwik
 import TargetSelection from './TargetSelection'
-
+import { slide as Menu } from 'react-burger-menu'
+import './Menu.css'
 
 export default class Home extends Component {
 
@@ -18,6 +19,11 @@ export default class Home extends Component {
 					/>
 					<h1>Simulateurs d'embauche</h1>
 				</div>
+				<Menu right>
+					<a className="menu-item" href="/">À propos</a>
+					<a className="menu-item" href="/about">Intégrer le module</a>
+					<a className="menu-item" href="/contact">Contribuer</a>
+				</Menu>
 				<div id="content">
 					<TargetSelection />
 				</div>
