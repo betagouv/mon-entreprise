dd03b4598bb22d4e7e3a5188f173a9c10a6b78b1
mama <mael.thomas@data.gouv.fr>
2017-12-12 10:30:24 +0100

    :art: Compatible grands écrans

--- source/components/pages/Header.js
+++ source/components/pages/Header.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { slide as Menu } from 'react-burger-menu'
-import 'Components/pages/Menu.css'
+import 'Components/pages/Header.css'
 import { Link } from 'react-router-dom'
 
 export default class Header extends Component {
@@ -14,24 +14,32 @@ export default class Header extends Component {
 						alt="Un service de l'État français"
 					/>
 				</Link>
-				<Menu right>
-					<Link className="menu-item" to="/">
-						Accueil
-					</Link>
-					<Link className="menu-item" to="/à-propos">
-						À propos
-					</Link>
-					<Link className="menu-item" to="/intégrer">
-						Intégrer le module
-					</Link>
-					<Link className="menu-item" to="/contribuer">
-						Contribuer
-					</Link>
-					<Link className="menu-item" to="/regles">
-						Toutes nos règles
-					</Link>
-				</Menu>
+				<nav>
+					<Menu right>
+						<Links />
+					</Menu>
+					{/* Et maintenant le menu pour grand écran, activé en CSS */}
+					<Links />
+				</nav>
 			</div>
 		)
 	}
 }
+
+let Links = () => <>
+	<Link className="menu-item" to="/">
+		<i className="fa fa-home" aria-hidden="true"></i>
+	</Link>
+	<Link className="menu-item" to="/intégrer">
+		<em>Intégrer le module</em>
+	</Link>
+	<Link className="menu-item" to="/contribuer">
+		Contribuer
+	</Link>
+	<Link className="menu-item" to="/regles">
+		Toutes nos règles
+	</Link>
+	<Link className="menu-item" to="/à-propos">
+		À propos
+	</Link>
+</>
