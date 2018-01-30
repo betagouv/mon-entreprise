ae610eb2b41c32834de06351e9c77c01d6c8a089
mama <mael.thomas@data.gouv.fr>
2018-01-30 17:11:33 +0100

    :bug: Si erreur de saisie, pas de mise à jour des résultats
    Cela évite de montrer un NaN

--- source/debounceFormChangeActions.js
+++ source/debounceFormChangeActions.js
@@ -10,11 +10,14 @@ export default () => {
 
 		let key = type
 
-		const shouldDebounce = key === '@@redux-form/CHANGE'
+		let shouldDebounce = key === '@@redux-form/CHANGE'
 
-		if (!shouldDebounce) {
-			return dispatch(action)
+		if (key === '@@redux-form/UPDATE_SYNC_ERRORS') {
+			dispatch(action)
+			return clearTimeout(timers['@@redux-form/CHANGE'])
 		}
+
+		if (!shouldDebounce) return dispatch(action)
 
 		if (timers[key]) {
 			clearTimeout(timers[key])
