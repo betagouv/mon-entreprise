6d625b4d9efe499c049f28a9f67c5d8f0bee73b3
mama <mael.thomas@data.gouv.fr>
2018-02-05 20:43:08 +0100

    :memo: Mise à jour de la documentation d'intégration

--- source/components/pages/Integration.js
+++ source/components/pages/Integration.js
@@ -16,7 +16,7 @@ export default class MyComponent extends Component {
 							id
 						</em>="script-simulateur-embauche" <em>
 							data-couleur
-						</em>="<span id="scriptColor">#2975D1</span>" <em>src</em>="https://embauche.beta.gouv.fr/modules/v2/dist/simulateur.js">
+						</em>="<span id="scriptColor">#2975D1</span>" <em>src</em>="https://embauche.beta.gouv.fr/dist/simulateur.js">
 						<span>{'<'}</span>
 						<span>/</span>
 						<em>script</em>
@@ -27,15 +27,9 @@ export default class MyComponent extends Component {
 						fondre dans le thème visuel de votre page : changez simplement la
 						valeur de <i>data-couleur</i> ci-dessus. Pour la choisir, utilisez
 						notre{' '}
-						<a href="/modules/v2/couleur.html" target="_blank">
+						<a href="/couleur.html" target="_blank">
 							outil interactif
 						</a>.
-					</p>
-					<p>
-						<i className="fa fa-info-circle" aria-hidden="true" />
-						Le simulateur utilisable sur embauche.beta.gouv.fr est la toute
-						dernière version. Elle sera bientôt disponible à l'intégration sous
-						forme de module.
 					</p>
 				</div>
 				<div className="blocks" id="integrations">
