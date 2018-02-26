bf587c3775482f50de63158523540535ab692dbb
Mael <mael.thomas@data.gouv.fr>
2018-02-26 15:53:03 +0100

    Le message de mise a jour a fait son temps

--- source/components/pages/Home.js
+++ source/components/pages/Home.js
@@ -18,20 +18,22 @@ export default class Home extends Component {
 		let opacityStyle = { opacity: this.state.showUpdateMessage ? 1 : 0 }
 		return (
 			<div id="home" className="page">
-				<div id="updateMessage">
-					<i
-						className="fa fa-newspaper-o"
-						aria-hidden="true"
-						style={{
-							...opacityStyle,
-							color: this.props.themeColours.textColourOnWhite
-						}}
-					/>
-					<p style={opacityStyle}>
-						Cette nouvelle version du site vous permet de simuler un CDD ou CDI,
-						aux taux de 2018.
-					</p>
-				</div>
+				{false && (
+					<div id="updateMessage">
+						<i
+							className="fa fa-newspaper-o"
+							aria-hidden="true"
+							style={{
+								...opacityStyle,
+								color: this.props.themeColours.textColourOnWhite
+							}}
+						/>
+						<p style={opacityStyle}>
+							Cette nouvelle version du site vous permet de simuler un CDD ou
+							CDI, aux taux de 2018.
+						</p>
+					</div>
+				)}
 				<div id="content">
 					<TargetSelection themeColours={this.props.themeColours} />
 				</div>
