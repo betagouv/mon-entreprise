418dc2063c9323a6f068a1d5d96f1b2523547dbd
Mael <mael.thomas@data.gouv.fr>
2018-03-01 19:39:36 +0100

    :sparkles: Nettoyage de la configuration Webpack

--- source/server.js
+++ source/server.js
@@ -1,6 +1,6 @@
 var webpack = require('webpack')
 var WebpackDevServer = require('webpack-dev-server')
-var config = require('./webpack.config')
+var config = require('./webpack.dev')
 
 const PORT = process.env.PORT || 3000
 
--- source/webpack.common.js
+++ source/webpack.common.js
@@ -1,27 +1,5 @@
-var webpack = require('webpack'),
-	path = require('path'),
-	prodEnv = process.env.NODE_ENV == 'production', // eslint-disable-line no-undef
-	HardSourceWebpackPlugin = require('hard-source-webpack-plugin')
-
+var path = require('path')
 module.exports = {
-	devtool: 'cheap-module-source-map',
-	entry: {
-		bundle: prodEnv
-			? ['@babel/polyfill', 'whatwg-fetch', './source/entry.js']
-			: [
-					'webpack-dev-server/client?http://0.0.0.0:3000/',
-					'webpack/hot/only-dev-server',
-					'@babel/polyfill',
-					'react-hot-loader/patch',
-					'./source/entry.js'
-				]
-		// le nom "simulateur" est là pour des raisons historiques
-	},
-	output: {
-		path: path.resolve('./dist/'),
-		filename: '[name].js',
-		publicPath: '/dist/'
-	},
 	resolve: {
 		alias: {
 			Engine: path.resolve('source/engine/'),
@@ -29,6 +7,11 @@ module.exports = {
 			Components: path.resolve('source/components/'),
 			Images: path.resolve('source/images/')
 		}
+	},
+	output: {
+		path: path.resolve('./dist/'),
+		filename: '[name].js',
+		publicPath: '/dist/'
 	},
 	module: {
 		loaders: [
@@ -86,18 +69,5 @@ module.exports = {
 				loader: 'babel-loader!nearley-loader'
 			}
 		]
-	},
-	plugins: [
-		new webpack.EnvironmentPlugin({ NODE_ENV: 'development' }),
-		new webpack.NoEmitOnErrorsPlugin()
-	]
-		.concat(
-			!prodEnv
-				? [
-						new webpack.HotModuleReplacementPlugin(),
-						new HardSourceWebpackPlugin()
-					]
-				: []
-		)
-		.concat(prodEnv ? [new webpack.optimize.UglifyJsPlugin()] : [])
+	}
 }
