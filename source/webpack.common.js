e1279b2b98355f656d338d83e2dacec1ca272ffb
Mael <mael.thomas@data.gouv.fr>
2018-03-01 19:39:36 +0100

    :sparkles: Nettoyage de la configuration Webpack

---
+++ source/webpack.common.js
@@ -0,0 +1,73 @@
+var path = require('path')
+module.exports = {
+	resolve: {
+		alias: {
+			Engine: path.resolve('source/engine/'),
+			Règles: path.resolve('source/règles/'),
+			Components: path.resolve('source/components/'),
+			Images: path.resolve('source/images/')
+		}
+	},
+	output: {
+		path: path.resolve('./dist/'),
+		filename: '[name].js',
+		publicPath: '/dist/'
+	},
+	module: {
+		loaders: [
+			{
+				test: /\.css$/,
+				use: [
+					{
+						loader: 'style-loader'
+					},
+					{
+						loader: 'css-loader',
+						options: {
+							sourceMap: true,
+							importLoaders: 1
+						}
+					},
+					{
+						loader: 'postcss-loader'
+					}
+				]
+			},
+			{
+				test: /\.html$/,
+				loader: 'html-loader'
+			},
+			{
+				test: /\.(jpe?g|png|svg)$/,
+				use: {
+					loader: 'file-loader',
+					options: {
+						name: 'images/[name].[ext]'
+					}
+				}
+			},
+			{
+				test: /\.yaml$/,
+				loader: 'json-loader!yaml-loader'
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				loader: 'babel-loader'
+			},
+			{
+				test: /\.csv$/,
+				loader: 'csv-loader',
+				options: {
+					dynamicTyping: true,
+					header: true,
+					skipEmptyLines: true
+				}
+			},
+			{
+				test: /\.ne$/,
+				loader: 'babel-loader!nearley-loader'
+			}
+		]
+	}
+}
