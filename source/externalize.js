c66279f19d88b28f43c36719019bfd8c1d407832
Mael <mael.thomas@data.gouv.fr>
2018-03-06 17:16:30 +0100

    La traduction automatique n'excrase pas la traduction manuelle
    La traduction automatique est signalee par un double tilde

--- source/externalize.js
+++ source/externalize.js
@@ -4,6 +4,8 @@ var translate = require('google-translate-api')
 var path = require('path')
 let R = require('ramda')
 
+let externalizationPath = 'source/règles/externalized.yaml'
+// Test the translation agent
 translate(
 	'Bonjour, je suis votre agent traducteur. Je vais traduire votre application, puis ensuite dominer le monde',
 	{ from: 'fr', to: 'en' }
@@ -15,8 +17,12 @@ translate(
 		console.error(err)
 	})
 
-rules = yaml.safeLoad(
+let rules = yaml.safeLoad(
 	fs.readFileSync(path.resolve('source/règles/base.yaml'), 'utf-8')
+)
+
+let currentExternalization = yaml.safeLoad(
+	fs.readFileSync(path.resolve(externalizationPath), 'utf-8')
 )
 
 let attributesToExternalize = [
@@ -27,6 +33,9 @@ let attributesToExternalize = [
 	'résumé'
 ]
 
+let buildDottedName = rule =>
+	rule.espace ? rule.espace + ' . ' + rule.nom : rule.nom
+
 let promises = rules
 	.map(rule => (!rule.titre ? { ...rule, titre: rule.nom } : rule))
 	.map(rule =>
@@ -34,20 +43,28 @@ let promises = rules
 			R.toPairs(rule).map(([k, v]) => {
 				let attrToTranslate = attributesToExternalize.find(R.equals(k))
 				if (!attrToTranslate) return Promise.resolve({})
+				let enTrad = attrToTranslate + '.en',
+					frTrad = attrToTranslate + '.fr'
+
+				let currentTranslation = currentExternalization[buildDottedName(rule)]
+				//Check if a human traduction exists already for this attribute
+				if (currentTranslation[enTrad])
+					return Promise.resolve({
+						[enTrad]: currentTranslation[enTrad],
+						[frTrad]: v
+					})
 				return translate(v, { from: 'fr', to: 'en' }).then(({ text }) => ({
-					[attrToTranslate + '.fr']: v,
-					[attrToTranslate + '.en']: text
+					[frTrad]: v,
+					// Mark auto translated text with a double tild symbol
+					[enTrad]: '~~' + text
 				}))
 			})
 		).then(attributes => {
-			let id = rule.espace ? rule.espace + ' . ' + rule.nom : rule.nom
+			let id = buildDottedName(rule)
 			return { [id]: R.mergeAll(attributes) }
 		})
 	)
 
 Promise.all(promises).then(resolved =>
-	fs.writeFileSync(
-		'source/règles/externalized.yaml',
-		yaml.safeDump(R.mergeAll(resolved))
-	)
+	fs.writeFileSync(externalizationPath, yaml.safeDump(R.mergeAll(resolved)))
 )
