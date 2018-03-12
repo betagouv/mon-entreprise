b3f46c0cdc1728a0245354efd146d9164016b843
Mael <mael.thomas@data.gouv.fr>
2018-03-12 10:38:29 +0100

    :fire: Composant inutile

--- source/components/ProgressTip.js
+++ source/components/ProgressTip.js
@@ -6,31 +6,27 @@ import { Line } from 'rc-progress'
 
 @withRouter
 @connect(state => ({
-	done: state.done,
 	foldedSteps: state.foldedSteps,
 	nextSteps: state.nextSteps,
 	colour: state.themeColours.colour
 }))
 export default class ProgressTip extends Component {
-	state = {
-		nbFoldedStepsForFirstEstimation: null
-	}
-	componentWillReceiveProps(newProps) {
-		if (newProps.done && this.state.nbFoldedStepsForFirstEstimation == null)
-			this.setState({
-				nbFoldedStepsForFirstEstimation: newProps.foldedSteps.length
-			})
-	}
 	render() {
-		let { done, nextSteps, foldedSteps, colour } = this.props,
+		let {
+				done,
+				nextSteps,
+				foldedSteps,
+				colour,
+				conversationVisible,
+				selectingTargets
+			} = this.props,
 			nbQuestions = nextSteps.length
-		if (!done) return null
+
+		if (selectingTargets && !conversationVisible)
+			return <p>Vous aurez {nextSteps.length} questions !</p>
+		if (!conversationVisible) return null
 		return (
 			<div className="tip">
-				{nbQuestions != 0 &&
-					this.state.nbFoldedStepsForFirstEstimation === foldedSteps.length && (
-						<p>Votre première estimation est disponible !</p>
-					)}
 				{nbQuestions != 0 && (
 					<p>
 						{nbQuestions === 1
