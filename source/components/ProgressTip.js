1f939e59ef25fd443aaf17e6e3ebd4ccb4dbb362
Laurent Bossavit <laurent.git@bossavit.com>
2018-03-27 13:38:02 +0200

    :alien: Traduire ProgressTips

--- source/components/ProgressTip.js
+++ source/components/ProgressTip.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import { Trans } from 'react-i18next'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router'
 import './ProgressTip.css'
@@ -29,13 +30,18 @@ export default class ProgressTip extends Component {
 			<div className="tip">
 				{nbQuestions != 0 &&
 					this.state.nbFoldedStepsForFirstEstimation === foldedSteps.length && (
-						<p>Votre première estimation est disponible !</p>
+						<p><Trans i18nKey="first">Votre première estimation est disponible !</Trans></p>
 					)}
 				{nbQuestions != 0 && (
 					<p>
 						{nbQuestions === 1
-							? 'Une dernière question !'
-							: `Il reste moins de ${nbQuestions} questions`}
+							?
+							<Trans i18nKey="lastQ">Une dernière question !</Trans>
+							:
+							<Trans i18nKey="questionsLeft" count={nbQuestions}>
+							Il reste moins de {{nbQuestions}} questions.
+							</Trans>
+						}
 						<Line
 							percent={
 								100 * foldedSteps.length / (foldedSteps.length + nbQuestions)
