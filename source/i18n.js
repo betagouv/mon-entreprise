5e1dfb98c649a5b62311e7d2168e993c0d041de3
Étienne Charignon <etienne.charignon@gmail.com>
2018-04-04 12:10:34 +0200

    :alien: translate the suggestion tooltip
    I had to force the default language to a value (I forced it to 'fr') so
    that the fr resource is effectively loaded in the first place

--- source/i18n.js
+++ source/i18n.js
@@ -1,4 +1,4 @@
-import { I18nextProvider } from 'react-i18next';
+import { I18nextProvider } from 'react-i18next'
 import i18next from 'i18next'
 import queryString from 'query-string'
 
@@ -12,16 +12,19 @@ let lang = getIframeOption('lang') ||
 
 sessionStorage['lang'] = lang
 
-i18next
-	.init({
+i18next.init(
+	{
 		debug: true,
 		lng: lang,
 		resources: {
-				en: {
-			translation: enTranslations
-		}}
-	}, (err, t) => {
-		console.log("Error from i18n load",err,t)
-	})
+			en: {
+				translation: enTranslations
+			}
+		}
+	},
+	(err, t) => {
+		console.log('Error from i18n load', err, t)
+	}
+)
 
 export default lang
