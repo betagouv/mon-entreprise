957ff046112c195351dd6ae9e894c8f0b4a08911
Mael <mael.thomas@data.gouv.fr>
2018-04-30 17:22:40 +0200

    Simplification du texte d'avertissement

--- source/components/Explanation.js
+++ source/components/Explanation.js
@@ -27,10 +27,10 @@ export default class Explanation extends Component {
 					<p>
 						<i className="fa fa-info-circle" aria-hidden="true" />
 						<Trans i18nKey="disclaimer">
-							Le simulateur vous aide à comprendre votre bulletin de paie, mais
-							n'y est pas opposable. Il ne prend pour l'instant pas en compte
-							les conventions et accords collectifs, ni la myriade d'aides aux
-							entreprises que vous pouvez explorer sur
+							Le simulateur vous aide à comprendre votre bulletin de paie, sans
+							lui être opposable. Il ne prend pour l'instant pas en compte les
+							conventions et accords collectifs, ni la myriade d'aides à
+							explorer sur
 						</Trans>
 						<a href="https://www.aides-entreprises.fr" target="_blank">
 							{' '}
