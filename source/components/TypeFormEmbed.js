0b1fdb738d11421fa6b39396e03148664bfb5f82
Mael <mael.thomas@data.gouv.fr>
2018-05-03 15:46:24 +0200

    Utilisation de typeform en mode popup

--- source/components/TypeFormEmbed.js
+++ source/components/TypeFormEmbed.js
@@ -15,14 +15,18 @@ export default ({ hiddenVariables }) => (
 		<div
 			className="typeform-widget"
 			data-url={url(hiddenVariables)}
-			style={{ width: '100%', height: '500px' }}
+			data-mode="popup"
+			data-auto-open={true}
+			data-hide-headers={true}
+			data-hide-footer={true}
+			target="_blank"
 		/>
 		<Helmet
 			script={[
 				{
 					type: 'text/javascript',
 					innerHTML:
-						'(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm", b="https://embed.typeform.com/"; if(!gi.call(d,id)) { js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })()'
+						'(function() { var qs,js,q,s,d=document, gi=d.getElementById, ce=d.createElement, gt=d.getElementsByTagName, id="typef_orm_share", b="https://embed.typeform.com/"; if(!gi.call(d,id)){ js=ce.call(d,"script"); js.id=id; js.src=b+"embed.js"; q=gt.call(d,"script")[0]; q.parentNode.insertBefore(js,q) } })() '
 				}
 			]}
 		/>
