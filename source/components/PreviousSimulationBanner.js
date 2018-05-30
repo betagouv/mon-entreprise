274b381f3bec5b2df75d486ebef5f35d12119965
Johan Girod <johangirod@gmail.com>
2018-05-30 18:43:27 +0200

    :alien: ajoute la traduction pour la sauvegarde

--- source/components/PreviousSimulationBanner.js
+++ source/components/PreviousSimulationBanner.js
@@ -1,10 +1,12 @@
 /* @flow */
+import { compose } from 'ramda'
 import React from 'react'
-import type { SavedSimulation } from '../types/State'
-import { loadPreviousSimulation } from '../actions'
-import Banner from './Banner'
+import { Trans, translate } from 'react-i18next'
 import { connect } from 'react-redux'
-import { Trans } from 'react-i18next'
+import { loadPreviousSimulation as loadPreviousSimulationAction } from '../actions'
+import Banner from './Banner'
+
+import type { SavedSimulation } from '../types/State'
 
 type ConnectedPropTypes = {
 	previousSimulation: SavedSimulation,
@@ -15,19 +17,22 @@ const PreviousSimulationBanner = ({
 	loadPreviousSimulation
 }: ConnectedPropTypes) => (
 	<Banner hidden={!previousSimulation}>
-		<Trans key="previousSimulationBanner">
+		<Trans i18nKey="previousSimulationBanner.info">
 			Votre précédente simulation a été automatiquement sauvegardée.
 		</Trans>
 		<button
 			className="unstyledButton linkButton"
 			onClick={loadPreviousSimulation}>
-			<Trans key="previousSimulationBanner.retrieveButton">
+			<Trans i18nKey="previousSimulationBanner.retrieveButton">
 				Retrouver ma dernière simulation
 			</Trans>
 		</button>
 	</Banner>
 )
 
-export default connect(({ previousSimulation }) => ({ previousSimulation }), {
-	loadPreviousSimulation
-})(PreviousSimulationBanner)
+export default compose(
+	translate(),
+	connect(({ previousSimulation }) => ({ previousSimulation }), {
+		loadPreviousSimulation: loadPreviousSimulationAction
+	})
+)(PreviousSimulationBanner)
