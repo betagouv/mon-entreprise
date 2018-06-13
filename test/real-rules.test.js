21dfa157171a7ca2dbefe9bd96c914b4a34492aa
Mael <mael.thomas@data.gouv.fr>
2018-06-13 18:50:51 +0200

    Reparation des tests de regles

--- test/real-rules.test.js
+++ test/real-rules.test.js
@@ -4,20 +4,46 @@
   puis on teste idéalement tous ses comportements sans en faire intervenir d'autres.
 */
 
-import { expect, AssertionError } from 'chai'
-import { enrichRule, rules } from '../source/engine/rules'
-import { analyse, parseAll } from '../source/engine/traverse'
-import { collectMissingVariables } from '../source/engine/generateQuestions'
-import testSuites from './load-mecanism-tests'
-import R from 'ramda'
-import { runExamples, isFloat } from '../source/components/rule/Examples'
+import { AssertionError } from 'chai'
+import { rules } from '../source/engine/rules'
+import { parseAll } from '../source/engine/traverse'
+import { exampleAnalysisSelector } from 'Selectors/analyseSelectors'
+import { merge } from 'ramda'
+
+// les variables dans les tests peuvent être exprimées relativement à l'espace de nom de la règle,
+// comme dans sa formule
+let runExamples = (examples, rule) =>
+	examples.map(ex => {
+		let runExample = exampleAnalysisSelector(
+				{
+					currentExample: {
+						situation: ex.situation,
+						dottedName: rule.dottedName
+					}
+				},
+				{ dottedName: rule.dottedName }
+			),
+			exampleValue = runExample.nodeValue,
+			goal = ex['valeur attendue'],
+			ok =
+				exampleValue === goal
+					? true
+					: typeof goal === 'number'
+						? Math.abs((exampleValue - goal) / goal) < 0.001
+						: goal === null && exampleValue === 0
+
+		return merge(ex, {
+			ok,
+			rule: runExample
+		})
+	})
 
 let parsedRules = parseAll(rules)
 describe('Tests des règles de notre base de règles', () =>
 	parsedRules.map(rule => {
 		if (!rule.exemples) return null
 		describe(rule.name, () => {
-			let examples = runExamples(rule.exemples, rule, parsedRules)
+			let examples = runExamples(rule.exemples, rule)
 			examples.map(example =>
 				it(example.nom + '', () => {
 					if (!example.ok)
