010dc5ec53c739a6f16dc11ddb4d385399815726
Mael <mael.thomas@data.gouv.fr>
2018-06-25 16:28:41 +0000

    :fire: Allègement des données versement transport

--- source/règles/transform-taux-versement-transport.js
+++ source/règles/transform-taux-versement-transport.js
@@ -4,11 +4,25 @@
  * il manque les taux des villes avec arrondissement (les taux de chaque arrondissement y sont, mais pas celui de la ville globale, c'est le même évidemment)
  */
 
-let taux = require('./taux.json')
-let { pipe, map, assoc, merge, find, propEq, toPairs } = require('ramda')
+let taux = require('./taux-versement-transport-bruts.json')
+let {
+	sort,
+	head,
+	pipe,
+	map,
+	assoc,
+	merge,
+	find,
+	propEq,
+	toPairs,
+	pick
+} = require('ramda')
 let fs = require('fs')
 
-let result = pipe(toPairs, map(([k, v]) => assoc('codeCommune', k, v)))(taux)
+let result = pipe(
+	toPairs,
+	map(([k, v]) => assoc('codeCommune', k, v))
+)(taux)
 
 let villesAvecArrondissements = [
 	[
@@ -48,9 +62,29 @@ let additionnalResults = villesAvecArrondissements.map(
 		)
 )
 
+let extractLastTaux = pipe(
+	toPairs,
+	sort(([date1], [date2]) => (date1 > date2 ? -1 : 1)),
+	head,
+	([, rate]) => rate
+)
+
+let trimmed = result
+	.concat(additionnalResults)
+	.map(pick(['codeCommune', 'nomLaposte', 'aot', 'smt']))
+	//get last rate only
+	.map(({ aot, smt, ...rest }) => ({
+		...rest,
+		...(aot ? { aot: extractLastTaux(aot.taux) } : {}),
+		...(smt ? { smt: extractLastTaux(smt.taux) } : {})
+	}))
+// almost useless
+//
+//	.filter(({ aot, smt }) => (aot && aot !== '0') || (smt && smt !== '0'))
+
 fs.writeFile(
 	'./taux-versement-transport.json',
-	JSON.stringify(result.concat(additionnalResults)),
+	JSON.stringify(trimmed),
 	function(err) {
 		if (err) {
 			return console.log(err)
