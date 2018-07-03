41eaffb6baa733bfbc5542d395ae09c521c22760
Johan Girod <johangirod@gmail.com>
2018-07-03 18:28:45 +0200

    Réorganise le flow de la simulation
    On a choisit de privilegier la simplicité et de réduire le nombre d'informations et d'actions disponibles àchaque instant.
    
    Pour cela, on a fait les choix suivants :
    ### Lorsque l'utilisateur vient de rentrer une valeur
    Dans tous les cas, on affiche les résultats simplifiés qui apportent la réponse direct à la question 'à quoi servent mes cotisations'.
    
    On propose des raccourcis aux utilisateurs experimentés pour simuler un type de contrat particulier
    
    Pour les utilisateurs curieux on affiche un texte explicatif à la fin des résultats encourageant à continuer la simulation.
    
    On a fait le choix d'enlever la fiche de paie, que l'on considère comme ayant de fausse valeures donc peu utile tant que l'utilisateur n'a pas affiné sa situation
    
    ### Lorsque l'utilisateur commence la simulation
    On enlève toutes les partie résultat et questions, qui seront cachées derrière des boutons, pour garder l'utilisateur focus sur les questions.
    
    ### A faire
    - [] Rajouter les boutons Réponses / Résultat
    - [] Implémenter les raccourcis
    - [] Afficher par défaut le texte d'aide aux questions

--- source/components/Simu.js
+++ source/components/Simu.js
@@ -1,54 +1,112 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import { animated, Spring } from 'react-spring'
 import {
-	noUserInputSelector,
-	blockingInputControlsSelector
+	blockingInputControlsSelector,
+	noUserInputSelector
 } from 'Selectors/analyseSelectors'
-import ContinueButton from './Continue'
 import Conversation from './conversation/Conversation'
-import FoldedSteps, { GoToAnswers } from './conversation/FoldedSteps'
-import GoToExplanations from './GoToExplanations'
-import ResultView from './ResultView/ResultView'
+import * as Animate from './inFranceApp/animate'
+import Distribution from './ResultView/Distribution'
 import './Simu.css'
-import Sondage from './Sondage'
 import TargetSelection from './TargetSelection'
 import withColours from './withColours'
 
 @withColours
-@connect(state => ({
-	noUserInput: noUserInputSelector(state),
-	blockingInputControls: blockingInputControlsSelector(state),
-	conversationStarted: state.conversationStarted
-}))
+@connect(
+	state => ({
+		noUserInput: noUserInputSelector(state),
+		blockingInputControls: blockingInputControlsSelector(state),
+		conversationStarted: state.conversationStarted
+	}),
+	{
+		onSimulationContinued: () => ({ type: 'START_CONVERSATION' })
+	}
+)
 export default class Simu extends Component {
 	render() {
 		let {
 			colours,
 			conversationStarted,
 			noUserInput,
+			onSimulationContinued,
 			blockingInputControls
 		} = this.props
-
+		const displayConversation = conversationStarted && !blockingInputControls
+		const displayResults =
+			!noUserInput && !blockingInputControls && !conversationStarted
 		return (
-			<div id="simu">
-				<div id="focusZone">
-					<FoldedSteps />
-					<GoToAnswers />
-					{conversationStarted &&
-						!blockingInputControls && (
+			<>
+				<div id="simu">
+					<Spring
+						to={{
+							height: displayResults ? 'auto' : 0,
+							opacity: displayResults ? 1 : 0
+						}}
+						delay={2000}
+						native>
+						{styles => (
+							<animated.div
+								style={{
+									...styles,
+									display: 'flex',
+									overflow: 'hidden',
+									flexWrap: 'wrap',
+									fontSize: '110%',
+									justifyContent: 'space-between'
+								}}>
+								<button
+									className="ui__ link-button"
+									onClick={onSimulationContinued}>
+									Permanent/Fixed term contract
+								</button>
+								<button
+									className="ui__ link-button"
+									onClick={onSimulationContinued}>
+									Part-time
+								</button>
+								<button
+									className="ui__ link-button"
+									onClick={onSimulationContinued}>
+									Cadre status
+								</button>
+								<button
+									className="ui__ link-button"
+									onClick={onSimulationContinued}>
+									Other
+								</button>
+							</animated.div>
+						)}
+					</Spring>
+					<div id="focusZone">
+						{displayConversation && (
 							<>
 								<Conversation textColourOnWhite={colours.textColourOnWhite} />
 							</>
 						)}
-					<TargetSelection colours={colours} />
-					<ContinueButton />
-					{conversationStarted &&
-						!blockingInputControls && <GoToExplanations />}
+						<TargetSelection colours={colours} />
+					</div>
 				</div>
-				{!noUserInput && !blockingInputControls && <ResultView />}
-				{!noUserInput && !blockingInputControls && <ContinueButton />}
-				{!blockingInputControls && <Sondage />}
-			</div>
+
+				{displayResults && (
+					<Animate.fromBottom>
+						<h2>What&apos;s included in my contributions?</h2>
+						<Distribution />
+						<h2>Estimate my real costs</h2>
+						<p>
+							This is a <strong>rough estimate</strong> based on a pre-made
+							generic contract. French legislation provides for a multitude of
+							special cases, and specific rules that can considerably change
+							hiring costs.
+						</p>
+						<p style={{ textAlign: 'center' }}>
+							<button className="ui__ button" onClick={onSimulationContinued}>
+								Estimate my real costs
+							</button>
+						</p>
+					</Animate.fromBottom>
+				)}
+			</>
 		)
 	}
 }
