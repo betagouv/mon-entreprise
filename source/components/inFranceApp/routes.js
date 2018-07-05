f4b9b6d3c9fc24683bd7697c26f3ed0012e1be8f
Johan Girod <johangirod@gmail.com>
2018-07-05 18:57:35 +0200

    ajoute un header de navigation pour infrance.org

--- source/components/inFranceApp/routes.js
+++ source/components/inFranceApp/routes.js
@@ -8,6 +8,7 @@ import Landing from './Landing'
 import Hiring from './Steps/Cost and benefits'
 import CreateMyCompany from './Steps/Create/index'
 import FindMyCompany from './Steps/Find'
+import StepsHeader from './Steps/StepsHeader'
 import './ui/index.css'
 class Layout extends Component {
 	componentDidMount() {
@@ -27,12 +28,15 @@ class Layout extends Component {
 				</Helmet>
 				<Switch>
 					<Route exact path="/" component={Landing} />
-					<div className="ui__ container">
-						<Route path="/règle/:name" component={RulePage} />
-						<Route path="/create-my-company" component={CreateMyCompany} />
-						<Route path="/hiring-and-social-security" component={Hiring} />
-						<Route path="/find-my-company" component={FindMyCompany} />
-					</div>
+					<>
+						<StepsHeader/>
+						<div className="ui__ container">
+							<Route path="/règle/:name" component={RulePage} />
+							<Route path="/create-my-company" component={CreateMyCompany} />
+							<Route path="/hiring-and-social-security" component={Hiring} />
+							<Route path="/find-my-company" component={FindMyCompany} />
+						</div>
+					</>
 				</Switch>
 			</>
 		)
