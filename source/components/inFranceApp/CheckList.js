7a3e260fba1edc5cd36c6f4f7f984a939761642f
Mael <mael.thomas@data.gouv.fr>
2018-07-06 14:03:25 +0000

    Checklist générique dans le state

--- source/components/inFranceApp/CheckList.js
+++ source/components/inFranceApp/CheckList.js
@@ -6,13 +6,14 @@ import { toPairs } from 'ramda'
 
 type Props = {
 	data: { [string]: boolean },
-	onChecklistItemChange: (string, boolean) => void
+	onChecklistItemChange: (string, string, boolean) => void
 }
 
-export default ({ checklistName, title, subtitle, items, conclusion }) =>
+export default ({ name: checklistName, title, subtitle, items, conclusion }) =>
 	connect(
 		state => ({
-			data: state.inFranceApp[checklistName]
+			data: state.inFranceApp.checklists[checklistName],
+			state
 		}),
 		{
 			onChecklistItemChange: (checklist, name, value) => ({
@@ -22,33 +23,40 @@ export default ({ checklistName, title, subtitle, items, conclusion }) =>
 				value
 			})
 		}
-	)(({ onChecklistItemChange, data }: Props) => (
-		<Animate.fromBottom>
-			<h1 className="question__title">{title}</h1>
-			<p>
-				{subtitle}
-				This checklist will guide you thoughout all the necessary steps to
-				register your company with the French administration.
-			</p>
-			<ul className="ui__ no-bullet">
-				{toPairs(items).map(([key, content]) => (
-					<li key={key}>
-						<input
-							type="checkbox"
-							name={key}
-							defaultChecked={data[key]}
-							onChange={({ target }) =>
-								onChecklistItemChange(target.name, target.checked)
-							}
-						/>
-						{content}
-					</li>
-				))}
-			</ul>
-			<p>
-				You can add this page to your favorite and keep track of your progress
-				in the different administrative tasks.
-			</p>
-			{conclusion}
-		</Animate.fromBottom>
-	))
+	)(
+		({ onChecklistItemChange, data, state }: Props) =>
+			console.log(checklistName, state.inFranceApp) || (
+				<Animate.fromBottom>
+					<h1 className="question__title">{title}</h1>
+					<p>
+						{subtitle}
+						This checklist will guide you thoughout all the necessary steps to
+						register your company with the French administration.
+					</p>
+					<ul className="ui__ no-bullet">
+						{toPairs(items).map(([key, content]) => (
+							<li key={key}>
+								<input
+									type="checkbox"
+									name={key}
+									defaultChecked={data[key]}
+									onChange={({ target }) =>
+										onChecklistItemChange(
+											checklistName,
+											target.name,
+											target.checked
+										)
+									}
+								/>
+								{content}
+							</li>
+						))}
+					</ul>
+					<p>
+						You can add this page to your favorite and keep track of your
+						progress in the different administrative tasks.
+					</p>
+					{conclusion}
+				</Animate.fromBottom>
+			)
+	)
