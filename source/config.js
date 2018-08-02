55bd6dc35d66f24d1423fd3dac225d84c7bb688f
Mael <mael.thomas@data.gouv.fr>
2018-08-02 12:05:50 +0200

    Remplacement du salaire chargé par le total aidé

--- source/config.js
+++ source/config.js
@@ -1,5 +1,5 @@
 export let popularTargetNames = [
-	'contrat salarié . salaire . total',
+	'contrat salarié . salaire . total aidé',
 	'contrat salarié . salaire . brut de base',
 	'contrat salarié . salaire . net à payer',
 	'contrat salarié . salaire . net après impôt'
