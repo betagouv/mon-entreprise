5dd3af1b3358c4092f40df402b904b0544da8ca7
Mael <mael.thomas@data.gouv.fr>
2018-08-10 17:58:00 +0200

    Traduction du bas de page et :art:
    Images plus petites : elles sont déjà en haut de la page principale

--- source/sites/mycompanyinfrance.fr/pages/Footer/Footer.js
+++ source/sites/mycompanyinfrance.fr/pages/Footer/Footer.js
@@ -8,18 +8,22 @@ const Footer = ({ colours: { colour } }) => (
 	<footer className="footer" style={{ backgroundColor: `${colour}22` }}>
 		<div className="ui__ container">
 			<p>
-				Un service fourni par <a href="https://urssaf.fr">l'URSSAF</a> et incubé
-				par <a href="https://beta.gouv.fr">beta.gouv.fr</a>
+				This website is provided by the{' '}
+				<a href="https://www.urssaf.fr">URSSAF</a>, the French social security
+				contributions collector, with the help of the French government's
+				startup incubator, <a href="https://beta.gouv.fr">beta.gouv.fr</a>.
 			</p>
-			<a href="https://urssaf.fr">
-				<img src={urssafSvg} alt="un service fourni par l'URSSAF" />
-			</a>
-			<a href="https://beta.gouv.fr">
-				<img
-					src={betaGouvSvg}
-					alt="un service de l'état français incubé par beta.gouv.fr"
-				/>
-			</a>
+			<div id="footerIcons">
+				<a href="https://urssaf.fr">
+					<img src={urssafSvg} alt="un service fourni par l'URSSAF" />
+				</a>
+				<a href="https://beta.gouv.fr">
+					<img
+						src={betaGouvSvg}
+						alt="un service de l'état français incubé par beta.gouv.fr"
+					/>
+				</a>
+			</div>
 		</div>
 	</footer>
 )
