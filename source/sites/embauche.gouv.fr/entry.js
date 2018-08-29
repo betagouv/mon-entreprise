844e3cfc4daa887fd1cdbb003a3a6750bf71d250
Johan Girod <johangirod@gmail.com>
2018-08-29 14:35:41 +0200

    Réajuste automatiquement la taille de l'iframe pour éviter les barre de scroll

--- source/sites/embauche.gouv.fr/entry.js
+++ source/sites/embauche.gouv.fr/entry.js
@@ -1,3 +1,4 @@
+import 'iframe-resizer'
 import React from 'react'
 import { render } from 'react-dom'
 import App from './App'
--- source/sites/embauche.gouv.fr/iframe-script.js
+++ source/sites/embauche.gouv.fr/iframe-script.js
@@ -1,30 +1,6 @@
-import { forEachObjIndexed } from 'ramda'
 
-function ReplaceWith(Ele) {
-	'use-strict' // For safari, and IE > 10
-	var parent = this.parentNode,
-		i = arguments.length,
-		firstIsNode = +(parent && typeof Ele === 'object')
-	if (!parent) return
 
-	while (i-- > firstIsNode) {
-		if (parent && typeof arguments[i] !== 'object') {
-			arguments[i] = document.createTextNode(arguments[i])
-		}
-		if (!parent && arguments[i].parentNode) {
-			arguments[i].parentNode.removeChild(arguments[i])
-			continue
-		}
-		parent.insertBefore(this.previousSibling, arguments[i])
-	}
-	if (firstIsNode) parent.replaceChild(Ele, this)
-}
-if (!Element.prototype.replaceWith) Element.prototype.replaceWith = ReplaceWith
-if (!CharacterData.prototype.replaceWith)
-	CharacterData.prototype.replaceWith = ReplaceWith
-if (!DocumentType.prototype.replaceWith)
-	DocumentType.prototype.replaceWith = ReplaceWith
-
+import { iframeResizer } from 'iframe-resizer';
 let script = document.getElementById('script-simulateur-embauche'),
 	couleur = script.dataset.couleur,
 	baseUrl =
@@ -40,14 +16,16 @@ const iframeAttributes = {
 	id: 'simulateurEmbauche',
 	src,
 	style:
-		'border: none; width: 100%; display: block; margin: 0 auto; height: 45em',
+		'border: none; width: 100%; display: block; margin: 0 auto',
 	allowfullscreen: true,
 	webkitallowfullscreen: true,
 	mozallowfullscreen: true
 }
-forEachObjIndexed(
-	(value, key) => iframe.setAttribute(key, value),
-	iframeAttributes
-)
-
-script.replaceWith(iframe, null)
+for (var key in iframeAttributes) {
+	iframe.setAttribute(key, iframeAttributes[key])
+}
+iframeResizer({
+	interval: 0,
+	heightCalculationMethod: 'lowestElement'
+}, iframe)
+script.parentNode.insertBefore(iframe, script)
