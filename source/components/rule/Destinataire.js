0908e23b2c1c431d2901eb05c1e3036ef41929c1
Johan Girod <johangirod@gmail.com>
2018-09-05 12:03:53 +0200

    :art: Améliore le style des pages règles

--- source/components/rule/Destinataire.js
+++ source/components/rule/Destinataire.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
+import { Trans, translate } from 'react-i18next'
 import possiblesDestinataires from 'Règles/ressources/destinataires/destinataires.yaml'
 import './Destinataire.css'
-import { Trans, translate } from 'react-i18next'
 
 @translate()
 export default class Rule extends Component {
@@ -12,7 +12,8 @@ export default class Rule extends Component {
 		return destinataire && destinataireData ? (
 			<div className="infobox__item" id="destinataire">
 				<h4>
-					<Trans>Destinataire</Trans>&nbsp;:
+					<Trans>Destinataire</Trans>
+					&nbsp;:
 				</h4>
 				<div>
 					<a href={destinataireData.lien} target="_blank">
