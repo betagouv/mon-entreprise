a0d9d63c1342eadc507645030d89717eb0b5de3e
Johan Girod <johangirod@gmail.com>
2018-09-06 11:38:10 +0200

    :white_check_mark: Ajoute un test end to end pour la saisie du salaire net

--- cypress/integration/embauche/landing.js
+++ cypress/integration/embauche/landing.js
@@ -1,9 +1,22 @@
+const salaryInput = inputTitle => {
+	const inputContainer = cy
+		.contains(inputTitle)
+		.closest('.main')
+		.find('.targetInputOrValue')
+	inputContainer.click()
+	return inputContainer.find('input')
+}
 describe('Landing basic test', function() {
-	it('should not crash when requesting the page', function() {
+	it('should not crash when entering salary', function() {
 		cy.visit('/')
 	})
 	it('should display the simulateur after loading', function() {
 		cy.visit('/')
 		cy.contains('Entrez un salaire mensuel')
+	})
+	it('should display cotisation repartition when entering net salary', function() {
+		cy.visit('/')
+		salaryInput('Salaire net').type('2000')
+		cy.get('.distribution-chart__container')
 	})
 })
