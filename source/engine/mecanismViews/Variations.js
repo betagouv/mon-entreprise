59d88087cc9fe9863d315feacd079f01b882997f
Mael <mael.thomas@data.gouv.fr>
2018-09-07 11:31:44 +0200

    :art: Variations mieux expliquées

--- source/engine/mecanismViews/Variations.js
+++ source/engine/mecanismViews/Variations.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Node } from './common'
 import { makeJsx } from '../evaluation'
+import './Variations.css'
 
 export default function Variations(nodeValue, explanation) {
 	return (
@@ -15,8 +16,15 @@ export default function Variations(nodeValue, explanation) {
 							className="variation"
 							key={JSON.stringify(c.condition | 'sinon')}>
 							<div className="condition">
-								{c.condition ? <span>Si {makeJsx(c.condition)}</span> : 'Sinon'}
-								<div className="content">{makeJsx(c.consequence)}</div>
+								{c.condition && (
+									<>
+										<span>Si</span> {makeJsx(c.condition)}
+									</>
+								)}
+								<div>
+									<span>{c.condition ? 'Alors : ' : 'Sinon : '}</span>
+									<span className="content">{makeJsx(c.consequence)}</span>
+								</div>
 							</div>
 						</li>
 					))}
