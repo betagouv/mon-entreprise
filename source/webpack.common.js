2d966b9e2fd67c40307d29fac1031e04ff1a3828
Mael <mael.thomas@data.gouv.fr>
2018-10-08 15:10:39 +0000

    Nouvelle configuration webpack pour la librairie de calcul

--- source/webpack.common.js
+++ source/webpack.common.js
@@ -2,6 +2,8 @@
 const HTMLPlugin = require('html-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const path = require('path')
+const { universal, web } = require('./webpack.commonLoaders.js')
+
 module.exports = {
 	resolve: {
 		alias: {
@@ -20,76 +22,24 @@ module.exports = {
 		infrance: ['./source/sites/mycompanyinfrance.fr/entry.js'],
 		embauche: ['./source/sites/embauche.gouv.fr/entry.js'],
 		// To not introduce breaking into the iframe integration, we serve simulateur.js from a 'dist' subdirectory
-		'dist/simulateur': ['./source/sites/embauche.gouv.fr/iframe-script.js'],
-		engine: ['./source/engine/index.js']
+		'dist/simulateur': ['./source/sites/embauche.gouv.fr/iframe-script.js']
 	},
 	output: {
 		path: path.resolve('./dist/'),
 		filename: ({ chunk }) =>
-			['dist/simulateur', 'engine'].includes(chunk.name)
+			['dist/simulateur'].includes(chunk.name)
 				? '[name].js'
-				: '[name].[hash].js',
-		libraryTarget: 'umd',
-		library: 'Syso'
+				: '[name].[hash].js'
 	},
 	module: {
-		rules: [
-			{
-				test: /\.css$/,
-				use: [
-					{
-						loader: 'style-loader'
-					},
-					{
-						loader: 'css-loader',
-						options: {
-							sourceMap: true,
-							importLoaders: 1
-						}
-					},
-					{
-						loader: 'postcss-loader'
-					}
-				]
-			},
-			{
-				test: /\.(jpe?g|png|svg)$/,
-				use: {
-					loader: 'file-loader',
-					options: {
-						name: 'images/[name].[ext]'
-					}
-				}
-			},
-			{
-				test: /\.yaml$/,
-				loader: 'json-loader!yaml-loader'
-			},
-			{
-				test: /\.js$/,
-				exclude: /node_modules|dist/,
-				loader: 'babel-loader'
-			},
-			{
-				test: /\.csv$/,
-				loader: 'csv-loader',
-				options: {
-					dynamicTyping: true,
-					header: true,
-					skipEmptyLines: true
-				}
-			},
-			{
-				test: /\.ne$/,
-				loader: 'babel-loader!nearley-loader'
-			}
-		]
+		rules: [...web, ...universal]
 	},
 	plugins: [
 		new HTMLPlugin({
 			template: 'index.html',
 			chunks: ['infrance'],
-			title: 'My company in France: A step-by-step guide to start a business in France',
+			title:
+				'My company in France: A step-by-step guide to start a business in France',
 			description:
 				'Find the type of company that suits you and follow the steps to register your company. Discover the French social security system by simulating your hiring costs. Discover the procedures to hire in France and learn the basics of French labour law.',
 			filename: 'infrance.html'
@@ -105,9 +55,18 @@ module.exports = {
 		new CopyPlugin([
 			'./manifest.webmanifest',
 			'./source/sites/embauche.gouv.fr/images/logo',
-			{ from: './source/sites/embauche.gouv.fr/robots.txt', to: 'robots.embauche.txt' },
-			{ from: './source/sites/mycompanyinfrance.fr/robots.txt', to: 'robots.infrance.txt' },
-			{ from: './source/sites/mycompanyinfrance.fr/sitemap.txt', to: 'sitemap.infrance.txt' },
+			{
+				from: './source/sites/embauche.gouv.fr/robots.txt',
+				to: 'robots.embauche.txt'
+			},
+			{
+				from: './source/sites/mycompanyinfrance.fr/robots.txt',
+				to: 'robots.infrance.txt'
+			},
+			{
+				from: './source/sites/mycompanyinfrance.fr/sitemap.txt',
+				to: 'sitemap.infrance.txt'
+			}
 		])
 	]
 }
