aa55f88fdb91592fc8e360a60646949a89f0e7cc
Mael <mael.thomas@data.gouv.fr>
2018-10-17 15:23:48 +0000

    :art: Affichage des périodes sur les pages règle

--- source/components/rule/Algorithm.js
+++ source/components/rule/Algorithm.js
@@ -1,16 +1,13 @@
 import { makeJsx } from 'Engine/evaluation'
-import knownMecanisms from 'Engine/known-mecanisms.yaml'
 import classNames from 'classnames'
 import { path, values } from 'ramda'
 import React from 'react'
 import { Trans, translate } from 'react-i18next'
-import { AttachDictionary } from '../AttachDictionary'
 import './Algorithm.css'
 
 // The showValues prop is passed as a context. It used to be delt in CSS (not(.showValues) display: none), both coexist right now
 import { ShowValuesProvider } from './ShowValuesContext'
 
-@AttachDictionary(knownMecanisms)
 @translate()
 export default class Algorithm extends React.Component {
 	render() {
