61760839dc72299b93092c2c11808d337d283ec0
Mael <mael.thomas@data.gouv.fr>
2018-10-22 15:33:00 +0000

    Affichage du code source d'une règle et ses filles

--- source/components/rule/Rule.js
+++ source/components/rule/Rule.js
@@ -27,6 +27,8 @@ import References from './References'
 import './Rule.css'
 import { AttachDictionary } from '../AttachDictionary'
 import knownMecanisms from 'Engine/known-mecanisms.yaml'
+import emoji from 'react-easy-emoji'
+import Source from './RuleSource'
 
 export default compose(
 	connect((state, props) => ({
@@ -41,6 +43,7 @@ export default compose(
 	withLanguage
 )(
 	class Rule extends Component {
+		state = { viewSource: false }
 		render() {
 			let {
 					dottedName,
@@ -60,70 +63,82 @@ export default compose(
 			let showValues = valuesToShow || currentExample
 
 			return (
-				<div id="rule" className="ui__ container">
-					<Helmet>
-						<title>{title}</title>
-						<meta name="description" content={description} />
-					</Helmet>
-					<RuleHeader
-						{...{
-							ns,
-							type,
-							description,
-							question,
-							flatRule,
-							flatRules,
-							name,
-							title,
-							icon
-						}}
-					/>
+				<>
+					<button
+						onClick={() =>
+							this.setState({ viewSource: !this.state.viewSource })
+						}>
+						{emoji('🔍')}
+					</button>
+					{this.state.viewSource ? (
+						<Source dottedName={dottedName} />
+					) : (
+						<div id="rule" className="ui__ container">
+							<Helmet>
+								<title>{title}</title>
+								<meta name="description" content={description} />
+							</Helmet>
+							<RuleHeader
+								{...{
+									ns,
+									type,
+									description,
+									question,
+									flatRule,
+									flatRules,
+									name,
+									title,
+									icon
+								}}
+							/>
 
-					<section id="rule-content">
-						{displayedRule.nodeValue ? (
-							<div id="ruleValue">
-								<i className="fa fa-calculator" aria-hidden="true" />{' '}
-								{displayedRule.format === 'euros' || displayedRule.formule
-									? Intl.NumberFormat(language, {
-											style: 'currency',
-											currency: 'EUR'
-									  }).format(displayedRule.nodeValue)
-									: typeof displayedRule.nodeValue !== 'object'
-									? displayedRule.nodeValue
-									: null}
-							</div>
-						) : null}
+							<section id="rule-content">
+								{displayedRule.nodeValue ? (
+									<div id="ruleValue">
+										<i className="fa fa-calculator" aria-hidden="true" />{' '}
+										{displayedRule.format === 'euros' || displayedRule.formule
+											? Intl.NumberFormat(language, {
+													style: 'currency',
+													currency: 'EUR'
+											  }).format(displayedRule.nodeValue)
+											: typeof displayedRule.nodeValue !== 'object'
+											? displayedRule.nodeValue
+											: null}
+									</div>
+								) : null}
 
-						{displayedRule.defaultValue != null &&
-						typeof displayedRule.defaultValue !== 'object' ? (
-							<div id="ruleDefault">
-								Valeur par défaut : {displayedRule.defaultValue}
-							</div>
-						) : null}
+								{displayedRule.defaultValue != null &&
+								typeof displayedRule.defaultValue !== 'object' ? (
+									<div id="ruleDefault">
+										Valeur par défaut : {displayedRule.defaultValue}
+									</div>
+								) : null}
 
-						{//flatRule.question &&
-						// Fonctionnalité intéressante, à implémenter correctement
-						false && <UserInput {...{ flatRules, dottedName }} />}
-						{flatRule.ns && (
-							<Algorithm rule={displayedRule} showValues={showValues} />
-						)}
-						{flatRule.note && (
-							<section id="notes">
-								<h3>Note: </h3>
-								{createMarkdownDiv(flatRule.note)}
+								{//flatRule.question &&
+								// Fonctionnalité intéressante, à implémenter correctement
+								false && <UserInput {...{ flatRules, dottedName }} />}
+								{flatRule.ns && (
+									<Algorithm rule={displayedRule} showValues={showValues} />
+								)}
+								{flatRule.note && (
+									<section id="notes">
+										<h3>Note: </h3>
+										{createMarkdownDiv(flatRule.note)}
+									</section>
+								)}
+								<Examples
+									currentExample={currentExample}
+									situationExists={valuesToShow}
+									rule={displayedRule}
+								/>
+								{!isEmpty(namespaceRules) && (
+									<NamespaceRulesList {...{ namespaceRules }} />
+								)}
+								{this.renderReferences(flatRule)}
 							</section>
-						)}
-						<Examples
-							currentExample={currentExample}
-							situationExists={valuesToShow}
-							rule={displayedRule}
-						/>
-						{!isEmpty(namespaceRules) && (
-							<NamespaceRulesList {...{ namespaceRules }} />
-						)}
-						{this.renderReferences(flatRule)}
-					</section>
-				</div>
+						</div>
+					)}
+				</>
 			)
 		}
 
--- source/sites/embauche.gouv.fr/pages/Source.js
+++ source/sites/embauche.gouv.fr/pages/Source.js
@@ -2,29 +2,16 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import { flatRulesSelector } from 'Selectors/analyseSelectors'
 import { render } from 'react-dom'
-import MonacoEditor from 'react-monaco-editor'
 import yaml from 'js-yaml'
-import rawRules from '!raw-loader!Règles/base.yaml'
+import rawRulesText from '!raw-loader!Règles/base.yaml'
+import rawRules from 'Règles/base.yaml'
 
-@connect(state => ({
-	flatRules: flatRulesSelector(state)
-}))
 export default class Source extends Component {
-	state = {
-		code: rawRules
-	}
 	render() {
-		let { flatRules } = this.props
 		return (
 			<div id="Source" className="ui__ container">
-				<h1>La source</h1>
-				<MonacoEditor
-					width="800"
-					height="600"
-					language="yaml"
-					theme="vs-dark"
-					value={this.state.code}
-				/>
+				<h1>La sourceee</h1>
+				<div style={{ whiteSpace: 'pre-line' }}>{yaml.safeDump(rawRules)}</div>
 			</div>
 		)
 	}
