bd025cd62ce6d8de119250e4c0ed639a41e9dd2f
Mael <mael.thomas@data.gouv.fr>
2018-11-01 09:51:45 +0000

    :alien: Introduction à la forme juridique

--- source/sites/mycompanyinfrance.fr/pages/Company/Home.js
+++ source/sites/mycompanyinfrance.fr/pages/Company/Home.js
@@ -7,15 +7,20 @@ import { nextQuestionUrlSelector } from 'Selectors/companyStatusSelectors'
 import LegalStatusChoices from './LegalStatusChoice'
 
 import type { Match } from 'react-router'
+
+import { Trans, translate } from 'react-i18next'
+
 type Props = {
 	match: Match,
 	nextQuestionUrl: string
 }
 const CreateMyCompany = ({ match, nextQuestionUrl }: Props) => (
 	<>
-		<h1 className="question__title">Create your company</h1>
+		<h1 className="question__title">
+			<Trans>Créez votre entreprise</Trans>
+		</h1>
 		<Helmet>
-			<title>Find the right type of company for your business in France</title>
+			<title>Trouvez la bonne forme juridique pour votre entreprise.</title>
 			<meta
 				name="description"
 				content="The French business law defines more than 20 possible legal statuses to
@@ -25,22 +30,26 @@ const CreateMyCompany = ({ match, nextQuestionUrl }: Props) => (
 		</Helmet>
 		<p>
 			<Link className="ui__ link-button" to="/company/find">
-				My company is already registered with the French Administration
+				<Trans i18nKey="entreprise.existeDéjà">
+					Mon entreprise existe déjà
+				</Trans>
 			</Link>
 		</p>
 		<p>
-			The French business law defines more than 20 possible legal statuses to
-			declare a company with various acronyms and processes : SAS, SARL, SA,
-			EIRL... This guide will help you find quickly the right status for your
-			needs.
+			<Trans i18nKey="entreprise.formeJuridique.intro">
+				Le droit des sociétés définit plus de 20 statuts juridiques possibles
+				pour déclarer une société avec différents acronymes et démarches : SAS,
+				SARL, SA, EIRL.... Ce guide vous aide à trouver rapidement le statut qui
+				vous convient.
+			</Trans>
 		</p>
 		{match.isExact && (
 			<div className="ui__ answer-group">
 				<Link className="ui__ button" to={nextQuestionUrl}>
-					Choose your legal status
+					<Trans>Choisir la forme juridique</Trans>
 				</Link>
 				<Link to={'/social-security'} className="ui__ skip-button">
-					Do it later ›
+					<Trans>Passer</Trans> ›
 				</Link>
 			</div>
 		)}
@@ -51,4 +60,4 @@ const CreateMyCompany = ({ match, nextQuestionUrl }: Props) => (
 export default connect(
 	state => ({ nextQuestionUrl: nextQuestionUrlSelector(state) }),
 	null
-)(CreateMyCompany)
+)(translate()(CreateMyCompany))
