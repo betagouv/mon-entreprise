6d147c7c086b8fc079bc05468b72f5713b7e0d73
Johan Girod <johangirod@gmail.com>
2018-11-07 18:10:01 +0100

    :alien: ajoute la traduction pour le gérant majoritaire / minoritaire

--- source/sites/mycompanyinfrance.fr/pages/Company/MinorityDirector.js
+++ source/sites/mycompanyinfrance.fr/pages/Company/MinorityDirector.js
@@ -1,39 +1,54 @@
 /* @flow */
 import { directorIsInAMinority } from 'Actions/companyStatusActions'
-import React from 'react'
+import { React, T } from 'Components'
+import { compose } from 'ramda'
 import Helmet from 'react-helmet'
+import { translate } from 'react-i18next'
 import { connect } from 'react-redux'
 import { SkipButton } from 'Ui/Button'
+
 type Props = {
-	directorIsInAMinority: (?boolean) => void
+	directorIsInAMinority: (?boolean) => void,
+	t: (string, string) => string
 }
 
-const MinorityDirector = ({ directorIsInAMinority }: Props) => (
+const MinorityDirector = ({ directorIsInAMinority, t }: Props) => (
 	<>
 		<Helmet>
-			<title>Chairman or managing director </title>
+			<title>
+				{t(
+					'gérant minoritaire.page.titre',
+					'Gérant majoritaire ou minoritaire'
+				)}{' '}
+			</title>
 			<meta
 				name="description"
-				content="Some special rules apply depending on the amount of shares owned by the
-				director and this can lead to a different status when creating your company in France"
+				content={t(
+					'gérant minoritaire.page.description',
+					`Certaines règles particulières s'appliquent en fonction du nombre d'actions détenues par l'administrateur, ce qui peut conduire à un statut différent lors de la création de votre société`
+				)}
 			/>
 		</Helmet>
-		<h2>Chairman or managing director </h2>
-		<p>
-			Some special rules apply depending on the amount of shares owned by the
-			director.
-		</p>
-		<ul>
-			<li>
-				<strong>Chairman</strong>: The director is in majority, or is part of a
-				managing board that is in majority.
-			</li>
-			<li>
-				<strong>Managing director</strong>: The director is in minority (or
-				equality), or is part of a managing board that is in minority (or
-				equality).
-			</li>
-		</ul>
+		<h2>
+			<T k="gérant minoritaire.titre">Gérant majoritaire ou minoritaire</T>{' '}
+		</h2>
+		<T k="gérant minoritaire.description">
+			<p>
+				Certaines règles spéciales s'appliquent selon le nombre d'actions
+				détenues par le gérant.
+			</p>
+			<ul>
+				<li>
+					<strong>Gérant majoritaire</strong> : L'administrateur est majoritaire
+					ou fait partie d'un conseil d'administration majoritaire.
+				</li>
+				<li>
+					<strong>Gérant minoritaire</strong> : L'administrateur est minoritaire
+					(ou égalitaire), ou fait partie d'un conseil d'administration
+					minoritaire (ou égalitaire).
+				</li>
+			</ul>
+		</T>
 
 		<div className="ui__ answer-group">
 			<button
@@ -41,21 +56,24 @@ const MinorityDirector = ({ directorIsInAMinority }: Props) => (
 					directorIsInAMinority(false)
 				}}
 				className="ui__ button">
-				Chairman
+				<T>Gérant majoritaire</T>
 			</button>
 			<button
 				onClick={() => {
 					directorIsInAMinority(true)
 				}}
 				className="ui__ button">
-				Managing director
+				<T>Gérant minoritaire</T>
 			</button>
 			<SkipButton onClick={() => directorIsInAMinority(null)} />
 		</div>
 	</>
 )
 
-export default connect(
-	null,
-	{ directorIsInAMinority }
+export default compose(
+	translate(),
+	connect(
+		null,
+		{ directorIsInAMinority }
+	)
 )(MinorityDirector)
