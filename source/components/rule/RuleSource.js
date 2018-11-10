b4988b29739ba513d4706a0010af699907cb7ed9
Mael <mael.thomas@data.gouv.fr>
2018-11-10 16:02:31 +0000

    Chargement différé de Monaco Editor

--- source/components/rule/RuleSource.js
+++ source/components/rule/RuleSource.js
@@ -1,9 +1,10 @@
-import React, { Component } from 'react'
+import React, { Component, Suspense } from 'react'
 import yaml from 'js-yaml'
 import rules from 'Règles/base.yaml'
 import emoji from 'react-easy-emoji'
-import MonacoEditor from 'react-monaco-editor'
 import { buildDottedName } from 'Engine/rules'
+
+let MonacoEditor = React.lazy(() => import('react-monaco-editor'))
 
 export default class Source extends Component {
 	render() {
@@ -16,12 +17,14 @@ export default class Source extends Component {
 					{emoji('⛰️ ')}
 					Source de <code>{dottedName}</code>
 				</h2>
-				<MonacoEditor
-					height="800"
-					width="1000"
-					language={'yaml'}
-					value={yaml.safeDump(source)}
-				/>
+				<Suspense fallback={<div>Chargement du code source...</div>}>
+					<MonacoEditor
+						height="800"
+						width="1000"
+						language={'yaml'}
+						value={yaml.safeDump(source)}
+					/>
+				</Suspense>
 			</div>
 		)
 	}
