48419bad831487b93fa97825b2eebf8c1e39f2ba
Mael <mael.thomas@data.gouv.fr>
2018-11-11 21:54:17 +0000

    :art: Design de la conversation générique
    Prochainement :
    
    - l'objectif de calcul dans l'URL
    - faire marcher toutes les règles de co2.yaml
    - rendre l'exploration correcte (ex. formatter correctemnt les nombres,
    bien afficher les 'une possibilité')
    - faire marcher publi.codes/App.js plutôt que d'écraser /embauche

--- source/components/Targets.js
+++ source/components/Targets.js
@@ -1,15 +1,39 @@
 import React from 'react'
 import { analysisWithDefaultsSelector } from 'Selectors/analyseSelectors'
 import { connect } from 'react-redux'
+import './Targets.css'
+import withColours from 'Components/utils/withColours'
+import { Link } from 'react-router-dom'
+import emoji from 'react-easy-emoji'
 
 @connect(state => ({ analysis: analysisWithDefaultsSelector(state) }))
+@withColours
 export default class Targets extends React.Component {
 	render() {
-		let { title, nodeValue } = this.props.analysis.targets[0]
+		let {
+			title,
+			nodeValue,
+			unité: unit,
+			dottedName
+		} = this.props.analysis.targets[0]
 		return (
-			<div>
-				<span className="targetTitle">{title}</span>{' '}
-				<span className="targetValue">{nodeValue}</span>
+			<div id="targets">
+				<span className="icon">→</span>
+				<span className="content">
+					{/*<span className="title">{title}</span>
+					{' : '} */}
+					<span className="figure">
+						<span className="value">{nodeValue}</span>{' '}
+						<span className="unit">{unit}</span>
+					</span>
+					<Link
+						title="Quel est calcul ?"
+						style={{ color: this.props.colours.colour }}
+						to={'/règle/' + dottedName}
+						className="explanation">
+						{emoji('📖')}
+					</Link>
+				</span>
 			</div>
 		)
 	}
