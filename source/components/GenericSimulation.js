7713a390c7a27d7559ed8c402f6331b2c0971f56
Mael <mael.thomas@data.gouv.fr>
2018-11-11 15:58:34 +0000

    Composant pour la simulation générique

--- source/components/GenericSimulation.js
+++ source/components/GenericSimulation.js
@@ -1,3 +1,29 @@
 import React from 'react'
+import Answers from 'Components/AnswerList'
+import Conversation from 'Components/conversation/Conversation'
+import withColours from 'Components/utils/withColours'
+import Targets from 'Components/Targets'
 
-export default () => <div>Salut, toi</div>
+@withColours
+export default class YO extends React.Component {
+	state = {
+		displayAnswers: false
+	}
+	render() {
+		return (
+			<div>
+				<button onClick={() => this.setState({ displayAnswers: true })}>
+					Mes réponses
+				</button>
+
+				{this.state.displayAnswers && (
+					<Answers onClose={() => this.setState({ displayAnswers: false })} />
+				)}
+				<Conversation
+					textColourOnWhite={this.props.colours.textColourOnWhite}
+				/>
+				<Targets />
+			</div>
+		)
+	}
+}
