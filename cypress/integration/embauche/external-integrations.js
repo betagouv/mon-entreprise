7596d3ac79b80b4bc2d4a1203485e88ffc72a9ce
Johan Girod <johangirod@gmail.com>
2018-11-15 13:24:56 +0100

    :bug: répare les liens entre langage v3

--- cypress/integration/embauche/external-integrations.js
+++ cypress/integration/embauche/external-integrations.js
@@ -4,6 +4,7 @@ describe('Pole emploi test', function() {
 			return !err.message.contains('Unexpected token <')
 		})
 		cy.visit('https://entreprise.pole-emploi.fr/cout-salarie/')
+		cy.reload(true)
 		cy.get('#simulateurEmbauche')
 			.iframe()
 			.contains('Entrez un salaire mensuel')
