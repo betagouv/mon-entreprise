bb1656e65f3e916345d29e52e352fbfb0124a07a
Mael <mael.thomas@data.gouv.fr>
2018-11-17 14:02:24 +0100

    Amélioration des pages /règle

--- source/components/rule/Rule.js
+++ source/components/rule/Rule.js
@@ -118,12 +118,10 @@ export default compose(
 								{//flatRule.question &&
 								// Fonctionnalité intéressante, à implémenter correctement
 								false && <UserInput {...{ flatRules, dottedName }} />}
-								{flatRule.ns && (
-									<Algorithm
-										rule={displayedRule}
-										showValues={valuesToShow || currentExample}
-									/>
-								)}
+								<Algorithm
+									rule={displayedRule}
+									showValues={valuesToShow || currentExample}
+								/>
 								{flatRule.note && (
 									<section id="notes">
 										<h3>Note: </h3>
