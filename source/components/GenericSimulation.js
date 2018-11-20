c9ae8cacbf563cd9f13f837395c6ca5c7bf7b2ca
Mael <mael.thomas@data.gouv.fr>
2018-11-20 21:02:59 +0100

    :art: Image et style de l'entête

--- source/components/GenericSimulation.js
+++ source/components/GenericSimulation.js
@@ -21,29 +21,34 @@ export default class YO extends React.Component {
 		let { colours, noNextSteps, previousAnswers } = this.props
 		console.log(noNextSteps)
 		return (
-			<div className="ui__ container" id="GenericSimulation">
-				<h1>Quel est l'impact de vos douches ? </h1>
-				{!isEmpty(previousAnswers) && (
-					<button
-						style={{ background: colours.colour, color: colours.textColour }}
-						onClick={() => this.setState({ displayAnswers: true })}>
-						Mes réponses
-					</button>
-				)}
+			<div id="GenericSimulation">
+				<header>
+					<img src="https://images.unsplash.com/photo-1507652313519-d4e9174996dd?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=014e46e03c4b8f6472e77c50c5088377&auto=format&fit=crop&w=800&q=80" />
+					<h1>Quel est l'impact de vos douches ? </h1>
+				</header>
+				<div className="ui__ container" id="simulationContent">
+					{!isEmpty(previousAnswers) && (
+						<button
+							style={{ background: colours.colour, color: colours.textColour }}
+							onClick={() => this.setState({ displayAnswers: true })}>
+							Mes réponses
+						</button>
+					)}
 
-				{this.state.displayAnswers && (
-					<Answers onClose={() => this.setState({ displayAnswers: false })} />
-				)}
-				<Conversation
-					textColourOnWhite={this.props.colours.textColourOnWhite}
-				/>
-				{noNextSteps && (
-					<>
-						<h2>Plus de questions ! </h2>
-						<p>Vous avez atteint l'estimation la plus précise.</p>
-					</>
-				)}
-				<Targets />
+					{this.state.displayAnswers && (
+						<Answers onClose={() => this.setState({ displayAnswers: false })} />
+					)}
+					<Conversation
+						textColourOnWhite={this.props.colours.textColourOnWhite}
+					/>
+					{noNextSteps && (
+						<>
+							<h2>Plus de questions ! </h2>
+							<p>Vous avez atteint l'estimation la plus précise.</p>
+						</>
+					)}
+					<Targets />
+				</div>
 			</div>
 		)
 	}
