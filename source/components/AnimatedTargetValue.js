239515a1792cff18002ab29b5adae3ff4056f1ef
Johan Girod <johangirod@gmail.com>
2018-11-20 19:00:08 +0100

    Affiche la différence en euro lors du choix des options dans le simulateur

--- source/components/AnimatedTargetValue.js
+++ source/components/AnimatedTargetValue.js
@@ -1,32 +1,87 @@
+/* @flow */
 import withLanguage from 'Components/utils/withLanguage'
-import React, { Component } from 'react'
+import React, { Component, PureComponent } from 'react'
 import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 import './AnimatedTargetValue.css'
 
+type Props = {
+	value: ?number,
+	language: string
+}
+type State = {
+	difference: number
+}
 export default withLanguage(
-	class AnimatedTargetValue extends Component {
+	class AnimatedTargetValue extends Component<Props, State> {
+		previousValue: ?number = null
+		timeoutId: ?TimeoutID = null
+		state = { difference: 0 }
+
+		componentDidUpdate(prevProps) {
+			if (prevProps.value === this.props.value) {
+				return
+			}
+			if (this.timeoutId) {
+				clearTimeout(this.timeoutId)
+			}
+			this.previousValue =
+				this.previousValue === null ? prevProps.value : this.previousValue
+
+			this.timeoutId = setTimeout(() => {
+				this.setState({
+					difference: (this.props.value || 0) - (this.previousValue || 0)
+				})
+				this.previousValue = null
+				this.timeoutId = null
+			}, 200)
+		}
+		format = value => {
+			return value == null
+				? ''
+				: Intl.NumberFormat(this.props.language, {
+						style: 'currency',
+						currency: 'EUR',
+						maximumFractionDigits: 0,
+						minimumFractionDigits: 0
+				  }).format(value)
+		}
 		render() {
-			let { value, language } = this.props
-			let formattedValue =
-				value == null
-					? ''
-					: Intl.NumberFormat(language, {
-							style: 'currency',
-							currency: 'EUR',
-							maximumFractionDigits: 0,
-							minimumFractionDigits: 0
-					  }).format(value)
+			const formattedValue = this.format(this.props.value)
+			const formattedDifference = this.format(this.state.difference)
 			return (
-				<ReactCSSTransitionGroup
-					transitionName="flash"
-					transitionEnterTimeout={100}
-					transitionLeaveTimeout={100}>
-					<span key={value} className="Rule-value">
-						{' '}
-						<span>{formattedValue}</span>
+				<>
+					<span key={this.props.value} className="Rule-value">
+						{Math.abs(this.state.difference) > 1 &&
+							formattedDifference !== formattedValue && (
+								<Evaporate
+									style={{
+										color: this.state.difference > 0 ? 'chartreuse' : 'red'
+									}}>
+									{(this.state.difference > 0 ? '+' : '') + formattedDifference}
+								</Evaporate>
+							)}{' '}
+						<span>{this.format(this.props.value)}</span>
 					</span>
-				</ReactCSSTransitionGroup>
+				</>
 			)
 		}
 	}
 )
+
+class Evaporate extends PureComponent<{ children: string, style: Object }> {
+	render() {
+		return (
+			<ReactCSSTransitionGroup
+				transitionName="evaporate"
+				transitionEnterTimeout={1000}
+				transitionLeaveTimeout={1}>
+				<span
+					key={this.props.children}
+					style={this.props.style}
+					className="evaporate">
+					{this.props.children}
+				</span>
+			</ReactCSSTransitionGroup>
+		)
+	}
+}
