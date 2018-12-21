442aeb2cbea9a30ce5a93ad720bb7bc1835d29a2
Mael <mael.thomas@data.gouv.fr>
2018-12-21 12:07:47 +0100

    Meilleur affichage des conditions d'applicabilité

--- source/components/rule/Algorithm.js
+++ source/components/rule/Algorithm.js
@@ -1,11 +1,34 @@
 import classNames from 'classnames'
 import { makeJsx } from 'Engine/evaluation'
-import { compose, path, values } from 'ramda'
+import { identity, any, compose, path, values } from 'ramda'
 import React from 'react'
 import { Trans, withNamespaces } from 'react-i18next'
 import './Algorithm.css'
 // The showValues prop is passed as a context. It used to be delt in CSS (not(.showValues) display: none), both coexist right now
 import { ShowValuesProvider } from './ShowValuesContext'
+
+let Conditions = ({
+	parentDependency,
+	'applicable si': applicable,
+	'non applicable': notApplicable
+}) => {
+	let listElements = [
+		parentDependency?.nodeValue === false && (
+			<li key="parentDependency">Désactivée car {makeJsx(parentDependency)}</li>
+		),
+		applicable && <li key="applicable">{makeJsx(applicable)}</li>,
+		notApplicable && <li key="non applicable">{makeJsx(notApplicable)}</li>
+	]
+
+	return any(identity, listElements) ? (
+		<section id="declenchement">
+			<h2>
+				<Trans>Déclenchement</Trans>
+			</h2>
+			<ul>{listElements}</ul>
+		</section>
+	) : null
+}
 
 export default compose(withNamespaces())(
 	class Algorithm extends React.Component {
@@ -15,18 +38,7 @@ export default compose(withNamespaces())(
 				<div id="algorithm">
 					<section id="rule-rules" className={classNames({ showValues })}>
 						<ShowValuesProvider value={showValues}>
-							<section id="declenchement">
-								<h2>
-									<Trans>Déclenchement</Trans>
-								</h2>
-								<ul>
-									{[
-										'parentDependency',
-										'applicable si',
-										'non applicable si'
-									].map(k => rule[k] && <li key={k}>{makeJsx(rule[k])}</li>)}
-								</ul>
-							</section>
+							<Conditions {...rule} />
 							{do {
 								let formula =
 										rule['formule'] ||
