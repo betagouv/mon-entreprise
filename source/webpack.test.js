abf6a0760ef8cc2530da87d09bd24aa6faec9da2
Johan Girod <johangirod@gmail.com>
2019-01-03 13:27:10 +0100

    :bug: répare les sourcemap dans les stacktrace des tests

--- source/webpack.test.js
+++ source/webpack.test.js
@@ -3,6 +3,13 @@ var webpack = require('webpack'),
 
 module.exports = {
 	...common,
+	target: 'node',
 	mode: 'development',
+	devtool: 'inline-cheap-module-source-map',
+	output: {
+		// use absolute paths in sourcemaps (important for debugging via IDE)
+		devtoolModuleFilenameTemplate: '[absolute-resource-path]',
+		devtoolFallbackModuleFilenameTemplate: '[absolute-resource-path]?[hash]'
+	},
 	plugins: [new webpack.EnvironmentPlugin({ NODE_ENV: 'development' })]
 }
