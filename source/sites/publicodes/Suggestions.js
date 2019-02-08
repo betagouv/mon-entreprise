6bb149bf101de83e80bc75b11816fe5961b0daa0
Mael <mael.thomas@data.gouv.fr>
2019-02-08 21:45:48 +0100

    :art: Style des suggestions

--- source/sites/publicodes/Suggestions.js
+++ source/sites/publicodes/Suggestions.js
@@ -8,8 +8,8 @@ import { Link } from 'react-router-dom'
 export default connect(state => ({ rules: flatRulesSelector(state) }))(
 	({ rules }) => (
 		<section style={{ marginTop: '4rem' }}>
-			<h2>Suggestions </h2>
-			<ul>
+			<h2>Suggestions :</h2>
+			<ul css="display: flex; flex-wrap: wrap ">
 				{simulateurs
 					.map(dottedName => findRuleByDottedName(rules, dottedName))
 					.map(r => (
@@ -21,12 +21,28 @@ export default connect(state => ({ rules: flatRulesSelector(state) }))(
 )
 
 let Suggestion = ({ dottedName, formule, title, icônes }) => (
-	<li key={dottedName} css="background: red">
-		<Link
-			className={formule ? '' : 'comingSoon'}
-			to={formule ? '/simulateur/' + dottedName : '#'}>
+	<li
+		key={dottedName}
+		css={`
+			font-size: 135%;
+			list-style-type: none;
+			border: 3px solid var(--colour);
+			padding: 1rem;
+			margin: 1rem;
+			width: 12rem;
+			min-height: 6em;
+			text-align: center;
+			position: relative;
+			${formule ? '' : 'filter: grayscale(70%); opacity: 0.6'}
+		`}>
+		<div css=" img { width: 2rem !important; height: 2rem !important}}">
 			{icônes && emoji(icônes + ' ')}
-			{title}
-		</Link>
+		</div>
+		<Link to={formule ? '/simulateur/' + dottedName : '#'}>{title}</Link>
+		{!formule && (
+			<div css="position: absolute; bottom: 0; left: 0; width: 100%; background: var(--colour); color: white; font-size: 80%">
+				Prochainement !
+			</div>
+		)}
 	</li>
 )
