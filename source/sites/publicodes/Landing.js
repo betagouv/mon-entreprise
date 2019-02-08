dcead4e56c43b825a7647cd162b6cf462051982a
Mael <mael.thomas@data.gouv.fr>
2019-02-08 22:05:14 +0100

    :art: style 2

--- source/sites/publicodes/Landing.js
+++ source/sites/publicodes/Landing.js
@@ -1,6 +1,7 @@
 import { React } from 'Components'
 import { Link } from 'react-router-dom'
 import Suggestions from './Suggestions'
+import emoji from 'react-easy-emoji'
 
 export default () => (
 	<div className="ui__ container">
@@ -20,22 +21,25 @@ class Search extends React.Component {
 	state = { input: '' }
 	render() {
 		return (
-			<input
-				style={{
-					display: 'block',
-					width: '80%',
-					border: '1px solid black',
-					fontSize: '2rem',
-					borderRadius: '.3rem',
-					boxShadow: '#06060624 4px 6px 15px'
-				}}
-				type="text"
-				value={this.state.input}
-				onChange={event => {
-					console.log('Enregistrer la saisie dans un JSON en ligne') ||
-						this.setState({ input: event.target.value })
-				}}
-			/>
+			<>
+				{emoji('🔎')}
+				<input
+					css={`
+						display: inline-block;
+						width: 80%;
+						border: 3px solid var(--colour);
+						font-size: 2rem;
+						border-radius: 0.3rem;
+						box-shadow: #06060624 4px 6px 15px;
+					`}
+					type="text"
+					value={this.state.input}
+					onChange={event => {
+						console.log('Enregistrer la saisie dans un JSON en ligne') ||
+							this.setState({ input: event.target.value })
+					}}
+				/>
+			</>
 		)
 	}
 }
