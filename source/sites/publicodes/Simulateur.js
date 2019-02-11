9e5d3633b37dc434de4439deb5396fdadd43835b
Mael <mael.thomas@data.gouv.fr>
2019-02-11 22:15:07 +0100

    Affichage du titre de la simulation

--- source/sites/publicodes/Simulateur.js
+++ source/sites/publicodes/Simulateur.js
@@ -1,28 +1,37 @@
 import Simulation from 'Components/Simulation'
 import withSimulationConfig from 'Components/simulationConfigs/withSimulationConfig'
-import React from 'react'
+import { React, emoji } from 'Components'
 import { Helmet } from 'react-helmet'
 import Target from './SimpleTarget'
+import { findRuleByDottedName } from 'Engine/rules'
+import { connect } from 'react-redux'
+import { flatRulesSelector } from 'Selectors/analyseSelectors'
 
-export default props => {
-	let Simulateur = withSimulationConfig({
-		objectifs: [props.match.params.name]
-	})(() => (
-		<div className="ui__ container">
-			<Helmet>
-				<title>TITRE</title>
-				<meta name="description" content="DESCRIPTION" />
-			</Helmet>
-			<h1>TITRE</h1>
-			<p>TEXTE INTRO</p>
-			<Simulation
-				showTargetsAnyway
-				targetsTriggerConversation={false}
-				targets={<Target />}
-				explication={<p>PTITE EXPLICATION DU RESULTAT</p>}
-			/>
-		</div>
-	))
+export default connect(state => ({ rules: flatRulesSelector(state) }))(
+	props => {
+		let objectif = props.match.params.name,
+			rule = findRuleByDottedName(props.rules, objectif),
+			Simulateur = withSimulationConfig({
+				objectifs: [objectif]
+			})(() => (
+				<div className="ui__ container">
+					<Helmet>
+						<title>{rule.title}</title>
+						<meta name="description" content="DESCRIPTION" />
+					</Helmet>
+					<h1>
+						{rule.icônes && emoji(rule.icônes + ' ')}
+						{rule.title}
+					</h1>
+					<Simulation
+						showTargetsAnyway
+						targetsTriggerConversation={false}
+						targets={<Target />}
+						explanation={<p>PTITE EXPLICATION DU RESULTAT</p>}
+					/>
+				</div>
+			))
 
-	return <Simulateur />
-}
+		return <Simulateur />
+	}
+)
