6a8869d30e151feeb7b683ab77a4692fd7e90d05
Mael <mael.thomas@data.gouv.fr>
2019-02-12 14:04:51 +0100

    Améliorer la page En savoir plus

--- source/sites/publicodes/About.js
+++ source/sites/publicodes/About.js
@@ -4,7 +4,6 @@ import about from 'raw-loader!./about.md'
 
 export default () => (
 	<section className="ui__ container" id="about">
-		<h1>À propos</h1>
 		{createMarkdownDiv(about)}
 	</section>
 )
