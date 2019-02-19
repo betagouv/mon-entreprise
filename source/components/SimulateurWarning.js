4ddce31c7832f35b6be778fd4972f4db48fb8066
Johan Girod <johangirod@gmail.com>
2019-02-19 14:16:02 +0100

    :bug: corrige l'inversion EI / Société dans les précédentes réponses

--- source/components/SimulateurWarning.js
+++ source/components/SimulateurWarning.js
@@ -9,31 +9,27 @@ export default function SimulateurWarning() {
 			<p>
 				{emoji('🚩')}{' '}
 				<T k="simulationWarning">
-					Ce simulateur donne une estimation sur des données{' '}
-					<strong>purement théoriques</strong> de chiffre d'affaires, de charges
-					et de base fiscale célibataire sans enfant hors tout autre revenu{' '}
-					<strong>
-						qui ne saurait engager la responsabilité des organismes sociaux
-						concernés au regard des déclarations et des calculs réels.
-					</strong>
+					<ul>
+						<li>
+							Le chiffre d'affaires déduit des charges va à 100% dans la
+							rémunération du dirigeant.
+						</li>
+						<li>
+							Le calcul de l'impôt sur le revenu, est basé sur un célibataire
+							sans enfant sans autre revenu.{' '}
+						</li>
+						<li>
+							Ce simulateur donne une estimation purement indicative des
+							cotisations
+						</li>
+					</ul>
 				</T>
 			</p>
 			<div className="beta__container">
 				<small className="beta__tag">Version beta</small>
 				<p>
-					<strong>Ce simulateur est en cours de développement.</strong> Les
-					calculs sont effectués sur la base des hypothèses suivantes :
+					<strong>Ce simulateur est en cours de développement.</strong>
 				</p>
-				<ul>
-					<li>
-						Tout le chiffre d'affaires part dans la rémunération du dirigeant et
-						les charges (pas de bénéfice).
-					</li>
-					<li>
-						L'impôt sur le revenu est calculé pour un célibataire sans enfants
-						et sans autre revenu.
-					</li>
-				</ul>
 			</div>
 		</>
 	)
