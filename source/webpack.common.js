0dc384fb742a16e33bc672c888b37f4ef680ca92
Mael <mael.thomas@data.gouv.fr>
2019-04-07 21:57:29 +0200

    Optimisation de la compilation + :bug: de chargement du js

--- source/webpack.common.js
+++ source/webpack.common.js
@@ -19,46 +19,11 @@ module.exports.default = {
 		}
 	},
 	entry: {
-		'mon-entreprise': './source/sites/mon-entreprise.fr/entry.fr.js',
-		infrance: './source/sites/mon-entreprise.fr/entry.en.js',
-		'simulateur-iframe-integration':
-			'./source/sites/mon-entreprise.fr/iframe-integration-script.js',
 		publicodes: './source/sites/publicodes/entry.js'
 	},
 	output: {
 		path: path.resolve('./dist/')
 	},
-	plugins: [
-		new EnvironmentPlugin({
-			EN_SITE: '/infrance${path}',
-			FR_SITE: '/mon-entreprise${path}',
-			MASTER: false
-		}),
-		new HTMLPlugin({}),
-
-		new CopyPlugin([
-			'./manifest.webmanifest',
-			{
-				from: './source/sites/mon-entreprise.fr/robots.txt',
-				to: 'robots.infrance.txt'
-			},
-			{
-				from: './source/sites/mon-entreprise.fr/sitemap.fr.txt',
-				to: 'sitemap.infrance.fr.txt'
-			},
-			{
-				from: './source/sites/mon-entreprise.fr/sitemap.en.txt',
-				to: 'sitemap.infrance.en.txt'
-			},
-			{
-				from: './source/images',
-				to: 'images'
-			},
-			{
-				from: './source/sites/mon-entreprise.fr/favicon',
-				to: 'favicon'
-			}
-		])
 	]
 }
 
@@ -127,33 +92,6 @@ module.exports.commonLoaders = ({ legacy = false } = {}) => {
 }
 
 module.exports.HTMLPlugins = ({ injectTrackingScript = false } = {}) => [
-	new HTMLPlugin({
-		template: 'index.html',
-		inject: false,
-		injectTrackingScript,
-		// mycompanyinfrance.fr :
-		chunks: ['infrance'],
-		title:
-			'My company in France: A step-by-step guide to start a business in France',
-		description:
-			'Find the type of company that suits you and follow the steps to register your company. Discover the French social security system by simulating your hiring costs. Discover the procedures to hire in France and learn the basics of French labour law.',
-		filename: 'infrance.html',
-		shareImage: 'https://mon-entreprise.fr/images/logo-mycompany-share.png',
-		logo: 'images/logo-mycompany.svg'
-	}),
-	new HTMLPlugin({
-		template: 'index.html',
-		inject: false,
-		injectTrackingScript,
-		// mon-entreprise.fr :
-		chunks: ['mon-entreprise'],
-		title: "Mon-entreprise.fr : L'assistant officiel du créateur d'entreprise",
-		description:
-			'Du statut juridique à la première embauche, en passant par la simulation des cotisations, vous trouverez ici toutes les ressources pour démarrer votre activité.',
-		filename: 'mon-entreprise.html',
-		shareImage: 'https://mon-entreprise.fr/images/logo-share.png',
-		logo: 'images/logo.svg'
-	}),
 	new HTMLPlugin({
 		inject: false,
 
