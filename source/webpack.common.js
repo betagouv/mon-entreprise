0b0e4f0bf4e9c6952efbab4223926233ca95c2ae
Mael <mael.thomas@data.gouv.fr>
2019-04-07 21:57:29 +0200

    WIP

--- source/webpack.common.js
+++ source/webpack.common.js
@@ -19,81 +19,18 @@ module.exports = {
 		}
 	},
 	entry: {
-		embauche: './source/sites/embauche.gouv.fr/entry.js',
-		'mon-entreprise': './source/sites/mycompanyinfrance.fr/entry.fr.js',
-		infrance: './source/sites/mycompanyinfrance.fr/entry.en.js',
-
-		// To not introduce breaking into the iframe integration, we serve simulateur.js from a 'dist' subdirectory
-		'dist/simulateur': ['./source/sites/embauche.gouv.fr/iframe-script.js'],
-		publicodes: ['./source/sites/publicodes/entry.js']
+		publicodes: './source/sites/publicodes/entry.js'
 	},
 	output: {
 		path: path.resolve('./dist/')
 	},
 	plugins: [
-		new EnvironmentPlugin({
-			EN_SITE: '/infrance${path}',
-			FR_SITE: '/mon-entreprise${path}'
-		}),
-		new HTMLPlugin({
-			template: 'index.html',
-			inject: false,
-			chunks: ['infrance'],
-			title:
-				'My company in France: A step-by-step guide to start a business in France',
-			description:
-				'Find the type of company that suits you and follow the steps to register your company. Discover the French social security system by simulating your hiring costs. Discover the procedures to hire in France and learn the basics of French labour law.',
-			filename: 'infrance.html'
-		}),
-		new HTMLPlugin({
-			template: 'index.html',
-			inject: false,
-			chunks: ['embauche'],
-			title: "Simulateur d'embauche 🤝",
-			description:
-				"Simulation du prix d'une embauche en France et calcul du salaire net à partir du brut : CDD, statut cadre, cotisations sociales, retraite...",
-			filename: 'embauche.html'
-		}),
-		new HTMLPlugin({
-			template: 'index.html',
-			inject: false,
-			chunks: ['mon-entreprise'],
-			title: "Mon-entreprise.fr : Le guide officiel du créateur d'entreprise",
-			description:
-				'Du statut juridique à la première embauche, en passant par la simulation des cotisations, vous trouverez ici toutes les ressources pour démarrer votre activité.',
-			filename: 'mon-entreprise.html'
-		}),
 		new HTMLPlugin({
 			template: 'index.html',
 			chunks: ['publicodes'],
 			title: 'Futureco ✍️',
 			description: "L'impact sur le climat de vos gestes quotidiens",
 			filename: 'publicodes.html'
-		}),
-
-		new CopyPlugin([
-			'./manifest.webmanifest',
-			'./source/sites/embauche.gouv.fr/images/logo',
-			{
-				from: './source/sites/embauche.gouv.fr/robots.txt',
-				to: 'robots.embauche.txt'
-			},
-			{
-				from: './source/sites/mycompanyinfrance.fr/robots.txt',
-				to: 'robots.infrance.txt'
-			},
-			{
-				from: './source/sites/mycompanyinfrance.fr/sitemap.fr.txt',
-				to: 'sitemap.infrance.fr.txt'
-			},
-			{
-				from: './source/sites/mycompanyinfrance.fr/sitemap.en.txt',
-				to: 'sitemap.infrance.en.txt'
-			},
-			{
-				from: './source/sites/mycompanyinfrance.fr/images',
-				to: 'static-images'
-			}
-		])
+		})
 	]
 }
