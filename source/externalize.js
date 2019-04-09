ce99dd24ab536143da6e53cdf9f5a3d7db255145
Johan Girod <johangirod@gmail.com>
2019-04-09 19:01:59 +0200

    :alien: traductions manquantes

--- source/externalize.js
+++ source/externalize.js
@@ -1,21 +1,9 @@
 var { safeLoad, safeDump } = require('js-yaml')
 var fs = require('fs')
-var translate = require('google-translate-api')
 var path = require('path')
 let R = require('ramda')
 
 let externalizationPath = 'source/règles/externalized.yaml'
-// Test the translation agent
-translate(
-	'Bonjour, je suis votre agent traducteur. Je vais traduire votre application, puis ensuite dominer le monde',
-	{ from: 'fr', to: 'en' }
-)
-	.then(res => {
-		console.log(res.text)
-	})
-	.catch(err => {
-		console.error(err)
-	})
 
 let rules = safeLoad(
 	fs.readFileSync(path.resolve('source/règles/base.yaml'), 'utf-8')
@@ -37,35 +25,31 @@ let attributesToExternalize = [
 let buildDottedName = rule =>
 	rule.espace ? rule.espace + ' . ' + rule.nom : rule.nom
 
-let promises = rules
+let resolved = rules
 	.map(rule => (!rule.titre ? { ...rule, titre: rule.nom } : rule))
-	.map(rule =>
-		Promise.all(
-			R.toPairs(rule).map(([k, v]) => {
-				let attrToTranslate = attributesToExternalize.find(R.equals(k))
-				if (!attrToTranslate) return Promise.resolve({})
-				let enTrad = attrToTranslate + '.en',
-					frTrad = attrToTranslate + '.fr'
+	.map(rule => ({
+		[buildDottedName(rule)]: R.mergeAll(
+			R.toPairs(rule)
+				.filter(([_, v]) => !!v)
+				.map(([k, v]) => {
+					let attrToTranslate = attributesToExternalize.find(R.equals(k))
+					if (!attrToTranslate) return {}
+					let enTrad = attrToTranslate + '.en',
+						frTrad = attrToTranslate + '.fr'
 
-				let currentTranslation = currentExternalization[buildDottedName(rule)]
-				//Check if a human traduction exists already for this attribute
-				if (currentTranslation && currentTranslation[enTrad])
-					return Promise.resolve({
-						[enTrad]: currentTranslation[enTrad],
+					let currentTranslation = currentExternalization[buildDottedName(rule)]
+					//Check if a human traduction exists already for this attribute
+					if (currentTranslation && currentTranslation[enTrad])
+						return {
+							[enTrad]: currentTranslation[enTrad],
+							[frTrad]: v
+						}
+
+					return {
+						[enTrad]: '!!' + v,
 						[frTrad]: v
-					})
-				return translate(v, { from: 'fr', to: 'en' }).then(({ text }) => ({
-					[frTrad]: v,
-					// Mark auto translated text with a double tild symbol
-					[enTrad]: '~~' + text
-				}))
-			})
-		).then(attributes => {
-			let id = buildDottedName(rule)
-			return { [id]: R.mergeAll(attributes) }
-		})
-	)
-
-Promise.all(promises).then(resolved =>
-	fs.writeFileSync(externalizationPath, safeDump(R.mergeAll(resolved)))
-)
+					}
+				})
+		)
+	}))
+fs.writeFileSync(externalizationPath, safeDump(R.mergeAll(resolved)))
