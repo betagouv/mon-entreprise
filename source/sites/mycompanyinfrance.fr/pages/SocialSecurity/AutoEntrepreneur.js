7a8bf6c58a8ab9d8587651ea84173e5e07ea682f
Johan Girod <johangirod@gmail.com>
2019-04-10 11:29:10 +0200

    :alien: dernières traductions et ajustement

--- source/sites/mycompanyinfrance.fr/pages/SocialSecurity/AutoEntrepreneur.js
+++ source/sites/mycompanyinfrance.fr/pages/SocialSecurity/AutoEntrepreneur.js
@@ -1,24 +1,37 @@
+import { T } from 'Components'
 import Warning from 'Components/SimulateurWarning'
 import Simulation from 'Components/Simulation'
 import indépendantConfig from 'Components/simulationConfigs/auto-entrepreneur.yaml'
 import withSimulationConfig from 'Components/simulationConfigs/withSimulationConfig'
 import TargetSelection from 'Components/TargetSelection'
+import { compose } from 'ramda'
 import React from 'react'
 import { Helmet } from 'react-helmet'
+import { withTranslation } from 'react-i18next'
 import { AvertissementProtectionSocialeIndépendants } from './Indépendant'
 
-const AutoEntrepreneur = ({ noUserInput }) => (
+const AutoEntrepreneur = ({ noUserInput, t }) => (
 	<>
 		<Helmet>
 			<title>
-				Auto-entrepreneur : simulateur officiel de revenus et cotisations
+				{t([
+					'simulateurs.auto-entrepreneur.page.titre',
+					'Auto-entrepreneur : simulateur officiel de revenus et de cotisations'
+				])}
 			</title>
 			<meta
 				name="description"
-				content="Estimez vos revenus en tant qu'auto-entrepreneur à partir de votre chiffre d'affaire. Prise en compte de toutes les cotisations et de l'impôt sur le revenu. Simulateur officiel de l'Urssaf"
+				content={t([
+					'simulateurs.auto-entrepreneur.page.description',
+					"Estimez vos revenus en tant qu'auto-entrepreneur à partir de votre chiffre d'affaire. Prise en compte de toutes les cotisations et de l'impôt sur le revenu. Simulateur officiel de l'Urssaf"
+				])}
 			/>
 		</Helmet>
-		<h1>Simulateur auto-entrepreneur 2019</h1>
+		<h1>
+			<T k="simulateurs.auto-entrepreneur.titre">
+				Simulateur de revenus auto-entrepreneur
+			</T>
+		</h1>
 		<Warning autoFolded={!noUserInput} simulateur="auto-entreprise" />
 		<Simulation
 			targetsTriggerConversation={true}
@@ -27,4 +40,7 @@ const AutoEntrepreneur = ({ noUserInput }) => (
 		/>
 	</>
 )
-export default withSimulationConfig(indépendantConfig)(AutoEntrepreneur)
+export default compose(
+	withTranslation(),
+	withSimulationConfig(indépendantConfig)
+)(AutoEntrepreneur)
--- source/sites/mycompanyinfrance.fr/pages/SocialSecurity/Indépendant.js
+++ source/sites/mycompanyinfrance.fr/pages/SocialSecurity/Indépendant.js
@@ -1,24 +1,37 @@
-import { React } from 'Components'
+import { React, T } from 'Components'
 import Warning from 'Components/SimulateurWarning'
 import Simulation from 'Components/Simulation'
 import indépendantConfig from 'Components/simulationConfigs/indépendant.yaml'
 import withSimulationConfig from 'Components/simulationConfigs/withSimulationConfig'
 import TargetSelection from 'Components/TargetSelection'
+import { compose } from 'ramda'
 import emoji from 'react-easy-emoji'
 import { Helmet } from 'react-helmet'
+import { withTranslation } from 'react-i18next'
 
-const Indépendant = () => (
+const Indépendant = ({ t }) => (
 	<>
 		<Helmet>
 			<title>
-				Indépendant : simulateur officiel de revenus et de cotisations
+				{t([
+					'simulateurs.indépendant.page.titre',
+					'Indépendant : simulateur officiel de revenus et de cotisations'
+				])}
 			</title>
 			<meta
 				name="description"
-				content="Estimez vos revenus en tant qu'indépendant à partir de votre chiffre d'affaire (pour les EI et les gérants EURL et SARL majoritaires). Prise en compte de toutes les cotisations et de l'impôt sur le revenu. Simulateur officiel de l'Urssaf"
+				content={t([
+					'simulateurs.indépendant.page.description',
+					"Estimez vos revenus en tant qu'indépendant à partir de votre chiffre d'affaire (pour les EI et les gérants EURL et SARL majoritaires). Prise en compte de toutes les cotisations et de l'impôt sur le revenu. Simulateur officiel de l'Urssaf"
+				])}
 			/>
 		</Helmet>
-		<h1>Simulateur indépendant 2019</h1>
+		<h1>
+			<T k="simulateurs.indépendant.titre">
+				Simulateur de revenus pour indépendants
+			</T>
+		</h1>
+
 		<Warning />
 		<Simulation
 			targetsTriggerConversation={true}
@@ -35,22 +48,30 @@ const Indépendant = () => (
 
 let AvertissementForfaitIndépendants = () => (
 	<p>
-		{emoji('💶')} Notre estimation prend en compte les{' '}
-		<em>cotisations réelles</em> dues par le travailleur indépendant. Pendant la
-		première année de son activité, il paiera un forfait réduit (une somme de
-		l'ordre de 1300€ / an pour un artisan bénéficiant de l'ACRE)... mais il sera
-		régularisé l'année suivante selon ce montant réel.
+		{emoji('💶')}{' '}
+		<T k="simulateurs.indépendant.explication1">
+			Notre estimation prend en compte les <em>cotisations réelles</em> dues par
+			le travailleur indépendant. Pendant la première année de son activité, il
+			paiera un forfait réduit (une somme de l'ordre de 1300€ / an pour un
+			artisan bénéficiant de l'ACRE)... mais il sera régularisé l'année suivante
+			selon ce montant réel.
+		</T>
 	</p>
 )
 
 export let AvertissementProtectionSocialeIndépendants = () => (
 	<p>
-		{emoji('☂️')}
-		Les assurances chômage et accidents du travail ne sont pas prises en charge
-		au sein de la Sécurité sociale des indépendants. La retraite basée sur le
-		revenu professionnel est généralement plus faible. Pour être couvert le
-		professionnel peut souscrire des assurances complémentaires.
+		{emoji('☂️')}{' '}
+		<T k="simulateurs.indépendant.explication1">
+			Les assurances chômage et accidents du travail ne sont pas prises en
+			charge au sein de la Sécurité sociale des indépendants. La retraite basée
+			sur le revenu professionnel est généralement plus faible. Pour être
+			couvert le professionnel peut souscrire des assurances complémentaires.
+		</T>
 	</p>
 )
 
-export default withSimulationConfig(indépendantConfig)(Indépendant)
+export default compose(
+	withTranslation(),
+	withSimulationConfig(indépendantConfig)
+)(Indépendant)
