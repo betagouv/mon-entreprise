c1629e21998e6a8da228f9d056205a44b3106b35
Mael <mael.thomas@data.gouv.fr>
2019-04-10 18:31:26 +0200

    :bug: Les règles sont dans le state

--- source/components/rule/RuleSource.js
+++ source/components/rule/RuleSource.js
@@ -1,21 +1,25 @@
 import { buildDottedName } from 'Engine/rules'
-import { safeDump } from 'js-yaml'
 import React from 'react'
+import { safeDump } from 'js-yaml'
 import emoji from 'react-easy-emoji'
-import rules from 'Règles/base.yaml'
 import ColoredYaml from './ColoredYaml'
+import { connect } from 'react-redux'
 
-export default function RuleSource({ dottedName }) {
-	let source = rules.filter(rule => buildDottedName(rule).includes(dottedName))
+export default connect(state => ({ rules: state.rules }))(
+	({ rules, dottedName }) => {
+		let source = rules.filter(rule =>
+			buildDottedName(rule).includes(dottedName)
+		)
 
-	return (
-		<div id="RuleSource" className="ui__ container">
-			<h2>
-				{emoji('⚙️ ')}
-				Code source <br />
-				<code>{dottedName}</code>
-			</h2>
-			<ColoredYaml source={safeDump(source)} />
-		</div>
-	)
-}
+		return (
+			<div id="RuleSource" className="ui__ container">
+				<h2>
+					{emoji('⚙️ ')}
+					Code source <br />
+					<code>{dottedName}</code>
+				</h2>
+				<ColoredYaml source={safeDump(source)} />
+			</div>
+		)
+	}
+)
