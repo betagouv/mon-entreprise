ad8944111c06a852878d3508f3e1a9c337d6caa7
Mael <mael.thomas@data.gouv.fr>
2019-04-26 19:19:19 +0200

    Texte d'intro plus court et percutant

--- source/sites/publicodes/Landing.js
+++ source/sites/publicodes/Landing.js
@@ -16,13 +16,21 @@ export default () => {
 					display: flex;
 					align-items: center;
 				`}>
-				<img css="width: 6em; margin-right: 1em;" src={require('./logo.png')} />
+				<img
+					css={`
+						width: 6em;
+						@media (min-width: 800px) {
+							width: 8em;
+						}
+						margin-right: 1em;
+					`}
+					src={require('./logo.png')}
+				/>
 				<p>
-					Le <strong>dérèglement climatique</strong> n'est plus une menace
+					La <strong>catastrophe climatique</strong> n'est plus une menace
 					lointaine et incertaine, c'est une <strong>actualité</strong>. Comment
-					éviter la catastrophe ? Chaque aspect de notre vie moderne a un
-					impact. Il suffit de le connaître !{' '}
-					<Link to="/à-propos">En savoir plus</Link>.{' '}
+					l'éviter ? Chaque aspect de notre vie moderne a un impact,
+					découvrez-le ! <Link to="/à-propos">En savoir plus</Link>.{' '}
 				</p>
 			</section>
 			<h1>Quel est l'impact de ...</h1>
