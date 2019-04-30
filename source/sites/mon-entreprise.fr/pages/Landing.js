f2ed3c951ff17138a7a793ee4782caf3e99d0616
Mael <mael.thomas@data.gouv.fr>
2019-04-30 17:02:23 +0200

    :art: Idée de refonte de l'accueil avec le nouveau logo

--- source/sites/mon-entreprise.fr/pages/Landing.js
+++ source/sites/mon-entreprise.fr/pages/Landing.js
@@ -14,6 +14,7 @@ import Footer from '../layout/Footer/Footer'
 import './Landing.css'
 import type { ThemeColours } from 'Components/utils/withColours'
 import type { SitePaths } from 'Components/utils/withSitePaths'
+import logo from '../images/logo mon-entreprise.png'
 
 type Props = {
 	colours: ThemeColours,
@@ -25,28 +26,16 @@ export default compose(
 	withTranslation()
 )(({ colours: { colour }, sitePaths }: Props) => (
 	<div className="app-content">
-		<section className="landing__banner" style={{ backgroundColor: colour }}>
+		<section className="landing__banner">
 			<header>
-				<h1>
-					<T k="siteName">Mon-entreprise.fr</T>
-				</h1>
+				<img src={logo} style={{ width: '20em' }} />
 				<p className="ui__ lead" style={{ maxWidth: '35rem' }}>
 					<T k="subtitle">Le guide officiel du créateur d'entreprise</T>
 				</p>
-
-				<svg
-					className="landing__banner__svg white"
-					preserveAspectRatio="none"
-					viewBox="5 0 495 150">
-					<path fill="white" d="M 0 150 Q 150 0 500 0 L 500 150 Z" />
-				</svg>
 			</header>
 		</section>
 		<section className="ui__ container" style={{ flexGrow: 1 }}>
-			<div className="landing__header">
-				<img alt="logo marianne" src={marianneSvg} />
-				<img alt="logo urssaf" src={urssafSvg} />
-			</div>
+			<div className="landing__header" />
 			<h2 style={{ textAlign: 'center', marginBottom: '2rem' }}>
 				<T>Que souhaitez-vous faire ?</T>
 			</h2>
