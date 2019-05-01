e629e8f567561118578f91643af0417fb087a847
Mael <mael.thomas@data.gouv.fr>
2019-05-01 19:47:29 +0200

    Barre du haut dynamique, affiche le scénario

--- source/sites/publicodes/TopBar.js
+++ source/sites/publicodes/TopBar.js
@@ -1,37 +1,70 @@
 import { React, emoji } from 'Components'
+import { useContext } from 'react'
 import { Link } from 'react-router-dom'
+import scenarios from './scenarios.yaml'
+import { StoreContext } from './StoreContext'
+import { withRouter } from 'react-router'
 
-export default () => (
-	<section
-		id="intro"
-		css={`
-			margin-top: 1em;
-			display: flex;
-			align-items: center;
-			justify-content: space-between;
-		`}>
-		<img
+export default withRouter(({ location }) => {
+	let {
+			state: { scenario: scenarioName }
+		} = useContext(StoreContext),
+		scenario = scenarios[scenarioName],
+		displayIntro = location.pathname === '/'
+
+	return (
+		<section
 			css={`
-				width: 6em;
-				@media (min-width: 800px) {
-					width: 8em;
-				}
-				margin-right: 1em;
-			`}
-			src={require('./logo.png')}
-		/>
-		<p css="max-width: 28rem">
-			La <strong>catastrophe climatique</strong> n'est plus une menace lointaine
-			et incertaine, c'est une <strong>actualité</strong>. Comment l'éviter ?
-			Chacun de nos gestes a un impact, découvrez-le !{' '}
-			<Link to="/à-propos">En savoir plus</Link>.{' '}
-		</p>
-		<Link
-			to="/scénarios"
-			css={`
-				font-size: 200%;
+				margin-top: 0.6em;
+				display: flex;
+				align-items: center;
+				justify-content: center;
 			`}>
-			{emoji('⚙️')}
-		</Link>
-	</section>
-)
+			<img
+				css={`
+					width: ${displayIntro ? '6rem' : '5em'};
+					@media (min-width: 800px) {
+						width: ${displayIntro ? '8em' : '5em'};
+					}
+					margin-right: 1em;
+				`}
+				src={require('./logo.png')}
+			/>
+			{displayIntro && (
+				<p id="intro" css="max-width: 28rem">
+					La <strong>catastrophe climatique</strong> n'est plus une menace
+					lointaine et incertaine, c'est une <strong>actualité</strong>. Comment
+					l'éviter ? Chacun de nos gestes a un impact, découvrez-le !{' '}
+					<Link to="/à-propos">En savoir plus</Link>.{' '}
+				</p>
+			)}
+			{!displayIntro && (
+				<div
+					className="ui__ card"
+					css={`
+						.key {
+							font-size: 80%;
+						}
+					`}>
+					<div>
+						<span className="key">Scénario choisi : </span>
+						<Link to="/scénarios" css={``}>
+							« {scenario.titre} »
+						</Link>
+					</div>
+					<div
+						css={`
+							img {
+								font-size: 140%;
+							}
+							display: flex;
+							align-items: center;
+						`}>
+						<span className="key">Futur : &nbsp;</span>
+						<Link to="/scénarios">{emoji(scenario.icône)}</Link>
+					</div>
+				</div>
+			)}
+		</section>
+	)
+})
