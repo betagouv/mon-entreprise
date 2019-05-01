77aabdc2fe3be2e8d53854cfb35aa4d4ced5ec1b
Mael <mael.thomas@data.gouv.fr>
2019-05-01 20:35:49 +0200

    Lien vers github

--- source/sites/publicodes/About.js
+++ source/sites/publicodes/About.js
@@ -5,5 +5,9 @@ import about from 'raw-loader!./about.md'
 export default () => (
 	<section className="ui__ container" id="about">
 		{createMarkdownDiv(about)}
+		<p>
+			Le code de ce site{' '}
+			<a href="https://github.com/laem/futureco"> est libre</a>.
+		</p>
 	</section>
 )
