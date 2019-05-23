cc49ef16a3ed78f461113dd888fc36a28e297003
Mael <mael.thomas@data.gouv.fr>
2019-05-23 15:45:33 +0200

    Les bloc de suggestions entiers sont des liens, pas slt les textes

--- source/sites/publicodes/Suggestions.js
+++ source/sites/publicodes/Suggestions.js
@@ -55,65 +55,66 @@ let Suggestion = ({ dottedName, formule, title, icônes }) => {
 	let hasFormule = formule != null
 
 	return (
-		<li
-			key={dottedName}
-			css={`
-				font-size: 100%;
-				list-style-type: none;
-				border-radius: 1rem;
-				padding: 0.6rem;
-				margin: 0.6rem;
-				width: 10rem;
-				min-height: 7em;
-				position: relative;
-				display: flex;
-				align-items: center;
-				justify-content: middle;
-				text-align: center;
-				flex-wrap: wrap;
-				${hasFormule ? '' : 'filter: grayscale(70%); opacity: 0.6;'}
+		<Link
+			to={hasFormule ? '/simulateur/' + encodeRuleName(dottedName) : '#'}
+			css=":hover {opacity: 1 !important}">
+			<li
+				key={dottedName}
+				css={`
+					font-size: 100%;
+					list-style-type: none;
+					border-radius: 1rem;
+					padding: 0.6rem;
+					margin: 0.6rem;
+					width: 10rem;
+					min-height: 7em;
+					position: relative;
+					display: flex;
+					align-items: center;
+					justify-content: middle;
+					text-align: center;
+					flex-wrap: wrap;
+					line-height: 1.2em;
+					${hasFormule ? '' : 'filter: grayscale(70%); opacity: 0.6;'}
 
-				background: var(--colour);
-				color: white;
-				box-shadow: 0 2px 6px rgba(32, 33, 36, 0.5);
-				:hover {
-					box-shadow: 0 4px 10px rgba(32, 33, 36, 0.5);
-				}
-				a {
+					background: var(--colour);
 					color: white;
-					text-decoration: none;
-				}
-				:hover a {
-					text-decoration: underline;
-				}
-			`}>
-			<div css="width: 100%; img { width: 1.8rem !important; height: 1.8rem !important}}">
-				{icônes && emoji(icônes + ' ')}
-			</div>
-			<Link
-				css="width: 100%"
-				to={hasFormule ? '/simulateur/' + encodeRuleName(dottedName) : '#'}>
-				{title}
-			</Link>
-			{!hasFormule && (
-				<>
-					<div css="visibility: hidden">placeholder</div>
-					<div
-						css={`
-							position: absolute;
-							border-bottom-left-radius: 1.2rem;
-							border-bottom-right-radius: 1.2rem;
-							bottom: 0;
-							left: 0;
-							width: 100%;
-							background: var(--colour);
-							color: white;
-							font-size: 80%;
-						`}>
-						Prochainement !
-					</div>
-				</>
-			)}
-		</li>
+					box-shadow: 0 2px 6px rgba(32, 33, 36, 0.5);
+					:hover {
+						box-shadow: 0 4px 10px rgba(32, 33, 36, 0.5);
+					}
+					a {
+						color: white;
+						text-decoration: none;
+					}
+					:hover a {
+						text-decoration: underline;
+					}
+				`}>
+				<div css="width: 100%; img { font-size: 180%}}">
+					{icônes && emoji(icônes + ' ')}
+				</div>
+				<span css="width: 100%">{title}</span>
+				{!hasFormule && (
+					<>
+						<div css="visibility: hidden">placeholder</div>
+						<div
+							css={`
+								position: absolute;
+								border-bottom-left-radius: 1.2rem;
+								border-bottom-right-radius: 1.2rem;
+								bottom: 0;
+								left: 0;
+								width: 100%;
+								background: var(--colour);
+								color: white;
+								font-size: 80%;
+							`}>
+							Prochainement !
+						</div>
+					</>
+				)}
+			</li>
+		</Link>
 	)
 }
