8759e37b46943791035a882ab1b1b447adb23d56
Mael <mael.thomas@data.gouv.fr>
2019-06-03 18:53:55 +0200

    :art: Utilisation d'une librairie pour afficher les références
    Permet d'avoir de meilleurs aperçus, de ne pas réécrire le titre de la
    page.
    Problème pour merger sur syso : legifrance, URSSAF et compagnie
    n'exposent pas de données sémantiques, et leur image est très moche
    (pixellisée ou hors de propos).
    Voir aussi si l'usage d'un service tiers, qui plus est payant au bout
    d'un moment (une composante serveur semble
    dans l'état des librairies dispo, obligatoire) n'est pas un problème

--- source/components/rule/References.js
+++ source/components/rule/References.js
@@ -1,39 +1,23 @@
 import { toPairs } from 'ramda'
 import React from 'react'
-import { capitalise0 } from '../../utils'
-import './References.css'
+import Microlink from '@microlink/react'
 
-export default function References({ refs }) {
-	const renderRef = ([name, link]) => {
-		let refKey = findRefKey(link),
-			refData = (refKey && references[refKey]) || {},
-			domain = cleanDomain(link)
-
-		return (
-			<li key={name}>
-				<span className="imageWrapper">
-					{refData.image && (
-						<img
-							src={require('Règles/ressources/références/' + refData.image)}
-						/>
-					)}
-				</span>
-				<a href={link} target="_blank">
-					{capitalise0(name)}
-				</a>
-				<span className="url">{domain}</span>
-			</li>
-		)
-	}
-	const findRefKey = link =>
-		Object.keys(references).find(r => link.indexOf(r) > -1)
-
-	const cleanDomain = link =>
-		(link.indexOf('://') > -1
-			? link.split('/')[2]
-			: link.split('/')[0]
-		).replace('www.', '')
-
-	let references = toPairs(refs)
-	return <ul className="references">{references.map(renderRef)}</ul>
+export default ({ refs }) => {
+	let references = Array.isArray(refs) ? refs : Object.values(refs)
+	return (
+		<ul className="references">
+			{references.map(link => (
+				<li
+					key={link}
+					css={`
+						list-style-type: none;
+						a {
+							max-width: 100%;
+						}
+					`}>
+					<Microlink url={link} style={{ width: '100%' }} />
+				</li>
+			))}
+		</ul>
+	)
 }
