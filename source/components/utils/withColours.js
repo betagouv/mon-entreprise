271855d377b15ddd3058cc2b3029780080c0afde
Johan Girod <johan.girod@beta.gouv.fr>
2019-06-12 17:56:30 +0200

    :art: meilleur gestion des textes couleur claire

--- source/components/utils/withColours.js
+++ source/components/utils/withColours.js
@@ -68,7 +68,7 @@ const generateTheme = (themeColour?: ?string): ThemeColours => {
 			textColour === '#ffffff'
 				? 'rgba(255, 255, 255, .6)'
 				: 'rgba(0, 0, 0, .6)',
-		lighterTextColour = lightenTextColour(textColour),
+		lighterTextColour = darkColour + '99',
 		lighterInverseTextColour = lightenTextColour(inverseTextColour),
 		textColourOnWhite = textColour === '#ffffff' ? colour : '#333'
 
