0f17cfc096f126cc466695d5efa5d6d1b41cb1a1
Mael <mael.thomas@data.gouv.fr>
2019-06-18 11:51:16 +0200

    Récupération de l'erreur du composant d'eplicaiton de la simulation
    Ajouter une variable de type question aux cotiz patronales sans
    spécficier une valeur par défaut faisait casser l'app

--- source/components/SalaryExplanation.js
+++ source/components/SalaryExplanation.js
@@ -8,6 +8,21 @@ import { connect } from 'react-redux'
 import { formValueSelector } from 'redux-form'
 import * as Animate from 'Ui/animate'
 
+class ErrorBoundary extends React.Component {
+	state = {}
+	static getDerivedStateFromError() {
+		return {
+			error:
+				'The SalaryExplanation component triggered an error. This often happens in its subcomponents reducers'
+		}
+	}
+	render() {
+		if (this.state.error)
+			return <div css="background: red; ">Erreur : {this.state.error}</div>
+		return this.props.children
+	}
+}
+
 export default compose(
 	withTracker,
 	connect(state => ({
@@ -15,42 +30,47 @@ export default compose(
 	}))
 )(function SalaryExplanation({ showDistributionFirst }) {
 	return (
-		<Animate.fromTop key={showDistributionFirst}>
-			{showDistributionFirst ? (
-				<>
-					<DistributionSection />
-					<PaySlipSection />
-				</>
-			) : (
-				<>
-					<PaySlipSection />
-					<DistributionSection />
-				</>
-			)}
-			<br />
-			<p className="ui__ notice">
-				<Trans i18nKey="payslip.notice">
-					Le simulateur vous aide à comprendre votre bulletin de paie, sans lui
-					être opposable. Pour plus d&apos;informations, rendez vous sur&nbsp;
-					<a
-						alt="service-public.fr"
-						href="https://www.service-public.fr/particuliers/vosdroits/F559">
-						service-public.fr
-					</a>
-					.
-				</Trans>
-			</p>
-			<p className="ui__ notice">
-				<Trans i18nKey="payslip.disclaimer">
-					Il ne prend pour l'instant pas en compte les accords et conventions
-					collectives, ni la myriade d'aides aux entreprises. Trouvez votre
-					convention collective{' '}
-					<a href="https://socialgouv.github.io/conventions-collectives">ici</a>
-					, et explorez les aides sur&nbsp;
-					<a href="https://www.aides-entreprises.fr">aides-entreprises.fr</a>.
-				</Trans>
-			</p>
-		</Animate.fromTop>
+		<ErrorBoundary>
+			<Animate.fromTop key={showDistributionFirst}>
+				{showDistributionFirst ? (
+					<>
+						<DistributionSection />
+						<PaySlipSection />
+					</>
+				) : (
+					<>
+						<PaySlipSection />
+						<DistributionSection />
+					</>
+				)}
+				<br />
+				<p className="ui__ notice">
+					<Trans i18nKey="payslip.notice">
+						Le simulateur vous aide à comprendre votre bulletin de paie, sans
+						lui être opposable. Pour plus d&apos;informations, rendez vous
+						sur&nbsp;
+						<a
+							alt="service-public.fr"
+							href="https://www.service-public.fr/particuliers/vosdroits/F559">
+							service-public.fr
+						</a>
+						.
+					</Trans>
+				</p>
+				<p className="ui__ notice">
+					<Trans i18nKey="payslip.disclaimer">
+						Il ne prend pour l'instant pas en compte les accords et conventions
+						collectives, ni la myriade d'aides aux entreprises. Trouvez votre
+						convention collective{' '}
+						<a href="https://socialgouv.github.io/conventions-collectives">
+							ici
+						</a>
+						, et explorez les aides sur&nbsp;
+						<a href="https://www.aides-entreprises.fr">aides-entreprises.fr</a>.
+					</Trans>
+				</p>
+			</Animate.fromTop>
+		</ErrorBoundary>
 	)
 })
 
