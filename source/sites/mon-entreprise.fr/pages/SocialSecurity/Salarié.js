7ea44a7b463e85687e1fb88a337ad4deb3328e64
Maxime Quandalle <maxime@quandalle.com>
2019-07-09 10:51:01 +0200

    Ajout d'explications sur le simulateur salarié

--- source/sites/mon-entreprise.fr/pages/SocialSecurity/Salarié.js
+++ source/sites/mon-entreprise.fr/pages/SocialSecurity/Salarié.js
@@ -7,6 +7,7 @@ import salariéConfig from 'Components/simulationConfigs/salarié.yaml'
 import withSimulationConfig from 'Components/simulationConfigs/withSimulationConfig'
 import withLanguage from 'Components/utils/withLanguage'
 import withSitePaths from 'Components/utils/withSitePaths'
+import { createMarkdownDiv } from 'Engine/marked'
 import { compose } from 'ramda'
 import emoji from 'react-easy-emoji'
 import { Helmet } from 'react-helmet'
@@ -30,7 +31,7 @@ export default compose(
 					name="description"
 					content={t(
 						'simulateurs.salarié.page.description',
-						'Estimez les cotisations pour un salarié à partir du salaire brut, net ou "superbrut". Prise en comptes de toutes les cotisations du régime général et de l\'impôt sur le revenu. Découvrez les contreparties garanties par sécurité sociale'
+						'Estimez les cotisations pour un salarié à partir du salaire brut, net ou "superbrut". Prise en compte de toutes les cotisations du régime général et de l\'impôt sur le revenu. Découvrez les contreparties garanties par sécurité sociale'
 					)}
 				/>
 			</Helmet>
@@ -48,9 +49,32 @@ export default compose(
 				</Banner>
 			)}
 			<SalarySimulation />
+			<SalariéExplications />
 		</>
 	)
 })
+
+const SalariéExplications = withSitePaths(({ sitePaths }) =>
+	createMarkdownDiv(`
+## Calculer son salaire net
+
+Lors de l'entretien d'embauche l'employeur propose en général une rémunération exprimée en « brut ». Le montant annoncé inclut ainsi les cotisations salariales, qui servent à financer la protection sociale du salarié (retraite, maladie, famille, etc.) et qui sont retranchées du salaire « net » perçu par le salarié.
+
+Vous pouvez utiliser notre simulateur pour convertir le **salaire brut en net** : il vous suffit pour cela de saisir la rémunération annoncée dans la case salaire brut. La simulation peut-être affinée en répondant aux différentes questions (sur le CDD, statut cadre, etc.).
+
+Par ailleurs depuis 2019, l'[impôt sur le revenu](/documentation/impôt/impôt-sur-le-revenu) est prélevé à la source. Pour ce faire, la direction générale des finances publiques (DGFiP) transmet à l'employeur le taux d'imposition calculé à partir de la déclaration de revenu du salarié. Si ce taux est inconnu, par exemple lors d'une première année d'activité, l'employeur utilise le [taux neutre](/documentation/impôt/neutre). C'est aussi le taux neutre que nous utilisons dans le simulateur pour calculer le « net après impôt » qui est versé sur le compte bancaire du salarié.
+
+Attention : L'impôt sur le revenu est calculé à partir du [salaire net imposable](/documentation/contrat-salarié/rémunération/net-imposable) qui inclut certaines cotisations non déductibles et les avantages en nature en plus du salaire net.
+
+## Coût d'embauche
+
+Si vous cherchez à embaucher, vous pouvez calculer le coût total de la rémunération de votre salarié, ainsi que les montants de cotisations patronales et salariales correspondants. Cela vous permet de définir le niveau de rémunération en connaissant le montant global de charge que cela représente pour votre entreprise.
+
+En plus du salaire, notre simulateur prend en compte le calcul des avantages en nature (téléphone, véhicule de fonction, etc.), ainsi que la mutuelle santé obligatoire.
+
+Il existe des [aides différées](/documentation/aides-employeur) à l'embauche qui ne sont pas prises en compte par notre simulateur, vous pouvez les retrouver sur [le portail officiel](http://www.aides-entreprises.fr).
+`)
+)
 
 export let SalarySimulation = compose(
 	withSimulationConfig(salariéConfig),
