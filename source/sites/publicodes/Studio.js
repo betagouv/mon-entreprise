6d5b8909ba8aedb27ae4a1e3846860cc365be62a
Mael <mael.thomas@data.gouv.fr>
2019-09-05 15:54:29 +0200

    Ajouter une règle au state
    Mais la recherche est cassée depuis l'abandon de React-Select

--- source/sites/publicodes/Studio.js
+++ source/sites/publicodes/Studio.js
@@ -1,39 +1,57 @@
 import React from 'react'
 import MonacoEditor from 'react-monaco-editor'
+import { connect } from 'react-redux'
+import { safeLoad } from 'js-yaml'
+import { Link } from 'react-router-dom'
 
-export default class extends React.Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			code: '// type your code...'
+export default connect(
+	state => ({ rules: state.rules }),
+	dispatch => ({ setRules: rules => dispatch({ type: 'SET_RULES', rules }) })
+)(
+	class Studio extends React.Component {
+		constructor(props) {
+			super(props)
+			this.state = {
+				code: '# ajoutez ici vos nouvelles règles'
+			}
+		}
+		editorDidMount(editor, monaco) {
+			editor.focus()
+		}
+		onChange = (newValue, e) => {
+			this.setState({ code: newValue })
+		}
+		updateRules() {
+			this.props.setRules([...this.props.rules, ...safeLoad(this.state.code)])
+		}
+		render() {
+			const code = this.state.code
+			const options = {
+				selectOnLineNumbers: true
+			}
+			return (
+				<section className="ui__ container" id="about">
+					<p>Fais-toi plaiz</p>
+					{code}
+					{JSON.stringify(this.props.rules[this.props.rules.length - 1])}
+					<MonacoEditor
+						width="800"
+						height="600"
+						language="yaml"
+						theme="vs-dark"
+						value={code}
+						options={options}
+						onChange={this.onChange}
+						editorDidMount={this.editorDidMount}
+					/>
+					<button
+						onClick={() => this.updateRules()}
+						className="ui__ buttton plain">
+						Sauver
+					</button>
+					<Link to="/documentation">Doc</Link>
+				</section>
+			)
 		}
 	}
-	editorDidMount(editor, monaco) {
-		console.log('editorDidMount', editor)
-		editor.focus()
-	}
-	onChange(newValue, e) {
-		console.log('onChange', newValue, e)
-	}
-	render() {
-		const code = this.state.code
-		const options = {
-			selectOnLineNumbers: true
-		}
-		return (
-			<section className="ui__ container" id="about">
-				<p>Fais-toi plaiz</p>
-				<MonacoEditor
-					width="800"
-					height="600"
-					language="yaml"
-					theme="vs-dark"
-					value={code}
-					options={options}
-					onChange={this.onChange}
-					editorDidMount={this.editorDidMount}
-				/>
-			</section>
-		)
-	}
-}
+)
