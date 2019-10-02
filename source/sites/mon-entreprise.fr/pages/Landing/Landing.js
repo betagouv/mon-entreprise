f55314ed29cd426469f558ce4d0ea30b28765719
Johan Girod <johan.girod@beta.gouv.fr>
2019-10-02 18:12:40 +0200

    :alien: ajoute les traductions manquantes

--- source/sites/mon-entreprise.fr/pages/Landing/Landing.js
+++ source/sites/mon-entreprise.fr/pages/Landing/Landing.js
@@ -72,80 +72,87 @@ export default withSitePaths(({ sitePaths }: Props) => {
 					className="ui__ interactive card box"
 					to={sitePaths.entreprise.index}>
 					<div className="ui__ big box-icon">{emoji('💡')}</div>
-					<h3>
-						<T k="landing.choix1">Créer une entreprise</T>
-					</h3>
-					<p className="ui__ notice" css="flex: 1">
-						Un accompagnement au choix du statut et la liste complète des
-						démarches de création
-					</p>
-					<div className="ui__ small simple button">Commencer</div>
+					<T k="landing.choice.create">
+						<h3>Créer une entreprise</h3>
+						<p className="ui__ notice" css="flex: 1">
+							Un accompagnement au choix du statut et la liste complète des
+							démarches de création
+						</p>
+					</T>
+					<div className="ui__ small simple button">
+						<T>Commencer</T>
+					</div>
 				</Link>
 				<Link
 					className="ui__ interactive card box "
 					to={sitePaths.sécuritéSociale.index}>
 					<div className="ui__ big box-icon">{emoji('💶')}</div>
-					<h3>
-						<T k="landing.choix2">Gérer mon activité</T>
-					</h3>
-					<p className="ui__ notice" css="flex: 1">
-						Des simulateurs pour anticiper le montant des cotisation et mieux
-						gérer votre trésorerie
-					</p>
-					<div className="ui__ small simple button">Commencer</div>
+					<T k="landing.choice.manage">
+						<h3>Gérer mon activité</h3>
+						<p className="ui__ notice" css="flex: 1">
+							Des simulateurs pour anticiper le montant des cotisation et mieux
+							gérer votre trésorerie
+						</p>
+					</T>
+					<div className="ui__ small simple button">
+						<T>Commencer</T>
+					</div>
 				</Link>
 				<Link
 					className="ui__ interactive card box"
 					to={sitePaths.économieCollaborative.index}>
 					<div className="ui__ big box-icon">{emoji('🙋')}</div>
-					<h3>
-						<T k="landing.choix3">Que dois-je déclarer ?</T>
-					</h3>
-					<p className="ui__ notice" css="flex: 1">
-						Un guide pour savoir comment déclarer vos revenus issus de
-						plateformes en ligne (AirBnb, leboncoin, blablacar, etc.)
-					</p>
-					<br />
-					<div className="ui__ small simple button">Commencer</div>
+					<T k="landing.choice.declare">
+						<h3>Que dois-je déclarer ?</h3>
+						<p className="ui__ notice" css="flex: 1">
+							Un guide pour savoir comment déclarer vos revenus issus de
+							plateformes en ligne (AirBnb, leboncoin, blablacar, etc.)
+						</p>
+					</T>
+					<div className="ui__ small simple button">
+						<T>Commencer</T>
+					</div>
 				</Link>
 			</section>
 
 			<section className="ui__ container">
-				<h2>Qui sommes-nous ?</h2>
-				<p>
-					Ce site a été développé par l’administration pour{' '}
-					<strong>accompagner les créateurs d’entreprise</strong> dans le
-					développement de leur activité.
-				</p>
-				<p>
-					Nous partons du constat qu’il est{' '}
-					<strong>difficile de s’y retrouver</strong> dans toute la
-					documentation en ligne : il manque d'informations claires, à jour et
-					pertinentes sur la création d'entreprise.
-				</p>
-				<p>
-					Notre objectif est de{' '}
-					<strong>
-						lever toutes les incertitudes vis à vis de l’administration
-					</strong>{' '}
-					afin que vous puissiez vous concentrer sur ce qui compte : votre
-					activité.
-				</p>
-				<p>
-					Nous sommes une petite{' '}
-					<strong>équipe autonome et pluridisciplinaire</strong> au sein de l’
-					<a href="https://urssaf.fr">URSSAF</a>. Nous avons à coeur d’être au
-					plus proche de nos usagers afin d’améliorer en permanence ce site
-					conformément à la méthode des{' '}
-					<a href="https://beta.gouv.fr">Startup d’État</a>.
-				</p>
-				<p>
-					N’hésitez pas à nous remonter vos remarques et suggestions à{' '}
-					<a href="mailto:contact@mon-entreprise.beta.gouv.fr">
-						contact@mon-entreprise.beta.gouv.fr
-					</a>
-					.
-				</p>
+				<T k="landing.aboutUs">
+					<h2>Qui sommes-nous ?</h2>
+					<p>
+						Ce site a été développé par l’administration pour{' '}
+						<strong>accompagner les créateurs d’entreprise</strong> dans le
+						développement de leur activité.
+					</p>
+					<p>
+						Nous partons du constat qu’il est{' '}
+						<strong>difficile de s’y retrouver</strong> dans toute la
+						documentation en ligne : il manque d'informations claires, à jour et
+						pertinentes sur la création d'entreprise.
+					</p>
+					<p>
+						Notre objectif est de{' '}
+						<strong>
+							lever toutes les incertitudes vis à vis de l’administration
+						</strong>{' '}
+						afin que vous puissiez vous concentrer sur ce qui compte : votre
+						activité.
+					</p>
+					<p>
+						Nous sommes une petite{' '}
+						<strong>équipe autonome et pluridisciplinaire</strong> au sein de l’
+						<a href="https://urssaf.fr">URSSAF</a>. Nous avons à coeur d’être au
+						plus proche de nos usagers afin d’améliorer en permanence ce site
+						conformément à la méthode des{' '}
+						<a href="https://beta.gouv.fr">Startup d’État</a>.
+					</p>
+					<p>
+						N’hésitez pas à nous remonter vos remarques et suggestions à{' '}
+						<a href="mailto:contact@mon-entreprise.beta.gouv.fr">
+							contact@mon-entreprise.beta.gouv.fr
+						</a>
+						.
+					</p>
+				</T>
 			</section>
 
 			<Footer />
