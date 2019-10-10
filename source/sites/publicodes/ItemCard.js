a9c8b3ec53b68d50e831176d53a86d00002c4c24
Maël <mael.thomas@data.gouv.fr>
2019-10-10 17:22:45 +0200

    :up_arrow: Mise à jour styled components
    Bug de style lors du déploiement

--- source/sites/publicodes/ItemCard.js
+++ source/sites/publicodes/ItemCard.js
@@ -39,7 +39,7 @@ export default withFigure => {
 					:hover {
 					    opacity: 1 !important;
 						box-shadow: 0px 2px 4px -1px rgba(41, 117, 209, 0.2), 0px 4px 5px 0px rgba(41, 117, 209, 0.14), 0px 1px 10px 0px rgba(41, 117, 209, 0.12);
-}
+					}
 				     ${
 								withFigure
 									? `
