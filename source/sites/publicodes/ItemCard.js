0996c72ca4492e56631a62fae39b74844f7e111b
Maël <mael.thomas@data.gouv.fr>
2019-10-10 21:06:16 +0200

    :sparkles: Réécriture du CSS de itemcard

--- source/sites/publicodes/ItemCard.js
+++ source/sites/publicodes/ItemCard.js
@@ -10,9 +10,8 @@ export default withFigure => {
 				key={dottedName}
 				css={`
 					font-size: 120%;
-					padding: 1rem 0 0;
-					margin: 0.6rem;
-					width: 10rem;
+					padding: ${withFigure ? '1rem 0 0' : '1rem'};
+					width: ${withFigure ? '18rem' : '10rem'};
 					min-height: 7em;
 					position: relative;
 					display: flex;
@@ -23,35 +22,20 @@ export default withFigure => {
 					line-height: 1.2em;
 					${formule != null ? '' : 'filter: grayscale(70%); opacity: 0.6;'}
 
-					display: flex;
-					align-items: center;
-					flex-wrap: wrap;
-				    background-color: var(--lightestColour);
+					background-color: var(--lightestColour);
 					color: var(--darkColour);
 					margin: 1rem 0;
-					position: relative;
 					border-radius: 0.3rem;
-					text-decoration: none;
 					box-shadow: 0 1px 3px rgba(41, 117, 209, 0.12),
 						0 1px 2px rgba(41, 117, 209, 0.24);
 					transition: box-shadow 0.2s;
 
 					:hover {
-					    opacity: 1 !important;
-						box-shadow: 0px 2px 4px -1px rgba(41, 117, 209, 0.2), 0px 4px 5px 0px rgba(41, 117, 209, 0.14), 0px 1px 10px 0px rgba(41, 117, 209, 0.12);
+						opacity: 1 !important;
+						box-shadow: 0px 2px 4px -1px rgba(41, 117, 209, 0.2),
+							0px 4px 5px 0px rgba(41, 117, 209, 0.14),
+							0px 1px 10px 0px rgba(41, 117, 209, 0.12);
 					}
-				     ${
-								withFigure
-									? `
-						width: 18rem
-				     `
-									: `
-
-						padding: 1rem;
-									 `
-							}
-					}
-
 				`}>
 				<div css="width: 100%; img { font-size: 150%}}">
 					{icônes && emoji(icônes + ' ')}
