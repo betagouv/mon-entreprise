39c2885ec811da77c30db04a3c08b0ee0c90221a
Maxime Quandalle <maxime@quandalle.com>
2019-10-10 11:14:44 +0200

    Ajout d'un lien vers les release GitHub dans le footer

--- source/sites/mon-entreprise.fr/layout/Footer/Footer.js
+++ source/sites/mon-entreprise.fr/layout/Footer/Footer.js
@@ -88,6 +88,10 @@ const Footer = ({ sitePaths }) => {
 						{'  •  '}
 						<Privacy />
 						{'  •  '}
+						<a href="https://github.com/betagouv/mon-entreprise/releases">
+							Nouveautés
+						</a>
+						{'  •  '}
 						<a href="https://mon-entreprise.fr/stats">Stats</a>
 						{i18n.language === 'fr' && (
 							<>
