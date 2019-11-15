f22d297ce7594e30f8520c57a00282a4fff185e8
Maxime Quandalle <maxime@quandalle.com>
2019-11-15 22:23:21 +0100

    Ajout d'un graphique répartition pour les artistes-auteurs
    implémentation est hacky et il faudrait faire une refacto du système
    de distribution pour pouvoir l'intégrer correctement sur l'ensemble des
    simulateurs.

--- source/components/Distribution.tsx
+++ source/components/Distribution.tsx
@@ -1,20 +1,21 @@
+import useDisplayOnIntersecting from 'Components/utils/useDisplayOnIntersecting'
 import { ThemeColoursContext } from 'Components/utils/withColours'
 import Value from 'Components/Value'
 import { findRuleByDottedName } from 'Engine/rules'
 import React, { useContext } from 'react'
 import emoji from 'react-easy-emoji'
 import { useSelector } from 'react-redux'
-import { config, animated, useSpring } from 'react-spring'
+import { animated, config, useSpring } from 'react-spring'
 import { flatRulesSelector } from 'Selectors/analyseSelectors'
 import répartitionSelector from 'Selectors/repartitionSelectors'
+import { Rule } from 'Types/rule'
 import { isIE } from '../utils'
 import './Distribution.css'
 import './PaySlip'
 import RuleLink from './RuleLink'
-import useDisplayOnIntersecting from 'Components/utils/useDisplayOnIntersecting'
 
 export default function Distribution() {
-	const distribution = useSelector(répartitionSelector)
+	const distribution = useSelector(répartitionSelector) as any
 
 	if (!Object.values(distribution).length) {
 		return null
@@ -27,12 +28,9 @@ export default function Distribution() {
 					([brancheDottedName, { partPatronale, partSalariale }]) => (
 						<DistributionBranch
 							key={brancheDottedName}
-							{...{
-								brancheDottedName,
-								partPatronale,
-								partSalariale,
-								distribution
-							}}
+							dottedName={brancheDottedName}
+							value={partPatronale + partSalariale}
+							distribution={distribution}
 						/>
 					)
 				)}
@@ -41,34 +39,42 @@ export default function Distribution() {
 	)
 }
 
+type DistributionBranchProps = {
+	dottedName: Rule['dottedName']
+	value: number
+	distribution: { maximum: number; total: number }
+	icon?: string
+}
+
 const ANIMATION_SPRING = config.gentle
-function DistributionBranch({
-	brancheDottedName,
-	partPatronale,
-	partSalariale,
+export function DistributionBranch({
+	dottedName,
+	value,
+	icon,
 	distribution
-}) {
+}: DistributionBranchProps) {
 	const rules = useSelector(flatRulesSelector)
 	const [intersectionRef, brancheInViewport] = useDisplayOnIntersecting({
 		threshold: 0.5
 	})
 	const colours = useContext(ThemeColoursContext)
-	const branche = findRuleByDottedName(rules, brancheDottedName)
-	const montant = brancheInViewport ? partPatronale + partSalariale : 0
+	const branche = findRuleByDottedName(rules, dottedName)
+	const montant = brancheInViewport ? value : 0
 	const styles = useSpring({
 		config: ANIMATION_SPRING,
 		to: {
-			flex: montant / distribution.cotisationMaximum,
+			flex: montant / distribution.maximum,
 			opacity: montant ? 1 : 0
 		}
-	})
+	}) as { flex: number; opacity: number } // TODO: problème avec les types de react-spring ?
 
 	return (
 		<animated.div
 			ref={intersectionRef}
 			className="distribution-chart__item"
-			style={{ opacity: styles.opacity }}>
-			<BranchIcône icône={branche.icons} />
+			style={{ opacity: styles.opacity }}
+		>
+			<BranchIcône icône={icon ?? branche.icons} />
 			<div className="distribution-chart__item-content">
 				<p className="distribution-chart__counterparts">
 					<span className="distribution-chart__branche-name">
@@ -106,7 +112,8 @@ let ChartItemBar = ({ styles, colour, montant }) => (
 				font-weight: bold;
 				margin-left: 1rem;
 				color: var(--textColourOnWhite);
-			`}>
+			`}
+		>
 			<Value maximumFractionDigits={0} unit="€">
 				{montant}
 			</Value>
