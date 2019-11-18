cae22f651d8b528ceca294c2d7a5423bac1fb25a
Johan Girod <johangirod@gmail.com>
2019-11-18 19:17:44 +0100

    :hammer: :gear: corrige un bug qui faisait que les rend non applicable n'apparaissait pas dans les missings variables

--- source/engine/evaluateRule.js
+++ source/engine/evaluateRule.js
@@ -38,6 +38,7 @@ export const evaluateApplicability = (
 				: mergeAll([
 						...parentDependencies.map(parent => parent.missingVariables),
 						notApplicable?.missingVariables || {},
+						disabled?.missingVariables || {},
 						applicable?.missingVariables || {}
 				  ])
 
