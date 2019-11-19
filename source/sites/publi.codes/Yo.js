8657435f463df6f4e31ad2ecb49738e4986baf53
Mael <mael.thomas@data.gouv.fr>
2019-11-19 18:49:24 +0100

    Première ébauche de studio de création de règles pour publi.codes

--- source/sites/publi.codes/Yo.js
+++ source/sites/publi.codes/Yo.js
@@ -1,37 +1,113 @@
+import Editor from '@monaco-editor/react'
 import { buildFlatRules } from 'Engine/rules'
 import { safeLoad } from 'js-yaml'
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { analysisWithDefaultsSelector } from 'Selectors/analyseSelectors'
 import { setSimulationConfig } from '../../actions/actions'
 
 let initialInput = `
+
 a: 
   formule: 10
 b: 
   formule: a + 18
+c:
+  formule:
+    multiplication: 
+      assiette: 2000
+      taux: 3%
+d:
+  formule: a + b + c
+
 `
 
-export default function Yo() {
+export default function SafeStudio() {
+	return (
+		<ErrorBoundary>
+			<Studio />
+		</ErrorBoundary>
+	)
+}
+
+export function Studio() {
+	const defaultTarget = 'b'
 	const [ready, setReady] = useState(false)
+	const [target, setTarget] = useState(defaultTarget)
+
 	const dispatch = useDispatch()
 	const stateConfig = useSelector(state => state.simulation?.config)
-	useEffect(() => {
+
+	const [isEditorReady, setIsEditorReady] = useState(false)
+	const valueGetter = useRef()
+	const setRules = rulesString =>
 		dispatch({
 			type: 'SET_RULES',
-			rules: buildFlatRules(safeLoad(initialInput))
+			rules: buildFlatRules(safeLoad(rulesString))
 		})
-		dispatch(setSimulationConfig({ objectifs: ['b'] }))
+	const setTargets = targets =>
+		dispatch(setSimulationConfig({ objectifs: targets }))
+
+	useEffect(() => {
+		setRules(initialInput)
+		setTargets([defaultTarget])
 		setReady(true)
 	}, [])
+
+	function handleEditorDidMount(_valueGetter) {
+		setIsEditorReady(true)
+		valueGetter.current = _valueGetter
+	}
+
+	function handleShowValue() {
+		setRules(valueGetter.current())
+		setTargets([target])
+	}
+
 	return (
 		<div>
 			Saissez des formules
+			<Editor
+				height="40vh"
+				language="yaml"
+				value={initialInput}
+				editorDidMount={handleEditorDidMount}
+			/>
+			<h3>Que voulez-vous calculer ?</h3>
+			<input value={target} onChange={e => setTarget(e.target.value)} />
+			<br />
+			<button onClick={handleShowValue} disabled={!isEditorReady}>
+				Calculer
+			</button>
 			{ready && <Results />}
 		</div>
 	)
 }
 export const Results = () => {
 	const analysis = useSelector(state => analysisWithDefaultsSelector(state))
-	return <div>{analysis.targets[0].nodeValue}</div>
+	return (
+		<div>
+			<h2>Résultats</h2>
+			{analysis.targets[0].nodeValue}
+		</div>
+	)
+}
+
+class ErrorBoundary extends React.Component {
+	state = {
+		error: false
+	}
+	static getDerivedStateFromError(error) {
+		// Mettez à jour l'état, de façon à montrer l'UI de repli au prochain rendu.
+		return { error }
+	}
+
+	render() {
+		return (
+			<>
+				{this.state.error && <p>Erreur : {JSON.stringify(this.state.error)}</p>}
+				{this.props.children}
+			</>
+		)
+	}
 }
