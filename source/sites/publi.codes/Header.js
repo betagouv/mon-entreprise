daa94090e7f876f323ca9cf3d4f07b0373a82d5c
Mael <mael.thomas@data.gouv.fr>
2019-11-20 16:27:19 +0100

    Le titre est un lien vers l'accueil

--- source/sites/publi.codes/Header.js
+++ source/sites/publi.codes/Header.js
@@ -1,14 +1,17 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 export const Header = ({ noSubtitle }) => (
-	<header css="text-align: center">
-		<h1 css="">
-			<span css="border: 3px solid var(--colour); color: var(--colour); padding: 0.1rem 0.4rem 0.1rem 0.6rem ; width: 5rem">
-				publi
-			</span>
-			<span css="background: var(--colour); color: white; padding: 0.1rem 0.6rem 0.1rem 0.3rem; width: 5rem; border: 3px solid var(--colour)">
-				codes
-			</span>
-		</h1>
+	<header css="text-align: center; a {text-decoration: none}">
+		<Link to="/">
+			<h1>
+				<span css="border: 3px solid var(--colour); color: var(--colour); padding: 0.1rem 0.4rem 0.1rem 0.6rem ; width: 5rem">
+					publi
+				</span>
+				<span css="background: var(--colour); color: white; padding: 0.1rem 0.6rem 0.1rem 0.3rem; width: 5rem; border: 3px solid var(--colour)">
+					codes
+				</span>
+			</h1>
+		</Link>
 		{!noSubtitle && (
 			<p css="width: 28rem; margin: 0 auto; font-size: 120%">
 				Un nouveau langage de calcul pour encoder les algorithmes d'intérêt
