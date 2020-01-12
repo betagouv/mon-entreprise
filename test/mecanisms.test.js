12ab3a3c4c5cb205d79ae210e0b811c5e06630ed
Maxime Quandalle <maxime@quandalle.com>
2020-01-12 22:23:54 +0100

    Format objet pour les tests des mécanismes
    Dans #719 nous changions la structure de données Yaml de premier niveau
    d'une liste vers un objet (indexé sur le nom des règles) pour les
    fichiers Publicode. Ce commit réplique ce changement pour les fichiers
    de tests de mécanismes qui n'avaient pas encore été migré vers le
    nouveau format.
    
    L'attribut "test" qui servait à définir le nom du test est supprimé et
    on utilise maintenant directement le nom de la règle (ou son titre s'il
    est défini) comme nom du test.

--- test/mecanisms.test.js
+++ test/mecanisms.test.js
@@ -6,7 +6,6 @@
 
 import { expect } from 'chai'
 import { serialiseUnit } from 'Engine/units'
-import * as R from 'ramda'
 import { collectMissingVariables } from '../source/engine/generateQuestions'
 import { enrichRule } from '../source/engine/rules'
 import { analyse, parseAll } from '../source/engine/traverse'
@@ -14,50 +13,54 @@ import testSuites from './load-mecanism-tests'
 
 describe('Mécanismes', () =>
 	testSuites.map(([suiteName, suite]) =>
-		suite.map(
-			({ exemples, test, 'unité attendue': unit }) =>
-				exemples &&
-				describe(`Suite ${suiteName}, test : ${test ||
-					'Nom de test (propriété "test") manquant dans la variable contenant ces "exemples"'}`, () =>
-					exemples.map(
-						({
-							nom: testTexte,
-							situation,
-							'unités par défaut': defaultUnits,
-							'valeur attendue': valeur,
-							'variables manquantes': expectedMissing
-						}) =>
-							it(testTexte == null ? '' : testTexte + '', () => {
-								let rules = parseAll(
-										suite
-											.map(item =>
-												item.test != null
-													? R.assoc('nom', item.test, item)
-													: item
-											)
-											.map(enrichRule)
-									),
-									state = situation || {},
-									stateSelector = name => state[name],
-									analysis = analyse(rules, test, defaultUnits)(stateSelector),
-									missing = collectMissingVariables(analysis.targets),
-									target = analysis.targets[0]
+		Object.keys(suite)
+			.map(key => [key, suite[key] ?? undefined])
+			.map(
+				([name, { exemples, titre, 'unité attendue': unit } = {}]) =>
+					exemples &&
+					describe(`Suite ${suiteName}, test : ${titre ?? name}`, () =>
+						exemples.map(
+							({
+								nom: testTexte,
+								situation,
+								'unités par défaut': defaultUnits,
+								'valeur attendue': valeur,
+								'variables manquantes': expectedMissing
+							}) =>
+								it(testTexte == null ? '' : testTexte + '', () => {
+									let rules = parseAll(
+											Object.entries(suite)
+												.map(([dottedName, rule]) => ({
+													dottedName,
+													...rule
+												}))
+												.map(enrichRule)
+										),
+										state = situation || {},
+										stateSelector = name => state[name],
+										analysis = analyse(
+											rules,
+											name,
+											defaultUnits
+										)(stateSelector),
+										missing = collectMissingVariables(analysis.targets),
+										target = analysis.targets[0]
 
-								if (typeof valeur === 'number') {
-									expect(target.nodeValue).to.be.closeTo(valeur, 0.001)
-								} else if (valeur !== undefined) {
-									expect(target).to.have.property('nodeValue', valeur)
-								}
+									if (typeof valeur === 'number') {
+										expect(target.nodeValue).to.be.closeTo(valeur, 0.001)
+									} else if (valeur !== undefined) {
+										expect(target).to.have.property('nodeValue', valeur)
+									}
 
-								if (expectedMissing) {
-									expect(missing).to.eql(expectedMissing)
-								}
+									if (expectedMissing) {
+										expect(missing).to.eql(expectedMissing)
+									}
 
-								if (unit) {
-									expect(target.unit).not.to.be.equal(undefined)
-									expect(serialiseUnit(target.unit)).to.eql(unit)
-								}
-							})
-					))
-		)
+									if (unit) {
+										expect(target.unit).not.to.be.equal(undefined)
+										expect(serialiseUnit(target.unit)).to.eql(unit)
+									}
+								})
+						))
+			)
 	))
