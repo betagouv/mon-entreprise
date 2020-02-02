283bd5bf8e30bf1121aaca5dd5c64a44163a6bb9
Maxime Quandalle <maxime@quandalle.com>
2020-02-02 21:45:52 +0100

    🎨 Suppression des actions du pied de page sur les intégrations

--- source/sites/mon-entreprise.fr/pages/Iframes/IframeFooter.tsx
+++ source/sites/mon-entreprise.fr/pages/Iframes/IframeFooter.tsx
@@ -1,47 +1,13 @@
-import LangSwitcher from 'Components/LangSwitcher'
-import React, { useEffect, useState } from 'react'
-import emoji from 'react-easy-emoji'
-import { Trans } from 'react-i18next'
-import screenfull from 'screenfull'
-import { isIE } from '../../../../utils'
+import React from 'react'
 import Privacy from '../../layout/Footer/Privacy'
 
 export default function IframeFooter() {
-	const [isFullscreen, setIsFullscreen] = useState(screenfull.isFullscreen)
-	useEffect(() => {
-		screenfull.enabled &&
-			screenfull.onchange(() => setIsFullscreen(screenfull.isFullscreen))
-	}, [])
-
 	return (
 		<>
 			<div
 				className="ui__ container"
-				style={{
-					textAlign: 'right',
-					display: 'flex',
-					alignItems: 'center',
-					justifyContent: 'space-between'
-				}}
+				style={{ textAlign: 'right', paddingTop: 20 }}
 			>
-				<LangSwitcher className="ui__ button simple" />
-				{screenfull.enabled && !isFullscreen && !isIE() && (
-					<button
-						className="ui__ button small"
-						onClick={() => {
-							screenfull.toggle()
-						}}
-					>
-						{emoji('🖵')}&nbsp;
-						<Trans>Plein écran</Trans>
-					</button>
-				)}
-				{window?.print && (
-					<button className="ui__ button small" onClick={() => window.print()}>
-						{emoji('🖨')}
-						<Trans>Imprimer</Trans>
-					</button>
-				)}
 				<Privacy />
 			</div>
 		</>
