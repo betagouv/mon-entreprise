996fb628478ad531597c4ce6f0f7cb7a816e7ba8
Maxime Quandalle <maxime@quandalle.com>
2020-02-09 23:01:59 +0100

    🎨 Pages dev : Ajout d'un bloc "Contribuer sur Github"
    Plus style “dark mode” car les développeurs vivent dans le noir et ne
    veulent pas être éblouis.

--- source/sites/mon-entreprise.fr/pages/integration/index.tsx
+++ source/sites/mon-entreprise.fr/pages/integration/index.tsx
@@ -2,7 +2,7 @@ import { ScrollToTop } from 'Components/utils/Scroll'
 import { SitePathsContext } from 'Components/utils/withSitePaths'
 import React, { useContext } from 'react'
 import { Trans } from 'react-i18next'
-import { Route, Switch } from 'react-router'
+import { Route, Switch, useLocation } from 'react-router'
 import { Link } from 'react-router-dom'
 import Iframe from './Iframe'
 import Library from './Library'
@@ -10,18 +10,24 @@ import Options from './Options'
 
 export default function Integration() {
 	const sitePaths = useContext(SitePathsContext)
+	const { pathname } = useLocation()
 	return (
 		<>
 			<ScrollToTop />
-			<div className="ui__ plain card" css="text-align: center">
-				<h2>
-					🛠{' '}
-					<Link className="ui__ simple button" to={sitePaths.integration.index}>
-						<Trans>Outils pour les développeurs</Trans>
-					</Link>{' '}
-					🛠
-				</h2>
-			</div>
+			{pathname !== sitePaths.integration.index && (
+				<div className="ui__ card dark-bg" css="text-align: center">
+					<h2>
+						🛠{' '}
+						<Link
+							className="ui__ simple button"
+							to={sitePaths.integration.index}
+						>
+							<Trans>Outils pour les développeurs</Trans>
+						</Link>{' '}
+						🛠
+					</h2>
+				</div>
+			)}
 			<Switch>
 				<Route exact path={sitePaths.integration.index} component={Options} />
 				<Route path={sitePaths.integration.iframe} component={Iframe} />
