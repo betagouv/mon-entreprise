cf9426e150cfd83dbd453177af949271bc26fa2c
Loïc Guillois <contact@loicguillois.fr>
2020-03-19 11:14:40 +0100

    Simulateur AE: affiche le détail de l'entreprise quand situation connue

--- source/sites/mon-entreprise.fr/pages/Simulateurs/AutoEntrepreneur.tsx
+++ source/sites/mon-entreprise.fr/pages/Simulateurs/AutoEntrepreneur.tsx
@@ -1,25 +1,53 @@
 import { setSimulationConfig } from 'Actions/actions'
+import { resetEntreprise } from 'Actions/existingCompanyActions'
+import CompanyDetails from 'Components/CompanyDetails'
+import FindCompany from 'Components/FindCompany'
+import Overlay from 'Components/Overlay'
 import Warning from 'Components/SimulateurWarning'
 import Simulation from 'Components/Simulation'
 import autoEntrepreneurConfig from 'Components/simulationConfigs/auto-entrepreneur.yaml'
 import StackedBarChart from 'Components/StackedBarChart'
 import { ThemeColorsContext } from 'Components/utils/colors'
 import { IsEmbeddedContext } from 'Components/utils/embeddedContext'
+import { ScrollToTop } from 'Components/utils/Scroll'
 import { getRuleFromAnalysis } from 'Engine/rules'
-import { default as React, useContext } from 'react'
+import {
+	default as React,
+	useContext,
+	useEffect,
+	useRef,
+	useState
+} from 'react'
 import { Helmet } from 'react-helmet'
 import { Trans, useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation } from 'react-router'
+import { Company } from 'Reducers/inFranceAppReducer'
+import { RootState } from 'Reducers/rootReducer'
 import { analysisWithDefaultsSelector } from 'Selectors/analyseSelectors'
 
 export default function AutoEntrepreneur() {
 	const dispatch = useDispatch()
 	const location = useLocation<{ fromGérer?: boolean }>()
 	const inIframe = useContext(IsEmbeddedContext)
+	const [searchModal, showSearchModal] = useState(false)
 	dispatch(
 		setSimulationConfig(autoEntrepreneurConfig, location.state?.fromGérer)
 	)
+
+	const company = useSelector(
+		(state: RootState) => state.inFranceApp.existingCompany
+	)
+
+	const companyRef = useRef<Company | null>(null)
+	useEffect(() => {
+		if (companyRef.current !== company) {
+			companyRef.current = company
+			if (searchModal && company) {
+				showSearchModal(false)
+			}
+		}
+	}, [company, searchModal])
 
 	const { t } = useTranslation()
 
@@ -46,6 +74,30 @@ export default function AutoEntrepreneur() {
 						Simulateur de revenus auto-entrepreneur
 					</Trans>
 				</h1>
+			)}
+			{company && (
+				<>
+					<CompanyDetails siren={company.siren} />
+					<button
+						className="ui__ simple small button"
+						onClick={() => {
+							dispatch(resetEntreprise())
+							showSearchModal(true)
+						}}
+					>
+						<Trans i18nKey="gérer.entreprise.changer">
+							Changer l'entreprise sélectionnée
+						</Trans>
+					</button>
+				</>
+			)}
+			{searchModal && (
+				<>
+					<ScrollToTop />
+					<Overlay onClose={() => showSearchModal(false)}>
+						<FindCompany />
+					</Overlay>
+				</>
 			)}
 			<Warning simulateur="auto-entrepreneur" />
 			<Simulation explanations={<ExplanationSection />} />
