eeea3978430e45b04db6f6706a82603511450fc4
Elodie Quandalle <elodie.quandalle@gmail.com>
2020-04-02 09:48:10 +0200

    test of creation of fetch-stats.js
    fetch data from matomo working and working on ploting a graph
    
    fetch stats par mois pour chacun des simulateurs
    
    Graphe pour les simulateurs
    
    fetch stats + graph BarChart de base

---
+++ source/scripts/fetch-stats.js
@@ -0,0 +1,111 @@
+// This script uses the Matomo API which requires an access token
+// Once you have your access token you can put it in a `.env` file at the root
+// of the project to enable it during development. For instance:
+// MATOMO_TOKEN=f4336c82cb1e494752d06e610614eab12b65f1d1
+//
+require('dotenv').config()
+require('isomorphic-fetch')
+var d3 = require('d3')
+const fs = require('fs')
+const path = require('path')
+var querystring = require('querystring')
+
+// We use the Matomo API.
+// Documentation can be found here : https://developer.matomo.org/api-reference/reporting-api
+
+const apiURL = params => {
+  const query = querystring.stringify({
+    period: 'day',
+    date: 'last365',
+    method: 'Actions.getPageUrls',
+    format: 'JSON',
+    ...params,
+    search_recursive: 1,
+    keep_totals_row: 0,
+    idSubtable: 2,
+    module: 'API',
+    idSite: 39,
+    language: 'fr',
+    apiAction: 'get',
+    token_auth: process.env.MATOMO_TOKEN,
+  })
+  return `https://stats.data.gouv.fr/index.php?${query}`;
+};
+
+//console.log(apiURL())
+
+// In case we cannot fetch the release (the API is down or the Authorization
+// token isn't valid) we fallback to some fake data -- it would be better to
+// have a static ressource accessible without authentification.
+const fakeData = [
+  {
+    "0":
+    {
+      "/salarié": 0,
+      "/auto-entrepreneur": 0,
+      "/indépendant": 0,
+      "/comparaison-régimes-sociaux": 0,
+      "/assimilé-salarié": 0,
+      "/salarie": 0,
+      "/artiste-auteur": 0
+    }
+  }
+]
+
+const dataDir = path.resolve(__dirname, '../data/')
+
+async function main() {
+  createDataDir()
+  writeStatsInDataDir(await fetchStats())
+}
+
+function createDataDir() {
+  if (!fs.existsSync(dataDir)) {
+    fs.mkdirSync(dataDir)
+  }
+}
+
+async function fetchStats() {
+  try {
+    const response = await fetch(apiURL())
+    const data = await response.json()
+    let result = Object.fromEntries(
+      // convert to array, map, and then fromEntries gives back the object
+      Object.entries(data).map(([key, value]) => [key, value.
+        filter(function (page) {
+          return ['/salarié', '/auto-entrepreneur', '/artiste-auteur', '/indépendant', '/comparaison-régimes-sociaux', '/assimilé-salarié'].includes(page.label) // '/salarie' pas pris en compte
+        }).
+        map(function (page) {
+          var { label, nb_visits } = page
+          return { month: new Date(key).getMonth(), label, nb_visits };
+        })])
+    );
+    result = Object.
+      values(result).
+      reduce(function (prev, curr) { return [...prev, ...curr]; });
+
+    result = d3.nest()
+      .key(function (p) { return p.month; })
+      .key(function (p) { return p.label; })
+      .rollup(function (v) { return d3.sum(v, function (p) { return p.nb_visits; }) })
+      .entries(result);
+    return result
+  } catch (e) {
+    console.log('toto')
+    return fakeData
+  }
+}
+
+
+function writeStatsInDataDir(stats) {
+  // The last release name is fetched on all pages (to display the banner)
+  // whereas the full release data is used only in the dedicated page, that why
+  // we deduplicate the releases data in two separated files that can be
+  // bundled/fetched separately.
+  fs.writeFileSync(
+    path.join(dataDir, 'stats.json'),
+    JSON.stringify(stats)
+  )
+}
+
+main()
