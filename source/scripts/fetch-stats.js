5122958c4fa571ccd2453a8bdb8d5a6c0fadfe91
Elodie Quandalle <elodie.quandalle@gmail.com>
2020-04-09 12:03:33 +0200

    graphe nombre de visite par mois et vertical barChart pour les simulateurs

--- source/scripts/fetch-stats.js
+++ source/scripts/fetch-stats.js
@@ -14,50 +14,55 @@ var { createDataDir, writeInDataDir } = require('./utils.js')
 
 const apiURL = params => {
 	const query = querystring.stringify({
-		period: 'day',
-		date: 'last365',
-		method: 'Actions.getPageUrls',
+		period: 'month',
+		date: 'last1',
+		method: 'API.get',
 		format: 'JSON',
-		...params,
-		search_recursive: 1,
-		keep_totals_row: 0,
-		idSubtable: 2,
 		module: 'API',
 		idSite: 39,
 		language: 'fr',
 		apiAction: 'get',
-		token_auth: process.env.MATOMO_TOKEN
+		token_auth: process.env.MATOMO_TOKEN,
+		...params
 	})
 	return `https://stats.data.gouv.fr/index.php?${query}`
 }
-
-//console.log(apiURL())
 
 // In case we cannot fetch the release (the API is down or the Authorization
 // token isn't valid) we fallback to some fake data -- it would be better to
 // have a static ressource accessible without authentification.
 const fakeData = [
 	{
-		'0': {
-			'/salarié': 0,
-			'/auto-entrepreneur': 0,
-			'/indépendant': 0,
-			'/comparaison-régimes-sociaux': 0,
-			'/assimilé-salarié': 0,
-			'/salarie': 0,
-			'/artiste-auteur': 0
-		}
+		key: '0',
+		values: [
+			{ key: '/salarié', value: 0 },
+			{ key: '/auto-entrepreneur', value: 0 },
+			{ key: '/artiste-auteur', value: 0 },
+			{ key: '/indépendant', value: 0 },
+			{ key: '/comparaison-régimes-sociaux', value: 0 },
+			{ key: '/assimilé-salarié', value: 0 }
+		]
 	}
 ]
 
 async function main() {
 	createDataDir()
 	writeInDataDir('stats.json', await fetchStats())
+	writeInDataDir('total_visits.json', await fetchNumberofVisitsbyMonth())
 }
 
 async function fetchStats() {
 	try {
-		const response = await fetch(apiURL())
+		const response = await fetch(
+			apiURL({
+				period: 'day',
+				date: 'last365',
+				method: 'Actions.getPageUrls',
+				search_recursive: 1,
+				keep_totals_row: 0,
+				idSubtable: 2
+			})
+		)
 		const data = await response.json()
 		let result = Object.fromEntries(
 			// convert to array, map, and then fromEntries gives back the object
@@ -86,12 +91,8 @@ async function fetchStats() {
 
 		result = d3
 			.nest()
-			.key(function(p) {
-				return p.month
-			})
-			.key(function(p) {
-				return p.label
-			})
+			.key(p => p.month)
+			.key(p => p.label)
 			.rollup(function(v) {
 				return d3.sum(v, function(p) {
 					return p.nb_visits
@@ -102,6 +103,44 @@ async function fetchStats() {
 	} catch (e) {
 		console.log('toto')
 		return fakeData
+	}
+}
+const visitsIn2019 = {
+	'2019-01': 119541,
+	'2019-02': 99065,
+	'2019-03': 122931,
+	'2019-04': 113454,
+	'2019-05': 118637,
+	'2019-06': 152981,
+	'2019-07': 141079,
+	'2019-08': 127326,
+	'2019-09': 178474,
+	'2019-10': 198260,
+	'2019-11': 174515,
+	'2019-12': 116305
+}
+async function fetchNumberofVisitsbyMonth() {
+	try {
+		const response = await fetch(
+			apiURL({
+				period: 'month',
+				date: 'last12',
+				method: 'VisitsSummary.getUniqueVisitors'
+			})
+		)
+		const data = await response.json()
+		var result = Object.entries(
+			JSON.parse(JSON.stringify({ ...data, ...visitsIn2019 }))
+		)
+			.sort(([t1], [t2]) => (t1 > t2 ? 1 : -1))
+			.map(([x, y]) => ({ year: x, nb_uniq_visitors: y }))
+
+		return result
+	} catch (e) {
+		console.log('toto')
+		return Object.entries(visitsIn2019).map(([a, b]) => {
+			return { year: a, nb_uniq_visitors: b }
+		})
 	}
 }
 
--- source/sites/mon-entreprise.fr/pages/Stats/Stats.tsx
+++ source/sites/mon-entreprise.fr/pages/Stats/Stats.tsx
@@ -5,41 +5,72 @@ import {
 	Bar,
 	BarChart,
 	CartesianGrid,
-	Legend,
+	Line,
+	LineChart,
 	Tooltip,
 	XAxis,
 	YAxis
 } from 'recharts'
 import * as data from '../../../../data/stats.json'
+import * as uniq_visitors from '../../../../data/total_visits.json'
 
 export default function Stats() {
 	let d202004 = data[11].values
+	console.log('data202004', d202004)
 	return (
 		<>
 			<ScrollToTop />
 			<h1>
 				Statistiques <>{emoji('📊')}</>
 			</h1>
-			Ici on pourra mettre différents graphes dont le premier qui est la
-			répartition de l'utilisation des différents simulateurs export default (
-			<BarChart
-				width={900}
-				height={300}
-				data={d202004}
-				margin={{
-					top: 20,
-					right: 30,
-					left: 20,
-					bottom: 5
-				}}
-			>
-				<CartesianGrid strokeDasharray="3 3" />
-				<XAxis dataKey="key" />
-				<YAxis />
-				<Tooltip />
-				<Legend />
-				<Bar dataKey="value" fill="#8884d8" />
-			</BarChart>
+			<section>
+				<h2>Nombre de visites par mois</h2>
+				<LineChart
+					width={700}
+					height={300}
+					data={uniq_visitors.slice()}
+					margin={{
+						top: 5,
+						right: 30,
+						left: 20,
+						bottom: 5
+					}}
+				>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis dataKey="year" padding={{ right: 30 }} />
+					<YAxis />
+					<Line
+						type="monotone"
+						dataKey="nb_uniq_visitors"
+						stroke="#8884d8"
+						strokeWidth={3}
+					/>
+				</LineChart>
+				<div id="visits-indicators"></div>
+			</section>
+
+			<section>
+				<h2> Nombre d'utilisation des simulateurs</h2>
+				<BarChart
+					width={700}
+					height={300}
+					data={d202004}
+					layout="vertical"
+					margin={{
+						top: 20,
+						right: 30,
+						left: 20,
+						bottom: 5
+					}}
+				>
+					<CartesianGrid strokeDasharray="3 3" />
+					<XAxis type="number" dataKey="value" />
+					<YAxis type="category" dateKey="key" />
+					<Tooltip />
+					<Bar dataKey="value" fill="#8884d8" label="value" />
+				</BarChart>
+				<div id="simulteurs-indicators"></div>
+			</section>
 		</>
 	)
 }
