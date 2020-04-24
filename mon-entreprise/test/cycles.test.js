59d4be7d4f0f411b02ee32c4856f09d2246db8ba
Alexandre Hajjar <alexandre.hajjar@gmail.com>
2020-04-24 20:32:53 +0200

    WIP ⚙️ Detect cycles: test for cycles using @dagrejs/graphlib

--- mon-entreprise/test/cycles.test.js
+++ mon-entreprise/test/cycles.test.js
@@ -1,11 +1,26 @@
+<<<<<<< HEAD:mon-entreprise/test/cycles.test.js
 import { parseRules, buildRulesDependencies } from 'publicodes'
+=======
+import graphlib from '@dagrejs/graphlib'
+import { expect } from 'chai'
+import { buildRulesDependencies } from 'Engine/cyclesLib'
+import parseRules from 'Engine/parseRules'
+>>>>>>> cde4da75... WIP ⚙️ Detect cycles: test for cycles using @dagrejs/graphlib:test/cycles.test.js
 import rules from 'Rules'
 
-describe('Graph', () => {
-	it('should get a graph', () => {
-		debugger // Keep this for allowing deactivating other debugger statements via the Chrome inspector
+describe('DottedNames graph', () => {
+	it("shouldn't have cycles", () => {
 		let parsedRules = parseRules(rules)
 		let ruleDependencies = buildRulesDependencies(parsedRules)
-		console.log(ruleDependencies)
+		let g = new graphlib.Graph()
+
+		ruleDependencies.forEach(([ruleDottedName, dependenciesDottedNames]) => {
+			dependenciesDottedNames.forEach(depDottedName => {
+				g.setNode(ruleDottedName, depDottedName)
+			})
+		})
+		const cycles = graphlib.alg.findCycles(g)
+
+		expect(cycles).to.be.an('array').that.is.empty
 	})
 })
