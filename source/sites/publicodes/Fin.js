a3e5558b485f3cdfa9a3dd1623fc92c0d696c4db
Mael <mael.thomas@data.gouv.fr>
2020-06-09 19:29:26 +0200

    :art: Animation de l'écran de résultat

--- source/sites/publicodes/Fin.js
+++ source/sites/publicodes/Fin.js
@@ -1,40 +1,69 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import emoji from 'react-easy-emoji'
+import tinygradient from 'tinygradient'
+import { animated, useSpring, config } from 'react-spring'
+
+const gradient = tinygradient([
+		'#78e08f',
+		'#f6b93b',
+		'#b71540',
+		'#e84393',
+		'#8854d0',
+		'#000000',
+	]),
+	colors = gradient.rgb(40)
+
+const getBackgroundColor = (score) =>
+	colors[
+		Math.round((score < 2000 ? 0 : score > 30000 ? 29 : score - 2000) / 1000)
+	]
 
 export default ({}) => {
 	const { score } = useParams()
+	const { value } = useSpring({
+		config: { mass: 1, tension: 100, friction: 220 },
+		value: +score,
+		from: { value: 0 },
+	})
 
-	return (
+	return <AnimatedDiv value={value} score={score} />
+}
+
+const AnimatedDiv = animated(({ score, value }) => (
+	<div
+		className="ui__ full-width"
+		css={`
+			background: ${getBackgroundColor(value)};
+			width: 100vw;
+			height: 85vh;
+			display: flex;
+			flex-direction: column;
+			justify-content: space-evenly;
+
+			text-align: center;
+			h1 {
+				margin: 0.3rem;
+			}
+		`}
+	>
+		<h1>Mon empreinte sur le climat</h1>
+
 		<div
-			className="ui__ full-width"
 			css={`
-				background: red;
-				width: 100vw;
-				height: 100vh;
-				display: flex;
-				flex-direction: column;
-				justify-content: space-evenly;
-
-				text-align: center;
-				h1 {
-					margin: 0.3rem;
+				> p:first-child {
+					font-size: 250%;
 				}
 			`}
 		>
-			<h1>Mon empreinte sur le climat</h1>
-
-			<div
-				css={`
-					> p:first-child {
-						font-size: 250%;
-					}
-				`}
-			>
-				<p>{Math.round(score / 100) / 10} tonnes</p>
-				<p>Moyenne {emoji('🇫🇷')} : 11 tonnes</p>
-				<p>Objectif {emoji('😇')} : 2 tonnes</p>
-			</div>
+			<p>
+				<span css="width: 4.8rem; text-align: left; display: inline-block">
+					{Math.round(value / 100) / 10}
+				</span>{' '}
+				tonnes
+			</p>
+			<p>Moyenne {emoji('🇫🇷')} : 11 tonnes</p>
+			<p>Objectif {emoji('😇')} : 2 tonnes</p>
 		</div>
-	)
-}
+	</div>
+))
