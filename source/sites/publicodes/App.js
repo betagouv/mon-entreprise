6519bd902c2d6e923ca4366e566256afdfd2916b
Mael <mael.thomas@data.gouv.fr>
2020-06-29 19:26:29 +0200

    Ajustements

--- source/sites/publicodes/App.js
+++ source/sites/publicodes/App.js
@@ -107,14 +107,14 @@ class App extends Component {
 									src="https://ecolab-transport.netlify.com/images/ecolab.png"
 								/>
 								<span css="font-weight: 400;color: black; position: relative; ">
-									<span css="position: absolute; top: -.85rem; left: 0rem; font-size: 60%;">
+									<span css="position: absolute; top: -.95rem; left: 0rem; font-size: 60%;">
 										nos
 									</span>
 									<span css="color: var(--color); font-weight: bold; text-transform: uppercase; font-size: 75%">
 										ges
 									</span>
 									tes
-									<span css="position: absolute; top: 1rem; left: 2.1rem; font-size: 60%; ">
+									<span css="position: absolute; top: 1.2rem; left: 2.3rem; font-size: 60%; ">
 										climat
 									</span>
 								</span>
