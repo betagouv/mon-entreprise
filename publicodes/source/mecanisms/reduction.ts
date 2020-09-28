dbcadc525b864693bedbaf52ebf07595ed097aaa
Maxime Quandalle <maxime@quandalle.com>
2020-09-28 14:52:51 +0100

    🔥 Supprime la décote et la franchise
    Le mécanisme "allègement" proposaient ces deux paramètres ajoutant
    beaucoup de complexité pour une seule utilisation dans la base de règle
    qui peut être remplacée par une formule littérale.
    
    Closes #1119

--- publicodes/source/mecanisms/reduction.ts
+++ publicodes/source/mecanisms/reduction.ts
@@ -1,34 +1,26 @@
-import { map, max, min } from 'ramda'
+import { max, min } from 'ramda'
 import Allègement from '../components/mecanisms/Allègement'
 import { typeWarning } from '../error'
 import { defaultNode, evaluateObject, parseObject } from '../evaluation'
 import { convertNodeToUnit } from '../nodeUnits'
-import { parseUnit, serializeUnit } from '../units'
+import { serializeUnit } from '../units'
 
-export const mecanismReduction = (recurse, v) => {
-	const objectShape = {
-		assiette: false,
-		abattement: defaultNode(0),
-		plafond: defaultNode(Infinity),
-		franchise: defaultNode(0)
-	}
+const objectShape = {
+	assiette: false,
+	abattement: defaultNode(0),
+	plafond: defaultNode(Infinity)
+}
 
-	const effect = (
-		{ assiette, abattement, plafond, franchise, décote },
-		cache
-	) => {
+const evaluate = evaluateObject(
+	objectShape,
+	({ assiette, abattement, plafond }, cache) => {
 		const assietteValue = assiette.nodeValue
 		if (assietteValue == null) return { nodeValue: null }
 		if (assiette.unit) {
 			try {
-				franchise = convertNodeToUnit(assiette.unit, franchise)
 				plafond = convertNodeToUnit(assiette.unit, plafond)
 				if (serializeUnit(abattement.unit) !== '%') {
 					abattement = convertNodeToUnit(assiette.unit, abattement)
-				}
-				if (décote) {
-					décote.plafond = convertNodeToUnit(assiette.unit, décote.plafond)
-					décote.taux = convertNodeToUnit(parseUnit(''), décote.taux)
 				}
 			} catch (e) {
 				typeWarning(
@@ -38,61 +30,35 @@ export const mecanismReduction = (recurse, v) => {
 				)
 			}
 		}
-		const montantFranchiséDécoté =
-			franchise.nodeValue && assietteValue < franchise.nodeValue
-				? 0
-				: décote
-				? (function() {
-						const plafondDécote = décote.plafond.nodeValue,
-							taux = décote.taux.nodeValue
-
-						return assietteValue > plafondDécote
-							? assietteValue
-							: max(0, (1 + taux) * assietteValue - taux * plafondDécote)
-				  })()
-				: assietteValue
 		const nodeValue = abattement
 			? abattement.nodeValue == null
-				? montantFranchiséDécoté === 0
+				? assietteValue === 0
 					? 0
 					: null
 				: serializeUnit(abattement.unit) === '%'
 				? max(
 						0,
-						montantFranchiséDécoté -
+						assietteValue -
 							min(
 								plafond.nodeValue,
-								(abattement.nodeValue / 100) * montantFranchiséDécoté
+								(abattement.nodeValue / 100) * assietteValue
 							)
 				  )
-				: max(
-						0,
-						montantFranchiséDécoté -
-							min(plafond.nodeValue, abattement.nodeValue)
-				  )
-			: montantFranchiséDécoté
+				: max(0, assietteValue - min(plafond.nodeValue, abattement.nodeValue))
+			: assietteValue
 		return {
 			nodeValue,
 			unit: assiette.unit,
 			explanation: {
-				franchise,
 				plafond,
 				abattement
 			}
 		}
 	}
+)
 
-	const base = parseObject(recurse, objectShape, v),
-		explanation = v.décote
-			? {
-					...base,
-					décote: {
-						...map(recurse, v.décote),
-						evaluate: () => map(recurse, v.décote)
-					}
-			  }
-			: base,
-		evaluate = evaluateObject(objectShape, effect)
+export const mecanismReduction = (recurse, v) => {
+	const explanation = parseObject(recurse, objectShape, v)
 
 	return {
 		evaluate,
