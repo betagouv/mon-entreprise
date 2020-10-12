d407e0e82fbfeccce73a9a36ec90582deda437da
Johan Girod <dev@johangirod.com>
2020-10-12 15:17:57 +0200

    cache la réparition de la rémunération totale quand le revenu est négatif

--- mon-entreprise/source/components/simulationExplanation/IndépendantExplanation.tsx
+++ mon-entreprise/source/components/simulationExplanation/IndépendantExplanation.tsx
@@ -29,26 +29,28 @@ export default function IndépendantExplanation() {
 			<Condition expression="entreprise . catégorie d'activité . libérale règlementée">
 				<PLExplanation />
 			</Condition>
-			<section>
-				<h2>Répartition de la rémunération totale</h2>
-				<StackedBarChart
-					data={[
-						{
-							...engine.evaluate('revenu net après impôt'),
-							title: t('Revenu disponible'),
-							color: palettes[0][0]
-						},
-						{ ...engine.evaluate('impôt'), color: palettes[1][0] },
-						{
-							...engine.evaluate(
-								'dirigeant . indépendant . cotisations et contributions'
-							),
-							title: t('Cotisations'),
-							color: palettes[1][1]
-						}
-					]}
-				/>
-			</section>
+			<Condition expression="revenu net après impôt > 0 €/an">
+				<section>
+					<h2>Répartition de la rémunération totale</h2>
+					<StackedBarChart
+						data={[
+							{
+								...engine.evaluate('revenu net après impôt'),
+								title: t('Revenu disponible'),
+								color: palettes[0][0]
+							},
+							{ ...engine.evaluate('impôt'), color: palettes[1][0] },
+							{
+								...engine.evaluate(
+									'dirigeant . indépendant . cotisations et contributions'
+								),
+								title: t('Cotisations'),
+								color: palettes[1][1]
+							}
+						]}
+					/>
+				</section>
+			</Condition>
 			<section>
 				<h2>
 					<Trans>À quoi servent mes cotisations ?</Trans>
