2baff28de33821e88d8b0fd12542cb5afbee6c30
Mael <mael.thomas@data.gouv.fr>
2020-12-07 17:25:45 +0000

    Sous-pages langage/documentation et /installation

--- mon-entreprise/source/sites/publi.codes/Langage.tsx
+++ mon-entreprise/source/sites/publi.codes/Langage.tsx
@@ -1,16 +1,22 @@
 import { MarkdownWithAnchorLinks } from 'Components/utils/markdown'
 import { ScrollToTop } from 'Components/utils/Scroll'
 import content from '../../../../publicodes/docs/langage.md'
-import { NavLink } from 'react-router-dom'
+import installation from '../../../../publicodes/docs/installation.md'
+import documentation from '../../../../publicodes/docs/documentation.md'
+import { Route, Switch } from 'react-router-dom'
 import { Navigation } from './Header'
 
-export default function Landing() {
+export default function Langage() {
 	return (
 		<div>
-			<Navigation items={items} sub />
 			<ScrollToTop />
+			<Navigation items={items} sub="langage" />
 			<main>
-				<MarkdownWithAnchorLinks source={content} />
+				<Switch>
+					<Route path="/langage/installation" component={Installation} />
+					<Route path="/langage/documentation" component={Documentation} />
+					<Route path="/langage/" component={Main} />
+				</Switch>
 			</main>
 		</div>
 	)
@@ -22,3 +28,8 @@ const items = [
 	['documentation', 'Documentation'],
 	['mécanismes', 'Liste des mécanismes']
 ]
+
+const Main = () => <MarkdownWithAnchorLinks source={content} />
+
+const Installation = () => <MarkdownWithAnchorLinks source={installation} />
+const Documentation = () => <MarkdownWithAnchorLinks source={documentation} />
