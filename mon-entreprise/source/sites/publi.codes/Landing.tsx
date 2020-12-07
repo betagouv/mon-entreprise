0a734e1664c393fc5b302ff72686219961f3d0c2
Mael <mael.thomas@data.gouv.fr>
2020-12-07 16:50:55 +0000

    Réorganisation de publi.codes; double menu; contenu en +

--- mon-entreprise/source/sites/publi.codes/Landing.tsx
+++ mon-entreprise/source/sites/publi.codes/Landing.tsx
@@ -1,30 +1,22 @@
 import { MarkdownWithAnchorLinks } from 'Components/utils/markdown'
 import { ScrollToTop } from 'Components/utils/Scroll'
-import publicodeReadme from '../../../../publicodes/README.md'
-import { useEffect } from 'react'
-import { Header } from './Header'
-import { NavLink } from 'react-router-dom'
+import content from '../../../../publicodes/docs/introduction.md'
 
 export default function Landing() {
-	useEffect(() => {
-		const css = document.createElement('style')
-		css.type = 'text/css'
-		css.innerHTML = `
-		#js {
-			animation: appear 0.5s;
-			opacity: 1;
-		}
-		#loading {
-			display: none !important;
-		}`
-		document.body.appendChild(css)
-	}, [])
 	return (
-		<div className="app-content ui__ container" css="margin: 2rem 0">
+		<main>
 			<ScrollToTop />
-			<Header />
-			<br />
-			<MarkdownWithAnchorLinks source={publicodeReadme} />
-		</div>
+			<p
+				css={`
+					font-size: 200%;
+					max-width: 60%;
+					line-height: 2rem;
+					margin-bottom: 2rem;
+				`}
+			>
+				Le langage pour les algorithmes d'intérêt public.
+			</p>
+			<MarkdownWithAnchorLinks source={content} />
+		</main>
 	)
 }
---
+++ publicodes/docs/communauté.md
@@ -0,0 +1,18 @@
+## 👥 Projets phares
+
+-   **[mon-entreprise.fr](https://mon-entreprise.fr/simulateurs)** utilise publicodes
+    pour spécifier l'ensemble des calculs relatifs à la législation socio-fiscale
+    en France. Le site permet entre autre de simuler une fiche de paie complète,
+    de calculer les cotisations sociales pour un indépendant ou encore connaître
+    le montant du chômage partiel.
+-   **[futur.eco](https://futur.eco/)** utilise publicodes pour calculer les bilans
+    carbone d'un grand nombre d'activités, plats, transports ou biens.
+-   **[Nos Gestes Climat](https://ecolab.ademe.fr/apps/climat)** utilise publicodes pour proposer un calculateur d'empreinte climat personnel de référence complètement ouvert
+
+## ✍️ Contribuer
+
+Tout le développement se fait dans le monorepo [github.com/betagouv/mon-entreprise](https://github.com/betagouv/mon-entreprise/tree/master/publicodes).
+
+## 💬 Discuter
+
+Venez dire bonjour, poser vos questions sur [gitter.im/publicodes](https://gitter.im/publicodes/community)
