5075df7e7bcbc89c9e98481c4a07fb69e78fb1bc
Johan Girod <johan.girod@beta.gouv.fr>
2021-01-12 15:50:39 +0100

    :bug::art: fix modale qui s'affiche tout en haut

--- mon-entreprise/source/site/entry.en.tsx
+++ mon-entreprise/source/site/entry.en.tsx
@@ -1,7 +1,3 @@
-// We disable Speedy mode for Styled-Component in production because it created a bug for overlay appearance
-// https://stackoverflow.com/questions/53486470/react-styled-components-stripped-out-from-production-build
-global.SC_DISABLE_SPEEDY = true
-
 import 'core-js/stable'
 import { hot } from 'react-hot-loader/root'
 import { render } from 'react-dom'
