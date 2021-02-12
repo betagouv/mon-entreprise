32fc8bf386d26962e96328bf7ecbcd1f95131c52
Maxime Quandalle <maxime@quandalle.com>
2021-02-12 01:37:08 +0100

    Prise en compte des retours Aide Embauche
    - Ajout d'une vue employeur sur le simulateur salarié
    - Référence les-aides.fr
    - Ajout d'un warning

--- mon-entreprise/source/pages/Simulateurs/Page.tsx
+++ mon-entreprise/source/pages/Simulateurs/Page.tsx
@@ -4,9 +4,11 @@ import Meta from 'Components/utils/Meta'
 import useSimulationConfig from 'Components/utils/useSimulationConfig'
 import PreviousSimulationBanner from 'Components/PreviousSimulationBanner'
 import { default as React, useContext, useEffect } from 'react'
-import { useLocation } from 'react-router-dom'
+import { useLocation, useParams } from 'react-router-dom'
 import { SimulatorData } from './metadata'
 import useSearchParamsSimulationSharing from 'Components/utils/useSearchParamsSimulationSharing'
+import { SitePathsContext } from 'Components/utils/SitePathsContext'
+import { useTranslation } from 'react-i18next'
 
 export default function SimulateurPage({
 	meta,
@@ -16,11 +18,30 @@ export default function SimulateurPage({
 	description,
 	component: Component,
 	seoExplanations,
+	path,
 }: SimulatorData[keyof SimulatorData]) {
 	const inIframe = useContext(IsEmbeddedContext)
 	const fromGérer = !!useLocation<{ fromGérer?: boolean }>().state?.fromGérer
 	useSimulationConfig(config, { useExistingCompanyFromSituation: fromGérer })
 	useSearchParamsSimulationSharing()
+
+	// TODO : Move this logic elsewhere.
+	//
+	// Some user where expecting to be on a simulator for employers instead of the
+	// one for employees coming from the page listing all incentives for
+	// employers. This makes sense, but at the same time our main simulator works
+	// for both employers and employees, so for now we just use a URL parameter
+	// `?view=employeur` to cusomize the title of the page. We may want to provide
+	// additional customization in the future depending to the targeted audience.
+	const sitePaths = useContext(SitePathsContext)
+	const view = new URLSearchParams(useLocation().search).get('view')
+	const { t } = useTranslation()
+	if (view === 'employeur' && path === sitePaths.simulateurs.salarié) {
+		title = t(
+			'pages.simulateurs.salarié.title-employeur',
+			"Simulateur de coûts d'embauche"
+		)
+	}
 
 	return (
 		<>
