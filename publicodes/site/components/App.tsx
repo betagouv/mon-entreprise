eb511f0bba26021eca425f9546a94617a12cd038
Johan Girod <johan.girod@beta.gouv.fr>
2021-04-14 11:15:50 +0200

    Ajoute un script pour rafraichir les dépendances publicodes (utile en développement)

--- publicodes/site/components/App.tsx
+++ publicodes/site/components/App.tsx
@@ -1,7 +1,3 @@
-// TODO: This is need to display the documentation UI, because the translations
-// for UI components are handled in the mon-entreprise site and not in the
-// publicodes-react package. This should be fixed.
-import '../../../mon-entreprise/source/locales/i18n'
 import 'Components/ui/index.css'
 import { useEffect } from 'react'
 import { hot } from 'react-hot-loader'
