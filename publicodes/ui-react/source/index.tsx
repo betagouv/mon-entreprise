71475830819d4d86f7cb535c05081ec7e7d9a50f
Johan Girod <johan.girod@beta.gouv.fr>
2021-05-05 13:55:59 +0200

    :gear::art: Ajoute la possibilité d'explorer les calculs dans un recalcul

--- publicodes/ui-react/source/index.tsx
+++ publicodes/ui-react/source/index.tsx
@@ -1,14 +1,15 @@
 import Engine, { utils } from 'publicodes'
-import { Route } from 'react-router-dom'
+import { useCallback, useRef } from 'react'
+import { Route, useLocation } from 'react-router-dom'
 import {
 	BasepathContext,
 	EngineContext,
 	ReferencesImagesContext,
+	RegisterEngineContext,
 } from './contexts'
 import References from './rule/References'
 import RulePage from './rule/RulePage'
 const { decodeRuleName, encodeRuleName } = utils
-
 export { default as Explanation } from './Explanation'
 export { RuleLink } from './RuleLink'
 export { References }
@@ -20,30 +21,73 @@ type DocumentationProps = {
 	referenceImages?: Record<string, string>
 }
 
+function useCacheEngineBySituation(
+	defaultEngine: Engine,
+	currentSituation?: Engine['parsedSituation']
+) {
+	const registeredEngines = useRef(
+		new WeakMap().set(
+			defaultEngine.parsedSituation,
+			defaultEngine.shallowCopy()
+		)
+	)
+	const registerEngine = useCallback(
+		(engine: Engine) =>
+			registeredEngines.current.set(
+				engine.parsedSituation,
+				engine.shallowCopy()
+			),
+		[registeredEngines]
+	)
+	if (currentSituation && !registeredEngines.current.has(currentSituation)) {
+		registeredEngines.current.set(
+			currentSituation,
+			defaultEngine.shallowCopy().setSituation(currentSituation)
+		)
+	}
+	const engine = currentSituation
+		? registeredEngines.current.get(currentSituation)
+		: defaultEngine
+	return [engine, registerEngine]
+}
+
 export function Documentation({
 	documentationPath,
-	engine,
+	engine: defaultEngine,
 	referenceImages = {},
 }: DocumentationProps) {
+	const { state } = useLocation<
+		| {
+				situation?: Engine['parsedSituation']
+				situationName?: string
+		  }
+		| undefined
+	>()
+	const [engine, cacheEngine] = useCacheEngineBySituation(
+		defaultEngine,
+		state?.situation
+	)
 	return (
-		<EngineContext.Provider value={engine}>
-			<BasepathContext.Provider value={documentationPath}>
-				<ReferencesImagesContext.Provider value={referenceImages}>
-					<Route
-						path={documentationPath + '/:name+'}
-						render={({ match }) => {
-							return (
-								<RulePage
-									dottedName={decodeRuleName(match.params.name)}
-									engine={engine}
-									language={'fr'}
-								/>
-							)
-						}}
-					/>
-				</ReferencesImagesContext.Provider>
-			</BasepathContext.Provider>
-		</EngineContext.Provider>
+		<RegisterEngineContext.Provider value={cacheEngine}>
+			<EngineContext.Provider value={engine}>
+				<BasepathContext.Provider value={documentationPath}>
+					<ReferencesImagesContext.Provider value={referenceImages}>
+						<Route
+							path={documentationPath + '/:name+'}
+							render={({ match }) => {
+								return (
+									<RulePage
+										situationName={state?.situationName}
+										dottedName={decodeRuleName(match.params.name)}
+										language={'fr'}
+									/>
+								)
+							}}
+						/>
+					</ReferencesImagesContext.Provider>
+				</BasepathContext.Provider>
+			</EngineContext.Provider>
+		</RegisterEngineContext.Provider>
 	)
 }
 
