6faf98964a025f318443f66d41489ceeb26ff31a
Alexandre Hajjar <alexandre.hajjar@gmail.com>
2021-05-27 19:52:43 +0200

    ✅ Split Cypress simulateur tests & some improvements
    * modularize generic simulateurs tests
    * split salarié
    * improve perf (mostly: remove cy.wait calls)
    * ignore downloads folder

--- mon-entreprise/cypress/integration/publi.codes/index.js
+++ mon-entreprise/cypress/integration/publi.codes/index.js
@@ -9,7 +9,6 @@ describe('Navigation', function () {
 	})
 	it('bac à sable should work', function () {
 		cy.contains('Bac à sable').click()
-		cy.wait(5000)
 		cy.contains('Dépenses primeur')
 		cy.contains('11,50')
 	})
