ea1abb530af45b9a5236aeee8e885e007c2524a3
Maxime Quandalle <maxime@quandalle.com>
2021-06-22 16:09:11 +0200

    Ajoute un bloc DGFiP

--- mon-entreprise/source/components/simulationExplanation/InstitutionsPartenaires.tsx
+++ mon-entreprise/source/components/simulationExplanation/InstitutionsPartenaires.tsx
@@ -1,8 +1,9 @@
-import Value, { Condition } from 'Components/EngineValue'
+import Value, { Condition, WhenNotApplicable } from 'Components/EngineValue'
 import { FromBottom } from 'Components/ui/animate'
 import Emoji from 'Components/utils/Emoji'
 import { useEngine } from 'Components/utils/EngineContext'
 import assuranceMaladieSrc from 'Images/assurance-maladie.svg'
+import dgfipSrc from 'Images/logo-dgfip.svg'
 import * as logosSrc from 'Images/logos-caisses-retraite'
 import urssafSrc from 'Images/Urssaf.svg'
 import { DottedName } from 'modele-social'
@@ -11,49 +12,60 @@ import { useSelector } from 'react-redux'
 import { targetUnitSelector } from 'Selectors/simulationSelectors'
 import styled from 'styled-components'
 
-export default function PLExplanation() {
+export default function InstitutionsPartenaires() {
 	const unit = useSelector(targetUnitSelector)
 	return (
 		<section>
-			<Trans i18nKey="simulateurs.explanation.pamc">
-				<FromBottom>
-					<h2>Vos institutions partenaires</h2>
-					<div className="ui__ box-container">
+			<FromBottom>
+				<h2>
+					<Trans i18nKey="simulateurs.explanation.institutions.titre">
+						Vos institutions partenaires
+					</Trans>
+				</h2>
+				<div className="ui__ full-width box-container">
+					<Condition expression="entreprise . activité . libérale réglementée">
 						<CotisationsUrssaf rule="dirigeant . indépendant . PL . cotisations Urssaf" />
 						<CaisseRetraite />
-						<Condition expression="dirigeant . indépendant . PL . PAMC . participation CPAM > 0">
-							<div className="ui__  card box">
-								<a
-									target="_blank"
-									href="https://www.ameli.fr/assure/droits-demarches/salaries-travailleurs-independants-et-personnes-sans-emploi/emploi-independant-non-salarie/praticien-auxiliaire-medical"
-								>
-									<LogoImg src={assuranceMaladieSrc} title="Logo CPAM" />
-								</a>
-								<p className="ui__ notice">
+					</Condition>
+					<WhenNotApplicable dottedName="entreprise . activité . libérale réglementée">
+						<CotisationsUrssaf rule="dirigeant . indépendant . cotisations et contributions" />
+					</WhenNotApplicable>
+					<ImpôtsDGFIP />
+					<Condition expression="dirigeant . indépendant . PL . PAMC . participation CPAM > 0">
+						<div className="ui__  card box">
+							<a
+								target="_blank"
+								href="https://www.ameli.fr/assure/droits-demarches/salaries-travailleurs-independants-et-personnes-sans-emploi/emploi-independant-non-salarie/praticien-auxiliaire-medical"
+							>
+								<LogoImg src={assuranceMaladieSrc} title="Logo CPAM" />
+							</a>
+							<p className="ui__ notice">
+								<Trans i18nKey="simulateurs.explanation.institutions.cpam">
 									En tant que professionnel de santé conventionné, vous
 									bénéficiez d'une prise en charge d'une partie de vos
 									cotisations par l'Assurance Maladie.
-								</p>
-								<p className="ui__ lead">
-									<Emoji emoji="🎁" />{' '}
-									<Value
-										unit={unit}
-										displayedUnit="€"
-										expression="dirigeant . indépendant . PL . PAMC . participation CPAM"
-									/>
-								</p>
-							</div>
-						</Condition>
-					</div>
-					<Condition expression="dirigeant . indépendant . cotisations et contributions . exonérations . ACRE > 0">
-						<p className="ui__ notice">
-							{' '}
+								</Trans>
+							</p>
+							<p className="ui__ lead">
+								<Emoji emoji="🎁" />{' '}
+								<Value
+									unit={unit}
+									displayedUnit="€"
+									expression="dirigeant . indépendant . PL . PAMC . participation CPAM"
+								/>
+							</p>
+						</div>
+					</Condition>
+				</div>
+				<Condition expression="dirigeant . indépendant . cotisations et contributions . exonérations . ACRE > 0">
+					<p className="ui__ notice">
+						<Trans i18nKey="simulateurs.explanation.institutions.notice acre">
 							Les montants indiqués ci-dessus sont calculés sans prendre en
 							compte l'exonération de début d'activité ACRE
-						</p>
-					</Condition>
-				</FromBottom>
-			</Trans>
+						</Trans>
+					</p>
+				</Condition>
+			</FromBottom>
 		</section>
 	)
 }
@@ -74,6 +86,28 @@ export function CotisationsUrssaf({ rule }: { rule: DottedName }) {
 				<Value unit={unit} displayedUnit="€" expression={rule} />
 			</p>
 		</div>
+	)
+}
+
+export function ImpôtsDGFIP() {
+	const unit = useSelector(targetUnitSelector)
+	return (
+		<Condition expression="impôt > 0">
+			<div className="ui__  card box">
+				<a target="_blank" href="https://www.impots.gouv.fr">
+					<LogoImg src={dgfipSrc} title="logo DGFiP" />
+				</a>
+				<p className="ui__ notice">
+					<Trans i18nKey="simulateurs.explanation.institutions.dgfip">
+						La direction générale des finances publiques (DGFiP) est l'organisme
+						qui collecte l'impôt sur le revenu.
+					</Trans>
+				</p>
+				<p className="ui__ lead">
+					<Value unit={unit} displayedUnit="€" expression="impôt . montant" />
+				</p>
+			</div>
+		</Condition>
 	)
 }
 
@@ -130,4 +164,5 @@ function CaisseRetraite() {
 const LogoImg = styled.img`
 	padding: 1rem;
 	height: 5rem;
+	max-width: 100%;
 `
