b6953989825edd79c0ecfaf20dd41d050720b87a
Alexandre S <bobylito@gmail.com>
2021-06-28 19:31:13 +0200

    feat(recherche-globale): remplacement de la recherche actuelle par Algolia

--- mon-entreprise/source/components/SearchBar.tsx
+++ mon-entreprise/source/components/SearchBar.tsx
@@ -1,184 +1,37 @@
-import React, { useEffect, useMemo, useState } from 'react'
-import { Trans, useTranslation } from 'react-i18next'
-import { DottedName } from 'modele-social'
-import RuleLink from './RuleLink'
+import 'instantsearch.css/themes/satellite.css'
 import './SearchBar.css'
-import { useEngine } from './utils/EngineContext'
-import { utils } from 'publicodes'
 
-// TODO: We should use a normal import here
-// We use a dynamic import to work around a typing problem https://github.com/betagouv/mon-entreprise/pull/1616#issuecomment-858629506
-let worker: any
-;(async function () {
-	const Worker = ((await import('./SearchBar.worker.js')) as any).default
-	worker = new Worker()
-})()
+import RuleLink from './RuleLink'
+import algoliasearch from 'algoliasearch/lite'
+import {
+	InstantSearch,
+	SearchBox,
+	Hits,
+	Highlight,
+} from 'react-instantsearch-dom'
+import { Names } from '../../../modele-social/dist/names'
 
-type SearchBarProps = {
-	showListByDefault?: boolean
-}
+type SearchBarProps = {}
 
-type SearchItem = {
-	title: string
-	dottedName: DottedName
-	espace: Array<string>
-}
+const ALGOLIA_APP_ID = process.env.ALGOLIA_APP_ID || 'QYX2LLVOAD'
+const ALGOLIA_SEARCH_KEY =
+	process.env.ALGOLIA_SEARCH_KEY || '9c77be10ac3d3f5217e0a417c859fc90'
+const searchClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_KEY)
 
-type Matches = Array<{
-	key: string
-	value: string
-	indices: Array<[number, number]>
-}>
+const Hit = ({ hit }: { hit: { path: Array<String>; objectID: Names } }) => (
+	<div className="hit-content">
+		<Highlight hit={hit} attribute="path" separator=" > " />
+		<RuleLink dottedName={hit.objectID}>Voir</RuleLink>
+	</div>
+)
 
-function highlightMatches(str: string, matches: Matches) {
-	if (!matches?.length) {
-		return str
-	}
-	const indices = matches[0].indices
-		.sort(([a], [b]) => a - b)
-		.map(([x, y]) => [x, y + 1])
-		.reduce(
-			(acc, value) =>
-				acc[acc.length - 1][1] <= value[0] ? [...acc, value] : acc,
-			[[0, 0]]
-		)
-		.flat()
-	return [...indices, str.length].reduce(
-		([highlight, prevIndice, acc], currentIndice, i) => {
-			const currentStr = str.slice(prevIndice, currentIndice)
-			return [
-				!highlight,
-				currentIndice,
-				[
-					...acc,
-					<span
-						style={highlight ? { fontWeight: 'bold' } : {}}
-						className={highlight ? 'ui__ light-bg' : ''}
-						key={i}
-					>
-						{currentStr}
-					</span>,
-				],
-			] as [boolean, number, Array<React.ReactNode>]
-		},
-		[false, 0, []] as [boolean, number, Array<React.ReactNode>]
-	)[2]
-}
-export default function SearchBar({
-	showListByDefault = false,
-}: SearchBarProps) {
-	const rules = useEngine().getParsedRules()
-	const [input, setInput] = useState('')
-	const [results, setResults] = useState<
-		Array<{
-			item: SearchItem
-			matches: Matches
-		}>
-	>([])
-	const { i18n } = useTranslation()
-
-	const searchIndex: Array<SearchItem> = useMemo(
-		() =>
-			Object.values(rules)
-				.filter(utils.ruleWithDedicatedDocumentationPage)
-				.map((rule) => ({
-					title:
-						rule.title +
-						(rule.rawNode.acronyme ? ` (${rule.rawNode.acronyme})` : ''),
-					dottedName: rule.dottedName,
-					espace: rule.dottedName.split(' . ').reverse(),
-				})),
-		[rules]
-	)
-
-	useEffect(() => {
-		worker.postMessage({
-			rules: searchIndex,
-		})
-
-		worker.onmessage = ({ data: results }: any) => setResults(results)
-		return () => {
-			worker.onmessage = null
-		}
-	}, [searchIndex, setResults])
-
+export default function SearchBar({}: SearchBarProps) {
 	return (
-		<>
-			<input
-				type="search"
-				className="ui__"
-				value={input}
-				placeholder={i18n.t('Entrez des mots clefs ici')}
-				onChange={(e) => {
-					const input = e.target.value
-					if (input.length > 0) worker.postMessage({ input })
-					setInput(input)
-				}}
-			/>
-			{!!input.length && !results.length ? (
-				<p
-					className="ui__ notice light-bg"
-					css={`
-						padding: 0.4rem;
-						border-radius: 0.3rem;
-						margin-top: 0.6rem;
-					`}
-				>
-					<Trans i18nKey="noresults">
-						Aucun résultat ne correspond à cette recherche
-					</Trans>
-				</p>
-			) : (
-				<ul
-					css={`
-						padding: 0;
-						margin: 0;
-						list-style: none;
-					`}
-				>
-					{(showListByDefault && !results.length && !input.length
-						? searchIndex
-								.filter((item) => item.espace.length === 2)
-								.map((item) => ({ item, matches: [] }))
-						: results
-					)
-						.slice(0, showListByDefault ? 100 : 6)
-						.map(({ item, matches }) => (
-							<li key={item.dottedName}>
-								<RuleLink
-									dottedName={item.dottedName}
-									style={{
-										width: '100%',
-										textDecoration: 'none',
-										lineHeight: '1.5rem',
-									}}
-								>
-									<small>
-										{item.espace
-											.slice(1)
-											.reverse()
-											.map((name) => (
-												<span key={name}>
-													{highlightMatches(
-														name,
-														matches.filter(
-															(m) => m.key === 'espace' && m.value === name
-														)
-													)}{' '}
-													›{' '}
-												</span>
-											))}
-										<br />
-									</small>
-									{highlightMatches(
-										item.title,
-										matches.filter((m) => m.key === 'title')
-									)}
-								</RuleLink>
-							</li>
-						))}
-				</ul>
-			)}
-		</>
+		<InstantSearch indexName="rules" searchClient={searchClient}>
+			<SearchBox />
+			<div className="hit-container">
+				<Hits hitComponent={Hit} />
+			</div>
+		</InstantSearch>
 	)
 }
