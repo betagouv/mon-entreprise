8847e821c2c52762def365c05c800b03032d6fea
Alexandre S <bobylito@gmail.com>
2021-06-29 18:40:38 +0200

    feat(nouvelle-recherche): ajout d'un index pour les simulations

--- mon-entreprise/scripts/search/update-data.js
+++ mon-entreprise/scripts/search/update-data.js
@@ -2,18 +2,19 @@ require('dotenv').config()
 
 const algoliasearch = require('algoliasearch')
 const rules = require('modele-social')
+const getSimulationData = require('../../source/pages/Simulateurs/metadata-src')
 
 const { ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY } = process.env
 
 const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_ADMIN_KEY)
 
 const rulesIndex = client.initIndex('rules')
+const simulateursIndex = client.initIndex('simulateurs')
 
 const formatRulesToAlgolia = (rules) => {
-	const ruleNames = Object.keys(rules)
-	return ruleNames
-		.map((n) => {
-			const rule = rules[n]
+	const ruleEntries = Object.entries(rules)
+	return ruleEntries
+		.map(([n, rule]) => {
 			if (rule) {
 				const path = n.split(' . ')
 				return {
@@ -30,10 +31,38 @@ const formatRulesToAlgolia = (rules) => {
 		.filter((r) => Boolean(r))
 }
 
+const formatSimulationDataToAlgolia = (simulations) => {
+	const entries = Object.entries(simulations)
+
+	const data = entries.map(([id, simulation]) => {
+		return {
+			...simulation,
+			objectID: id,
+			title: simulation.title || simulation.shortName || simulation.meta.title,
+			description: simulation.meta ? simulation.meta.description : undefined,
+		}
+	})
+
+	return data
+}
+
 ;(async function () {
 	try {
+		console.log('Algolia update START')
+
+		console.log('Clearing: rules')
 		await rulesIndex.clearObjects().wait()
+		console.log('Uploading: rules')
 		await rulesIndex.saveObjects(formatRulesToAlgolia(rules)).wait()
+
+		console.log('Clearing: simulateurs')
+		await simulateursIndex.clearObjects().wait()
+		console.log('Updloading: simulateurs')
+		await simulateursIndex
+			.saveObjects(formatSimulationDataToAlgolia(getSimulationData()))
+			.wait()
+
+		console.log('Algolia update DONE')
 	} catch (e) {
 		console.log(e)
 	}
