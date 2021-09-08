betagouv/syso

🎨 Improve StackedBarChart component with percentage precision

mon-entreprise/source/components/StackedBarChart.tsx
@@ -3,6 +3,7 @@ import useDisplayOnIntersecting from 'Components/utils/useDisplayOnIntersecting'
 import { Names } from 'modele-social/dist/names'
 import { EvaluatedNode } from 'publicodes'
 import React from 'react'
+import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 import { animated, useSpring } from 'react-spring'
 import { targetUnitSelector } from 'Selectors/simulationSelectors'
@@ -54,32 +55,52 @@ const SmallCircle = styled.span`
 	border-radius: 100%;
 `
 
+type Precision = 1 | 0.1 | 0.01
+
 function integerAndDecimalParts(value: number) {
 	const integer = Math.floor(value)
 	const decimal = value - integer
 	return { integer, decimal }
 }
 
-// This function calculates rounded percentages so that the sum of all
-// returned values is always 100. For instance: [60, 30, 10].
-export function roundedPercentages(values: Array<number>) {
+/**
+ * This function only produces integers.
+ */
+function simpleRoundedPer(values: Array<number>, logScale: number) {
+	const scale = Math.pow(10, 2 - logScale) // 100, 1000, 10000
 	const sum = (a = 0, b: number) => a + b
 	const total = values.reduce(sum, 0)
-	const percentages = values.map((value) =>
-		integerAndDecimalParts((value / total) * 100)
+	// By default we are talking percentages, but this can be per-mille or more
+	const perscalages = values.map((value) =>
+		integerAndDecimalParts((value / total) * scale)
 	)
-	const totalRoundedPercentage = percentages
+	const totalRoundedPerscalage = perscalages
 		.map((v) => v.integer)
 		.reduce(sum, 0)
-	const indexesToIncrement = percentages
+	const indexesToIncrement = perscalages
 		.map((percentage, index) => ({ ...percentage, index }))
 		.sort((a, b) => b.decimal - a.decimal)
 		.map(({ index }) => index)
-		.splice(0, 100 - totalRoundedPercentage)
+		.splice(0, scale - totalRoundedPerscalage)
 
-	return percentages.map(
+	return perscalages.map(
 		({ integer }, index) =>
 			integer + (indexesToIncrement.includes(index) ? 1 : 0)
+	)
+}
+
+/**
+ * This function calculates rounded percentages so that the sum of all
+ * returned values is always 100. For instance: [60, 30, 10] or [60.1, 30, 9.9]
+ * depending on the precision.
+ */
+export function roundedPercentages(
+	values: Array<number>,
+	precision: Precision
+) {
+	const logScale = Math.log10(precision)
+	return simpleRoundedPer(values, logScale).map(
+		(int) => int / Math.pow(10, -logScale)
 	)
 }
 
@@ -90,14 +111,17 @@ type StackedBarChartProps = {
 		legend: React.ReactNode
 		key: string
 	}>
+	precision: Precision
 }
 
-export function StackedBarChart({ data }: StackedBarChartProps) {
+function StackedBarChart({ data, precision }: StackedBarChartProps) {
+	const { i18n } = useTranslation()
 	const [intersectionRef, displayChart] = useDisplayOnIntersecting({
 		threshold: 0.5,
 	})
 	const percentages = roundedPercentages(
-		data.map((d) => (typeof d.value === 'number' && d.value) || 0)
+		data.map((d) => (typeof d.value === 'number' && d.value) || 0),
+		precision
 	)
 	const dataWithPercentage = data.map((data, index) => ({
 		...data,
@@ -127,7 +151,9 @@ export function StackedBarChart({ data }: StackedBarChartProps) {
 					<BarStackLegendItem key={key}>
 						<SmallCircle style={{ backgroundColor: color }} />
 						{legend}
-						<strong>{percentage} %</strong>
+						<strong>
+							{Intl.NumberFormat(i18n.language).format(percentage)} %
+						</strong>
 					</BarStackLegendItem>
 				))}
 			</BarStackLegend>
@@ -137,13 +163,18 @@ export function StackedBarChart({ data }: StackedBarChartProps) {
 
 type StackedRulesChartProps = {
 	data: Array<{ color?: string; dottedName: Names; title?: string }>
+	precision: Precision
 }
 
-export default function StackedRulesChart({ data }: StackedRulesChartProps) {
+export default function StackedRulesChart({
+	data,
+	precision = 1,
+}: StackedRulesChartProps) {
 	const engine = useEngine()
 	const targetUnit = useSelector(targetUnitSelector)
 	return (
 		<StackedBarChart
+			precision={precision}
 			data={data.map(({ dottedName, title, color }) => ({
 				key: dottedName,
 				value: engine.evaluate({ valeur: dottedName, unité: targetUnit })

betagouv/mon-entreprise

mon-entreprise/source/components/StackedBarChart.tsx
@@ -3,6 +3,7 @@ import useDisplayOnIntersecting from 'Components/utils/useDisplayOnIntersecting'
 import { Names } from 'modele-social/dist/names'
 import { EvaluatedNode } from 'publicodes'
 import React, { useContext } from 'react'
+import { useTranslation } from 'react-i18next'
 import { useSelector } from 'react-redux'
 import { animated, useSpring } from 'react-spring'
 import { targetUnitSelector } from 'Selectors/simulationSelectors'
@@ -55,47 +56,52 @@ const SmallCircle = styled.span`
 	border-radius: 100%;
 `
 
+type Precision = 1 | 0.1 | 0.01
+
 function integerAndDecimalParts(value: number) {
 	const integer = Math.floor(value)
 	const decimal = value - integer
 	return { integer, decimal }
 }
 
-// This function calculates rounded percentages so that the sum of all
-// returned values is always 100. For instance: [60, 30, 10].
-export function roundedPercentages(values: Array<number>) {
+/**
+ * Produces integers only.
+ */
+function simpleRoundedPer(values: Array<number>, logScale: number) {
+	const scale = Math.pow(10, 2 - logScale) // 100, 1000, 10000
 	const sum = (a = 0, b: number) => a + b
 	const total = values.reduce(sum, 0)
-	const percentages = values.map((value) =>
-		integerAndDecimalParts((value / total) * 100)
+	// By default we are talking percentages, but this can be per-mille or more
+	const perscalages = values.map((value) =>
+		integerAndDecimalParts((value / total) * scale)
 	)
-	const totalRoundedPercentage = percentages
+	const totalRoundedPerscalage = perscalages
 		.map((v) => v.integer)
 		.reduce(sum, 0)
-	const indexesToIncrement = percentages
+	const indexesToIncrement = perscalages
 		.map((percentage, index) => ({ ...percentage, index }))
 		.sort((a, b) => b.decimal - a.decimal)
 		.map(({ index }) => index)
-		.splice(0, 100 - totalRoundedPercentage)
+		.splice(0, scale - totalRoundedPerscalage)
 
-	return percentages.map(
+	return perscalages.map(
 		({ integer }, index) =>
 			integer + (indexesToIncrement.includes(index) ? 1 : 0)
 	)
 }
 
-export function StackedBarChart({ data }: InnerStackedBarChartProps) {
-	const [intersectionRef, displayChart] = useDisplayOnIntersecting({
-		threshold: 0.5,
-	})
-
-	const styles = useSpring({ opacity: displayChart ? 1 : 0 })
-	return !useContext(DisableAnimationContext) ? (
-		<animated.div ref={intersectionRef} style={styles}>
-			<InnerStackedBarChart data={data} />
-		</animated.div>
-	) : (
-		<InnerStackedBarChart data={data} />
+/**
+ * Calculates rounded percentages so that the sum of all returned values is
+ * always 100. For instance: [60, 30, 10] or [60.1, 30, 9.9] depending on the
+ * precision.
+ */
+export function roundedPercentages(
+	values: Array<number>,
+	precision: Precision
+) {
+	const logScale = Math.log10(precision)
+	return simpleRoundedPer(values, logScale).map(
+		(int) => int / Math.pow(10, -logScale)
 	)
 }
 
@@ -106,11 +112,32 @@ type InnerStackedBarChartProps = {
 		legend: React.ReactNode
 		key: string
 	}>
+	precision: Precision
 }
 
-function InnerStackedBarChart({ data }: InnerStackedBarChartProps) {
+export function StackedBarChart({
+	data,
+	precision,
+}: InnerStackedBarChartProps) {
+	const [intersectionRef, displayChart] = useDisplayOnIntersecting({
+		threshold: 0.5,
+	})
+
+	const styles = useSpring({ opacity: displayChart ? 1 : 0 })
+	return !useContext(DisableAnimationContext) ? (
+		<animated.div ref={intersectionRef} style={styles}>
+			<InnerStackedBarChart data={data} precision={precision} />
+		</animated.div>
+	) : (
+		<InnerStackedBarChart data={data} precision={precision} />
+	)
+}
+
+function InnerStackedBarChart({ data, precision }: InnerStackedBarChartProps) {
+	const { i18n } = useTranslation()
 	const percentages = roundedPercentages(
-		data.map((d) => (typeof d.value === 'number' && d.value) || 0)
+		data.map((d) => (typeof d.value === 'number' && d.value) || 0),
+		precision
 	)
 	const dataWithPercentage = data.map((data, index) => ({
 		...data,
@@ -138,7 +165,9 @@ function InnerStackedBarChart({ data }: InnerStackedBarChartProps) {
 					<BarStackLegendItem key={key}>
 						<SmallCircle style={{ backgroundColor: color }} />
 						{legend}
-						<strong>{percentage} %</strong>
+						<strong>
+							{Intl.NumberFormat(i18n.language).format(percentage)} %
+						</strong>
 					</BarStackLegendItem>
 				))}
 			</BarStackLegend>
@@ -148,13 +177,18 @@ function InnerStackedBarChart({ data }: InnerStackedBarChartProps) {
 
 type StackedRulesChartProps = {
 	data: Array<{ color?: string; dottedName: Names; title?: string }>
+	precision: Precision
 }
 
-export default function StackedRulesChart({ data }: StackedRulesChartProps) {
+export default function StackedRulesChart({
+	data,
+	precision = 1,
+}: StackedRulesChartProps) {
 	const engine = useEngine()
 	const targetUnit = useSelector(targetUnitSelector)
 	return (
 		<StackedBarChart
+			precision={precision}
 			data={data.map(({ dottedName, title, color }) => ({
 				key: dottedName,
 				value: engine.evaluate({ valeur: dottedName, unité: targetUnit })