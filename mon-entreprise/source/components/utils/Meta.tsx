8029255b3fa05627c213648bb12502d6d7cb649e
jobailla <jbaillais77@gmail.com>
2021-10-20 16:39:37 +0200

    :chart_with_upwards_trend: Ajout lang dans la balise <html>
    L'élément <html> n'a pas d'attribut [lang]

--- mon-entreprise/source/components/utils/Meta.tsx
+++ mon-entreprise/source/components/utils/Meta.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from 'react-i18next'
 import { useLocation } from 'react-router'
 
 type PropType = {
-	page: string,
+	page: string
 	title: string
 	description: string
 	ogDescription?: string
@@ -20,21 +20,26 @@ export default function Meta({
 	ogImage,
 }: PropType) {
 	const { pathname } = useLocation()
-	const { t } = useTranslation()
+	const { t, i18n } = useTranslation()
 	const meta = {
 		title: t(`${page}.titre`, title) || title,
 		description: t(`${page}.description`, description) || description,
-		ogDescription: ogDescription ? t(`${page}.ogDescription`, ogDescription) || ogDescription : description,
+		ogDescription: ogDescription
+			? t(`${page}.ogDescription`, ogDescription) || ogDescription
+			: description,
 		ogTitle: ogTitle ? t(`${page}.ogTitle`, ogTitle) || ogTitle : title,
 		ogImage: ogImage ? t(`${page}.ogImage`, ogImage) || ogImage : null,
 	}
 	return (
-		<Helmet>
+		<Helmet htmlAttributes={{ lang: i18n.language }}>
 			<title>{meta.title}</title>
 			<meta name="description" content={meta.description} />
 			<meta property="og:type" content="website" />
 			<meta property="og:title" content={meta.ogTitle ?? meta.title} />
-			<meta property="og:description" content={meta.ogDescription ?? meta.description} />
+			<meta
+				property="og:description"
+				content={meta.ogDescription ?? meta.description}
+			/>
 			{meta.ogImage && (
 				<meta
 					property="og:image"
