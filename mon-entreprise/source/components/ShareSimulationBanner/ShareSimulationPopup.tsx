d63e55bc3d8fcfb0898619de7120d6c9c9117540
Alexandre Valsamou-Stanislawski <alexandre.valsamoustanislawski@beta.gouv.fr>
2021-11-10 16:14:56 +0100

    Passe le bandeau de partage de simulateur à la charte

---
+++ mon-entreprise/source/components/ShareSimulationBanner/ShareSimulationPopup.tsx
@@ -0,0 +1,81 @@
+import { Button } from 'DesignSystem/buttons'
+import { TextField } from 'DesignSystem/field'
+import { Body, SmallBody } from 'DesignSystem/typography/paragraphs'
+import { useContext, useEffect, useRef, useState } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
+import styled from 'styled-components'
+import { TrackingContext } from '../../ATInternetTracking'
+
+const LinkAndButton = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 1rem;
+
+	& > :first-child {
+		flex-grow: 1;
+	}
+`
+
+export function ShareSimulationPopup({ url }: { url: string }) {
+	const inputRef = useRef<HTMLInputElement>(null)
+	const { t } = useTranslation()
+	const [linkCopied, setLinkCopied] = useState(false)
+	const tracker = useContext(TrackingContext)
+
+	const selectInput = () => inputRef.current?.select()
+	useEffect(selectInput, [])
+	useEffect(() => {
+		const handler = setTimeout(() => setLinkCopied(false), 5000)
+		return () => {
+			clearTimeout(handler)
+		}
+	}, [linkCopied])
+
+	return (
+		<>
+			<Body>
+				<Trans key="shareSimulation.modal.notice">
+					Voici le lien que vous pouvez envoyer pour accéder à votre simulation.
+				</Trans>
+			</Body>
+
+			<LinkAndButton>
+				<TextField
+					inputRef={inputRef}
+					onFocus={selectInput}
+					value={url}
+					aria-label="URL de votre simulation"
+				/>
+				{navigator.clipboard ? (
+					<Button
+						size="XS"
+						onClick={() => {
+							tracker.click.set({
+								chapter1: 'feature:partage',
+								type: 'action',
+								name: 'lien copié',
+							})
+							tracker.dispatch()
+							navigator.clipboard.writeText(url)
+							setLinkCopied(true)
+						}}
+					>
+						{linkCopied ? (
+							<>✅ {t('shareSimulation.button.copied', 'Copié')}</>
+						) : (
+							<>📋 {t('shareSimulation.button.copy', 'Copier le lien')}</>
+						)}
+					</Button>
+				) : (
+					<SmallBody>
+						{t(
+							'shareSimulation.modal.helpText',
+							'Le lien est sélectionné, vous pouvez le copier/coller'
+						)}
+					</SmallBody>
+				)}
+			</LinkAndButton>
+		</>
+	)
+}
---
+++ mon-entreprise/source/components/ShareSimulationBanner/index.tsx
@@ -0,0 +1,112 @@
+import Emoji from 'Components/utils/Emoji'
+import { Button } from 'DesignSystem/buttons'
+import PopoverWithTrigger from 'DesignSystem/PopoverWithTrigger'
+import { useContext } from 'react'
+import { Trans, useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
+import { situationSelector } from 'Selectors/simulationSelectors'
+import styled from 'styled-components'
+import { TrackingContext } from '../../ATInternetTracking'
+import { useParamsFromSituation } from '../utils/useSearchParamsSimulationSharing'
+import { ShareSimulationPopup } from './ShareSimulationPopup'
+
+export function useUrl() {
+	const situation = useSelector(situationSelector)
+	const searchParams = useParamsFromSituation(situation)
+	searchParams.set('utm_source', 'sharing')
+	return [
+		window.location.origin,
+		window.location.pathname.replace('iframes/', ''),
+		'?',
+		searchParams.toString(),
+	].join('')
+}
+
+const ButtonLabel = styled.div`
+	display: inline;
+	margin-left: 1rem;
+`
+
+export default function ShareOrSaveSimulationBanner() {
+	const { t } = useTranslation()
+	const tracker = useContext(TrackingContext)
+	const shareAPIAvailable = !!window?.navigator?.share
+	const url = useUrl()
+	const startSharing = async () => {
+		if (shareAPIAvailable) {
+			await window.navigator.share({
+				title: document.title,
+				text: t(
+					'shareSimulation.navigatorShare',
+					'Ma simulation Mon Entreprise'
+				),
+				url,
+			})
+		}
+	}
+
+	return (
+		<SharingTools className=" print-display-none">
+			<PopoverWithTrigger
+				title={t('shareSimulation.modal.title', 'Votre lien de partage')}
+				trigger={(propsToDispatch) => (
+					<Button
+						{...propsToDispatch}
+						light
+						size="XS"
+						onClick={() => {
+							tracker.click.set({
+								chapter1: 'feature:partage',
+								type: 'action',
+								name: 'démarré',
+							})
+							tracker.dispatch()
+							startSharing()
+
+							propsToDispatch.onClick()
+						}}
+					>
+						<Emoji emoji="🔗" />
+						<ButtonLabel>
+							<Trans i18nKey="shareSimulation.banner">
+								Générer un lien de partage
+							</Trans>
+						</ButtonLabel>
+					</Button>
+				)}
+			>
+				<ShareSimulationPopup url={url} />
+			</PopoverWithTrigger>
+
+			<div>
+				<Button
+					light
+					size="XS"
+					onClick={() => {
+						window.print()
+					}}
+				>
+					<Emoji
+						css={`
+							margin-right: 1rem;
+						`}
+						emoji="🖨"
+					/>
+					<ButtonLabel>
+						<Trans i18nKey="ExportSimulation.Banner">
+							Imprimer ou sauvegarder en PDF
+						</Trans>
+					</ButtonLabel>
+				</Button>
+			</div>
+		</SharingTools>
+	)
+}
+
+const SharingTools = styled.div`
+	display: flex;
+	flex-wrap: wrap;
+	justify-content: space-around;
+	align-items: center;
+	margin: 0.5rem 0;
+`
