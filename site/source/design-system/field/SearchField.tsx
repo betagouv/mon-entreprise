67e8d7b865c5d7683e47941c7f8e9cffcf1b2ea8
Jérémy Rialland <wiinxt@gmail.com>
2022-01-26 13:39:34 +0100

    🐛 Fix css du champ de recherche blanc sur ios 15

--- site/source/design-system/field/SearchField.tsx
+++ site/source/design-system/field/SearchField.tsx
@@ -19,13 +19,13 @@ import {
 } from './TextField'
 
 const SearchInput = styled(StyledInput)`
+	&,
 	&::-webkit-search-decoration,
 	&::-webkit-search-cancel-button,
 	&::-webkit-search-results-button,
 	&::-webkit-search-results-decoration {
 		-webkit-appearance: none;
 	}
-	-webkit-appearance: textfield;
 `
 
 const SearchInputContainer = styled(StyledInputContainer)`
