0901efdf885915fd17b988f4dff54b90a1cf5b8c
Johan Girod <johan.girod@beta.gouv.fr>
2022-02-03 08:41:17 +0100

    Rétabli l'affichage des droits à la retraite sur le simulateur indépendant

--- site/source/components/simulationExplanation/IndépendantExplanation.tsx
+++ site/source/components/simulationExplanation/IndépendantExplanation.tsx
@@ -62,7 +62,7 @@ export default function IndépendantExplanation() {
 				</section>
 			</Condition>
 			<InstitutionsPartenaires />
-
+			<DroitsRetraite />
 			<DistributionSection>
 				<Distribution />
 			</DistributionSection>
