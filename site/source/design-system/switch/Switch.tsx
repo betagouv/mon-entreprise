88759c772bd00e3813b7b7595036b70784e723d2
Jérémy Rialland <wiinxt@gmail.com>
2022-02-22 09:59:53 +0100

    Fix du Switch

--- site/source/design-system/switch/Switch.tsx
+++ site/source/design-system/switch/Switch.tsx
@@ -2,8 +2,8 @@ import { FocusStyle, SROnly } from 'DesignSystem/global-style'
 import { useRef } from 'react'
 import { useTranslation } from 'react-i18next'
 import styled, { css } from 'styled-components'
-import { useCheckbox } from '@react-aria/checkbox'
 import { useToggleState } from '@react-stately/toggle'
+import { useSwitch } from '@react-aria/switch'
 
 const HiddenCheckbox = styled.input`
 	${SROnly}
@@ -23,14 +23,14 @@ const StyledCheckbox = styled.label<StyledProps>`
 	box-shadow: 0px 3px 1px 0px #0000000f, 0px 3px 8px 0px #00000026;
 	background-color: #ffffff;
 	text-align: center;
-	transition: transform 0.3s ease-in-out, box-shadow 0.3s ease-in-out;
+	transition: transform 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
 	transform: translateX(${({ checked }) => (checked ? '0.8rem' : '-0.8rem')});
 `
 
 const StyledSwitch = styled.span<StyledProps>`
 	display: inline-block;
 	position: relative;
-	transition: all 0.3s ease-in-out;
+	transition: all 0.15s ease-in-out;
 	background-color: ${({ theme, checked }) =>
 		checked
 			? theme.colors.bases.primary[700]
@@ -62,7 +62,7 @@ const StyledSwitch = styled.span<StyledProps>`
 			: ''}
 `
 
-type AriaCheckboxProps = Parameters<typeof useCheckbox>[0]
+type AriaCheckboxProps = Parameters<typeof useSwitch>[0]
 
 export type SwitchProps = AriaCheckboxProps
 
@@ -70,7 +70,7 @@ export const Switch = (props: SwitchProps) => {
 	const { t } = useTranslation()
 	const state = useToggleState(props)
 	const ref = useRef<HTMLInputElement>(null)
-	const { inputProps } = useCheckbox(props, state, ref)
+	const { inputProps } = useSwitch(props, state, ref)
 
 	const { isDisabled = false } = props
 	const { isSelected } = state
@@ -81,7 +81,7 @@ export const Switch = (props: SwitchProps) => {
 			disabled={isDisabled}
 			onClick={() => !isDisabled && state.toggle()}
 		>
-			<HiddenCheckbox {...inputProps} ref={ref} />
+			<HiddenCheckbox {...inputProps} aria-hidden="true" ref={ref} />
 			<StyledCheckbox checked={isSelected} disabled={isDisabled}>
 				{isSelected ? t('Oui') : t('Non')}
 			</StyledCheckbox>
