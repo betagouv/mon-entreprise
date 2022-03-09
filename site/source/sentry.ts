7ccd479d605702d5e05bb504e53772d521ad529b
Jérémy Rialland <wiinxt@gmail.com>
2022-03-09 13:25:02 +0100

    Ajout des fonctions isProduction, isStaging et isDevelopment

--- site/source/sentry.ts
+++ site/source/sentry.ts
@@ -1,18 +1,10 @@
 import * as Sentry from '@sentry/react'
 import { Integrations } from '@sentry/tracing'
+import { getBranch, isProduction, isStaging } from './utils'
 
-let branch: string | undefined = import.meta.env.VITE_GITHUB_REF?.split(
-	'/'
-)?.slice(-1)?.[0]
+const branch = getBranch()
 
-if (branch === 'merge') {
-	branch = import.meta.env.VITE_GITHUB_HEAD_REF
-}
-
-const release =
-	branch && `${branch}-` + import.meta.env.VITE_GITHUB_SHA?.substring(0, 7)
-
-if (branch && branch !== 'master') {
+if (branch && isStaging()) {
 	// eslint-disable-next-line no-console
 	console.info(
 		`ℹ Vous êtes sur la branche : %c${branch}`,
@@ -20,15 +12,10 @@ if (branch && branch !== 'master') {
 	)
 }
 
-// We use this variable to hide some features in production while keeping them
-// in feature-branches. In case we do A/B testing with several branches served
-// in production, we should add the public faced branch names in the test below.
-// This is different from the import.meta.env.MODE in that a feature branch may
-// be build in production mode (with the NODE_ENV) but we may still want to show
-// or hide some features.
-export const productionMode = ['master', 'next'].includes(branch ?? '')
+const release =
+	branch && `${branch}-` + import.meta.env.VITE_GITHUB_SHA?.substring(0, 7)
 
-if (productionMode) {
+if (isProduction()) {
 	Sentry.init({
 		dsn: 'https://92bbc21937b24136a2fe1b1d922b000f@o548798.ingest.sentry.io/5745615',
 		integrations: [new Integrations.BrowserTracing()],
