d328de7eaf8f586722181f7a50a66a066299a815
Johan Girod <johan.girod@beta.gouv.fr>
2022-03-24 16:23:37 +0100

    Répare les intégration lorsqu'aucune couleur n'est précisé
    Par ailleurs, répare les liens en dessous
    
    fix #2065

--- site/source/components/layout/Footer/Privacy.tsx
+++ site/source/components/layout/Footer/Privacy.tsx
@@ -33,7 +33,6 @@ export default function Privacy({ label }: { label?: string }) {
 				</Link>
 			)}
 			title={t('privacyContent.title', 'Données personnelles')}
-			small
 		>
 			<Trans i18nKey="privacyContent.texte">
 				<TrackPage chapter1="informations" name={'donnees_personnelles'} />
