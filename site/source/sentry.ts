545c02a9728c85cf30f5f687199d9e4beb386a0a
Jérémy Rialland <wiinxt@gmail.com>
2022-03-28 15:40:07 +0200

    Add capture console error in sentry Update Sentry

--- site/source/sentry.ts
+++ site/source/sentry.ts
@@ -1,6 +1,7 @@
 import * as Sentry from '@sentry/react'
 import { Integrations } from '@sentry/tracing'
 import { getBranch, isProduction, isStaging } from './utils'
+import { CaptureConsole } from '@sentry/integrations'
 
 const branch = getBranch()
 
@@ -18,7 +19,10 @@ const release =
 if (isProduction()) {
 	Sentry.init({
 		dsn: 'https://92bbc21937b24136a2fe1b1d922b000f@o548798.ingest.sentry.io/5745615',
-		integrations: [new Integrations.BrowserTracing()],
+		integrations: [
+			new Integrations.BrowserTracing(),
+			new CaptureConsole({ levels: ['error'] }),
+		],
 		release,
 		// Set tracesSampleRate to 1.0 to capture 100%
 		// of transactions for performance monitoring.
