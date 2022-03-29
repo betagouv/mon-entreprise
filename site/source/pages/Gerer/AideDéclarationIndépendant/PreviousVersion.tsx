dbb25fcf3ea99ac70ab5aea98469073aded7c762
Jérémy Rialland <wiinxt@gmail.com>
2022-03-29 13:48:06 +0200

    Update des textes du formulaire

--- site/source/pages/Gerer/AideDéclarationIndépendant/PreviousVersion.tsx
+++ site/source/pages/Gerer/AideDéclarationIndépendant/PreviousVersion.tsx
@@ -258,7 +258,8 @@ function ImpositionSection() {
 								</Condition>
 								<Condition expression="entreprise . imposition . IR . micro-fiscal = non">
 									<H2>
-										Quel est votre résultat fiscal en 2021 ?<br />
+										Quel est votre résultat fiscal au titre de l'année 2021 ?
+										<br />
 										<small>
 											Charges sociales et exonérations fiscales non incluses{' '}
 											<ExplicationsResultatFiscal />
