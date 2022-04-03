7bac94d45f86411bf5f64d3fb54cb2241235381f
Maxime Quandalle <maxime@quandalle.com>
2022-04-03 17:48:02 +0200

    ⬆ MAJ @storybook/builder-vite
    https://github.com/storybookjs/builder-vite#project-has-been-renamed

--- site/.storybook/main.cjs
+++ site/.storybook/main.cjs
@@ -1,3 +1,4 @@
+// TODO: Move to ESModule
 const { loadConfigFromFile, mergeConfig } = require('vite')
 const reactPlugin = require('@vitejs/plugin-react')
 const path = require('path')
@@ -10,14 +11,13 @@ module.exports = {
 	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
 	framework: '@storybook/react',
 	core: {
-		builder: 'storybook-builder-vite',
+		builder: '@storybook/builder-vite',
 	},
 	features: {
 		// Auto title not supported when not using storyStoreV7
 		// See https://github.com/eirslett/storybook-builder-vite/issues/201
 		storyStoreV7: true,
 	},
-
 	typescript: (config) => ({
 		...config,
 		reactDocgenTypescriptOptions: {
@@ -35,12 +35,10 @@ module.exports = {
 	async viteFinal(config, { configType }) {
 		const { config: userConfig } = await loadConfigFromFile(
 			path.resolve(__dirname, '../vite.config.ts')
-		)
+		) // Keep storybook plugins in storybookPlugins
 
-		// Keep storybook plugins in storybookPlugins
 		const storybookPlugins = config.plugins
 		config.plugins = []
-
 		const conf = mergeConfig(config, {
 			...userConfig,
 			base: configType === 'PRODUCTION' ? '/dev/storybook/' : userConfig.base,
@@ -51,14 +49,15 @@ module.exports = {
 						!(Array.isArray(plugin) && plugin[0].name === 'vite:react-babel')
 				),
 				reactPlugin({
-					babel: { plugins: ['babel-plugin-styled-components'] },
+					babel: {
+						plugins: ['babel-plugin-styled-components'],
+					},
 					// Copied from https://github.com/eirslett/storybook-builder-vite/blob/917d8868943ec5f58c9c2c6900e196637f0d05e3/packages/storybook-builder-vite/vite-config.ts#L95
 					// Do not treat story files as HMR boundaries, storybook itself needs to handle them.
 					exclude: [/\.stories\.([tj])sx?$/, /node_modules/],
 				}),
 			],
 		})
-
 		return conf
 	},
 
