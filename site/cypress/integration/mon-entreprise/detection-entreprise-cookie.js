7b9742e239bd384b9c5f534ae509c669af11c81b
Maxime Quandalle <maxime@quandalle.com>
2022-04-13 17:58:00 +0200

    ✅ Ajoute un test d'intégration
    Pour la détection de l'entreprise via le cookie Urssaf

---
+++ site/cypress/integration/mon-entreprise/detection-entreprise-cookie.js
@@ -0,0 +1,18 @@
+const fr = Cypress.env('language') === 'fr'
+
+describe("Détection de l'entreprise depuis le cookie urssaf.fr", function () {
+	if (!fr) {
+		return
+	}
+	before(function () {
+		cy.setCookie(
+			'EnLigne',
+			'siret=34473897600010&nom=Magnin&prenom=Clarisse&sl_retour_service='
+		)
+		return cy.visit('/')
+	})
+
+	it('should display the name of the company set in the cookie', function () {
+		cy.contains('MC KINSEY', { timeout: 10000 })
+	})
+})
