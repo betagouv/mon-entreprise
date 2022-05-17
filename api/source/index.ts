86c60611c8da65e61f44e508e7426505a80dca63
Jérémy Rialland <wiinxt@gmail.com>
2022-05-17 14:18:32 +0200

    Add /openapi.json endpoint

--- api/source/index.ts
+++ api/source/index.ts
@@ -4,8 +4,10 @@ import Koa from 'koa'
 import rules from 'modele-social'
 import Engine from 'publicodes'
 import { koaMiddleware as publicodesAPI } from 'publicodes-api'
+// @ts-ignore
 import openapi from './openapi.json'
 import { docRoutes } from './route/doc.js'
+import { openapiRoutes } from './route/openapi.js'
 
 type State = Koa.DefaultState
 
@@ -16,11 +18,9 @@ const router = new Router<State, Context>()
 
 app.use(cors())
 
-const apiRoutes = publicodesAPI(() => new Engine(rules), {
-	customOpenapi: openapi,
-})
+const apiRoutes = publicodesAPI(() => new Engine(rules))
 
-router.use('/v1', apiRoutes, docRoutes())
+router.use('/v1', apiRoutes, docRoutes(), await openapiRoutes(openapi))
 
 app.use(router.routes())
 app.use(router.allowedMethods())
