abfa61ce8fa5c9d4185cfe48f0b5345d232c828f
Jérémy Rialland <wiinxt@gmail.com>
2022-05-25 17:48:28 +0200

    Move to Scalingo

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node -v ; yarn -v ; echo "$PORT" ; yarn workspace api run start:prod
--- api/serverless.ts
+++
@@ -1,8 +0,0 @@
-import serverless from 'serverless-http'
-import { app } from './source/index.cjs'
-
-const handler = serverless(app, {
-	provider: 'aws',
-})
-
-export { handler }
