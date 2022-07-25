0bc3484b6c635ea89137075c046a0713519d0da4
Jérémy Rialland <wiinxt@gmail.com>
2022-07-25 14:14:49 +0200

    Update svg favicon

--- site/vite.config.ts
+++ site/vite.config.ts
@@ -83,12 +83,12 @@ export default defineConfig(({ command, mode }) => ({
 				background_color: '#ffffff',
 				icons: [
 					{
-						src: '/favicon/android-chrome-192x192-shadow.png?v=1.0',
+						src: '/favicon/android-chrome-192x192-shadow.png?v=2.0',
 						sizes: '192x192',
 						type: 'image/png',
 					},
 					{
-						src: '/favicon/android-chrome-512x512-shadow.png?v=1.0',
+						src: '/favicon/android-chrome-512x512-shadow.png?v=2.0',
 						sizes: '512x512',
 						type: 'image/png',
 					},
