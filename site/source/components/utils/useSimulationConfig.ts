06e0df7b0388ff86d7e267ffe0f8c00b014870a6
Jérémy Rialland <wiinxt@gmail.com>
2022-07-28 16:36:14 +0200

    Hack to fix flickering

--- site/source/components/utils/useSimulationConfig.ts
+++ site/source/components/utils/useSimulationConfig.ts
@@ -1,7 +1,7 @@
 import { loadPreviousSimulation, setSimulationConfig } from '@/actions/actions'
 import { SimulationConfig } from '@/reducers/rootReducer'
 import { configSelector } from '@/selectors/simulationSelectors'
-import { useEffect } from 'react'
+import { useEffect, useLayoutEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 
 export default function useSimulationConfig(
@@ -13,13 +13,20 @@ export default function useSimulationConfig(
 ) {
 	const dispatch = useDispatch()
 
-	// Initialize redux store for SSR
+	// Initialize redux store in SSR mode
 	if (import.meta.env.SSR) {
 		dispatch(setSimulationConfig(config ?? {}, path))
 	}
 
 	const lastConfig = useSelector(configSelector)
-	useEffect(() => {
+
+	// useLayoutEffect like useEffect does nothing in SSR mode but triggers a warning,
+	// so we replace it with useEffect which does not trigger any warning
+	const useLayoutEffectWithoutWarnInSSR = import.meta.env.SSR
+		? useEffect
+		: useLayoutEffect
+
+	useLayoutEffectWithoutWarnInSSR(() => {
 		if (config && lastConfig !== config) {
 			dispatch(setSimulationConfig(config ?? {}, path))
 		}
