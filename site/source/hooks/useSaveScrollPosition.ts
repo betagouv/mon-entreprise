d40be9f4caf6150db24f65e8fb57d0d7142514d6
Benjamin Arias <benjamin.j.arias@gmail.com>
2022-09-02 11:41:23 +0200

    feat: add useSaveScrollPosition hook + CustomRouter

---
+++ site/source/hooks/useSaveScrollPosition.ts
@@ -0,0 +1,30 @@
+import { Action, Location } from 'history'
+import { useEffect, useRef } from 'react'
+
+const POP_ACTION_LABEL = 'POP'
+
+export const useSaveScrollPosition = ({
+	location,
+	action,
+	scrollY,
+}: {
+	location: Location
+	action: Action
+	scrollY: number
+}) => {
+	const previousPathname = useRef(location.pathname)
+
+	useEffect(() => {
+		const scrollPosition = sessionStorage.getItem(location.pathname)
+
+		if (scrollPosition && action === POP_ACTION_LABEL) {
+			window.scrollTo(0, parseInt(scrollPosition))
+		}
+	}, [location, action, scrollY])
+
+	useEffect(() => {
+		sessionStorage.setItem(previousPathname.current, String(scrollY))
+		previousPathname.current = location.pathname
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, [location.pathname])
+}
