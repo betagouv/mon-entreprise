18b6f73946eb71360a114e930c26ce6c56eb3392
Benjamin Arias <benjamin.j.arias@gmail.com>
2022-09-09 16:37:01 +0200

    chore: Repasse plugin a11y en warn

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -143,6 +143,14 @@ module.exports = {
 			files: ['site/**/*.{jsx,js,tsx,ts}'],
 			plugins: ['jsx-a11y'],
 			extends: ['plugin:jsx-a11y/strict'],
+			rules: {
+				'jsx-a11y/no-autofocus': 'warn',
+				'jsx-a11y/alt-text': 'warn',
+				'jsx-a11y/no-noninteractive-tabindex': 'warn',
+				'jsx-a11y/iframe-has-title': 'warn',
+				'jsx-a11y/click-events-have-key-events': 'warn',
+				'jsx-a11y/no-static-element-interactions': 'warn',
+			},
 		},
 	],
 }
