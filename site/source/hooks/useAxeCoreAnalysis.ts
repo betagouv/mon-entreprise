6eef2735106c8146a765479c9729e1e11303364d
Benjamin Arias <benjamin.j.arias@gmail.com>
2022-09-09 10:53:14 +0200

    fix: Retire logique inutile du hook + converti les errors du plugin a11y en warn

--- site/source/hooks/useAxeCoreAnalysis.ts
+++ site/source/hooks/useAxeCoreAnalysis.ts
@@ -1,33 +1,17 @@
-import { useEffect, useRef } from 'react'
+import React, { useEffect } from 'react'
+import ReactDOM from 'react-dom'
 
 export const useAxeCoreAnalysis = () => {
-	// On stocke les packages importés dans des refs lors du premier import
-	// pour économiser les ressources
-	const axeRef = useRef<{
-		default: (_React: any, _ReactDOM: any, _timeout: number) => Promise<void>
-	}>()
-	const ReactRef = useRef<any>()
-	const ReactDOMRef = useRef<any>()
-
 	// useEffect déclenché à chaque re-render du composant ou des enfants du composant
 	// où il est placé
 	useEffect(() => {
-		// Ne se lance qu'en mode "development" avec le script "start:axe-debugging"
 		if (import.meta.env.PROD || !import.meta.env.VITE_AXE_CORE_ENABLED) return
 
 		const triggerAxeCoreAnalysis = async () => {
-			// On importe les packages requis uniquement si nécessaire
-			if (!axeRef.current && !ReactRef.current && !ReactDOMRef.current) {
-				axeRef.current = await import('@axe-core/react')
-				ReactRef.current = await import('react')
-				ReactDOMRef.current = await import('react-dom')
-			}
+			// On importe axe-core/react uniquement si nécessaire
+			const axe = await import('@axe-core/react')
 
-			await axeRef?.current?.default(
-				ReactRef.current,
-				ReactDOMRef.current,
-				1000
-			)
+			await axe.default(React, ReactDOM, 1000)
 		}
 
 		void triggerAxeCoreAnalysis()
