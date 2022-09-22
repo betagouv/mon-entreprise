440be4d4ad64255ca3eb31dd9c69462fe8276989
Benjamin Arias <benjamin.j.arias@gmail.com>
2022-09-22 15:49:19 +0200

    feat: Ajoute aria-hidden sur deux svg inutiles aux lecteurs d'écran

--- site/source/design-system/popover/Popover.tsx
+++ site/source/design-system/popover/Popover.tsx
@@ -102,6 +102,7 @@ export default function Popover(
 													Fermer
 													<svg
 														role="img"
+														aria-hidden
 														viewBox="0 0 24 24"
 														fill="none"
 														xmlns="http://www.w3.org/2000/svg"
