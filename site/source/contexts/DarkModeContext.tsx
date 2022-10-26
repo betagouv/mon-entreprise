b45273f682de18293003f99559e87f84a4d01f7a
Félix <legrelle.f@gmail.com>
2022-10-26 11:05:36 +0200

    :egg: Add easter egg on darkmode
    Co-authored-by: Lucas Stoebner <stoebnerl@gmail.com>

--- site/source/contexts/DarkModeContext.tsx
+++ site/source/contexts/DarkModeContext.tsx
@@ -14,11 +14,19 @@ export const DarkModeContext = React.createContext<DarkModeContextType>({
 })
 
 export const DarkModeProvider: React.FC = ({ children }) => {
-	const [darkMode, setDarkMode] = React.useState<boolean>(
+	const [darkMode, _setDarkMode] = React.useState<boolean>(
 		import.meta.env.DEV && typeof window !== 'undefined' ?
 			(window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches) :
 			false
 	)
+
+	const setDarkMode = (darkMode: boolean) => {
+		_setDarkMode(darkMode)
+
+		// https://www.youtube.com/watch?v=Pr8ETbGz35Q
+		// eslint-disable-next-line no-console
+		console.log(darkMode ? 'Nuit' : 'Jour')
+	}
 
 	return (
 		<DarkModeContext.Provider value={{ darkMode, setDarkMode }}>
