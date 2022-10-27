2e427647adfbf4d03df2abca31493e05e0810dbf
Félix Legrelle <legrelle.f@gmail.com>
2022-10-27 13:01:27 +0200

    :fix: search loader icon

--- site/source/design-system/icons/SearchIcon.tsx
+++ site/source/design-system/icons/SearchIcon.tsx
@@ -1,25 +1,15 @@
 export function SearchIcon({ ...props }) {
 	return (
-		<svg
-			width={24}
-			height={24}
-			fill="none"
-			xmlns="http://www.w3.org/2000/svg"
-			role="img"
-			aria-hidden
-			{...props}
-		>
+		<svg width={24} height={24} fill="white" xmlns="http://www.w3.org/2000/svg" role="img" aria-hidden {...props}>
 			<path
 				fillRule="evenodd"
 				clipRule="evenodd"
 				d="M10.25 6a4.25 4.25 0 100 8.5 4.25 4.25 0 000-8.5zM4 10.25a6.25 6.25 0 1112.5 0 6.25 6.25 0 01-12.5 0z"
-				fill="#212529"
 			/>
 			<path
 				fillRule="evenodd"
 				clipRule="evenodd"
 				d="M18.293 19.707l-5.056-5.056 1.414-1.414 5.056 5.056a1 1 0 01-1.414 1.414z"
-				fill="#212529"
 			/>
 		</svg>
 	)
