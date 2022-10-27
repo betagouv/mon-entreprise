a56a758fcfd830eb93d69e692015fe82d302ca91
Félix Legrelle <legrelle.f@gmail.com>
2022-10-27 13:10:42 +0200

    :technologist: Add trivago
    Co-authored-by: Lucas Stoebner <stoebnerl@gmail.com>

--- site/source/components/Route404.tsx
+++ site/source/components/Route404.tsx
@@ -1,20 +1,14 @@
-import image from '@/images/road-sign.svg'
 import { Trans } from 'react-i18next'
+
+import PageHeader from '@/components/PageHeader'
 import { Button } from '@/design-system/buttons'
 import { Container } from '@/design-system/layout'
-import PageHeader from '@/components/PageHeader'
+import image from '@/images/road-sign.svg'
 
 export default function Route404() {
 	return (
 		<Container>
-			<PageHeader
-				titre={
-					<Trans i18nKey="404.message">
-						Cette page n'existe pas ou n'existe plus
-					</Trans>
-				}
-				picture={image}
-			>
+			<PageHeader titre={<Trans i18nKey="404.message">Cette page n'existe pas ou n'existe plus</Trans>} picture={image}>
 				<Button size="XL" role="link" to={'/'}>
 					<Trans i18nKey="404.action">Revenir en lieu sûr</Trans>
 				</Button>
