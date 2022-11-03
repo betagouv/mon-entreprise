26fddf7458f953c3e14c65163e73fb5c624d7e4e
Jérémy Rialland <wiinxt@gmail.com>
2022-11-03 12:11:04 +0100

    Add cypress tests for "Activité mixte"

--- site/cypress/integration/mon-entreprise/simulateur-ae.ts
+++ site/cypress/integration/mon-entreprise/simulateur-ae.ts
@@ -32,4 +32,46 @@ describe('Simulateur auto-entrepreneur', function () {
 			})
 		})
 	})
+
+	it('should show a "Activité mixte" button which that shows three entries', function () {
+		cy.get(inputSelector)
+			.its('length')
+			.then((length) => {
+				cy.contains('Activité mixte').click()
+
+				cy.get(inputSelector).should('have.length', length + 3)
+			})
+	})
+
+	it('should display the correct CA when changing the second "Activité mixte"', function () {
+		cy.get(inputSelector).eq(2).type('{selectall}5000')
+
+		cy.get(inputSelector).first().should('have.value', '10 000 €')
+	})
+
+	const parseActivitéMixtes = (elems: HTMLInputElement[]) =>
+		elems
+			.slice(1, 4)
+			.map((elem) => parseInt(elem.value.replace(/[\s,.€]/g, '')))
+
+	it('should display the correct results of "Activité mixte" when changing CA', function () {
+		cy.get(inputSelector).first().type('{selectall}5000')
+
+		cy.get<HTMLInputElement>(inputSelector).should(($elem) => {
+			const activitéMixtes = parseActivitéMixtes($elem.toArray())
+
+			expect(activitéMixtes).to.deep.equal([1250, 2500, 1250])
+		})
+	})
+
+	it('should display the correct results of "Activité mixte" when changing "Revenu net Après impôt"', function () {
+		cy.get(inputSelector).last().type('{selectall}2500')
+
+		cy.get<HTMLInputElement>(inputSelector).should(($elem) => {
+			const activitéMixtes = parseActivitéMixtes($elem.toArray())
+
+			expect(activitéMixtes[0]).to.be.equal(activitéMixtes[2])
+			expect(activitéMixtes[1]).to.be.equal(activitéMixtes[0] * 2)
+		})
+	})
 })
