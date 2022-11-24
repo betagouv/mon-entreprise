11173654e3d4ee5e3e8a1b31829086ae1b6e66ea
Jérémy Rialland <wiinxt@gmail.com>
2022-11-24 18:40:10 +0100

    Fix cypress error

--- site/cypress/integration/mon-entreprise/english/status.ts
+++ site/cypress/integration/mon-entreprise/english/status.ts
@@ -7,23 +7,18 @@ describe('Status guide', function () {
 	})
 
 	it('should allow to go back clicking on the previous answers', function () {
-		cy.get('button')
-			.contains(fr ? 'Seul' : 'Alone')
-			.click()
+		cy.contains('button', fr ? 'Seul' : 'Alone').click()
 		cy.contains(fr ? 'Un seul associé' : 'Only one partner').click()
 		cy.contains(fr ? 'Seul ou à plusieurs' : 'Number of partners')
 	})
 
 	it('should guide thought the SASU status', function () {
-		cy.get('button')
-			.contains(fr ? 'Seul' : 'Alone')
-			.click()
-		cy.get('button')
-			.contains(fr ? 'Société' : 'Limited liability company')
-			.click()
+		cy.contains('button', fr ? 'Seul' : 'Alone').click()
 
-		cy.get('button').contains('Assimilé').click()
-		cy.contains(fr ? 'Créer une SASU' : 'Create a SASU').click()
+		cy.contains('button', fr ? 'Société' : 'Limited liability company').click()
+
+		cy.contains('button', 'Assimilé').click()
+		cy.contains('a', fr ? 'Créer une SASU' : 'Create a SASU').click()
 		cy.url().should('match', /\/SASU$/)
 	})
 })
