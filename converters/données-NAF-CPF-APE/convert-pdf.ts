c45e72412bcc58e9b8edf5f8140987cf05cdeb13
Jérémy Rialland <wiinxt@gmail.com>
2022-12-08 18:32:37 +0100

    Refacto convert NAF CPF pdf to json

--- converters/données-NAF-CPF-APE/convert-pdf.ts
+++ converters/données-NAF-CPF-APE/convert-pdf.ts
@@ -1,31 +1,34 @@
 import { readFileSync, writeFileSync } from 'fs'
+import { join } from 'path'
 import { PdfData, VerbosityLevel } from 'pdfdataextract'
+import { fileURLToPath } from 'url'
 
-const fileData = readFileSync('ape-naf.pdf')
+import { customTags } from './custom-tags.js'
 
-interface Custom {
-	[code: string]: {
-		contenuCentral: string[]
-		contenuAnnexe: string[]
-		contenuExclu: string[]
-	}
-}
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
 
-const CUSTOM: Custom = {
-	'62.01Z': {
-		contenuCentral: ['développeur', 'informatique', 'web'],
-		contenuAnnexe: [],
-		contenuExclu: [],
-	},
-}
+const INPUT_PDF_PATH = join(
+	__dirname,
+	'./Nomenclatures_NAF_et_CPF_Reedition_2020.pdf'
+)
+const OUTPUT_JSON_PATH = join(__dirname, './output.json')
+const OUTPUT_ORIGINAL_TEXT_PATH = join(__dirname, './output.txt')
+
+const fileData = readFileSync(INPUT_PDF_PATH)
 
 void PdfData.extract(fileData, {
-	// pages: 6, // how many pages should be read at most
-	sort: true, // sort the text by text coordinates
-	verbosity: VerbosityLevel.ERRORS, // set the verbosity level for parsing
+	// how many pages should be read at most
+	// pages: 6,
+	// sort the text by text coordinates
+	sort: true,
+	// set the verbosity level for parsing
+	verbosity: VerbosityLevel.ERRORS,
 }).then(({ text }) => {
-	writeFileSync('./test.txt', text?.join('\n\n[[[NEW PAGE]]]\n\n') ?? '')
-	writeFileSync('./test.json', JSON.stringify(transformText(text), null, 2))
+	writeFileSync(
+		OUTPUT_ORIGINAL_TEXT_PATH,
+		text?.join('\n\n[[[NEW PAGE]]]\n\n') ?? ''
+	)
+	writeFileSync(OUTPUT_JSON_PATH, JSON.stringify(transformText(text), null, 2))
 })
 
 const transformText = (pages: PdfData['text']) => {
@@ -71,7 +74,7 @@ const transformText = (pages: PdfData['text']) => {
 			.split('\n')
 
 		for (let j = 0; j < lines.length; j++) {
-			// pageIndex < 5 &&
+			// eslint-disable-next-line no-console
 			console.log('###', '>' + lines[j] + '<')
 
 			const line = lines[j].trim()
@@ -79,6 +82,7 @@ const transformText = (pages: PdfData['text']) => {
 				/Nomenclature d’Activités Française NAF|Classification des Produits Française CPF/
 			)
 			if (line === '' || passLine) {
+				// eslint-disable-next-line no-console
 				console.log('[pass]')
 
 				continue
@@ -157,7 +161,6 @@ const transformText = (pages: PdfData['text']) => {
 				!item &&
 				previous.contenuType
 			) {
-				// @ts-ignore
 				previousElement[previous.contenuType].push(
 					normalize('# ' + line),
 					...(DEBUG_DATA === true ? [line] : [])
@@ -174,7 +177,6 @@ const transformText = (pages: PdfData['text']) => {
 						}`
 					)
 				}
-				// @ts-ignore
 				previousElement[previous.contenuType].push(
 					(item[1] === '•' ? '• ' : '') + normalize(item[2]),
 					...(DEBUG_DATA === true ? [line] : [])
@@ -208,7 +210,6 @@ const transformText = (pages: PdfData['text']) => {
 						`In page n°${pageIndex + 1}, no ${type} in ${previousElement.type}`
 					)
 				}
-				// @ts-ignore
 				previousElement[type].push(
 					normalize(match[2]),
 					...(DEBUG_DATA === true ? [line] : [])
@@ -217,6 +218,7 @@ const transformText = (pages: PdfData['text']) => {
 
 				return true
 			}
+
 			parseContenu('contenuCentral', contenuCentral, previousElement, pageIndex)
 			parseContenu('contenuAnnexe', contenuAnnexe, previousElement, pageIndex)
 			parseContenu('contenuExclu', contenuExclu, previousElement, pageIndex)
@@ -231,12 +233,14 @@ const transformText = (pages: PdfData['text']) => {
 				}
 
 				if (parentType && previous.index[parentType] === null) {
+					// eslint-disable-next-line no-console
 					console.error('\n\n\n' + line)
 
 					throw new Error(`no ${parentType} found`)
 				}
 				previous.index[type] = arr.length
 
+				// eslint-disable-next-line no-console
 				console.log(`[${type}]:`, match[1], '-', normalize(match[2]))
 
 				const code = match[1]
@@ -244,7 +248,7 @@ const transformText = (pages: PdfData['text']) => {
 					contenuCentral = [],
 					contenuAnnexe = [],
 					contenuExclu = [],
-				} = CUSTOM[code] ?? {}
+				} = customTags[code] ?? {}
 
 				arr.push({
 					type,
@@ -257,6 +261,7 @@ const transformText = (pages: PdfData['text']) => {
 					parent: (parentType && previous.index[parentType]) ?? undefined,
 				})
 			}
+
 			parseNomenclatures('section', null, section)
 			parseNomenclatures('division', 'section', division)
 			parseNomenclatures('groupe', 'division', groupe)
@@ -289,7 +294,7 @@ type SousClasse = CommonData<'sousClasse'>
 type Catégorie = CommonData<'catégorie'>
 type SousCatégorie = CommonData<'sousCatégorie'>
 
-type Data =
+export type Data =
 	| Section
 	| Division
 	| Groupe
