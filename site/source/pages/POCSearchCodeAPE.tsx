71edf1051bde29ad15251787f91c27e9561975db
Jérémy Rialland <wiinxt@gmail.com>
2022-12-15 16:04:50 +0100

    wip

--- site/source/pages/POCSearchCodeAPE.tsx
+++ site/source/pages/POCSearchCodeAPE.tsx
@@ -1,4 +1,9 @@
 import UFuzzy from '@leeoniya/ufuzzy'
+// import Fuse from 'fuse.js'
+// import { fuzzyFilter, fuzzyMatch } from 'fuzzbunny'
+// import fuzzysort from 'fuzzysort'
+// import { matchSorter, rankings } from 'match-sorter'
+// import MiniSearch from 'minisearch'
 import { Fragment, useEffect, useRef, useState } from 'react'
 import styled from 'styled-components'
 
@@ -15,51 +20,198 @@ import { Body } from '@/design-system/typography/paragraphs'
 import data from '../../../converters/données-code-APE/output.min.json'
 import { Output as Data } from '../../../converters/données-code-APE/reduce-json'
 
+const {
+	apeData,
+	sortByEffectif,
+	indexByCodeApe,
+	indexByCodeDepartement,
+	nbEtablissements2021,
+} = data as Data
+
+const normalize = (term: string) =>
+	term.normalize('NFKD').replace(/[\u0300-\u036f]/g, '')
+
+const filteredData = apeData.map((el, id) => ({ ...el, id }))
+// .map((obj) => ({
+// 	...obj,
+// 	title: obj.title,
+// 	contenuCentral: fuzzysort.prepare(obj.contenuCentral.join('\n')),
+// 	contenuAnnexe: fuzzysort.prepare(obj.contenuAnnexe.join('\n')),
+// 	contenuExclu: fuzzysort.prepare(obj.contenuExclu.join('\n')),
+// 	titleNorm: fuzzysort.prepare(normalize(obj.title)),
+// 	contenuCentralNorm: fuzzysort.prepare(
+// 		obj.contenuCentral.map((s) => normalize(s)).join('\n')
+// 	),
+// 	contenuAnnexeNorm: fuzzysort.prepare(
+// 		obj.contenuAnnexe.map((s) => normalize(s)).join('\n')
+// 	),
+// 	contenuExcluNorm: fuzzysort.prepare(
+// 		obj.contenuExclu.map((s) => normalize(s)).join('\n')
+// 	),
+// }))
+
+// console.log(filteredData)
+
+// const fuse = new Fuse(apeData, {
+// 	keys: [
+// 		// 'title',
+// 		'contenuCentral',
+// 		'contenuAnnexe',
+// 		{
+// 			name: 'contenuExclu',
+// 			weight: 0.75,
+// 		},
+// 		// 'codeApe',
+// 	],
+// 	includeScore: true,
+// 	includeMatches: true,
+// 	findAllMatches: true,
+// 	ignoreLocation: true,
+// 	ignoreFieldNorm: true,
+// 	minMatchCharLength: 3,
+// 	threshold: 0.25,
+// })
+
 const fuzzy = new UFuzzy({
 	intraIns: 2,
 })
 
-const filteredData = (data as Data).apeData
+interface Xxxx {
+	original: string[]
+	latinized: string[]
+}
 
-const { specificList, genericList, excludedList } = filteredData.reduce(
-	(prev, { contenuCentral, contenuAnnexe, contenuExclu, title, codeApe }) => ({
-		specificList: [
-			...(prev.specificList ?? []),
-			[title, ...contenuCentral, codeApe].join(', '),
-		],
-		genericList: [...(prev.genericList ?? []), contenuAnnexe.join(', ')],
-		excludedList: [...(prev.excludedList ?? []), contenuExclu.join(', ')],
-	}),
-	{} as {
-		genericList: string[]
-		specificList: string[]
-		excludedList: string[]
-	}
+const { specificList, genericList, excludedList } = apeData.reduce(
+	(prev, { contenuCentral, contenuAnnexe, contenuExclu, title, codeApe }) => {
+		const specificText = [title, ...contenuCentral, codeApe].join('\t')
+		const genericText = contenuAnnexe.join('\t')
+		const excludedText = contenuExclu.join('\t')
+
+		return {
+			specificList: {
+				original: [...(prev.specificList?.original ?? []), specificText],
+				latinized: [],
+			},
+			genericList: {
+				original: [...(prev.genericList?.original ?? []), genericText],
+				latinized: [],
+			},
+			excludedList: {
+				original: [...(prev.excludedList?.original ?? []), excludedText],
+				latinized: [],
+			},
+		}
+	},
+	{} as { genericList: Xxxx; specificList: Xxxx; excludedList: Xxxx }
 )
 
-const latinizedSpecificList = UFuzzy.latinize(specificList)
-const latinizedGenericList = UFuzzy.latinize(genericList)
-const latinizedExcludedList = UFuzzy.latinize(excludedList)
+specificList.latinized = UFuzzy.latinize(specificList.original)
+genericList.latinized = UFuzzy.latinize(genericList.original)
+excludedList.latinized = UFuzzy.latinize(excludedList.original)
+
+const nbEtablissementParDepartement = (department: string, code: string) => {
+	const index =
+		indexByCodeApe[code] &&
+		indexByCodeDepartement[department]?.find((i) =>
+			indexByCodeApe[code].includes(i)
+		)
+
+	return typeof index === 'number' ? nbEtablissements2021[index] : -Infinity
+}
+
+// const stopWords = new Set([
+// 	'a',
+// 	'à',
+// 	'cf',
+// 	'de',
+// 	'des',
+// 	'elle',
+// 	'elles',
+// 	'en',
+// 	'et',
+// 	'etc',
+// 	'il',
+// 	'ils',
+// 	'je',
+// 	'l',
+// 	'la',
+// 	'le',
+// 	'les',
+// 	'nous',
+// 	'on',
+// 	'ont',
+// 	'ou',
+// 	'son',
+// 	'sur',
+// 	'tu',
+// 	'vous',
+// ])
+
+// const miniSearch = new MiniSearch({
+// 	fields: [
+// 		'title',
+// 		'contenuCentral',
+// 		'contenuAnnexe',
+// 		'contenuExclu',
+// 		'codeApe',
+// 	],
+// 	searchOptions: {
+// 		fuzzy: 0.3,
+// 		boost: {
+// 			// codeApe: 1.5,
+// 			// title: 1.2,
+// 			// contenuCentral: 1,
+// 			// contenuAnnexe: 0.9,
+// 			contenuExclu: 0.8,
+// 		},
+// 		prefix: true,
+// 		// weights: 0.5,
+// 	},
+
+// 	processTerm: (term) => {
+// 		const lower = term.toLowerCase()
+// 		const normalize = term.normalize('NFKD').replace(/[^\w]/g, '')
+
+// 		return stopWords.has(lower)
+// 			? null
+// 			: term !== normalize
+// 			? [term, normalize]
+// 			: term
+// 	},
+// })
+
+// miniSearch.addAll(filteredData)
+
+interface TestProps {
+	debug?: string
+	title: string
+	codeApe: string
+	selected: string
+	contenuCentral: string[]
+	contenuAnnexe: string[]
+	contenuExclu: string[]
+}
 
 const Test = ({
+	debug,
 	title,
 	codeApe,
 	selected,
 	contenuCentral,
 	contenuAnnexe,
 	contenuExclu,
-}) => {
-	const [open, setOpen] = useState(false)
+}: TestProps): JSX.Element => {
+	const [open, setOpen] = useState(!false)
 
 	return (
 		<Grid container style={{ alignItems: 'center' }}>
 			<Grid item xs={12} md={10}>
 				<H3 style={{ marginTop: 0, marginBottom: '.5rem' }}>{title}</H3>
 				<p style={{ fontWeight: 'bold', marginTop: 0 }}>
-					Code : <span>{codeApe}</span>
-					<br />
+					Code : <span>{codeApe}</span> - <br />
 					[selected : <span>{selected}</span>]
 				</p>
+				{debug && <pre>{debug}</pre>}
 			</Grid>
 
 			<Grid item xs={12} md={2}>
@@ -125,18 +277,140 @@ const Test = ({
 }
 
 export default function POCSearchCodeAPE() {
-	const [value, setValue] = useState('')
+	const [job, setJob] = useState('')
+	const [department, setDepartment] = useState('')
 	const [selected, setSelected] = useState('')
-	// const [list, setList] = useState<[number, ...string[]][]>([])
-	const [list, setList] = useState<Data['apeData']>([])
+	const [list, setList] = useState<(Data['apeData'][0] & { debug?: string })[]>(
+		[]
+	)
 
 	const lastIdxs = useRef<Record<string, UFuzzy.HaystackIdxs>>({})
-	const prevValue = useRef<string>(value)
+	const prevValue = useRef<string>(job)
 
 	useEffect(() => {
-		if (value.length) {
+		if (job.length) {
 			console.time('effect')
-			const latinizedValue = UFuzzy.latinize([value])[0]
+			// const latinizedValue = UFuzzy.latinize([value])[0]
+
+			// const results = miniSearch.search(value, {
+			// 	boostDocument(documentId, term) {
+			// 		const codeApe = filteredData[documentId].codeApe.replace(/\./g, '')
+			// 		const maxEffectif = nbEtablissements2021[sortByEffectif[0]]
+			// 		const xxx = (indexByCodeApe[codeApe] ?? [])
+			// 			.filter((index) => indexByCodeDepartement['44'].includes(index))
+			// 			.map((index) => nbEtablissements2021[index])
+
+			// 		if (xxx.length > 1) {
+			// 			console.log(xxx)
+			// 		}
+
+			// 		const [yyy = 0] = xxx
+
+			// 		console.log(codeApe, [
+			// 			maxEffectif,
+			// 			Object.keys(indexByCodeApe).sort(),
+			// 			indexByCodeApe[codeApe],
+			// 			indexByCodeDepartement['44'],
+			// 			yyy,
+			// 		])
+
+			// 		const score = yyy / maxEffectif
+			// 		filteredData[documentId].title =
+			// 			filteredData[documentId].title.replace(/\s*[0-9.]+$/, '') +
+			// 			' ' +
+			// 			yyy.toString()
+
+			// 		// return data.sortByEffectif[0]
+			// 		return score
+			// 	},
+			// })
+
+			/**
+			 * score
+			 * debut de mot
+			 * faute possible
+			 * accents
+			 *
+			 */
+
+			// const results = matchSorter(apeData, job, {
+			// 	keys: [
+			// 		'codeApe',
+			// 		'title',
+			// 		'contenuCentral.*',
+			// 		'contenuAnnexe.*',
+			// 		'contenuExclu.*',
+			// 	],
+			// 	threshold: rankings.WORD_STARTS_WITH,
+			// })
+			// console.log(results)
+
+			// const resultsBis = fuzzysort.go(job, filteredData, {
+			// 	keys: [
+			// 		'codeApe',
+			// 		'title',
+			// 		'titleNorm',
+			// 		'contenuCentral',
+			// 		'contenuCentralNorm',
+			// 		'contenuAnnexe',
+			// 		'contenuAnnexeNorm',
+			// 		'contenuExclu',
+			// 		'contenuExcluNorm',
+			// 	],
+			// 	limit: 50,
+
+			// 	// Create a custom combined score to sort by. -100 to the desc score makes it a worse match
+			// 	scoreFn: (a) => {
+			// 		const [
+			// 			scoreCodeApe,
+			// 			scoreTitle,
+			// 			scoreTitleNorm,
+			// 			scoreContenuCentral,
+			// 			scoreContenuCentralNorm,
+			// 			scoreContenuAnnexe,
+			// 			scoreContenuAnnexeNorm,
+			// 			scoreContenuExclu,
+			// 			scoreContenuExcluNorm,
+			// 		] = a
+
+			// 		const score = Math.max(
+			// 			...[
+			// 				scoreCodeApe,
+			// 				scoreTitle,
+			// 				scoreTitleNorm,
+			// 				scoreContenuCentral,
+			// 				scoreContenuCentralNorm,
+			// 				scoreContenuAnnexe,
+			// 				scoreContenuAnnexeNorm,
+			// 			]
+			// 				.map((obj) => obj?.score ?? null)
+			// 				.filter((el) => el !== null)
+			// 		)
+
+			// 		const lowerScore =
+			// 			Math.max(
+			// 				...[scoreContenuExclu, scoreContenuExcluNorm]
+			// 					.map((obj) => obj?.score ?? null)
+			// 					.filter((el) => el !== null)
+			// 			) * 2
+
+			// 		// console.log(score, lowerScore)
+
+			// 		return Math.max(lowerScore, score)
+			// 	},
+			// 	// 	Math.max(a[0] ? a[0].score : -1000, a[1] ? a[1].score - 100 : -1000),
+			// })
+
+			// console.log(
+			// 	// results,
+			// 	// miniSearch.autoSuggest(value, {
+			// 	// 	// combineWith: 'AND',
+			// 	// 	fuzzy: 0.3,
+			// 	// 	prefix: true,
+			// 	// 	//  (term, i, terms) => i === terms.length - 1,
+			// 	// }),
+			// 	resultsBis
+			// )
 
 			const search = (
 				list: string[],
@@ -150,63 +424,248 @@ export default function POCSearchCodeAPE() {
 				return { idxs, info, order }
 			}
 
-			const specific = search(latinizedSpecificList, latinizedValue)
-			const generic = search(latinizedGenericList, latinizedValue)
-			const excluded = search(latinizedExcludedList, latinizedValue)
+			const latinizedValue = UFuzzy.latinize([job])[0]
 
-			const specificResults = specific.order.map(
-				(i) => filteredData[specific.info.idx[i]]
-			)
-			const genericResults = generic.order.map(
-				(i) => filteredData[generic.info.idx[i]]
-			)
-			const excludedResults = excluded.order.map(
-				(i) => filteredData[excluded.info.idx[i]]
-			)
+			const specific = search(specificList.original, job)
+			const generic = search(genericList.original, job)
+			const excluded = search(excludedList.original, job)
+			const specificLat = search(specificList.latinized, latinizedValue)
+			const genericLat = search(genericList.latinized, latinizedValue)
+			const excludedLat = search(excludedList.latinized, latinizedValue)
 
-			const results = [
-				...new Set([...specificResults, ...genericResults]),
-			].filter((result) => !excludedResults.includes(result))
+			// const mergeArr = <T,>(arr1: T[], arr2: T[]) => {
+			// 	const len = Math.min(arr2.length, arr1.length)
+			// 	const ret = []
+
+			// 	for (let index = 0; index < len; index++) {
+			// 		ret.push(arr1[index], arr2[index])
+			// 	}
+			// 	if (arr2.length !== arr1.length) {
+			// 		ret.push(
+			// 			...(arr2.length > arr1.length
+			// 				? arr2.slice(arr1.length - arr2.length)
+			// 				: arr1.slice(arr2.length - arr1.length))
+			// 		)
+			// 	}
+
+			// 	return ret
+			// }
+
+			// const specificResults = []
+			// 	.map(
+			// 	(i, j) =>
+			// 		apeData[
+			// 			j < specific.order.length
+			// 				? specific.info.idx[i]
+			// 				: specificLat.info.idx[i]
+			// 		]
+			// )
+			// const genericResults = []
+			// .map(
+			// 	(i, j) =>
+			// 		apeData[
+			// 			j < generic.order.length
+			// 				? generic.info.idx[i]
+			// 				: genericLat.info.idx[i]
+			// 		]
+			// )
+			const excludedResults = [
+				...excludedLat.order.map((i) => apeData[excludedLat.info.idx[i]]),
+				...excluded.order.map((i) => apeData[excluded.info.idx[i]]),
+			]
+			// .map(
+			// 	(i, j) =>
+			// 		apeData[
+			// 			j < excluded.order.length
+			// 				? excluded.info.idx[i]
+			// 				: excludedLat.info.idx[i]
+			// 		]
+			// )
+
+			/**
+			 * Calcul le score final avec 3/4 * le score de la recherche + 1/4 * le score en fonction
+			 * du nombre de création d'entreprise par code APE et departement en 2021
+			 */
+			const computeScore = (scoreFuzzy: number, scoreNbEtablissement: number) =>
+				(3 / 4) * scoreFuzzy + (1 / 4) * scoreNbEtablissement
+
+			const resultsUFuzzy = [
+				...new Set([
+					...specificLat.order.map((i) => apeData[specificLat.info.idx[i]]),
+					...specific.order.map((i) => apeData[specific.info.idx[i]]),
+					...genericLat.order.map((i) => apeData[genericLat.info.idx[i]]),
+					...generic.order.map((i) => apeData[generic.info.idx[i]]),
+				]),
+			]
+				.filter((result) => !excludedResults.includes(result))
+				.map((item, index, arr) => ({
+					item,
+					scoreFuzzy: index / arr.length,
+					nbEtablissement: department
+						? nbEtablissementParDepartement(
+								department,
+								item.codeApe.replace('.', '')
+						  )
+						: 0,
+				}))
+				.sort(({ nbEtablissement: a }, { nbEtablissement: b }) => b - a)
+				.map(({ item, scoreFuzzy, nbEtablissement }, index, arr) => ({
+					item,
+					score: computeScore(scoreFuzzy, index / arr.length),
+					debug: JSON.stringify(
+						{
+							score: computeScore(scoreFuzzy, index / arr.length),
+							scoreFuzzy,
+							scoreNbEtablissement: index / arr.length,
+							nbEtablissement,
+						},
+						null,
+						2
+					),
+				}))
+
+				.sort(({ score: a }, { score: b }) => {
+					// a.codeApe
+					// b.codeApe
+					// nbEtablissements2021[
+					// ]
+
+					// sortByEffectif[
+					// ]
+
+					// indexByCodeApe[a.codeApe]
+					// const ppp =
+					// 	indexByCodeApe[a.codeApe.replace('.', '')] &&
+					// 	indexByCodeDepartement[department].filter((i) =>
+					// 		indexByCodeApe[a.codeApe.replace('.', '')].includes(i)
+					// 	)?.[0]
+					// const fff =
+					// 	indexByCodeApe[b.codeApe.replace('.', '')] &&
+					// 	indexByCodeDepartement[department].filter((i) =>
+					// 		indexByCodeApe[b.codeApe.replace('.', '')].includes(i)
+					// 	)?.[0]
+
+					// resultsUFuzzy
+
+					// if (typeof ppp !== 'undefined') {
+					// }
+					// console.log('=>', a, b)
+
+					return a - b
+
+					// indexByCodeApe[b.codeApe]
+				})
+			// sortByEffectif,
+			// indexByCodeApe,
+			// indexByCodeDepartement,
+			// nbEtablissements2021,
+
+			console.log('resultsUFuzzy', resultsUFuzzy, {
+				specificList,
+				genericList,
+				excludedList,
+				specific,
+				generic,
+				excluded,
+				specificLat,
+				genericLat,
+				excludedLat,
+				specificResults: [
+					...specificLat.order.map((i) => apeData[specificLat.info.idx[i]]),
+					...specific.order.map((i) => apeData[specific.info.idx[i]]),
+				],
+				genericResults: [
+					...genericLat.order.map((i) => apeData[genericLat.info.idx[i]]),
+					...generic.order.map((i) => apeData[generic.info.idx[i]]),
+				],
+				excludedResults,
+			})
+
+			// const fuseResults = fuse.search(job)
 
 			console.timeEnd('effect')
 
-			setList(results)
+			setList(
+				resultsUFuzzy.map(({ item, debug }) => ({
+					...item,
+					debug,
+					// score: `${score}.
+					// ${
+					// 	''
+					// 	// score !== undefined && nbEtablissements2021[score]
+					// }`,
+				}))
+				// .map(({ id, score }) => {
+				// 	// filteredData[id].title = filteredData[id].title + ' ' + score
+
+				// 	return apeData[id]
+				// })
+				// resultsBis.map(({ obj }) => apeData[obj.id])
+				// results
+				// fuseResults.map((fuseResult) => fuseResult.item)
+			)
 		} else {
 			lastIdxs.current = {}
 			setList([])
 		}
-		prevValue.current = value
-	}, [value])
+		prevValue.current = job
+	}, [department, job])
 
 	console.time('render')
 	const test = list
 		.slice(0, 25)
-		.map(({ codeApe, title, contenuCentral, contenuAnnexe, contenuExclu }) => {
-			return (
-				<StyledRadioSkeleton value={codeApe} key={codeApe} visibleRadioAs="div">
-					<Test
-						{...{
-							title,
-							codeApe,
-							selected,
-							contenuCentral,
-							contenuAnnexe,
-							contenuExclu,
-						}}
-					/>
-				</StyledRadioSkeleton>
-			)
-		})
+		.map(
+			({
+				codeApe,
+				title,
+				contenuCentral,
+				contenuAnnexe,
+				contenuExclu,
+				debug,
+			}) => {
+				return (
+					<StyledRadioSkeleton
+						value={codeApe}
+						key={codeApe}
+						visibleRadioAs="div"
+					>
+						<Test
+							{...{
+								department,
+								debug,
+								title,
+								codeApe,
+								selected,
+								contenuCentral,
+								contenuAnnexe,
+								contenuExclu,
+							}}
+						/>
+					</StyledRadioSkeleton>
+				)
+			}
+		)
 
 	const ret = (
 		<Body as="div">
-			<TextField value={value} onChange={setValue} />
+			<Grid container>
+				<Grid item xs={4}>
+					<TextField
+						value={department}
+						onChange={setDepartment}
+						label="Département"
+					/>
+				</Grid>
+				<Grid item xs={8}>
+					<TextField value={job} onChange={setJob} label="Votre métier" />
+				</Grid>
+			</Grid>
 
 			<StyledRadioCardGroup value={selected} onChange={setSelected}>
 				<Grid container>{test}</Grid>
 			</StyledRadioCardGroup>
 
-			<Ul>
+			{/* <Ul>
 				{list
 					.slice(0, 5)
 					.map(
@@ -225,7 +684,7 @@ export default function POCSearchCodeAPE() {
 										key="impot.gouv.fr"
 										hasChildItems={false}
 									></Item>
-								</Accordion> */}
+								</Accordion> * /}
 
 									<Grid item xs={12} md={10}>
 										<H3 style={{ marginTop: 0, marginBottom: '.5rem' }}>
@@ -235,14 +694,14 @@ export default function POCSearchCodeAPE() {
 											Code : <span>{codeApe}</span>
 										</p>
 
-										{/* <pre>{JSON.stringify(filteredData[index], null, 2)}</pre> */}
+										{/* <pre>{JSON.stringify(filteredData[index], null, 2)}</pre> * /}
 										{/* <pre>
 									{JSON.stringify(
 										{ code, contenuCentral, contenuAnnexe, contenuExclu },
 										null,
 										2
 									)}
-								</pre> */}
+								</pre> * /}
 									</Grid>
 									<Grid item xs={12} md={2}>
 										En savoir plus
@@ -299,14 +758,14 @@ export default function POCSearchCodeAPE() {
 										) : (
 											<span style={{ color: 'red' }}>{el}</span>
 										)
-									})} */}
+									})} * /}
 										</p>
 									</Grid>
 								</StyledCardContainer>
 							)
 						}
 					)}
-			</Ul>
+			</Ul> */}
 		</Body>
 	)
 	console.timeEnd('render')
@@ -320,9 +779,9 @@ const StyledRadioCardGroup = styled(RadioCardGroup)`
 	flex-wrap: wrap;
 `
 
-const StyledCardContainer = styled(CardContainer)`
-	height: initial;
-	margin-bottom: 1rem;
-	flex-direction: row;
-	flex-wrap: wrap;
-`
+// const StyledCardContainer = styled(CardContainer)`
+// 	height: initial;
+// 	margin-bottom: 1rem;
+// 	flex-direction: row;
+// 	flex-wrap: wrap;
+// `
