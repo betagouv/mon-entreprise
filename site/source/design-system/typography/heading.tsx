cb8c88eaf49fd634ec58fd96739cbe72eea30482
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-01-11 13:21:37 +0100

    fix: Corrige la transition de background sur les titres

--- site/source/design-system/typography/heading.tsx
+++ site/source/design-system/typography/heading.tsx
@@ -8,7 +8,7 @@ const baseHeading = css`
 		theme.darkMode
 			? theme.colors.extended.grey[100]
 			: theme.colors.bases.primary[700]};
-	background-color: inherit;
+	background-color: transparent;
 `
 
 export const HeadingUnderline = css`
