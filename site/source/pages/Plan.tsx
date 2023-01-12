9a9252f90ffc9b64d2dab0c8bc9a562e19fc6b3f
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-01-12 19:13:16 +0100

    feat: Améliore plan de site

--- site/source/pages/Plan.tsx
+++ site/source/pages/Plan.tsx
@@ -36,76 +36,76 @@ export default function Plan() {
 					</H3>
 				</Li>
 				<Li>
-					<h2>
+					<H3 as="h2">
 						<HeaderLink to={absoluteSitePaths.créer.index}>
 							<Trans>Créer une entreprise</Trans>
 						</HeaderLink>
-					</h2>
+					</H3>
 
 					<Ul>
 						<Li>
-							<h3>
+							<H3 as="h2">
 								<Link to={absoluteSitePaths.créer.après}>
 									<Trans>Après la création</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 						<Li>
-							<h3>
+							<H3 as="h2">
 								<Link to={absoluteSitePaths.créer.guideStatut.index}>
 									<Trans>Choix du statut juridique</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 					</Ul>
 				</Li>
 				<Li>
-					<h2>
+					<H3 as="h2">
 						<HeaderLink to={absoluteSitePaths.gérer.index}>
 							<Trans>Gérer mon activité</Trans>
 						</HeaderLink>
-					</h2>
+					</H3>
 
 					<Ul>
 						<Li>
-							<h3>
+							<H3>
 								<Link to={absoluteSitePaths.gérer.embaucher}>
 									<Trans>Embaucher</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 						<Li>
-							<h3>
+							<H3>
 								<Link to={absoluteSitePaths.gérer.sécuritéSociale}>
 									<Trans>Protection sociale</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 						<Li>
-							<h3>
+							<H3>
 								<Link to={absoluteSitePaths.gérer.déclarationIndépendant.index}>
 									<Trans>
 										Assistant à la détermination des charges sociales
 										déductibles
 									</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 						<Li>
-							<h3>
+							<H3>
 								<Link to={absoluteSitePaths.gérer.formulaireMobilité}>
 									<Trans>Demande de mobilité internationale</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 					</Ul>
 				</Li>
 				<Li>
-					<h2>
+					<H3 as="h2">
 						<HeaderLink to={absoluteSitePaths.simulateurs.index}>
 							<Trans>Simulateurs disponibles</Trans>
 						</HeaderLink>
-					</h2>
+					</H3>
 
 					<Ul>
 						{Object.entries(absoluteSitePaths.simulateurs)
@@ -118,7 +118,7 @@ export default function Plan() {
 							.map(([simulateurKey, simulateurPath]: [string, string]) => {
 								return (
 									<Li key={`list-item-${simulateurKey}`}>
-										<h3>
+										<H3>
 											<Link to={simulateurPath}>
 												{
 													metadata[
@@ -127,19 +127,19 @@ export default function Plan() {
 													].title
 												}
 											</Link>
-										</h3>
+										</H3>
 									</Li>
 								)
 							})}
 						<Li key="list-item-comparaison">
-							<h3>
+							<H3>
 								<Link to={absoluteSitePaths.simulateurs.comparaison}>
 									<Trans>Assistant au choix du statut juridique</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 						<Li key="list-item-economie-collaborative">
-							<h3>
+							<H3>
 								<Link
 									to={absoluteSitePaths.simulateurs.économieCollaborative.index}
 								>
@@ -148,86 +148,86 @@ export default function Plan() {
 										ligne
 									</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 					</Ul>
 				</Li>
 				<Li>
-					<h2>
+					<H3 as="h2">
 						<Link to={absoluteSitePaths.nouveautés}>
 							<Trans>Nouveautés</Trans>
 						</Link>
-					</h2>
+					</H3>
 				</Li>
 
 				<Li>
-					<h2>
+					<H3 as="h2">
 						<Link to={absoluteSitePaths.budget}>
 							<Trans>Budget</Trans>
 						</Link>
-					</h2>
+					</H3>
 				</Li>
 				<Li>
-					<h2>
+					<H3 as="h2">
 						<Link to={absoluteSitePaths.accessibilité}>
 							<Trans>Accessibilité</Trans>
 						</Link>
-					</h2>
+					</H3>
 				</Li>
 				<Li>
-					<h2>
+					<H3 as="h2">
 						<Link to={absoluteSitePaths.stats}>
 							<Trans>Statistiques</Trans>
 						</Link>
-					</h2>
+					</H3>
 				</Li>
 				<Li>
-					<h2>
+					<H3 as="h2">
 						<HeaderLink to={absoluteSitePaths.développeur.index}>
 							<Trans>Outils pour les développeurs</Trans>
 						</HeaderLink>
-					</h2>
+					</H3>
 
 					<Ul>
 						<Li key="list-item-développeur-api">
-							<h3>
+							<H3>
 								<Link to={absoluteSitePaths.développeur.api}>
 									<Trans>API REST de simulation</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 						<Li key="list-item-développeur-iframe">
-							<h3>
+							<H3>
 								<Link to={absoluteSitePaths.développeur.iframe}>
 									<Trans>Intégrer le module Web</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 						<Li key="list-item-développeur-library">
-							<h3>
+							<H3>
 								<Link to={absoluteSitePaths.développeur.library}>
 									<Trans>
 										Utiliser les calculs des simulateurs dans votre application
 									</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 						<Li key="list-item-développeur-spreadsheet">
-							<h3>
+							<H3>
 								<Link to={absoluteSitePaths.développeur.spreadsheet}>
 									<Trans>Utiliser avec un tableur</Trans>
 								</Link>
-							</h3>
+							</H3>
 						</Li>
 					</Ul>
 				</Li>
 
 				<Li>
-					<h2>
+					<H3 as="h2">
 						<Link to={absoluteSitePaths.documentation.index}>
 							<Trans>Documentation</Trans>
 						</Link>
-					</h2>
+					</H3>
 				</Li>
 			</StyledUl>
 		</>
