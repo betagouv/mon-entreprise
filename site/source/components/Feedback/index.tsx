ae5dcb9fc6e167f37c82e9540c1bd77f6a971f91
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-01-16 11:33:19 +0100

    🐛 Désactive les évènements souris sur la croix svg du bouton fermer dans le module feedback (#2460)
    fix: Disable pointer events on svg

--- site/source/components/Feedback/index.tsx
+++ site/source/components/Feedback/index.tsx
@@ -104,6 +104,7 @@ const FeedbackButton = ({ isEmbedded }: { isEmbedded?: boolean }) => {
 							viewBox="0 0 24 24"
 							fill="none"
 							xmlns="http://www.w3.org/2000/svg"
+							style={{ pointerEvents: 'none' }}
 						>
 							<path
 								fillRule="evenodd"
