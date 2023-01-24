c014cb3805d5f63668f5368caaa63d7e72932645
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-01-24 16:35:19 +0100

    feat: Affiche la documentation dans une modale

--- site/source/pages/Simulateurs/ComparateurStatuts/components/Comparateur.tsx
+++ site/source/pages/Simulateurs/ComparateurStatuts/components/Comparateur.tsx
@@ -1,5 +1,6 @@
 import Engine from 'publicodes'
 import { useTranslation } from 'react-i18next'
+import { Route, Routes, useNavigate } from 'react-router-dom'
 
 import { DottedName } from '@/../../modele-social'
 import PeriodSwitch from '@/components/PeriodSwitch'
@@ -8,6 +9,8 @@ import Simulation, {
 	SimulationGoals,
 } from '@/components/Simulation'
 import { Spacing } from '@/design-system/layout'
+import Popover from '@/design-system/popover/Popover'
+import Documentation from '@/pages/Documentation'
 
 import Détails from './Détails'
 import Résultats from './Résultats'
@@ -18,6 +21,8 @@ type ComparateurProps = {
 
 function Comparateur({ engines }: ComparateurProps) {
 	const { t } = useTranslation()
+	const navigate = useNavigate()
+	const [assimiléEngine, indépendantEngine, autoEntrepreneurEngine] = engines
 
 	return (
 		<>
@@ -43,6 +48,65 @@ function Comparateur({ engines }: ComparateurProps) {
 			<Spacing md />
 			<Résultats engines={engines} />
 			<Détails engines={engines} />
+			<Routes>
+				<Route
+					path="SASU/*"
+					element={
+						<div>
+							<Popover
+								isOpen
+								isDismissable
+								onClose={() => {
+									navigate(-1)
+								}}
+							>
+								<Documentation
+									engine={assimiléEngine}
+									documentationPath="/simulateurs/comparaison-régimes-sociaux/SASU"
+								/>
+							</Popover>
+						</div>
+					}
+				/>
+				<Route
+					path="EI/*"
+					element={
+						<div>
+							<Popover
+								isOpen
+								isDismissable
+								onClose={() => {
+									navigate(-1)
+								}}
+							>
+								<Documentation
+									engine={indépendantEngine}
+									documentationPath="/simulateurs/comparaison-régimes-sociaux/EI"
+								/>
+							</Popover>
+						</div>
+					}
+				/>
+				<Route
+					path="auto-entrepreneur/*"
+					element={
+						<div>
+							<Popover
+								isOpen
+								isDismissable
+								onClose={() => {
+									navigate(-1)
+								}}
+							>
+								<Documentation
+									engine={autoEntrepreneurEngine}
+									documentationPath="/simulateurs/comparaison-régimes-sociaux/auto-entrepreneur"
+								/>
+							</Popover>
+						</div>
+					}
+				/>
+			</Routes>
 		</>
 	)
 }
--- site/source/pages/Simulateurs/ComparateurStatuts/index.tsx
+++ site/source/pages/Simulateurs/ComparateurStatuts/index.tsx
@@ -2,7 +2,6 @@ import { DottedName } from 'modele-social'
 import Engine from 'publicodes'
 import { useMemo } from 'react'
 import { Trans } from 'react-i18next'
-import { Route, Routes } from 'react-router-dom'
 
 import { useEngine, useRawSituation } from '@/components/utils/EngineContext'
 import useSimulationConfig from '@/components/utils/useSimulationConfig'
@@ -10,7 +9,6 @@ import { Strong } from '@/design-system/typography'
 import { Intro } from '@/design-system/typography/paragraphs'
 import { useSitePaths } from '@/sitePaths'
 
-import Documentation from '../../Documentation'
 import { configComparateurStatuts } from '../configs/comparateurStatuts'
 import Comparateur from './components/Comparateur'
 import {
@@ -75,57 +73,20 @@ function ComparateurStatutsUI() {
 	] as [Engine<DottedName>, Engine<DottedName>, Engine<DottedName>]
 
 	return (
-		<Routes>
-			<Route
-				path="SASU/*"
-				element={
-					<>
-						<Documentation
-							engine={assimiléEngine}
-							documentationPath="/simulateurs/comparaison-régimes-sociaux/SASU"
-						/>
-					</>
-				}
-			/>
-			<Route
-				path="EI/*"
-				element={
-					<Documentation
-						engine={indépendantEngine}
-						documentationPath="/simulateurs/comparaison-régimes-sociaux/EI"
-					/>
-				}
-			/>
-			<Route
-				path="auto-entrepreneur/*"
-				element={
-					<Documentation
-						engine={autoEntrepreneurEngine}
-						documentationPath="/simulateurs/comparaison-régimes-sociaux/auto-entrepreneur"
-					/>
-				}
-			/>
-			<Route
-				path=""
-				element={
-					<>
-						<Intro>
-							<Trans i18nKey="comparaisonRégimes.description">
-								Lorsque vous créez votre société, le choix du statut juridique
-								va{' '}
-								<Strong>
-									déterminer à quel régime social le dirigeant est affilié
-								</Strong>
-								. Il en existe <Strong>trois différents</Strong>, avec chacun
-								ses avantages et inconvénients. Avec ce comparatif, trouvez
-								celui qui vous correspond le mieux.
-							</Trans>
-						</Intro>
-						<Comparateur engines={engines} />
-					</>
-				}
-			/>
-		</Routes>
+		<>
+			<Intro>
+				<Trans i18nKey="comparaisonRégimes.description">
+					Lorsque vous créez votre société, le choix du statut juridique va{' '}
+					<Strong>
+						déterminer à quel régime social le dirigeant est affilié
+					</Strong>
+					. Il en existe <Strong>trois différents</Strong>, avec chacun ses
+					avantages et inconvénients. Avec ce comparatif, trouvez celui qui vous
+					correspond le mieux.
+				</Trans>
+			</Intro>
+			<Comparateur engines={engines} />
+		</>
 	)
 }
 
