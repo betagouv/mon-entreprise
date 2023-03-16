0f863bb8a9e6a31c6f3515eb2e17a133a02a5855
Benjamin Arias <benjamin.j.arias@gmail.com>
2023-03-16 09:53:56 +0100

    fix: Retire tabIndex

--- site/source/design-system/field/Radio/Radio.tsx
+++ site/source/design-system/field/Radio/Radio.tsx
@@ -43,7 +43,13 @@ export const RadioSkeleton = (props: RadioProps) => {
 
 	return (
 		<Label $hideRadio={hideRadio} htmlFor={id} className={props.className}>
-			<InputRadio {...inputProps} className="sr-only" ref={ref} id={id} />
+			<InputRadio
+				{...inputProps}
+				tabIndex={undefined}
+				className="sr-only"
+				ref={ref}
+				id={id}
+			/>
 			<VisibleRadio as={visibleRadioAs}>{children}</VisibleRadio>
 		</Label>
 	)
