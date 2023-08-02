478fa2b57b2c88a1cb8b9b492de120e2c23c25ba
Jérémy Rialland <wiinxt@gmail.com>
2023-08-02 10:06:07 +0200

    Fix test

--- site/cypress/integration/mon-entreprise/localisation-field.ts
+++ site/cypress/integration/mon-entreprise/localisation-field.ts
@@ -6,11 +6,18 @@ describe('Champs localisation (simulateur salarié)', function () {
 	}
 
 	it('should not crash when selecting localisation', function () {
+		cy.intercept({
+			method: 'GET',
+			hostname: 'geo.api.gouv.fr',
+			url: '/communes*',
+		}).as('communes')
+
 		cy.visit(encodeURI('/simulateurs/salaire-brut-net'))
 		cy.contains('SMIC').click()
 		cy.contains('button', 'Commune').click()
 		cy.contains('Commune ou code postal').click({ force: true })
 		cy.focused().type('Steenvoorde')
+		cy.wait('@communes')
 		cy.contains('Steenvoorde (59114)').click({ force: true })
 		cy.contains('Suivant').click({ force: true })
 		cy.contains('Modifier mes réponses').click({ force: true })
