5c823515fa6fe95fbe88e68e7668dc54a4e5c613
Johan Girod <johan.girod@beta.gouv.fr>
2023-08-02 12:10:08 +0200

    Modifie le parcours pour les entreprises à associés multiples

--- site/source/pages/assistants/choix-du-statut/rémunération.tsx
+++ site/source/pages/assistants/choix-du-statut/rémunération.tsx
@@ -15,11 +15,22 @@ import { debounce } from '@/utils'
 import Layout from './_components/Layout'
 import Navigation from './_components/Navigation'
 
-type State = { CA: number | undefined; charges: number | undefined }
-
 export default function Rémunération() {
+	const multipleAssociés = useEngine().evaluate(
+		'entreprise . associés . multiples'
+	).nodeValue
+
+	return multipleAssociés ? (
+		<RémunérationSociétéAssociésMultiples />
+	) : (
+		<RémunérationEntrepriseUnipersonnelle />
+	)
+}
+
+function RémunérationEntrepriseUnipersonnelle() {
 	const { t } = useTranslation()
-	const [{ CA, charges }, setState, reset, isComplete] = useRémunérationState()
+	const [{ CA, charges }, setState, reset, isComplete] =
+		useChiffreAffairesState()
 
 	return (
 		<>
@@ -93,21 +104,68 @@ export default function Rémunération() {
 	)
 }
 
-function useRémunérationState(): [
-	state: State,
-	setState: (value: Partial<State>) => void,
+function RémunérationSociétéAssociésMultiples() {
+	const { t } = useTranslation()
+	const [{ rémunérationTotale }, setState, reset, isComplete] =
+		useRémunérationTotaleState()
+
+	return (
+		<>
+			<Layout
+				title={
+					<Trans i18nKey="choix-statut.rémunération.totale.title">
+						En tant que dirigeant, je souhaite que l'entreprise me rémunère en
+						dépensant au total...
+						<HelpButtonWithPopover
+							title={t(
+								'choix-statut.rémunération.totale.help.title',
+								'Rémunération totale du dirigeant'
+							)}
+							type="info"
+						>
+							<Body>
+								C'est ce que l'entreprise dépense en tout pour la rémunération
+								du dirigeant. Cette rémunération "super-brute" inclut toutes les
+								cotisations sociales à payer. On peut aussi considérer que c'est
+								la valeur monétaire du travail du dirigeant.
+							</Body>
+						</HelpButtonWithPopover>
+					</Trans>
+				}
+			>
+				<NumberField
+					value={rémunérationTotale}
+					onChange={(value) => setState({ rémunérationTotale: value })}
+					label={t(
+						'choix-statut.rémunération.rémunérationTotale.label',
+						'Montant de la rémunération totale'
+					)}
+					displayedUnit="€/an"
+					id="rémunérationTotale"
+				/>
+
+				<Navigation currentStepIsComplete={isComplete} onPreviousStep={reset} />
+			</Layout>
+		</>
+	)
+}
+
+type CAState = { CA: number | undefined; charges: number | undefined }
+function useChiffreAffairesState(): [
+	state: CAState,
+	setState: (value: Partial<CAState>) => void,
 	reset: () => void,
 	isComplete: boolean
 ] {
 	const defaultState = { CA: undefined, charges: undefined }
-	const [state, setState] = usePersistingState<State>(
+	const [state, setState] = usePersistingState<CAState>(
 		'choix-statut:associés',
 		defaultState
 	)
 
 	const dispatch = useDispatch()
 	const debouncedUpdateSituation = useCallback(
-		debounce(1000, (newState: State) => {
+		debounce(1000, (newState: CAState) => {
 			dispatch(
 				batchUpdateSituation({
 					"entreprise . chiffre d'affaires": newState.CA
@@ -119,13 +177,14 @@ function useRémunérationState(): [
 					'entreprise . charges': newState.charges
 						? { valeur: newState.charges, unité: '€/an' }
 						: undefined,
+					'dirigeant . rémunération . totale': undefined,
 				})
 			)
 		}),
 		[]
 	)
 
-	const handleChange = (value: Partial<State>) => {
+	const handleChange = (value: Partial<CAState>) => {
 		const newState = { ...state, ...value }
 		setState(newState)
 		debouncedUpdateSituation(newState)
@@ -143,6 +202,58 @@ function useRémunérationState(): [
 		engine.evaluate("entreprise . chiffre d'affaires").nodeValue !==
 			undefined &&
 		engine.evaluate('entreprise . charges').nodeValue !== undefined
+
+	return [state, handleChange, reset, isComplete]
+}
+
+type RémunérationState = { rémunérationTotale: number | undefined }
+function useRémunérationTotaleState(): [
+	state: RémunérationState,
+	setState: (value: Partial<RémunérationState>) => void,
+	reset: () => void,
+	isComplete: boolean
+] {
+	const defaultState = { rémunérationTotale: undefined }
+	const [state, setState] = usePersistingState<RémunérationState>(
+		'choix-statut:associés',
+		defaultState
+	)
+
+	const dispatch = useDispatch()
+	const debouncedUpdateSituation = useCallback(
+		debounce(1000, (newState: RémunérationState) => {
+			dispatch(
+				batchUpdateSituation({
+					"entreprise . chiffre d'affaires": undefined,
+					'entreprise . charges': undefined,
+					'dirigeant . rémunération . totale': newState.rémunérationTotale
+						? {
+								valeur: newState.rémunérationTotale,
+								unité: '€/an',
+						  }
+						: undefined,
+				})
+			)
+		}),
+		[]
+	)
+
+	const handleChange = (value: Partial<RémunérationState>) => {
+		const newState = { ...state, ...value }
+		setState(newState)
+		debouncedUpdateSituation(newState)
+	}
+
+	useEffect(() => {
+		handleChange(state)
+	}, [])
+	const reset = () => {
+		handleChange(defaultState)
+	}
+
+	const engine = useEngine()
+	const isComplete =
+		engine.evaluate('dirigeant . rémunération . totale').nodeValue !== undefined
 
 	return [state, handleChange, reset, isComplete]
 }
