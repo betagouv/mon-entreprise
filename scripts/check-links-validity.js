30e9a8861a14945411ebe4ae90a0eb8b8df00d6e
Jérémy Rialland <wiinxt@gmail.com>
2023-08-10 11:55:43 +0200

    remove api dir from check links script

--- scripts/check-links-validity.js
+++ scripts/check-links-validity.js
@@ -9,7 +9,7 @@ dotenv.config()
 
 // Extrait la liste des liens référencés dans la base de code
 const { stdout, stderr } = await promisify(exec)(
-	"rg -oNI -e 'https?://([\\w/_\\-?=%+@]|\\.\\w)+' -g '*.{yaml,ts,tsx,js,jsx}' -g '!*-en.yaml' ./ | sort | uniq"
+	"rg -oNI -e 'https?://([\\w/_\\-?=%+@]|\\.\\w)+' -g '*.{yaml,ts,tsx,js,jsx}' -g '!*-en.yaml' -g '!api' ./ | sort | uniq"
 )
 if (stderr) {
 	throw new Error(stderr)
