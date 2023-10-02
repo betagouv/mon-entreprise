a883404cde4eb9192005bf81fc491a16788687f1
Johan Girod <johan.girod@beta.gouv.fr>
2023-10-02 17:56:15 +0200

    # This is a combination of 2 commits.
    # This is the 1st commit message:
    
    # This is the commit message #2:
    
    Edulcore la modale sur l'annonce de la fin du service
    
    with ';' will be ignored, and an empty message aborts the commit. ; ;
    Date:      Mon Oct 2 17:56:15 2023 +0200 ; ; interactive rebase in
    progress; onto 0d74d2c6f ; Last commands done (2 commands done): ;
    pick bde9c6384 supprime la modale d'explication sur l'interruption de
    service suite au retour de l'Urssaf' ;    squash d8186ee3b Edulcore la
    modale sur l'annonce de la fin du service ; No commands remaining. ; You
    are currently rebasing branch 'master' on '0d74d2c6f'. ; ; Changes to be
    committed: ;	modified:   site/source/components/BadNews.tsx ;

--- site/source/components/BadNews.tsx
+++ site/source/components/BadNews.tsx
@@ -6,13 +6,9 @@ import { Message, PopoverWithTrigger } from '@/design-system'
 import { Emoji } from '@/design-system/emoji'
 import { Strong } from '@/design-system/typography'
 import { Link } from '@/design-system/typography/link'
-import { Li, Ul } from '@/design-system/typography/list'
-import { Body, SmallBody } from '@/design-system/typography/paragraphs'
-import { useSitePaths } from '@/sitePaths'
+import { Body } from '@/design-system/typography/paragraphs'
 
 export function BadNews() {
-	const { absoluteSitePaths } = useSitePaths()
-
 	return (
 		<div>
 			<Message
@@ -39,51 +35,15 @@ export function BadNews() {
 						>
 							<Body>
 								L'équipe qui opérait les simulateurs et les assistants de
-								mon-entreprise.urssaf.fr depuis plus de cinq ans a pris la
-								décision d'arrêter son travail. En voici l'explication :
+								mon-entreprise.urssaf.fr n'est plus en mesure de continuer à
+								développer le site.
 							</Body>
-							<Ul>
-								<Li>
-									L'équipe est composée de{' '}
-									<Strong>deux développeurs indépendants</Strong> (travailleurs
-									non salariés) ;
-								</Li>
-								<Li>
-									Le paiement de leurs factures est bloqué{' '}
-									<Strong>depuis avril</Strong> ;
-								</Li>
-								<Li>
-									Le budget de développement du site, financé en totalité par
-									l'Urssaf, est en{' '}
-									<Link to={absoluteSitePaths.budget}>
-										forte baisse pour 2023
-									</Link>
-									, alors que{' '}
-									<Link to={absoluteSitePaths.stats}>
-										son usage est en forte hausse
-									</Link>
-									.
-								</Li>
-								<Li>
-									Ce budget n'est <Strong>pas suffisant</Strong> pour faire
-									travailler l'équipe sur l'année complète et continuer à
-									développer le produit.
-								</Li>
-							</Ul>
-
 							<Body>
 								Cette situation est <Strong>temporaire</Strong>, et devrait{' '}
 								<Strong>revenir à la normale</Strong> dans les prochains mois.
 								Veuillez nous excuser pour la gêne occasionnée.
 							</Body>
 							<StyledImg src={missedChance} aria-hidden alt="" />
-							<SmallBody $grey>
-								P.S de l'équipe : On vous le dit sincèrement, ça été très dur de
-								prendre cette décision. On aurait aimé continuer à faire évoluer
-								ce site comme on l'a fait toutes ces années. Quoi qu'il
-								advienne, on part très honoré de voir plus de 500 000 d'entre
-								vous utiliser nos outils chaque mois !
-							</SmallBody>
 						</PopoverWithTrigger>
 					</Trans>
 				</Body>
