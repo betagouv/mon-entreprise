c20a7cb80d290b2f3fd6a737225acda7d05fbe33
Johan Girod <johan.girod@beta.gouv.fr>
2024-01-25 17:56:36 +0100

    chore: change extension of publicodes files

--- scripts/build-rules.js
+++ scripts/build-rules.js
@@ -1,11 +1,11 @@
-import { writeFileSync } from 'fs'
+import { existsSync, mkdirSync, writeFileSync } from 'fs'
 import path from 'path'
 
 import { getModelFromSource } from '@publicodes/tools/compilation'
 import Engine from 'publicodes'
 
 const outDir = './dist'
-const rules = getModelFromSource(path.resolve('./règles/**/*.yaml'), {
+const rules = getModelFromSource(path.resolve('./règles'), {
 	verbose: true,
 })
 
@@ -13,6 +13,11 @@ export default function writeJSFile() {
 	const json = JSON.stringify(JSON.stringify(rules))
 	const names = Object.keys(new Engine(rules).getParsedRules())
 	const jsString = `export const json = /*@__PURE__*/ ${json};\nexport default /*@__PURE__*/ JSON.parse(json);`
+	// Create folder if doesn't exist
+	const folder = path.resolve(outDir)
+	if (!existsSync(folder)) {
+		mkdirSync(folder)
+	}
 	writeFileSync(path.resolve(outDir, 'index.js'), jsString)
 	writeFileSync(
 		path.resolve(outDir, 'names.ts'),
