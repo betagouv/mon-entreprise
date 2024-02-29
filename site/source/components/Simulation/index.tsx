38c50cdf5aedae7b77e6f6f74c3fdb2efd494515
Johan Girod <johan.girod@beta.gouv.fr>
2024-02-29 13:53:48 +0100

    feat: remove « faire une suggestion » button
    to reactivate when we hire somebody for treating the support

--- site/source/components/Simulation/index.tsx
+++ site/source/components/Simulation/index.tsx
@@ -9,7 +9,7 @@ import { styled } from 'styled-components'
 import { ConversationProps } from '@/components/conversation/Conversation'
 import ShareOrSaveSimulationBanner from '@/components/ShareSimulationBanner'
 import { PopoverWithTrigger } from '@/design-system'
-import { Container, Grid, Spacing } from '@/design-system/layout'
+import { Grid, Spacing } from '@/design-system/layout'
 import { Link } from '@/design-system/typography/link'
 import {
 	companySituationSelector,
@@ -129,17 +129,13 @@ export default function Simulation({
 				</FromTop>
 			</SimulationContainer>
 			{firstStepCompleted && !hideDetails && shouldShowFeedback && (
-				<Container
-					backgroundColor={(theme) => theme.colors.bases.primary[700]}
-					forceTheme="dark"
+				<div
 					style={{
 						textAlign: 'center',
-						padding: '1rem',
-						paddingBottom: '2rem',
 					}}
 				>
 					<Feedback />
-				</Container>
+				</div>
 			)}
 			{firstStepCompleted && !hideDetails && explanations}
 		</>
