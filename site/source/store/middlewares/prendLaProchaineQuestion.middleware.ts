e9986c634232e9808afc94c3476f535ac4ab3229
Jalil Arfaoui <jalil@arfaoui.net>
2024-05-29 09:39:17 +0200

    perf: utilise un moteur par contexte plutôt que le mécanisme contexte

--- site/source/store/middlewares/prendLaProchaineQuestion.middleware.ts
+++ site/source/store/middlewares/prendLaProchaineQuestion.middleware.ts
@@ -1,16 +1,31 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
+import deepEql from 'deep-eql'
 import * as Array from 'effect/Array'
+import { NonEmptyArray } from 'effect/Array'
 import * as String from 'effect/String'
 import Engine from 'publicodes'
 import { Dispatch, Middleware } from 'redux'
 
+import { isComparateurConfig } from '@/domaine/ComparateurConfig'
 import { détermineLesProchainesQuestions } from '@/domaine/engine/détermineLesProchainesQuestions'
-import { SimulationConfig } from '@/domaine/SimulationConfig'
 import { Action, questionsSuivantes } from '@/store/actions/actions'
-import { RootState } from '@/store/reducers/rootReducer'
-import { rawSituationSelector } from '@/store/selectors/simulationSelectors'
+import {
+	RootState,
+	SimulationConfig,
+	Situation,
+} from '@/store/reducers/rootReducer'
+import { Simulation } from '@/store/reducers/simulation.reducer'
+import {
+	rawSituationSelector,
+	rawSituationsSelonContextesSelector,
+} from '@/store/selectors/simulationSelectors'
 import { complement } from '@/utils/complement'
+
+let lastSimulation: Simulation | null = null
+let lastConfig: SimulationConfig | null = null
+let lastSituationsAvecContextes: NonEmptyArray<Situation> | null = null
+let engines: NonEmptyArray<Engine> | null = null
 
 export const prendLaProchaineQuestionMiddleware =
 	(engine: Engine): Middleware<object, RootState, Dispatch<Action>> =>
@@ -21,25 +36,61 @@ export const prendLaProchaineQuestionMiddleware =
 
 		const newState = store.getState()
 
-		const config = newState.simulation?.config
+		const simulation = newState.simulation
+		const config = simulation?.config
 		const situation = rawSituationSelector(newState)
-		const questionsRépondues = newState.simulation?.answeredQuestions
-		const questionsSuivantesActuelles =
-			newState.simulation?.questionsSuivantes || []
+		const questionsRépondues = simulation?.answeredQuestions
+		const questionsSuivantesActuelles = simulation?.questionsSuivantes || []
 
-		if (situation && config) {
-			engine.setSituation(situation)
+		const configHasChanged = lastConfig !== config
 
-			const prochainesQuestions = détermineLesProchainesQuestions(
-				engine,
-				config as SimulationConfig,
-				questionsRépondues
-			)
+		if (config && configHasChanged) {
+			engines = isComparateurConfig(config)
+				? (config.contextes.map(() =>
+						engine.shallowCopy()
+				  ) as NonEmptyArray<Engine>)
+				: [engine]
+			lastSituationsAvecContextes = null
+		}
 
-			if (
-				arraysAreDifferent(prochainesQuestions, questionsSuivantesActuelles)
-			) {
-				store.dispatch(questionsSuivantes(prochainesQuestions))
+		if (action.type === 'SET_SIMULATION') {
+			lastConfig = null
+			lastSituationsAvecContextes = null
+			lastSimulation = null
+		}
+
+		if (situation && config && engines && simulation !== lastSimulation) {
+			const situationsAvecContextes =
+				rawSituationsSelonContextesSelector(newState)
+
+			const situationAChangé =
+				!!lastSituationsAvecContextes &&
+				!deepEql<NonEmptyArray<Situation>, NonEmptyArray<Situation>>(
+					situationsAvecContextes,
+					lastSituationsAvecContextes
+				)
+
+			if (!lastSituationsAvecContextes || situationAChangé) {
+				lastSituationsAvecContextes = situationsAvecContextes
+
+				engines.forEach((engine, index) =>
+					engine.setSituation(situationsAvecContextes[index])
+				)
+
+				lastSimulation = simulation
+				lastConfig = config
+
+				const prochainesQuestions = détermineLesProchainesQuestions(
+					engines,
+					config,
+					questionsRépondues
+				)
+
+				if (
+					arraysAreDifferent(prochainesQuestions, questionsSuivantesActuelles)
+				) {
+					store.dispatch(questionsSuivantes(prochainesQuestions))
+				}
 			}
 		}
 
