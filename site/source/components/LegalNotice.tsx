a2d163b2cb28424431ebee72b1e657ed488ecacd
Alice Dahan <lilice.dhn@gmail.com>
2024-07-19 12:00:51 +0200

    feat(site): Met à jour les mentions légales

--- site/source/components/LegalNotice.tsx
+++ site/source/components/LegalNotice.tsx
@@ -12,51 +12,70 @@ export default function LegalNotice() {
 		<PopoverWithTrigger
 			trigger={(buttonProps) => (
 				<Link {...buttonProps} aria-haspopup="dialog" noUnderline>
-					<Trans i18nKey="legalNotice.title">Mentions légales</Trans>
+					{t('legalNotice.title', 'Mentions légales')}
 				</Link>
 			)}
 			title={t('legalNotice.title', 'Mentions légales')}
 			small
 		>
-			<H2>
-				<Trans i18nKey="legalNotice.editeur.title">Editeur</Trans>
-			</H2>
+			<H2>{t('legalNotice.editeur.title', 'Éditeur de la Plateforme')}</H2>
 			<Body>
-				Agence Centrale des Organismes de Sécurité Sociale (ACOSS)
-				<br />
-				36 rue de Valmy - 93108 Montreuil Cedex
-			</Body>
-			<H2>
-				<Trans i18nKey="legalNotice.publication.title">
-					Directeur de la publication
-				</Trans>
-			</H2>
-			<Body>
-				<Trans i18nKey="legalNotice.publication.content">
-					M. Yann-Gaël Amghar, Directeur de l'Acoss
-				</Trans>
-			</Body>
-			<H2>
-				<Trans i18nKey="legalNotice.hosting.title">
-					Prestataire d'hébergement
-				</Trans>
-			</H2>
-			<Body>
-				<Trans i18nKey="legalNotice.hosting.content">
-					Netlify
-					<br />
-					610 22nd Street, Suite 315,
-					<br />
-					San Francisco, CA 94107 <br />
-					Site web :&nbsp;
+				<Trans i18nKey="legalNotice.editeur.content">
+					La plateforme mon-entreprise.urssaf.fr est éditée par l'Agence
+					Centrale des Organismes de Sécurité Sociale (également appelée « Acoss
+					» ou « Urssaf Caisse nationale ») située : <br />
+					36 rue de Valmy <br />
+					93108 Montreuil Cedex <br />
+					01 77 93 65 00 <br />
 					<StyledLink
-						href="https://www.netlify.com"
-						aria-label="https://www.netlify.com, nouvelle fenêtre"
+						href="mailto:webmaster.general@acoss.fr"
+						aria-label={t(
+							'legalNotice.editeur.label',
+							'Envoyer un mail à webmaster.general@acoss.fr'
+						)}
 						target="_blank"
 						rel="noreferrer"
 					>
-						https://www.netlify.com
+						webmaster.general@acoss.fr
 					</StyledLink>
+				</Trans>
+			</Body>
+			<H2>
+				{t('legalNotice.publication.title', 'Directeur de la publication')}
+			</H2>
+			<Body>
+				{t(
+					'legalNotice.publication.content',
+					"M. Damien IENTILE, Directeur de l'Urssaf Caisse nationale"
+				)}
+			</Body>
+			<H2>{t('legalNotice.hosting.title', "Prestataire d'hébergement")}</H2>
+			<Body>
+				<Trans i18nKey="legalNotice.hosting.content">
+					Ce site est hébergé par Netlify : <br />
+					610 22nd Street, Suite 315 <br />
+					San Francisco, CA 94107 <br />
+					États-Unis d'Amérique <br />
+					+1 844-899-7312
+				</Trans>
+			</Body>
+			<H2>{t('legalNotice.accessibility.title', 'Accessibilité')}</H2>
+			<Body>
+				<Trans i18nKey="legalNotice.accessibility.content">
+					La plateforme est{' '}
+					<StyledLink
+						href="https://mon-entreprise.urssaf.fr/accessibilit%C3%A9"
+						aria-label={t(
+							'legalNotice.accessibility.label',
+							'Aller à la page https://mon-entreprise.urssaf.fr/accessibilit%C3%A9, nouvel onglet'
+						)}
+						target="_blank"
+						rel="noreferrer"
+					>
+						partiellement conforme
+					</StyledLink>{' '}
+					aux normes d’accessibilité numérique. Nous tâchons de rendre cette
+					plateforme accessible à toutes et à tous.
 				</Trans>
 			</Body>
 			<H2>
@@ -66,6 +85,10 @@ export default function LegalNotice() {
 				<Trans i18nKey="legalNotice.contact.content">
 					<StyledLink
 						href="mailto:contact@mon-entreprise.beta.gouv.fr"
+						aria-label={t(
+							'legalNotice.contact.label',
+							'Envoyer un mail à contact@mon-entreprise.beta.gouv.fr'
+						)}
 						target="_blank"
 						rel="noreferrer"
 					>
