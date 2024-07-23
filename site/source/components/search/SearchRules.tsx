a4e6ed96be44a3c45d360d39f8a320231a9e7869
Alice Dahan <lilice.dhn@gmail.com>
2024-07-23 18:24:51 +0200

    refactor(recherche): Factorise le client et l'index Algolia utilisé pour les recherches

--- site/source/components/search/SearchRules.tsx
+++ site/source/components/search/SearchRules.tsx
@@ -1,24 +1,18 @@
-import algoliasearch from 'algoliasearch/lite'
 import { Trans, useTranslation } from 'react-i18next'
 
 import { H2 } from '@/design-system/typography/heading'
 
+import { algoliaIndexPrefix, searchClient } from './Algolia'
 import { RulesInfiniteHits } from './RulesInfiniteHits'
 import { SearchBox } from './SearchBox'
 import { SearchRoot } from './SearchRoot'
-
-const ALGOLIA_APP_ID = import.meta.env.VITE_ALGOLIA_APP_ID || ''
-const ALGOLIA_SEARCH_KEY = import.meta.env.VITE_ALGOLIA_SEARCH_KEY || ''
-const ALGOLIA_INDEX_PREFIX = import.meta.env.VITE_ALGOLIA_INDEX_PREFIX || ''
-
-const searchClient = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_KEY)
 
 export default function SearchRules() {
 	const { t } = useTranslation()
 
 	return (
 		<SearchRoot
-			indexName={`${ALGOLIA_INDEX_PREFIX}rules`}
+			indexName={`${algoliaIndexPrefix}rules`}
 			searchClient={searchClient}
 		>
 			<SearchBox
