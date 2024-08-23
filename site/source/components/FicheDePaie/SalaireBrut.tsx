c65a558d17bdd3b4dd4716a70827c80bc744ad4a
Alice Dahan <lilice.dhn@gmail.com>
2024-08-23 18:20:15 +0200

    feat(salarié): Met à jour la présentation de la fiche de paie

--- site/source/components/FicheDePaie/SalaireBrut.tsx
+++ site/source/components/FicheDePaie/SalaireBrut.tsx
@@ -1,6 +1,6 @@
 import { Trans } from 'react-i18next'
 
-import { H4 } from '@/design-system/typography/heading'
+import { H3 } from '@/design-system/typography/heading'
 
 import { Condition } from '../EngineValue/Condition'
 import Line from './Line'
@@ -10,18 +10,18 @@ import './FicheDePaie.css'
 export default function SalaireBrut() {
 	return (
 		<div className="payslip__salarySection">
-			<H4 className="payslip__salaryTitle" as="h3">
-				<Trans>Salaire</Trans>
-			</H4>
+			<H3 className="payslip__salaryTitle">
+				<Trans>Salaire brut</Trans>
+			</H3>
 			<Line rule="salarié . contrat . salaire brut" />
-			<Line rule="salarié . rémunération . avantages en nature . montant" />
-			<Line rule="salarié . activité partielle . retrait absence" />
-			<Line rule="salarié . activité partielle . indemnités" />
 			<Line rule="salarié . rémunération . heures supplémentaires" />
 			<Line rule="salarié . rémunération . heures complémentaires" />
 			<Line rule="salarié . rémunération . primes" />
-			<Line rule="salarié . rémunération . frais professionnels" />
 			<Line rule="salarié . rémunération . indemnités CDD" />
+			<Line rule="salarié . rémunération . avantages en nature . montant" />
+			<Line rule="salarié . rémunération . frais professionnels" />
+			<Line rule="salarié . activité partielle . retrait absence" />
+			<Line rule="salarié . activité partielle . indemnités" />
 			<Condition expression="salarié . contrat . salaire brut != salarié . rémunération . brut">
 				<Line rule="salarié . rémunération . brut" />
 			</Condition>
