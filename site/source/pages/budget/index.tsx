9a84d6316a2780ae9f84c06ee64421089ebbec2a
Alice Dahan <lilice.dhn@gmail.com>
2024-10-04 14:19:49 +0200

    feat: publication du budget 2024

--- site/source/pages/budget/index.tsx
+++ site/source/pages/budget/index.tsx
@@ -27,6 +27,7 @@ const [
 	ressources2021,
 	ressources2022,
 	ressources2023,
+	ressources2024,
 	ressourcesDescription,
 ] = prose.split(/\r?\n-{3,}\r?\n/)
 
@@ -36,12 +37,13 @@ const ressources = {
 	2021: ressources2021,
 	2022: ressources2022,
 	2023: ressources2023,
+	2024: ressources2024,
 } as const
 
 const arraySum = (arr: number[]) => arr.reduce((a, b) => a + b, 0)
 
 export default function Budget() {
-	const years = ['2019', '2020', '2021', '2022', '2023'] as const
+	const years = ['2019', '2020', '2021', '2022', '2023', '2024'] as const
 	const quarters = [
 		{ label: 'T1', 'aria-label': 'Trimestre 1' },
 		{ label: 'T2', 'aria-label': 'Trimestre 2' },
