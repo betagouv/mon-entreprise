08e2d58660a69185b04513c19ef943b5b3539910
Alice Dahan <lilice.dhn@gmail.com>
2024-10-15 16:57:00 +0200

    feat: affiche la répartition de la RGCP mois par mois

--- site/source/pages/simulateurs/reduction-generale/components/Répartition.tsx
+++ site/source/pages/simulateurs/reduction-generale/components/Répartition.tsx
@@ -2,28 +2,33 @@ import { styled } from 'styled-components'
 
 import { SimulationValue } from '@/components/Simulation/SimulationValue'
 import { Li, Ul } from '@/design-system/typography/list'
+import { Contexte } from '@/domaine/Contexte'
 
 type Props = {
+	contexte?: Contexte
 	round?: boolean
 }
 
-export default function Répartition({ round = true }: Props) {
+export default function Répartition({ contexte = {}, round = true }: Props) {
 	return (
 		<StyledUl>
 			<StyledLi>
 				<SimulationValue
 					dottedName="salarié . cotisations . exonérations . réduction générale . part retraite"
+					contexte={contexte}
 					round={round}
 				/>
 			</StyledLi>
 			<StyledLi>
 				<SimulationValue
 					dottedName="salarié . cotisations . exonérations . réduction générale . part Urssaf"
+					contexte={contexte}
 					round={round}
 				/>
 				<SimulationValue
 					dottedName="salarié . cotisations . exonérations . réduction générale . part Urssaf . part chômage"
 					label="dont chômage"
+					contexte={contexte}
 					round={round}
 				/>
 			</StyledLi>
