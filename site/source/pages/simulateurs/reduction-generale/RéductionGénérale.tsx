ff29b59051219a08b490fb0a0655638e3c3f094d
Alice Dahan <lilice.dhn@gmail.com>
2024-10-15 16:59:51 +0200

    refactor: crée un composant avec les avertissements sur la RGCP

--- site/source/pages/simulateurs/reduction-generale/RéductionGénérale.tsx
+++ site/source/pages/simulateurs/reduction-generale/RéductionGénérale.tsx
@@ -1,7 +1,7 @@
 import { DottedName } from 'modele-social'
 import { PublicodesExpression } from 'publicodes'
 import { useCallback, useEffect, useState } from 'react'
-import { Trans, useTranslation } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 
 import { Condition } from '@/components/EngineValue/Condition'
@@ -14,15 +14,14 @@ import Simulation, {
 } from '@/components/Simulation'
 import { SimulationValue } from '@/components/Simulation/SimulationValue'
 import { useEngine } from '@/components/utils/EngineContext'
-import { Message } from '@/design-system'
 import { Spacing } from '@/design-system/layout'
-import { Body } from '@/design-system/typography/paragraphs'
 import { SimpleRuleEvaluation } from '@/domaine/engine/SimpleRuleEvaluation'
 import { ajusteLaSituation } from '@/store/actions/actions'
 import { situationSelector } from '@/store/selectors/simulationSelectors'
 
 import EffectifSwitch from './components/EffectifSwitch'
 import Répartition from './components/Répartition'
+import Warnings from './components/Warnings'
 import RéductionGénéraleMoisParMois from './RéductionGénéraleMoisParMois'
 import {
 	getInitialRéductionGénéraleMoisParMois,
@@ -163,39 +162,7 @@ function RéductionGénéraleSimulationGoals({
 						onUpdateSituation={initializeRéductionGénéraleMoisParMoisData}
 					/>
 
-					<Condition expression="salarié . cotisations . exonérations . JEI = oui">
-						<Message type="info">
-							<Body>
-								<Trans>
-									La réduction générale n'est pas cumulable avec l'exonération
-									Jeune Entreprise Innovante (JEI).
-								</Trans>
-							</Body>
-						</Message>
-					</Condition>
-
-					<Condition expression="salarié . contrat = 'stage'">
-						<Message type="info">
-							<Body>
-								<Trans>
-									La réduction générale ne s'applique pas sur les gratifications
-									de stage.
-								</Trans>
-							</Body>
-						</Message>
-					</Condition>
-
-					<Condition expression="salarié . cotisations . exonérations . réduction générale = 0">
-						<Message type="info">
-							<Body>
-								<Trans>
-									La RGCP concerne uniquement les salaires inférieurs à 1,6
-									SMIC. C'est-à-dire, pour 2024, une rémunération totale qui ne
-									dépasse pas <strong>2 827,07 €</strong> bruts par mois.
-								</Trans>
-							</Body>
-						</Message>
-					</Condition>
+					<Warnings />
 
 					<Condition expression="salarié . cotisations . exonérations . réduction générale >= 0">
 						<SimulationValue
