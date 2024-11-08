c5b59d3074a272061dff677d0446a9cf0a671f20
Alice Dahan <lilice.dhn@gmail.com>
2024-11-08 12:17:00 +0100

    chore: remplace SmartTags PA par Piano Analytics
    (cherry picked from commit b9ba32f4d6df44d3fd61d7c76145820ead5436e9)

--- site/source/components/Feedback/Feedback.tsx
+++ site/source/components/Feedback/Feedback.tsx
@@ -66,7 +66,7 @@ export function Feedback({
 	const submitFeedback = useCallback(
 		(rating: FeedbackT) => {
 			setFeedbackGivenForUrl(url)
-			tag.events.send('click.action', {
+			tag?.sendEvent('click.action', {
 				click_chapter1: 'satisfaction',
 				click: rating,
 			})
