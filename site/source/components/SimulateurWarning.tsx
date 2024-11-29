ce9e8534397c2fe2b540acc5aa08755d7048e7c6
Jalil Arfaoui <jalil@arfaoui.net>
2024-11-29 17:04:58 +0100

    feat(indépendant): améliorations présentation simulateur de cessation d'activité

--- site/source/components/SimulateurWarning.tsx
+++ site/source/components/SimulateurWarning.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useContext } from 'react'
+import { ReactNode, useContext } from 'react'
 import { Trans } from 'react-i18next'
 import { styled } from 'styled-components'
 
@@ -59,11 +59,7 @@ export default function SimulateurWarning({
 					</StyledLi>
 				</Ul>
 			)}
-			{informationsComplémentaires && (
-				<Ul>
-					<StyledLi>{informationsComplémentaires}</StyledLi>
-				</Ul>
-			)}
+			{informationsComplémentaires && <>{informationsComplémentaires}</>}
 			{simulateur === 'profession-libérale' && (
 				<Ul>
 					<StyledLi>
--- site/source/pages/simulateurs/cessation-activité/simulationConfig.ts
+++ site/source/pages/simulateurs/cessation-activité/simulationConfig.ts
@@ -3,5 +3,18 @@ import { configIndépendant } from '@/pages/simulateurs/indépendant/simulationC
 
 export const configCessationActivité: SimulationConfig = {
 	...configIndépendant,
+	'objectifs exclusifs': [
+		"entreprise . chiffre d'affaires",
+		'dirigeant . rémunération . totale',
+		'dirigeant . rémunération . net',
+	],
 	'unité par défaut': '€/an',
+	questions: {
+		...configIndépendant.questions,
+		'liste noire': [
+			...(configIndépendant.questions?.['liste noire'] || []),
+			'entreprise . TVA . franchise de TVA . notification',
+			'entreprise . date de cessation',
+		],
+	},
 }
