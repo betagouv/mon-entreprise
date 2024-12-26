4b2a89d9319da18a87ac3bb8dd18de879995a219
Alice Dahan <lilice.dhn@gmail.com>
2024-12-26 17:37:07 +0100

    feat(lodeom): ajout de la sélection de barème

---
+++ site/source/pages/simulateurs/lodeom/components/BarèmeSwitch.tsx
@@ -0,0 +1,7 @@
+import { SimpleField } from '@/pages/assistants/components/Fields'
+
+export default function BarèmeSwitch() {
+	return (
+		<SimpleField dottedName="salarié . cotisations . exonérations . lodeom . zone un . barèmes" />
+	)
+}
