f325b034a1e1d044396cf5f18113efc1cb27e6f2
Alice Dahan <lilice.dhn@gmail.com>
2024-12-27 11:03:31 +0100

    feat(lodeom): stylise les switchs

--- site/source/components/EffectifSwitch.tsx
+++ site/source/components/EffectifSwitch.tsx
@@ -2,9 +2,12 @@ import { DottedName } from 'modele-social'
 import { useEffect, useState } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useDispatch } from 'react-redux'
+import { styled } from 'styled-components'
 
 import { useEngine } from '@/components/utils/EngineContext'
 import { Radio, ToggleGroup } from '@/design-system'
+import { Strong } from '@/design-system/typography'
+import { Body } from '@/design-system/typography/paragraphs'
 import { enregistreLaRéponse } from '@/store/actions/actions'
 
 export default function EffectifSwitch() {
@@ -21,16 +24,46 @@ export default function EffectifSwitch() {
 	}, [currentEffectif, engineEffectif])
 
 	return (
-		<ToggleGroup
-			value={currentEffectif}
-			onChange={(value) => {
-				setCurrentEffectif(value)
-				dispatch(enregistreLaRéponse(dottedName, `'${value}'`))
-			}}
-			aria-label={t("Effectif de l'entreprise")}
-		>
-			<Radio value="10">{t('Moins de 50 salariés')}</Radio>
-			<Radio value="100">{t('Plus de 50 salariés')}</Radio>
-		</ToggleGroup>
+		<Container>
+			<StyledBody id="effectif-switch-label">
+				<Strong>{t("Effectif de l'entreprise")} :</Strong>
+			</StyledBody>
+			<StyledToggleGroup
+				value={currentEffectif}
+				onChange={(value) => {
+					setCurrentEffectif(value)
+					dispatch(enregistreLaRéponse(dottedName, `'${value}'`))
+				}}
+				aria-labelledby="effectif-switch-label"
+			>
+				<StyledRadio value="10">{t('Moins de 50 salariés')}</StyledRadio>
+				<StyledRadio value="100">{t('Plus de 50 salariés')}</StyledRadio>
+			</StyledToggleGroup>
+		</Container>
 	)
 }
+
+const Container = styled.div`
+	text-align: left;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	column-gap: ${({ theme }) => theme.spacings.sm};
+	width: 100%;
+`
+const StyledBody = styled(Body)`
+	margin: ${({ theme }) => theme.spacings.xxs} 0;
+`
+const StyledToggleGroup = styled(ToggleGroup)`
+	display: flex;
+	> * {
+		display: flex;
+		/* flex-direction: column; */
+	}
+`
+const StyledRadio = styled(Radio)`
+	white-space: nowrap;
+	> span {
+		width: 100%;
+	}
+`
--- site/source/components/RéductionDeCotisations/RégularisationSwitch.tsx
+++ site/source/components/RéductionDeCotisations/RégularisationSwitch.tsx
@@ -1,6 +1,9 @@
 import { useTranslation } from 'react-i18next'
+import { styled } from 'styled-components'
 
 import { Radio, ToggleGroup } from '@/design-system'
+import { Strong } from '@/design-system/typography'
+import { Body } from '@/design-system/typography/paragraphs'
 import { RégularisationMethod } from '@/utils/réductionDeCotisations'
 
 type Props = {
@@ -15,28 +18,62 @@ export default function RégularisationSwitch({
 	const { t } = useTranslation()
 
 	return (
-		<ToggleGroup
-			value={régularisationMethod}
-			onChange={(value) => {
-				setRégularisationMethod(value as RégularisationMethod)
-			}}
-			aria-label={t(
-				'pages.simulateurs.réduction-générale.régularisation.type',
-				'Type de régularisation'
-			)}
-		>
-			<Radio value="annuelle">
-				{t(
-					'pages.simulateurs.réduction-générale.régularisation.annuelle',
-					'Régularisation annuelle'
-				)}
-			</Radio>
-			<Radio value="progressive">
-				{t(
-					'pages.simulateurs.réduction-générale.régularisation.progressive',
-					'Régularisation progressive'
-				)}
-			</Radio>
-		</ToggleGroup>
+		<Container>
+			<StyledBody id="régularisation-switch-label">
+				<Strong>
+					{t(
+						'pages.simulateurs.réduction-générale.régularisation.type',
+						'Type de régularisation'
+					)}{' '}
+					:
+				</Strong>
+			</StyledBody>
+
+			<StyledToggleGroup
+				value={régularisationMethod}
+				onChange={(value) => {
+					setRégularisationMethod(value as RégularisationMethod)
+				}}
+				aria-labelledby="régularisation-switch-label"
+			>
+				<StyledRadio value="annuelle">
+					{t(
+						'pages.simulateurs.réduction-générale.régularisation.annuelle',
+						'Régularisation annuelle'
+					)}
+				</StyledRadio>
+				<StyledRadio value="progressive">
+					{t(
+						'pages.simulateurs.réduction-générale.régularisation.progressive',
+						'Régularisation progressive'
+					)}
+				</StyledRadio>
+			</StyledToggleGroup>
+		</Container>
 	)
 }
+
+const Container = styled.div`
+	text-align: left;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	column-gap: ${({ theme }) => theme.spacings.sm};
+	width: 100%;
+`
+const StyledBody = styled(Body)`
+	margin: ${({ theme }) => theme.spacings.xxs} 0;
+`
+const StyledToggleGroup = styled(ToggleGroup)`
+	display: flex;
+	> * {
+		display: flex;
+		/* flex-direction: column; */
+	}
+`
+const StyledRadio = styled(Radio)`
+	white-space: nowrap;
+	> span {
+		width: 100%;
+	}
+`
