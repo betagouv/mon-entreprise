fe82f98fbcd65c2b345e772db673b0f553ebd9c6
Alice Dahan <lilice.dhn@gmail.com>
2024-12-27 11:03:31 +0100

    feat(lodeom): stylise les switchs

--- site/source/pages/simulateurs/lodeom/components/BarèmeSwitch.tsx
+++ site/source/pages/simulateurs/lodeom/components/BarèmeSwitch.tsx
@@ -1,18 +1,49 @@
 import { DottedName } from 'modele-social'
+import { renderToString } from 'react-dom/server'
+import { useTranslation } from 'react-i18next'
+import { styled } from 'styled-components'
 
+import { Body } from '@/design-system/typography/paragraphs'
 import { useZoneLodeom } from '@/hooks/useZoneLodeom'
 import { SimpleField } from '@/pages/assistants/components/Fields'
 
 export default function BarèmeSwitch() {
 	const { currentZone } = useZoneLodeom()
+	const { t } = useTranslation()
 
 	return (
 		currentZone && (
-			<SimpleField
-				dottedName={
-					`salarié . cotisations . exonérations . lodeom . ${currentZone} . barèmes` as DottedName
-				}
-			/>
+			<Container>
+				<SimpleField
+					dottedName={
+						`salarié . cotisations . exonérations . lodeom . ${currentZone} . barèmes` as DottedName
+					}
+					label={renderToString(
+						<p>
+							<strong>
+								{t(
+									'pages.simulateurs.lodeom.barème-switch-label',
+									'Barème à appliquer :'
+								)}
+							</strong>
+						</p>
+					)}
+					labelStyle={StyledBody}
+				/>
+			</Container>
 		)
 	)
 }
+
+const Container = styled.div`
+	text-align: left;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	column-gap: ${({ theme }) => theme.spacings.sm};
+	width: 100%;
+	margin-bottom: -${({ theme }) => theme.spacings.xl};
+`
+const StyledBody = styled(Body)`
+	margin: ${({ theme }) => theme.spacings.xxs} 0;
+`
--- site/source/pages/simulateurs/lodeom/components/ZoneSwitch.tsx
+++ site/source/pages/simulateurs/lodeom/components/ZoneSwitch.tsx
@@ -1,6 +1,9 @@
 import { useTranslation } from 'react-i18next'
+import { styled } from 'styled-components'
 
 import { Radio, ToggleGroup } from '@/design-system'
+import { Strong } from '@/design-system/typography'
+import { Body } from '@/design-system/typography/paragraphs'
 import { useZoneLodeom } from '@/hooks/useZoneLodeom'
 
 export default function ZoneSwitch() {
@@ -8,17 +11,53 @@ export default function ZoneSwitch() {
 	const { t } = useTranslation()
 
 	return (
-		<ToggleGroup
-			value={currentZone}
-			onChange={(value) => {
-				updateZone(value)
-			}}
-			aria-label={t("Zone de l'entreprise")}
-		>
-			<Radio value="zone un">
-				{t('Guadeloupe, Guyane, Martinique, La Réunion')}
-			</Radio>
-			<Radio value="zone deux">{t('Saint-Barthélémy, Saint-Martin')}</Radio>
-		</ToggleGroup>
+		<Container>
+			<StyledBody id="zone-switch-label">
+				<Strong>
+					{t(
+						'pages.simulateurs.lodeom.zone-switch-label',
+						"Localisation de l'entreprise"
+					)}{' '}
+					:
+				</Strong>
+			</StyledBody>
+			<StyledToggleGroup
+				value={currentZone}
+				onChange={updateZone}
+				aria-labelledby="zone-switch-label"
+			>
+				<StyledRadio value="zone un">
+					{t('Guadeloupe, Guyane, Martinique, La Réunion')}
+				</StyledRadio>
+				<StyledRadio value="zone deux">
+					{t('Saint-Barthélémy, Saint-Martin')}
+				</StyledRadio>
+			</StyledToggleGroup>
+		</Container>
 	)
 }
+
+const Container = styled.div`
+	text-align: left;
+	display: flex;
+	flex-direction: column;
+	flex-wrap: wrap;
+	column-gap: ${({ theme }) => theme.spacings.sm};
+	width: 100%;
+`
+const StyledBody = styled(Body)`
+	margin: ${({ theme }) => theme.spacings.xxs} 0;
+`
+const StyledToggleGroup = styled(ToggleGroup)`
+	display: flex;
+	> * {
+		display: flex;
+		flex-direction: column;
+	}
+`
+const StyledRadio = styled(Radio)`
+	white-space: nowrap;
+	> span {
+		width: 100%;
+	}
+`
