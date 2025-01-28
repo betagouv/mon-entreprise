acb8e02baa91925602508c5844f31cee2e124329
Alice Dahan <lilice.dhn@gmail.com>
2025-01-28 11:11:14 +0100

    i18n: typo

--- site/source/pages/simulateurs/impot-societe/config.tsx
+++ site/source/pages/simulateurs/impot-societe/config.tsx
@@ -11,17 +11,17 @@ export function impôtSociétéConfig({ t, sitePaths }: SimulatorsDataParams) {
 		pathId: 'simulateurs.is',
 		iframePath: 'impot-societe',
 		meta: {
-			title: t('pages.simulateurs.is.meta.title', 'Impôt sur les sociétés'),
+			title: t('pages.simulateurs.impot-société.meta.title', 'Impôt sur les sociétés'),
 			description: t(
-				'pages.simulateurs.is.meta.description',
+				'pages.simulateurs.impot-société.meta.description',
 				'Calculez votre impôt sur les sociétés'
 			),
 			color: '#E71D66',
 		},
-		shortName: t('pages.simulateurs.is.meta.title', 'Impôt sur les sociétés'),
+		shortName: t('pages.simulateurs.impot-société.meta.title', 'Impôt sur les sociétés'),
 		title: t(
-			'pages.simulateurs.is.title',
-			"Simulateur d'impôt sur les sociétés"
+			'pages.simulateurs.impot-société.title',
+			"Simulateur d’impôt sur les sociétés"
 		),
 		nextSteps: ['salarié', 'comparaison-statuts'],
 		path: sitePaths.simulateurs.is,
