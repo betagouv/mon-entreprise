7cfb40cf6f4b9f5b9cc520824d56123e6fa7bb54
Jalil Arfaoui <jalil@arfaoui.net>
2025-02-03 16:09:10 +0100

    test: ajoute le matcher vitest `toEvaluate`

---
+++ site/test/modele-social/helpers/logValue.ts
@@ -0,0 +1,28 @@
+import { DottedName } from 'modele-social'
+import Engine from 'publicodes'
+
+export const logValue = (e: Engine, rule: DottedName) => {
+	const result = e.evaluate(rule)
+	// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+
+	const unit = result.unit?.denominators
+		? // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-base-to-string
+		  `${result.unit?.numerators}/-${result.unit?.denominators}`
+		: result.unit?.numerators
+		? // eslint-disable-next-line @typescript-eslint/restrict-template-expressions, @typescript-eslint/no-base-to-string
+		  `${result.unit?.numerators}`
+		: ''
+
+	// eslint-disable-next-line no-console,@typescript-eslint/restrict-template-expressions
+	console.log(`${rule} = ${result.nodeValue} ${unit}`)
+}
+
+export const logApplicability = (e: Engine, rule: DottedName) => {
+	const result = e.evaluate({ 'est applicable': rule })
+	// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+
+	// eslint-disable-next-line no-console,@typescript-eslint/restrict-template-expressions
+	console.log(
+		`${rule} ${result.nodeValue ? 'est applicable' : 'n’est pas applicable'}`
+	)
+}
