4808d242b967bdac7860e0974b66c1bbabc0a1f2
Jalil Arfaoui <jalil@arfaoui.net>
2025-02-03 16:39:27 +0100

    feat(économie-collaborative): renvoie vers AE ou TI si loyer net au-dessus du plafond RG
    #2916
    
    https://github.com/betagouv/mon-entreprise/issues/2916

--- site/source/pages/simulateurs/location-de-meublé/LocationDeMeublé.tsx
+++ site/source/pages/simulateurs/location-de-meublé/LocationDeMeublé.tsx
@@ -1,19 +1,53 @@
+import { Trans, useTranslation } from 'react-i18next'
+
+import AvertissementDansObjectifDeSimulateur from '@/components/AvertissementDansObjectifDeSimulateur'
+import Value from '@/components/EngineValue/Value'
+import { WhenApplicable } from '@/components/EngineValue/WhenApplicable'
+import { WhenNotApplicable } from '@/components/EngineValue/WhenNotApplicable'
 import SimulateurWarning from '@/components/SimulateurWarning'
 import Simulation, {
 	SimulationGoal,
 	SimulationGoals,
 } from '@/components/Simulation'
+import { useEngine } from '@/components/utils/EngineContext'
 
 export default function LocationDeMeublé() {
+	const engine = useEngine()
+	const { t } = useTranslation()
+
 	return (
 		<Simulation>
 			<SimulateurWarning simulateur="location-de-logement-meublé" />
 			<SimulationGoals legend="Montant de votre loyer net">
-				<SimulationGoal dottedName="location de logement . meublé . loyer . net" />
 				<SimulationGoal
-					dottedName="location de logement . meublé . cotisations"
-					small
+					dottedName="location de logement . meublé . loyer . net"
+					displayedUnit="€/an"
 				/>
+
+				<WhenNotApplicable
+					dottedName="location de logement . meublé . cotisations"
+					engine={engine}
+				>
+					<AvertissementDansObjectifDeSimulateur>
+						<Trans>
+							Vous dépassez le plafond autorisé (
+							<Value expression="location de logement . meublé . plafond de loyer autorisé pour le régime général" />
+							) pour déclarer vos revenus de l’économie collaborative avec un
+							statut social au régime général. Vous devez vous orienter vers les
+							statuts d’autoentrepreneur ou de travailleur indépendant.
+						</Trans>
+						{t('')}
+					</AvertissementDansObjectifDeSimulateur>
+				</WhenNotApplicable>
+				<WhenApplicable
+					dottedName="location de logement . meublé . cotisations"
+					engine={engine}
+				>
+					<SimulationGoal
+						dottedName="location de logement . meublé . cotisations"
+						small
+					/>
+				</WhenApplicable>
 			</SimulationGoals>
 		</Simulation>
 	)
--- site/test/modele-social/économie-collaborative.test.ts
+++ site/test/modele-social/économie-collaborative.test.ts
@@ -12,7 +12,7 @@ describe('Économie collaborative', () => {
 
 			it('applique correctement l’abattement et le taux de cotisation', () => {
 				const e = engine.setSituation({
-					'location de logement . meublé . loyer . net': '40000 €',
+					'location de logement . meublé . loyer . net': '40000 €/an',
 				})
 				expect(e).toEvaluate(
 					'location de logement . meublé . cotisations',
@@ -22,14 +22,15 @@ describe('Économie collaborative', () => {
 
 			it('applique le régime général jusqu’à 77 700 €', () => {
 				const e = engine.setSituation({
-					'location de logement . meublé . loyer . net': '77700 €',
+					'location de logement . meublé . loyer . net': '77700 €/an',
 				})
 				expect(e).toBeApplicable('location de logement . meublé . cotisations')
 			})
 			it('n’applique pas le régime général si plus de 77 700 €', () => {
 				const e = engine.setSituation({
-					'location de logement . meublé . loyer . net': '77701 €',
+					'location de logement . meublé . loyer . net': '77701 €/an',
 				})
+
 				expect(e).not.toBeApplicable(
 					'location de logement . meublé . cotisations'
 				)
