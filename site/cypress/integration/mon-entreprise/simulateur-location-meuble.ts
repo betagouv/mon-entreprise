b82e528c4b37b083c0934ff71bb193bf1ce9d66d
Jalil Arfaoui <jalil@arfaoui.net>
2025-02-05 22:43:16 +0100

    test(économie-collaborative): vérifie que les cotisations s’affichent bien quand on saisit les recettes

--- site/cypress/integration/mon-entreprise/simulateur-location-meuble.ts
+++ site/cypress/integration/mon-entreprise/simulateur-location-meuble.ts
@@ -17,9 +17,19 @@ describe('Simulateur de location de meublé', () => {
 	})
 
 	it('affiche le formulaire', () => {
-		cy.get('input#location_de_logement___meublé___loyer___net').should(
-			'be.visible'
+		cy.get(
+			'input#location_de_logement_meublé___courte_durée___recettes'
+		).should('be.visible')
+	})
+
+	it('chiffre les cotisations quand on saisi des revenus', () => {
+		cy.get('input#location_de_logement_meublé___courte_durée___recettes').type(
+			'1000'
 		)
+
+		cy.get('input#location_de_logement_meublé___cotisations')
+			.should('be.visible')
+			.should('have.lengthOf.at.least', 1)
 	})
 
 	it('est accessible', function () {
