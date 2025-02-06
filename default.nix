eb497f0698d6798589ca589906924fe9b99af40e
Jalil Arfaoui <jalil@arfaoui.net>
2025-02-06 16:10:31 +0100

    chore: better direnv config for cypress

--- default.nix
+++ default.nix
@@ -1,5 +1,14 @@
 { pkgs ? import <nixos-unstable> {} }:
   pkgs.mkShell {
-    nativeBuildInputs = with pkgs; [ nodejs yarn ];
+	nativeBuildInputs = with pkgs; [
+		nodejs
+		yarn
+		cypress
+	];
+
+    shellHook = ''
+	  export CYPRESS_INSTALL_BINARY=0
+	  export CYPRESS_RUN_BINARY=${pkgs.cypress}/bin/Cypress
+	'';
 }
 
