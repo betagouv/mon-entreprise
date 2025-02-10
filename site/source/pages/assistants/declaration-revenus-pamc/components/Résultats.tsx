89a21786e1b8a56b1b1855f9e4adcf97d7a7c4ef
Alice Dahan <lilice.dhn@gmail.com>
2025-02-10 14:26:55 +0100

    feat(pam): Mise en forme des résultats

--- site/source/pages/assistants/declaration-revenus-pamc/components/Résultats.tsx
+++ site/source/pages/assistants/declaration-revenus-pamc/components/Résultats.tsx
@@ -1,9 +1,15 @@
+import { Trans } from 'react-i18next'
+import { styled } from 'styled-components'
+
 import { Condition } from '@/components/EngineValue/Condition'
 import { WhenAlreadyDefined } from '@/components/EngineValue/WhenAlreadyDefined'
 import { SimulationValue } from '@/components/Simulation/SimulationValue'
 import { Emoji } from '@/design-system/emoji'
-import { Container } from '@/design-system/layout'
-import { H2, H3 } from '@/design-system/typography/heading'
+import { Container, Spacing } from '@/design-system/layout'
+import { H2, H4 } from '@/design-system/typography/heading'
+import { Body } from '@/design-system/typography/paragraphs'
+
+import { LigneImpôts } from './LigneImpôts'
 
 export default function Résultats() {
 	return (
@@ -13,8 +19,10 @@ export default function Résultats() {
 				backgroundColor={(theme) => theme.colors.bases.primary[600]}
 			>
 				<H2>
-					<Emoji emoji="📄" /> Montants à reporter dans votre déclaration de
-					revenus
+					<Emoji emoji="📄" />{' '}
+					<Trans i18nKey="pages.assistants.declaration-revenus-pamc.resultats.1">
+						Montants à reporter dans votre déclaration de revenus
+					</Trans>
 				</H2>
 
 				<SimulationValue
@@ -22,64 +30,119 @@ export default function Résultats() {
 					label="Situation au 1er janvier ou à la date du début d’activité"
 				/>
 
-				<H3>Recettes brutes</H3>
-				<SimulationValue
+				<LigneImpôts
 					dottedName="déclaration revenus PAMC . recettes brutes totales"
+					code="DSCS"
 					label="Recettes brutes totales tirées des activités non salariées"
 				/>
 
 				<Condition expression="déclaration revenus PAMC . revenus de remplacement . total">
-					<H3>Montant des revenus de remplacement</H3>
+					<H4 as="h3">
+						<Trans i18nKey="pages.assistants.declaration-revenus-pamc.resultats.2">
+							Montant des revenus de remplacement
+						</Trans>
+					</H4>
 				</Condition>
-				<SimulationValue
+				<LigneImpôts
 					dottedName="déclaration revenus PAMC . revenus de remplacement . AJPA"
+					code="DSAG"
 					label="Montant des allocations journalières du proche aidant (AJPA) versées par la CAF"
 				/>
 
 				<Condition expression="déclaration revenus PAMC . déductions et exonérations . total déductible">
-					<H3>Déductions et exonérations</H3>
+					<H4 as="h3">
+						<Trans i18nKey="pages.assistants.declaration-revenus-pamc.resultats.3">
+							Déductions et exonérations
+						</Trans>
+					</H4>
 				</Condition>
-				<SimulationValue
+				<LigneImpôts
 					dottedName="déclaration revenus PAMC . déductions et exonérations . zone déficitaire en offre de soins"
+					code="DSFA"
 					label="Exonération zone déficitaire en offre de soins"
 				/>
-				<SimulationValue
+				<LigneImpôts
 					dottedName="déclaration revenus PAMC . déductions et exonérations . chèques vacances"
+					code="DSCN"
 					label="Chèques vacances déduits du revenu imposable"
 				/>
 
-				<H3>Cotisations sociales obligatoires</H3>
-				<SimulationValue
+				<H4 as="h3">
+					<Trans i18nKey="pages.assistants.declaration-revenus-pamc.resultats.4">
+						Cotisations sociales obligatoires
+					</Trans>
+				</H4>
+				<LigneImpôts
 					dottedName="déclaration revenus PAMC . cotisations sociales obligatoires"
+					code="DSCA"
 					label="Cotisations sociales obligatoires déduites du résultat imposable"
 				/>
 
-				<H3>Répartition des revenus nets</H3>
-				<SimulationValue
+				<H4 as="h3">
+					<Trans i18nKey="pages.assistants.declaration-revenus-pamc.resultats.5">
+						Répartition des revenus nets
+					</Trans>
+				</H4>
+				<StyledBody>
+					<Trans i18nKey="pages.assistants.declaration-revenus-pamc.resultats.6">
+						Revenus nets de l’activité conventionnée&nbsp;:
+					</Trans>
+				</StyledBody>
+				<LigneImpôts
 					dottedName="déclaration revenus PAMC . revenus nets . revenus conventionnés"
-					label="Revenu net de l’activité conventionnée"
+					code="DSGA"
+					label="Bénéfice"
 				/>
-				<SimulationValue
+				<StyledBody>
+					<Trans i18nKey="pages.assistants.declaration-revenus-pamc.resultats.7">
+						Revenus nets tirés des autres activités non salariées&nbsp;:
+					</Trans>
+				</StyledBody>
+				<LigneImpôts
 					dottedName="déclaration revenus PAMC . revenus nets . revenus non conventionnés"
-					label="Revenus nets tirés des autres activités non salariées"
+					code="DSCR"
+					label="Bénéfice"
 				/>
-				<SimulationValue
+				<LigneImpôts
 					dottedName="déclaration revenus PAMC . revenus nets . revenus structures de soins"
+					code="DSAT"
 					label="Dont revenus nets de l’activité réalisée dans des structures de soins"
 				/>
 
-				<H3>Données transmises par l’Assurance Maladie</H3>
+				<H4 as="h3">
+					<Trans i18nKey="pages.assistants.declaration-revenus-pamc.resultats.8">
+						Données transmises par l’Assurance Maladie
+					</Trans>
+				</H4>
 				<Condition expression="déclaration revenus PAMC . profession != 'dentiste'">
-					<SimulationValue dottedName="déclaration revenus PAMC . SNIR . honoraires remboursables" />
-					<SimulationValue dottedName="déclaration revenus PAMC . SNIR . dépassements honoraires" />
+					<LigneImpôts
+						dottedName="déclaration revenus PAMC . SNIR . honoraires remboursables"
+						code="DSAV"
+					/>
+					<LigneImpôts
+						dottedName="déclaration revenus PAMC . SNIR . dépassements honoraires"
+						code="DSAW"
+					/>
 				</Condition>
-				<SimulationValue dottedName="déclaration revenus PAMC . SNIR . honoraires tarifs opposables" />
-				<SimulationValue dottedName="déclaration revenus PAMC . SNIR . honoraires hors forfaits" />
-				<SimulationValue
-					dottedName="déclaration revenus PAMC . SNIR . taux urssaf"
-					displayedUnit="%"
+				<LigneImpôts
+					dottedName="déclaration revenus PAMC . SNIR . honoraires tarifs opposables"
+					code="DSAX"
 				/>
+				<LigneImpôts
+					dottedName="déclaration revenus PAMC . SNIR . honoraires hors forfaits"
+					code="DSAY"
+				/>
+				<LigneImpôts
+					dottedName="déclaration revenus PAMC . SNIR . taux urssaf"
+					code="DSAZ"
+				/>
+
+				<Spacing xxl />
 			</Container>
 		</WhenAlreadyDefined>
 	)
 }
+
+const StyledBody = styled(Body)`
+	margin-bottom: 0;
+`
