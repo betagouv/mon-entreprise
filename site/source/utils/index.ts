eff291b75c98ee28d488f677951eeaa330cd90a9
logic-fabric <lowic@logic-fabric.fr>
2025-03-14 10:20:31 +0100

    refactor(security): replace generateUuid by uuid.v4

--- site/source/utils/index.ts
+++ site/source/utils/index.ts
@@ -225,10 +225,6 @@ export async function getIframeOffset(): Promise<number> {
 	})
 }
 
-export const generateUuid = () => {
-	return Math.floor(Math.random() * Date.now()).toString(16)
-}
-
 /**
  * Returns true if x is not null, useful for filtering out nulls from arrays
  * @example [1, null, 2].filter(isNotNull) // [1, 2]
