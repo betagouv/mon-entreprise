75816de2462a44d59fc1ebfb70559d9077314b41
Alice Dahan <lilice.dhn@gmail.com>
2025-05-29 11:54:35 +0200

    refactor(cmg): déplace les ressources au niveau de la situation

--- site/source/contextes/cmg/domaine/situationCMG.ts
+++ site/source/contextes/cmg/domaine/situationCMG.ts
@@ -1,16 +1,14 @@
-import * as O from 'effect/Option'
-
 import * as M from '@/domaine/Montant'
 
 import { DéclarationDeGarde } from './déclaration-de-garde'
 import { EnfantsÀCharge } from './enfant'
 
 export interface MoisHistorique<PrénomsEnfants extends string = string> {
-	ressources: O.Option<M.Montant<'Euro'>>
 	déclarationsDeGarde: Array<DéclarationDeGarde<PrénomsEnfants>>
 }
 
 export interface SituationCMG<PrénomsEnfants extends string = string> {
+	ressources: M.Montant<'EuroParAn'>
 	enfantsÀCharge: EnfantsÀCharge<PrénomsEnfants>
 	historique: {
 		mars: MoisHistorique<PrénomsEnfants>
