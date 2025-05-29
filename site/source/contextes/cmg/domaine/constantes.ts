68c46916a7124adc19c5b4375b7d631c196bc67c
Alice Dahan <lilice.dhn@gmail.com>
2025-05-29 14:27:08 +0200

    refactor(cmg): déplace toutes les constantes dans un seul fichier

--- site/source/contextes/cmg/domaine/constantes.ts
+++ site/source/contextes/cmg/domaine/constantes.ts
@@ -1,1 +1,49 @@
+import * as M from '@/domaine/Montant'
+
+import { TypologieDeGarde } from './typologie-de-garde'
+
 export const DATE_RÉFORME = new Date('2025-09-01')
+
+export const PLAFOND_DE_RESSOURCES = M.eurosParMois(8_500)
+
+export const NOMBRE_MIN_MOIS_EMPLOYEUREUSE = 2
+
+export const PLANCHER_HEURES_DE_GARDE_PAR_TYPOLOGIE: Record<
+	TypologieDeGarde,
+	number
+> = {
+	'AMA Enfant unique 0-3 ans': 100,
+	'AMA Enfant unique 3-6 ans': 50,
+	'AMA Fratrie 0-3 ans': 150,
+	'AMA Fratrie 0-6 ans': 100,
+	GED: 50,
+}
+
+export const ANNÉE_DE_NAISSANCE_EXCLUE = 2022
+
+export const TEH_PAR_GARDE_ET_NB_ENFANTS = {
+	AMA: {
+		1: 0.0619,
+		2: 0.0516,
+		3: 0.0413,
+		4: 0.031,
+		8: 0.0206,
+	},
+	GED: {
+		1: 0.1238,
+		2: 0.1032,
+		3: 0.0826,
+		4: 0.062,
+		8: 0.0412,
+	},
+}
+
+export const COÛT_HORAIRE_MAXIMAL = {
+	AMA: 8,
+	GED: 15,
+}
+
+export const COÛT_HORAIRE_MÉDIAN = {
+	AMA: 4.85,
+	GED: 10.38,
+}
