177ce4f95c6df58f9ec6f2da1f0f57ac7d72e890
Alice Dahan <lilice.dhn@gmail.com>
2025-05-29 18:56:38 +0200

    refactor(cmg): ajoute l'attribut prénom à Enfant

--- site/source/contextes/cmg/domaine/enfantFactory.ts
+++ site/source/contextes/cmg/domaine/enfantFactory.ts
@@ -1,34 +1,42 @@
+import * as O from 'effect/Option'
+
 import { Enfant } from './enfant'
 
 export class EnfantFactory {
-	private dateDeNaissance = new Date('2023-01-01')
+	private prénom
+	private dateDeNaissance = O.some(new Date('2023-01-01'))
+
+	constructor(prénom: string) {
+		this.prénom = O.some(prénom)
+	}
 
 	moinsDe3Ans() {
-		this.dateDeNaissance = new Date('2023-02-18')
+		this.dateDeNaissance = O.some(new Date('2023-02-18'))
 
 		return this
 	}
 
 	plusDe3Ans() {
-		this.dateDeNaissance = new Date('2020-07-31')
+		this.dateDeNaissance = O.some(new Date('2020-07-31'))
 
 		return this
 	}
 
 	néEn(année: number) {
-		this.dateDeNaissance = new Date(`${année}-12-31`)
+		this.dateDeNaissance = O.some(new Date(`${année}-12-31`))
 
 		return this
 	}
 
 	plusDe6Ans() {
-		this.dateDeNaissance = new Date('2019-08-31')
+		this.dateDeNaissance = O.some(new Date('2019-08-31'))
 
 		return this
 	}
 
 	build() {
 		return {
+			prénom: this.prénom,
 			dateDeNaissance: this.dateDeNaissance,
 		} as const satisfies Enfant
 	}
