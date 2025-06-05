475d850d72bc92caec6780d09c6afeac17185a6d
Alice Dahan <lilice.dhn@gmail.com>
2025-06-05 13:52:48 +0200

    feat(cmg): ajoute la question sur la situation familiale

--- site/source/pages/assistants/cmg/pages/InformationsGénérales.tsx
+++ site/source/pages/assistants/cmg/pages/InformationsGénérales.tsx
@@ -1,9 +1,11 @@
 import Ressources from '../components/informations-générales/Ressources'
+import SituationFamiliale from '../components/informations-générales/SituationFamiliale'
 import Navigation from '../components/Navigation'
 
 export default function InformationsGénérales() {
 	return (
 		<>
+			<SituationFamiliale />
 			<Ressources />
 
 			<Navigation précédent="index" suivant="enfants" />
