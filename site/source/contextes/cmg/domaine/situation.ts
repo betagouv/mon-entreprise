695dcaf2b6e183308092c99c0ed4ebb0704c8425
Alice Dahan <lilice.dhn@gmail.com>
2025-06-07 18:31:05 +0200

    refactor(cmg): modifie le retour de la fonction éligibilité

--- site/source/contextes/cmg/domaine/situation.ts
+++ site/source/contextes/cmg/domaine/situation.ts
@@ -1,10 +1,17 @@
+import * as A from 'effect/Array'
 import * as O from 'effect/Option'
+import * as R from 'effect/Record'
 
 import * as M from '@/domaine/Montant'
 import { Situation } from '@/domaine/Situation'
 
 import { EnfantsÀCharge } from './enfant'
-import { SalariéeAMA, SalariéeGED } from './salariée'
+import {
+	estSalariéeAMAValide,
+	estSalariéeGEDValide,
+	SalariéeAMA,
+	SalariéeGED,
+} from './salariée'
 
 export interface SituationCMG<PrénomsEnfants extends string = string>
 	extends Situation {
@@ -32,7 +39,13 @@ export const estSituationCMGValide = (
 	O.isSome(situation.aPerçuCMG) &&
 	O.isSome(situation.plusDe2MoisDeDéclaration) &&
 	O.isSome(situation.parentIsolé) &&
-	O.isSome(situation.ressources)
+	O.isSome(situation.ressources) &&
+	O.isSome(situation.enfantsÀCharge.perçoitAeeH) &&
+	!!R.size(situation.enfantsÀCharge.enfants) &&
+	(A.isNonEmptyArray(situation.modesDeGarde.AMA) ||
+		A.isNonEmptyArray(situation.modesDeGarde.GED)) &&
+	A.every(situation.modesDeGarde.AMA, estSalariéeAMAValide) &&
+	A.every(situation.modesDeGarde.GED, estSalariéeGEDValide)
 
 export const initialSituationCMG: SituationCMG = {
 	_tag: 'Situation',
