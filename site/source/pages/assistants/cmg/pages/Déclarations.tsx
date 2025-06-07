8663abe33c06c24b7e87300c9ef2624ce6112d69
Alice Dahan <lilice.dhn@gmail.com>
2025-06-07 20:40:26 +0200

    feat(cmg): affiche l'inéligibilité après la 2ème page

--- site/source/pages/assistants/cmg/pages/Déclarations.tsx
+++ site/source/pages/assistants/cmg/pages/Déclarations.tsx
@@ -1,4 +1,4 @@
-import { useCMG } from '@/contextes/cmg'
+import { RaisonInéligibilité, useCMG } from '@/contextes/cmg'
 
 import AMA from '../components/AMA/AMA'
 import GED from '../components/GED/GED'
@@ -6,9 +6,17 @@ import Navigation from '../components/Navigation'
 import NonÉligible from './NonÉligible'
 
 export default function Déclarations() {
-	const { estÉligible } = useCMG()
+	const { raisonsInéligibilité } = useCMG()
+	const raisonsInéligibilitéValables: Array<RaisonInéligibilité> = [
+		'ressources',
+		'enfants-à-charge',
+	]
 
-	if (estÉligible === false) {
+	if (
+		raisonsInéligibilitéValables.some((raison) =>
+			raisonsInéligibilité.includes(raison)
+		)
+	) {
 		return <NonÉligible précédent="enfants" />
 	}
 
