e4d66b9b4bf4abfc6367ca86ec0eb42221ef9830
Alice Dahan <lilice.dhn@gmail.com>
2025-06-12 12:32:32 +0200

    feat(cmg): supprime le paragraphe d'introduction

--- site/source/pages/assistants/cmg/pages/Accueil.tsx
+++ site/source/pages/assistants/cmg/pages/Accueil.tsx
@@ -1,42 +1,15 @@
-import { Trans, useTranslation } from 'react-i18next'
+import { Trans } from 'react-i18next'
 
 import { TrackPage } from '@/components/ATInternetTracking'
-import PageHeader from '@/components/PageHeader'
 import Warning from '@/components/ui/WarningBlock'
-import { Body, Intro, Link, Strong } from '@/design-system'
+import { Body, Strong } from '@/design-system'
 
 import Navigation from '../components/Navigation'
 
 export default function Accueil() {
-	const { t } = useTranslation()
-
 	return (
 		<>
 			<TrackPage name="accueil" />
-
-			<PageHeader>
-				<Trans i18nKey="pages.assistants.cmg.description">
-					<Intro>
-						La réforme du mode de calcul du CMG peut entraîner une diminution du
-						montant de votre prestation (voir le simulateur{' '}
-						<Link
-							href="https://www.urssaf.fr/accueil/outils-documentation/simulateurs/calculer-reste-a-charge-cmg-particulier-employeur.html"
-							target="_blank"
-							aria-label={t(
-								'pages.assistants.cmg.simulateur-1.aria-label',
-								'Aller sur la page "Évaluez votre reste à charge" sur urssaf.fr, nouvelle fenêtre.'
-							)}
-						>
-							Évaluez votre reste à charge
-						</Link>
-						). Dans ce cas, vous pouvez bénéficier d’un complément transitoire
-						si vous remplissez les conditions.
-						<br />
-						Cet outil vous permet d’estimer si vous avez droit à ce complément
-						et si oui, quel sera son montant <Strong>à titre indicatif</Strong>.
-					</Intro>
-				</Trans>
-			</PageHeader>
 
 			<Warning localStorageKey="pages.assistants.cmg.warning">
 				<Trans i18nKey="assistants.warning.cmg">
