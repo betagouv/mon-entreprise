af7a016c7580fedeede75d2ecdbfd8e414ee93fe
Alice Dahan <lilice.dhn@gmail.com>
2025-06-13 15:42:48 +0200

    feat(cmg): rend la page déclarations mobile friendly

--- site/source/pages/assistants/cmg/components/GED/AideSaisieGED.tsx
+++ site/source/pages/assistants/cmg/components/GED/AideSaisieGED.tsx
@@ -10,12 +10,27 @@ export default function AideSaisieGED() {
 			<AideSaisieHeuresDeGardeContainer>
 				<AideSaisieHeuresDeGarde />
 			</AideSaisieHeuresDeGardeContainer>
-			<AideSaisieRémunération />
-			<AideSaisieCMG />
+			<MobileHiddenContainer>
+				<AideSaisieHeuresDeGarde />
+			</MobileHiddenContainer>
+			<MobileHiddenContainer>
+				<AideSaisieRémunération />
+			</MobileHiddenContainer>
+			<MobileHiddenContainer>
+				<AideSaisieCMG />
+			</MobileHiddenContainer>
 		</>
 	)
 }
 
 const AideSaisieHeuresDeGardeContainer = styled.div`
 	grid-row-start: 2;
+	@media (max-width: ${({ theme }) => theme.breakpointsWidth.sm}) {
+		display: none;
+	}
+`
+const MobileHiddenContainer = styled.div`
+	@media (max-width: ${({ theme }) => theme.breakpointsWidth.sm}) {
+		display: none;
+	}
 `
