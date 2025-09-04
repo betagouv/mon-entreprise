8ac0a34865c8b8faf7e26cc6e02dc0cd3e2dd8b1
Alice Dahan <lilice.dhn@gmail.com>
2025-09-04 13:24:24 +0200

    tests(partage simulation): correction du test e2e

--- site/cypress/integration/mon-entreprise/partage-simulation.js
+++ site/cypress/integration/mon-entreprise/partage-simulation.js
@@ -4,37 +4,44 @@ describe('Partage (simulateur salarié)', function () {
 	const brutInputSelector = '#salarié___contrat___salaire_brut-input'
 	const simulatorUrl = '/simulateurs/salaire-brut-net'
 	const searchParams = new URLSearchParams({
-		'salaire-brut': '1539€/mois',
 		'salarié . contrat': "'CDD'",
-		unite: '€/mois',
+		'salarié . contrat . salaire brut': '2700 €/mois',
+		unité: '€/mois',
 	})
 
-	const urlWithState = `${simulatorUrl}?${searchParams.toString()}`
+	const urlWithSearchParams = `${simulatorUrl}?${searchParams.toString()}`
 	if (!fr) {
 		return
 	}
 
 	it('devrait remplir les champs à partir de l’URL de partage', function () {
-		cy.visit(urlWithState)
-		cy.contains('Modifier mes réponses')
+		cy.visit(urlWithSearchParams)
 
-		cy.get(brutInputSelector, { timeout: 30 * 1000 })
+		cy.contains('Modifier mes réponses').should('be.visible')
+
+		cy.get(brutInputSelector)
 			.invoke('val')
-			.should('match', /1[\s]539[\s]€/)
+			.should(($value) => {
+				expect($value).to.be.equal('2 700 €')
+			})
 
 		cy.contains('Modifier mes réponses').click()
-		cy.contains('CDD')
+		cy.get('div[data-cy="modal"]')
+			.should('include.text', 'CDD')
+			.and('include.text', '2 700 €/mois')
 	})
 
 	it('devrait générer une URL de partage à partir de champs remplis', function () {
 		cy.visit(simulatorUrl)
-		cy.get(brutInputSelector).first().type('{selectall}1539')
+
+		cy.get(brutInputSelector).first().type('{selectall}2700')
 		cy.contains('De quel type de contrat').should('be.visible')
 		cy.get('label').contains('CDD').should('be.visible').click('left')
 		cy.get('button').contains('Suivant').should('be.visible')
+
 		cy.contains('Générer un lien').click()
 		cy.get('input[id="simulation-share-url"]')
 			.invoke('val')
-			.should('eq', Cypress.config().baseUrl + urlWithState)
+			.should('eq', Cypress.config().baseUrl + urlWithSearchParams)
 	})
 })
