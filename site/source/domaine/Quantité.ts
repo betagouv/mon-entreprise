f1a8604399c3c017e3a196543a039d52863782ea
Alice Dahan <lilice.dhn@gmail.com>
2025-09-05 10:45:35 +0200

    refactor(Montant): utilisation du raccourci montant au lieu de makeMontant

--- site/source/domaine/Quantité.ts
+++ site/source/domaine/Quantité.ts
@@ -15,66 +15,36 @@ export const isQuantité = (something: unknown): something is Quantité =>
 const makeQuantité = Data.tagged<Quantité>('Quantité')
 
 export const pourcentage = (valeur: number): Quantité<'%'> =>
-	makeQuantité({
-		valeur,
-		unité: '%',
-	}) as Quantité<'%'>
+	quantité(valeur, '%')
 
 export const heuresParMois = (valeur: number): Quantité<'heures/mois'> =>
-	makeQuantité({
-		valeur,
-		unité: 'heures/mois',
-	}) as Quantité<'heures/mois'>
+	quantité(valeur, 'heures/mois')
 
 export const heuresParSemaine = (valeur: number): Quantité<'heures/semaine'> =>
-	makeQuantité({
-		valeur,
-		unité: 'heures/semaine',
-	}) as Quantité<'heures/semaine'>
+	quantité(valeur, 'heures/semaine')
 
 export const jours = (valeur: number): Quantité<'jours'> =>
-	makeQuantité({
-		valeur,
-		unité: 'jours',
-	}) as Quantité<'jours'>
+	quantité(valeur, 'jours')
 
 export const joursOuvrés = (valeur: number): Quantité<'jours ouvrés'> =>
-	makeQuantité({
-		valeur,
-		unité: 'jours ouvrés',
-	}) as Quantité<'jours ouvrés'>
+	quantité(valeur, 'jours ouvrés')
 
 export const mois = (valeur: number): Quantité<'mois'> =>
-	makeQuantité({
-		valeur,
-		unité: 'mois',
-	}) as Quantité<'mois'>
+	quantité(valeur, 'mois')
 
 export const trimestreCivil = (valeur: number): Quantité<'trimestre civil'> =>
-	makeQuantité({
-		valeur,
-		unité: 'trimestre civil',
-	}) as Quantité<'trimestre civil'>
+	quantité(valeur, 'trimestre civil')
 
 export const annéeCivile = (valeur: number): Quantité<'année civile'> =>
-	makeQuantité({
-		valeur,
-		unité: 'année civile',
-	}) as Quantité<'année civile'>
+	quantité(valeur, 'année civile')
 
 export const employés = (valeur: number): Quantité<'employés'> =>
-	makeQuantité({
-		valeur,
-		unité: 'employés',
-	}) as Quantité<'employés'>
+	quantité(valeur, 'employés')
 
 export const titresRestaurantParMois = (
 	valeur: number
 ): Quantité<'titre-restaurant/mois'> =>
-	makeQuantité({
-		valeur,
-		unité: 'titre-restaurant/mois',
-	}) as Quantité<'titre-restaurant/mois'>
+	quantité(valeur, 'titre-restaurant/mois')
 
 export const quantité = <U extends UnitéQuantité>(
 	valeur: number,
