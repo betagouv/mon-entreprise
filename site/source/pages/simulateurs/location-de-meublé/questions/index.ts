644ef01c6aee88e1c1d6d1e0dd9ad8a2cab7d53e
Jalil Arfaoui <jalil@arfaoui.net>
2025-09-30 16:59:28 +0200

    feat(économie-collaborative): ajoute la question sur le type de location

--- site/source/pages/simulateurs/location-de-meublé/questions/index.ts
+++ site/source/pages/simulateurs/location-de-meublé/questions/index.ts
@@ -1,3 +1,4 @@
+export { TypeLocationQuestion } from './TypeLocationQuestion'
 export { RegimeCotisationQuestion } from './RegimeCotisationQuestion'
 export { AlsaceMoselleQuestion } from './AlsaceMoselleQuestion'
 export { PremiereAnneeQuestion } from './questionPremiereAnnee'
