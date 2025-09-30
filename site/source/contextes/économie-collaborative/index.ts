26df8301f77bdb233c98764404a00f67c9e7fc72
Jalil Arfaoui <jalil@arfaoui.net>
2025-09-30 15:43:03 +0200

    refactor(économie-collaborative): utilise des constantes hors Publicodes
    - Remplace le composant `Value` par des constantes (`PLAFOND_REGIME_GENERAL`, `SEUIL_PROFESSIONNALISATION`) formatées avec `formatMontant`
    - Centralise les exports liés à l'économie collaborative pour simplifier les imports

--- site/source/contextes/économie-collaborative/index.ts
+++ site/source/contextes/économie-collaborative/index.ts
@@ -1,1 +1,10 @@
 export { useEconomieCollaborative } from './hooks/useEconomieCollaborative'
+export { ÉconomieCollaborativeProvider } from './hooks/ÉconomieCollaborativeContext'
+export {
+	estSituationValide,
+	usagerAChoisiUnRégimeDeCotisation,
+	type SituationÉconomieCollaborative,
+} from './domaine/location-de-meublé/situation'
+export { type SimulationImpossible } from './domaine/location-de-meublé/erreurs'
+export { SEUIL_PROFESSIONNALISATION } from './domaine/location-de-meublé/constantes'
+export { PLAFOND_REGIME_GENERAL } from './domaine/location-de-meublé/régime-général'
