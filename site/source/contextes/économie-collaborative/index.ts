c9c76596d19188481a1c3d6a2485dc90b6d8bda4
Jalil Arfaoui <jalil@arfaoui.net>
2025-10-16 08:53:19 +0200

    refactor(économie-collaborative): nomme les types de résultats de régime applicables et inapplicables

--- site/source/contextes/économie-collaborative/index.ts
+++ site/source/contextes/économie-collaborative/index.ts
@@ -8,3 +8,10 @@ export {
 export { type SimulationImpossible } from './domaine/location-de-meublé/erreurs'
 export { SEUIL_PROFESSIONNALISATION } from './domaine/location-de-meublé/constantes'
 export { PLAFOND_REGIME_GENERAL } from './domaine/location-de-meublé/régime-général'
+export { RégimeTag } from './ui/RégimeTag'
+export {
+	compareRégimes,
+	type RésultatRégimeApplicable,
+	type RésultatRégimeNonApplicable,
+} from './domaine/location-de-meublé/comparateur-régimes'
+export { RegimeCotisation } from './domaine/location-de-meublé/situation'
