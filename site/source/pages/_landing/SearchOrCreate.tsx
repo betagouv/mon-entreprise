a75fccb7326da2b70cce560eb5923b1bd098a519
Alice Dahan <lilice.dhn@gmail.com>
2025-10-20 13:35:00 +0200

    refactor(entreprise): crée le hook useSetEntrepriseFromUrssafConnection

--- site/source/pages/_landing/SearchOrCreate.tsx
+++ site/source/pages/_landing/SearchOrCreate.tsx
@@ -1,11 +1,10 @@
-import { useCallback, useEffect } from 'react'
+import { useCallback } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 import { generatePath, useNavigate } from 'react-router-dom'
 
 import { EntrepriseDetailsCard } from '@/components/entreprise/EntrepriseDetailsCard'
 import { EntrepriseSearchField } from '@/components/entreprise/EntrepriseSearchField'
-import { useEngine } from '@/components/utils/EngineContext'
 import {
 	AnswerGroup,
 	Body,
@@ -16,10 +15,9 @@ import {
 	Spacing,
 } from '@/design-system'
 import { Entreprise } from '@/domaine/Entreprise'
-import { useEntreprisesRepository } from '@/hooks/useRepositories'
 import { useSetEntreprise } from '@/hooks/useSetEntreprise'
+import useSetEntrepriseFromUrssafConnection from '@/hooks/useSetEntrepriseFromUrssafConnection'
 import { useSitePaths } from '@/sitePaths'
-import { getCookieValue } from '@/storage/readCookie'
 import { resetCompany } from '@/store/actions/companyActions'
 import { companySirenSelector } from '@/store/selectors/companySiren.selector'
 
@@ -115,45 +113,4 @@ function useHandleCompanySubmit() {
 	)
 
 	return handleCompanySubmit
-}
-
-function useSetEntrepriseFromUrssafConnection() {
-	const setEntreprise = useSetEntreprise()
-	const siret = siretFromUrssafFrConnection()
-	const companySIREN = useEngine().evaluate('entreprise . SIREN').nodeValue
-	const entreprisesRepository = useEntreprisesRepository()
-
-	useEffect(() => {
-		if (siret && !companySIREN) {
-			entreprisesRepository
-				.rechercheTexteLibre(siret)
-				.then((results) => {
-					if (results?.length !== 1) {
-						return
-					}
-					setEntreprise(results[0])
-				})
-				.catch((err) => {
-					console.log(err)
-					console.log(`Could not fetch company details for ${siret}`)
-				})
-		}
-	}, [siret, companySIREN])
-}
-
-// We can read cookies set on the urssaf.fr domain, which contain informations
-// such as the SIRET number. The cookie format could change at any time so we
-// wrap its read access in a `try / catch`.
-function siretFromUrssafFrConnection(): string | null {
-	try {
-		// Note: The `ctxUrssaf` contains more informations, but currently we only
-		// need to retreive the SIRET which is slightly more easy to read from the
-		// `EnLigne` cookie.
-		const cookieValue = decodeURIComponent(getCookieValue('EnLigne'))
-		const siret = cookieValue.match('siret=([0-9]{14})')?.pop()
-
-		return siret ?? null
-	} catch {
-		return null
-	}
 }
