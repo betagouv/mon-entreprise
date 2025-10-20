5438664b8346b40d09d43bb8d64a312d3cbebd8b
Alice Dahan <lilice.dhn@gmail.com>
2025-10-20 13:34:24 +0200

    refactor(entreprise): remplace useEngine par companySirenSelector

--- site/source/pages/_landing/SearchOrCreate.tsx
+++ site/source/pages/_landing/SearchOrCreate.tsx
@@ -1,6 +1,6 @@
 import { useCallback, useEffect } from 'react'
 import { Trans, useTranslation } from 'react-i18next'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { generatePath, useNavigate } from 'react-router-dom'
 
 import { EntrepriseDetailsCard } from '@/components/entreprise/EntrepriseDetailsCard'
@@ -21,15 +21,14 @@ import { useSetEntreprise } from '@/hooks/useSetEntreprise'
 import { useSitePaths } from '@/sitePaths'
 import { getCookieValue } from '@/storage/readCookie'
 import { resetCompany } from '@/store/actions/companyActions'
-
-// import { RootState } from '@/store/reducers/rootReducer'
+import { companySirenSelector } from '@/store/selectors/companySiren.selector'
 
 export default function SearchOrCreate() {
 	const { absoluteSitePaths } = useSitePaths()
 	// const statutChoisi = useSelector(
 	// 	(state: RootState) => state.choixStatutJuridique.companyStatusChoice
 	// )
-	const companySIREN = useEngine().evaluate('entreprise . SIREN').nodeValue
+	const companySIREN = useSelector(companySirenSelector)
 	useSetEntrepriseFromUrssafConnection()
 	const handleCompanySubmit = useHandleCompanySubmit()
 	const dispatch = useDispatch()
@@ -49,7 +48,7 @@ export default function SearchOrCreate() {
 								to={generatePath(
 									absoluteSitePaths.assistants['pour-mon-entreprise']
 										.entreprise,
-									{ entreprise: companySIREN as string }
+									{ entreprise: companySIREN }
 								)}
 								data-test-id="cta-see-custom-simulators"
 								aria-label={t(
--- site/source/pages/assistants/pour-mon-entreprise/AnnuaireEntreprises.tsx
+++ site/source/pages/assistants/pour-mon-entreprise/AnnuaireEntreprises.tsx
@@ -1,13 +1,17 @@
 import { useTranslation } from 'react-i18next'
+import { useSelector } from 'react-redux'
 
-import { useEngine } from '@/components/utils/EngineContext'
 import { Article } from '@/design-system'
+import { companySirenSelector } from '@/store/selectors/companySiren.selector'
 
 export function AnnuaireEntreprises() {
 	const { t } = useTranslation()
-	const engine = useEngine()
 
-	const siren = engine.evaluate('entreprise . SIREN').nodeValue as string
+	const siren = useSelector(companySirenSelector) as string
+
+	if (!siren) {
+		return null
+	}
 
 	return (
 		<>
