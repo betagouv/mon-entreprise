4bd0af2c84b2c0c743015bda914241acadaf1954
Jalil Arfaoui <jalil@arfaoui.net>
2025-10-24 12:52:09 +0200

    style(économie-collaborative): Corrige quelques problèmes ESLint / Prettier / TypeScript
    - Corrige l'appel à `i18n.changeLanguage` avec `void` pour éviter les avertissements TypeScript
    - Supprime un test vérifiant la présence d'une icône cachée en ARIA
    - Précise quelques types dans régime-micro-entreprise.test.ts

--- site/source/contextes/économie-collaborative/index.ts
+++ site/source/contextes/économie-collaborative/index.ts
@@ -3,18 +3,24 @@ export { ÉconomieCollaborativeProvider } from './hooks/ÉconomieCollaborativeCo
 export { isCotisationsEnabled } from './featureToggles'
 export {
 	estSituationValide,
+	RegimeCotisation,
 	type SituationÉconomieCollaborative,
+	type TypeLocation,
 } from './domaine/location-de-meublé/situation'
 export {
 	type SimulationImpossible,
 	RaisonInapplicabilité,
 } from './domaine/location-de-meublé/erreurs'
 export { SEUIL_PROFESSIONNALISATION } from './domaine/location-de-meublé/estActiviteProfessionnelle'
-export { PLAFOND_REGIME_GENERAL } from './domaine/location-de-meublé/régime-général'
+export {
+	ABATTEMENT_REGIME_GENERAL,
+	PLAFOND_REGIME_GENERAL,
+	TAUX_COTISATION_RG_ALSACE_MOSELLE,
+	TAUX_COTISATION_RG_NORMAL,
+} from './domaine/location-de-meublé/régime-général'
 export { RégimeTag } from './ui/RégimeTag'
 export {
 	compareRégimes,
 	type RésultatRégimeApplicable,
 	type RésultatRégimeNonApplicable,
 } from './domaine/location-de-meublé/comparateur-régimes'
-export { RegimeCotisation } from './domaine/location-de-meublé/situation'
