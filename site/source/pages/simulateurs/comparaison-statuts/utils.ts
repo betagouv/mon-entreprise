ed41e27d275de21b072420397952d785696c3167
Alice Dahan <lilice.dhn@gmail.com>
2025-10-27 12:08:54 +0100

    feat(comparateur): supprime la médaille

--- site/source/pages/simulateurs/comparaison-statuts/utils.ts
+++ site/source/pages/simulateurs/comparaison-statuts/utils.ts
@@ -8,21 +8,3 @@ export type OptionType = {
 	name: StatutType
 	value: Evaluation
 }
-export const getBestOption = (options: OptionType[], leastIsBest: boolean) => {
-	const sortedOptions = [...options].sort(
-		(option1: OptionType, option2: OptionType) => {
-			if (option1.value === null || option1.value === undefined) {
-				return leastIsBest ? -1 : 1
-			}
-			if (option2.value === null || option2.value === undefined) {
-				return leastIsBest ? 1 : -1
-			}
-
-			if (option1.value === option2.value) return 0
-
-			return (option1.value > option2.value ? -1 : 1) * (leastIsBest ? -1 : 1)
-		}
-	)
-
-	return sortedOptions?.[0]?.name
-}
