7c1d859f50d6281ed18625e1ca59a709a0275608
Alice Dahan <lilice.dhn@gmail.com>
2025-11-04 12:16:32 +0100

    refactor(engine): déplace useSetupSafeSituation vers hooks

--- site/source/components/Notifications.tsx
+++ site/source/components/Notifications.tsx
@@ -3,7 +3,7 @@ import { Trans, useTranslation } from 'react-i18next'
 import { useDispatch, useSelector } from 'react-redux'
 import { styled } from 'styled-components'
 
-import { useInversionFail } from '@/components/utils/EngineContext'
+import { useInversionFail } from '@/hooks/useSetupSafeSituation'
 import {
 	CloseButton,
 	Emoji,
