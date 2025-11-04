55abd87ba4f1184d96a9ff1c7298cc27bf13d8c9
Alice Dahan <lilice.dhn@gmail.com>
2025-11-04 17:36:46 +0100

    feat(docs): fait fonctionner la documentation des autres paquets

--- site/source/components/RuleLink.tsx
+++ site/source/components/RuleLink.tsx
@@ -1,30 +1,40 @@
 import { RuleLink as EngineRuleLink } from '@publicodes/react-ui'
 import Engine from 'publicodes'
-import React, { ReactNode } from 'react'
+import { ReactNode } from 'react'
+import { useSelector } from 'react-redux'
 
 import { Link } from '@/design-system'
 import { DottedName } from '@/domaine/publicodes/DottedName'
+import { useDocumentationPath } from '@/hooks/useDocumentationIndexPath'
 import { useEngine } from '@/hooks/useEngine'
-import { useSitePaths } from '@/sitePaths'
+import { nomModèleSelector } from '@/store/selectors/simulation/config/nomModèle.selector'
+
+type Props = {
+	documentationPath?: string
+	dottedName: DottedName
+	engine?: Engine<DottedName>
+	'aria-label'?: string
+	id?: string
+	children?: ReactNode
+}
 
 // TODO : quicklink -> en cas de variations ou de somme avec un seul élément actif, faire un lien vers cet élément
-export default function RuleLink(
-	props: {
-		dottedName: DottedName
-		displayIcon?: boolean
-		children?: React.ReactNode
-		documentationPath?: string
-		linkComponent?: ReactNode
-		engine?: Engine<DottedName>
-	} & Omit<React.ComponentProps<typeof Link>, 'to' | 'children'>
-) {
-	const { absoluteSitePaths } = useSitePaths()
+export default function RuleLink({
+	dottedName,
+	children,
+	documentationPath,
+	engine,
+	'aria-label': ariaLabel,
+	id,
+}: Props) {
+	const nomModèle = useSelector(nomModèleSelector)
+	const documentationIndex = useDocumentationPath(nomModèle)
 	const defaultEngine = useEngine()
 
-	const engineUsed = props?.engine ?? defaultEngine
+	const engineUsed = engine ?? defaultEngine
 
 	try {
-		engineUsed.getRule(props.dottedName)
+		engineUsed.getRule(dottedName)
 	} catch (error) {
 		// eslint-disable-next-line no-console
 		console.error(error)
@@ -34,13 +44,15 @@ export default function RuleLink(
 
 	return (
 		<EngineRuleLink
-			{...props}
+			id={id}
+			dottedName={dottedName}
+			aria-label={ariaLabel}
 			// @ts-ignore
-			linkComponent={props?.linkComponent || Link}
+			linkComponent={Link}
 			engine={engineUsed}
-			documentationPath={
-				props.documentationPath ?? absoluteSitePaths.documentation.index
-			}
+			documentationPath={documentationPath ?? documentationIndex}
+			// eslint-disable-next-line react/no-children-prop
+			children={children}
 		/>
 	)
 }
