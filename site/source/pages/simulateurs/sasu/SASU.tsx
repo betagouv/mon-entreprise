8e2d01cdf0870fb10113e6121607fb069c745a0f
Alice Dahan <lilice.dhn@gmail.com>
2025-11-04 17:37:06 +0100

    feat(modele-as): branche le simulateur SASU sur modele-as

--- site/source/pages/simulateurs/sasu/SASU.tsx
+++ site/source/pages/simulateurs/sasu/SASU.tsx
@@ -8,14 +8,15 @@ import Simulation, {
 	SimulationGoals,
 } from '@/components/Simulation'
 import { YearSelectionBanner } from '@/components/Simulation/YearSelectionBanner'
-import SalaryExplanation from '@/components/simulationExplanation/SalaryExplanation'
 import { Body, H2 } from '@/design-system'
+import { useDocumentationPath } from '@/hooks/useDocumentationIndexPath'
+import { useEngine } from '@/hooks/useEngine'
 
 export function SASUSimulation() {
 	return (
 		<>
 			<Simulation
-				explanations={<SalaryExplanation />}
+				// explanations={<SalaryExplanation />}
 				afterQuestionsSlot={<YearSelectionBanner />}
 			>
 				<SimulateurWarning
@@ -32,62 +33,79 @@ export function SASUSimulation() {
 				/>
 				<SimulationGoals>
 					<PeriodSwitch />
-					<SimulationGoal dottedName="dirigeant . rémunération . totale" />
+					<SimulationGoal dottedName="assimilé salarié . rémunération . totale" />
 					<SimulationGoal
 						editable
 						small
-						dottedName="salarié . rémunération . brut"
+						dottedName="assimilé salarié . rémunération . brute"
 					/>
 					<SimulationGoal
 						editable={false}
 						small
-						dottedName="salarié . cotisations"
+						dottedName="assimilé salarié . cotisations"
 					/>
-					<SimulationGoal dottedName="salarié . rémunération . net . à payer avant impôt" />
-					<SimulationGoal small editable={false} dottedName="impôt . montant" />
-					<SimulationGoal dottedName="salarié . rémunération . net . payé après impôt" />
+					<SimulationGoal dottedName="assimilé salarié . rémunération . nette . à payer avant impôt" />
+					<SimulationGoal
+						small
+						editable={false}
+						dottedName="assimilé salarié . rémunération . impôt"
+					/>
+					<SimulationGoal dottedName="assimilé salarié . rémunération . nette . après impôt" />
 				</SimulationGoals>
 			</Simulation>
 		</>
 	)
 }
 
-export const SeoExplanations = () => (
-	<Trans i18nKey="pages.simulateurs.sasu.seo-explanation">
-		<H2>Comment calculer le salaire d'un dirigeant de SAS ou SASU ? </H2>
-		<Body>
-			Comme pour un salarié classique, le <strong>dirigeant de SAS(U)</strong>{' '}
-			paye des cotisations sociales sur la rémunération qu'il se verse. Les
-			cotisations sont calculées de la même manière que pour le salarié : elles
-			sont décomposées en partie employeur et partie salarié et sont exprimées
-			comme un pourcentage du salaire brut.
-		</Body>
-		<Body>
-			Le dirigeant assimilé-salarié ne paye pas de{' '}
-			<strong>cotisations chômage</strong>. Par ailleurs, il ne bénéficie pas de
-			la{' '}
-			<RuleLink dottedName="salarié . cotisations . exonérations . réduction générale">
-				réduction générale de cotisations
-			</RuleLink>{' '}
-			ni des dispositifs encadrés par le code du travail comme les{' '}
-			<RuleLink dottedName="salarié . temps de travail . heures supplémentaires">
-				heures supplémentaires
-			</RuleLink>{' '}
-			ou les primes.
-		</Body>
-		<Body>
-			Il peut en revanche prétendre à la{' '}
-			<RuleLink dottedName="salarié . cotisations . exonérations . Acre">
-				réduction ACRE
-			</RuleLink>{' '}
-			en debut d'activité, sous certaines conditions.
-		</Body>
-		<Body>
-			Vous pouvez utiliser notre simulateur pour calculer la{' '}
-			<strong>rémunération nette</strong> à partir d'un montant superbrut alloué
-			à la rémunération du dirigeant. Il vous suffit pour cela saisir le montant
-			total alloué dans la case "total chargé". La simulation peut ensuite être
-			affinée en répondant aux différentes questions.
-		</Body>
-	</Trans>
-)
+export const SeoExplanations = () => {
+	const documentationPathModèleSocial = useDocumentationPath('modele-social')
+	const engineModeleSocial = useEngine('modele-social')
+
+	return (
+		<Trans i18nKey="pages.simulateurs.sasu.seo-explanation">
+			<H2>Comment calculer le salaire d'un dirigeant de SAS ou SASU ? </H2>
+			<Body>
+				Comme pour un salarié classique, le <strong>dirigeant de SAS(U)</strong>{' '}
+				paye des cotisations sociales sur la rémunération qu'il se verse. Les
+				cotisations sont calculées de la même manière que pour le salarié :
+				elles sont décomposées en partie employeur et partie salarié et sont
+				exprimées comme un pourcentage du salaire brut.
+			</Body>
+			<Body>
+				Le dirigeant assimilé-salarié ne paye pas de{' '}
+				<strong>cotisations chômage</strong>. Par ailleurs, il ne bénéficie pas
+				de la{' '}
+				<RuleLink
+					dottedName="salarié . cotisations . exonérations . réduction générale"
+					engine={engineModeleSocial}
+					documentationPath={documentationPathModèleSocial}
+				>
+					réduction générale de cotisations
+				</RuleLink>{' '}
+				ni des dispositifs encadrés par le code du travail comme les{' '}
+				<RuleLink
+					dottedName="salarié . temps de travail . heures supplémentaires"
+					engine={engineModeleSocial}
+					documentationPath={documentationPathModèleSocial}
+				>
+					heures supplémentaires
+				</RuleLink>{' '}
+				ou les primes.
+			</Body>
+			<Body>
+				Il peut en revanche prétendre à la{' '}
+				<RuleLink dottedName="assimilé salarié . exonérations . ACRE">
+					réduction ACRE
+				</RuleLink>{' '}
+				en debut d'activité, sous certaines conditions.
+			</Body>
+			<Body>
+				Vous pouvez utiliser notre simulateur pour calculer la{' '}
+				<strong>rémunération nette</strong> à partir d'un montant superbrut
+				alloué à la rémunération du dirigeant. Il vous suffit pour cela saisir
+				le montant total alloué dans la case "total chargé". La simulation peut
+				ensuite être affinée en répondant aux différentes questions.
+			</Body>
+		</Trans>
+	)
+}
