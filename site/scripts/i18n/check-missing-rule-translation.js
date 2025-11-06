73fe2c8e38e0f23d52108bc50f455540c9dc0871
Alice Dahan <lilice.dhn@gmail.com>
2025-11-06 16:40:45 +0100

    i18n: prépare les scripts de traduction à plusieurs modèles de règles

--- site/scripts/i18n/check-missing-rule-translation.js
+++ site/scripts/i18n/check-missing-rule-translation.js
@@ -1,13 +1,18 @@
 import { getRulesMissingTranslations } from './utils.js'
 
-const missingTranslations = getRulesMissingTranslations()[0]
+const [fileName, missingTranslations] = getRulesMissingTranslations()[0]
+
+const getErrorMessage = (fileName, missingTranslations) => {
+	const listOfMissingTranslations = missingTranslations
+		.map(([dottedName, attr]) => `\t- ${dottedName} [${attr}]\n`)
+		.join('')
+
+	return `Les traductions suivantes sont à corriger dans '${fileName}' : \n
+		${listOfMissingTranslations}\n
+		Utilisez la commande suivante pour traduire automatiquement les clés manquantes :
+		\n\n\tyarn run i18n:rules:translate\n`
+}
 
 if (missingTranslations.length) {
-	throw new Error(
-		`Les traductions suivantes sont à corriger dans 'rules-en.yaml' : \n${missingTranslations
-			.map(([dottedName, attr]) => `\t- ${dottedName} [${attr}]\n`)
-			.join(
-				''
-			)}\nUtilisez la commande suivante pour traduire automatiquement les clés manquantes :\n\n\tyarn run i18n:rules:translate\n`
-	)
+	throw new Error(getErrorMessage(fileName, missingTranslations))
 }
