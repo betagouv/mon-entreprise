acc7a88dafedaf9e5451a4e318e17e981ff95e0e
Alice Dahan <lilice.dhn@gmail.com>
2025-11-10 11:06:54 +0100

    i18n: ajoute les traductions pour les autres modèles de règles

--- site/scripts/i18n/translate-rules.js
+++ site/scripts/i18n/translate-rules.js
@@ -13,38 +13,42 @@ import {
 const missingTranslationsByFile = getRulesMissingTranslations()
 
 await Promise.all(
-	missingTranslationsByFile.map(([fileName, missingTranslations, resolved]) => {
-		const rulesTranslationPath = localesPath + fileName
-		missingTranslations.map(async ([dottedName, attr, value]) => {
-			try {
-				if (attr === 'avec') {
-					return await translateObject(resolved, [dottedName, attr], value)
-				}
+	missingTranslationsByFile.map(async ([, missingTranslations, resolved]) => {
+		await Promise.all(
+			missingTranslations.map(async ([dottedName, attr, value]) => {
+				try {
+					if (attr === 'avec') {
+						return await translateObject(resolved, [dottedName, attr], value)
+					}
 
-				if (typeof value === 'string') {
-					const translation = await fetchTranslation(value)
-					resolved[dottedName][attr] = '[automatic] ' + translation
-				} else {
-					console.warn(
-						"Warning: ⚠️ Can't translate anything other than a string",
-						{ dottedName, attr, value }
-					)
+					if (typeof value === 'string') {
+						const translation = await fetchTranslation(value)
+						resolved[dottedName][attr] = '[automatic] ' + translation
+					} else {
+						console.warn(
+							"Warning: ⚠️ Can't translate anything other than a string",
+							{ dottedName, attr, value }
+						)
+					}
+				} catch (e) {
+					console.error(e)
+					console.log({ dottedName, attr, value })
 				}
-			} catch (e) {
-				console.error(e)
-				console.log({ dottedName, attr, value })
-			}
-		})
-
-		prettier.resolveConfig(rulesTranslationPath).then(async (options) => {
-			const formattedYaml = await prettier.format(
-				yaml.stringify(resolved, { sortMapEntries: true }),
-				{
-					...options,
-					parser: 'yaml',
-				}
-			)
-			writeFileSync(rulesTranslationPath, formattedYaml)
-		})
+			})
+		)
 	})
 )
+
+missingTranslationsByFile.map(([fileName, , resolved]) => {
+	const rulesTranslationPath = localesPath + fileName
+	prettier.resolveConfig(rulesTranslationPath).then(async (options) => {
+		const formattedYaml = await prettier.format(
+			yaml.stringify(resolved, { sortMapEntries: true }),
+			{
+				...options,
+				parser: 'yaml',
+			}
+		)
+		writeFileSync(rulesTranslationPath, formattedYaml)
+	})
+})
