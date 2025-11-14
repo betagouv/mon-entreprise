af8d4d72792dd1e2913dfb5693d21f26857bd652
Alice Dahan <lilice.dhn@gmail.com>
2025-11-14 13:48:12 +0100

    tests(modele-as): màj les tests de régressions

--- site/test/regressions/comparateur-statuts.test.ts
+++ site/test/regressions/comparateur-statuts.test.ts
@@ -1,12 +1,15 @@
+import rules from 'modele-social'
 import { it } from 'vitest'
 
 import { configComparateurStatuts } from '@/pages/simulateurs/comparaison-statuts/simulationConfig'
+import { engineFactory } from '@/utils/publicodes/engineFactory'
 
 import situations from './comparateur-statuts.yaml'
 import { runSimulations } from './utils'
 
 it('calculate comparateur-statuts', () => {
 	runSimulations(
+		engineFactory(rules),
 		situations,
 		[
 			...(configComparateurStatuts['objectifs exclusifs'] ?? []),
--- site/test/regressions/convention-collective.test.ts
+++ site/test/regressions/convention-collective.test.ts
@@ -1,12 +1,15 @@
+import rules from 'modele-social'
 import { it } from 'vitest'
 
 import { configSalarié } from '@/pages/simulateurs/salarié/simulationConfig'
+import { engineFactory } from '@/utils/publicodes/engineFactory'
 
 import employeeSituations from './convention-collective.yaml'
 import { runSimulations } from './utils'
 
 it('calculate simulations-salarié', () => {
 	runSimulations(
+		engineFactory(rules),
 		employeeSituations,
 		[
 			...(configSalarié['objectifs exclusifs'] ?? []),
