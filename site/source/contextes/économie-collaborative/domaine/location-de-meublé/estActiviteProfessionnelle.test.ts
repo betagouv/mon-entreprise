df28cf9bc92fd0cf8a388fa9d52cd9f0d4be8617
Jalil Arfaoui <jalil@arfaoui.net>
2025-11-21 11:17:06 +0100

    feat(économie-collaborative): implémente tous les cas d’applicabilité des régimes (impact Loi Le Meur)

--- site/source/contextes/économie-collaborative/domaine/location-de-meublé/estActiviteProfessionnelle.test.ts
+++ site/source/contextes/économie-collaborative/domaine/location-de-meublé/estActiviteProfessionnelle.test.ts
@@ -1,57 +1,34 @@
-import { Option, pipe } from 'effect'
+import { pipe } from 'effect'
 import { describe, expect, it } from 'vitest'
 
 import { eurosParAn, moins, plus } from '@/domaine/Montant'
 
 import { estActiviteProfessionnelle } from './estActiviteProfessionnelle'
-import { SituationÉconomieCollaborativeValide } from './situation'
+import { situationMeubléDeTourismeBuilder } from './test/situationBuilder'
 
 const SEUIL_MEUBLÉ = eurosParAn(23_000)
-// const SEUIL_CHAMBRE_HÔTE = eurosParAn(6_028)
 
 describe('estActiviteProfessionnelle', () => {
 	it('est faux si les recettes sont inférieures au seuil de professionalisation', () => {
-		const situation: SituationÉconomieCollaborativeValide = {
-			_tag: 'Situation',
-			typeLocation: Option.none(),
-			recettes: Option.some(
-				pipe(SEUIL_MEUBLÉ, moins(eurosParAn(1)))
-			) as Option.Some<typeof SEUIL_MEUBLÉ>,
-			autresRevenus: Option.none(),
-			typeDurée: Option.none(),
-			estAlsaceMoselle: Option.none(),
-			premièreAnnée: Option.none(),
-		}
+		const situation = situationMeubléDeTourismeBuilder()
+			.avecRecettes(pipe(SEUIL_MEUBLÉ, moins(eurosParAn(1))).valeur)
+			.build()
 
 		expect(estActiviteProfessionnelle(situation)).toBe(false)
 	})
 
 	it('est vrai si les recettes sont égales au seuil de professionalisation', () => {
-		const situation: SituationÉconomieCollaborativeValide = {
-			_tag: 'Situation',
-			typeLocation: Option.none(),
-			recettes: Option.some(SEUIL_MEUBLÉ) as Option.Some<typeof SEUIL_MEUBLÉ>,
-			autresRevenus: Option.none(),
-			typeDurée: Option.none(),
-			estAlsaceMoselle: Option.none(),
-			premièreAnnée: Option.none(),
-		}
+		const situation = situationMeubléDeTourismeBuilder()
+			.avecRecettes(SEUIL_MEUBLÉ.valeur)
+			.build()
 
 		expect(estActiviteProfessionnelle(situation)).toBe(true)
 	})
 
 	it('est vrai si les recettes sont supérieures au seuil de professionalisation', () => {
-		const situation: SituationÉconomieCollaborativeValide = {
-			_tag: 'Situation',
-			typeLocation: Option.none(),
-			recettes: Option.some(
-				pipe(SEUIL_MEUBLÉ, plus(eurosParAn(1)))
-			) as Option.Some<typeof SEUIL_MEUBLÉ>,
-			autresRevenus: Option.none(),
-			typeDurée: Option.none(),
-			estAlsaceMoselle: Option.none(),
-			premièreAnnée: Option.none(),
-		}
+		const situation = situationMeubléDeTourismeBuilder()
+			.avecRecettes(pipe(SEUIL_MEUBLÉ, plus(eurosParAn(1))).valeur)
+			.build()
 
 		expect(estActiviteProfessionnelle(situation)).toBe(true)
 	})
