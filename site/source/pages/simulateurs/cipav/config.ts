77e26f257ab902c586f666b0c0e45b2d14752f4a
Alice Dahan <lilice.dhn@gmail.com>
2025-11-21 18:00:41 +0100

    chore: remplace extension tsx inutile par ts

--- site/source/pages/simulateurs/cipav/config.ts
+++ site/source/pages/simulateurs/cipav/config.ts
@@ -1,6 +1,6 @@
 import { premiersMoisUrssaf } from '@/external-links/premiersMoisUrssaf'
 import { servicePLR } from '@/external-links/servicePLR'
-import { IndépendantPLSimulation } from '@/pages/simulateurs/indépendant/IndépendantPLSimulation'
+import ProfessionLibérale from '@/pages/simulateurs/profession-libérale/ProfessionLibérale'
 
 import { config } from '../_configs/config'
 import { SimulatorsDataParams } from '../_configs/types'
@@ -38,6 +38,6 @@ export function cipavConfig({ t, sitePaths }: SimulatorsDataParams) {
 		path: sitePaths.simulateurs['profession-libérale'].cipav,
 		simulation: cipavSimulationConfig,
 		codesCatégorieJuridique: ['1000', '5410', '5499'],
-		component: IndépendantPLSimulation,
+		component: ProfessionLibérale,
 	} as const)
 }
