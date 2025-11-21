0ed5d5bbd24884f4ad98c11825ea32bf792b321c
Alice Dahan <lilice.dhn@gmail.com>
2025-11-21 17:22:42 +0100

    fix: corrige le simulateurs écolab et l'assistant CMG

--- site/source/pages/assistants/cmg/config.ts
+++ site/source/pages/assistants/cmg/config.ts
@@ -35,5 +35,10 @@ export function CMGConfig({ t, sitePaths }: SimulatorsDataParams) {
 				'Ce simulateur permet d’estimer le montant du complément transitoire au CMG Rémunération.'
 			),
 		},
+		// Nécessaire pour que useEngine ne plante pas.
+		// Étant un hook, on ne peut pas l'appeler conditionnellement.
+		simulation: {
+			nomModèle: 'modele-social',
+		},
 	} as const)
 }
