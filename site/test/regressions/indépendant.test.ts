4227819d7633372c1f84c254f4ec03e3ff9e3e4b
Alice Dahan <lilice.dhn@gmail.com>
2025-11-21 18:11:44 +0100

    tests: màj tests indépendants

--- site/test/regressions/indépendant.test.ts
+++ site/test/regressions/indépendant.test.ts
@@ -1,4 +1,4 @@
-import rules from 'modele-social'
+import rules from 'modele-ti'
 import { expect, it } from 'vitest'
 
 import { DottedName } from '@/domaine/publicodes/DottedName'
@@ -12,15 +12,15 @@ const engine = engineFactory(rules)
 
 it('calculate simulations-indépendant', () => {
 	const objectifs = [
-		'dirigeant . rémunération . totale',
-		'dirigeant . rémunération . cotisations',
-		'dirigeant . rémunération . net',
-		'dirigeant . indépendant . revenu professionnel',
+		'indépendant . rémunération . totale',
+		'indépendant . rémunération . cotisations',
+		'indépendant . rémunération . nette',
+		'indépendant . revenu professionnel',
 		'impôt . montant',
-		'dirigeant . rémunération . net . après impôt',
+		'indépendant . rémunération . nette . après impôt',
 		'entreprise . charges',
 		"entreprise . chiffre d'affaires",
-		'dirigeant . indépendant . cotisations et contributions . début activité',
+		'indépendant . cotisations et contributions . début activité',
 	] as DottedName[]
 	runSimulations(
 		engine,
@@ -33,27 +33,27 @@ it('calculate simulations-indépendant', () => {
 		getMissingVariables(
 			engine
 				.setSituation(configIndépendant.situation)
-				.evaluate('dirigeant . rémunération . net')
+				.evaluate('indépendant . rémunération . nette')
 		)
 	).toMatchInlineSnapshot(`
 		[
-		  "dirigeant . indépendant . IJSS",
-		  "dirigeant . indépendant . conjoint collaborateur",
-		  "dirigeant . indépendant . cotisations et contributions . exonérations . pension invalidité",
-		  "dirigeant . indépendant . cotisations facultatives",
-		  "dirigeant . indépendant . revenus étrangers",
-		  "entreprise . activité . nature",
-		  "entreprise . activités",
-		  "entreprise . activités . commerciale",
-		  "entreprise . activités . saisonnière",
+		  "entreprise . activité",
+		  "entreprise . activité . commerciale . débit de tabac",
+		  "entreprise . activité . saisonnière",
 		  "entreprise . charges",
 		  "entreprise . date de création",
 		  "entreprise . imposition . régime",
 		  "entreprise . imposition . régime . micro-entreprise",
+		  "impôt . foyer fiscal . autres revenus imposables",
 		  "impôt . foyer fiscal . enfants à charge",
-		  "impôt . foyer fiscal . revenu imposable . autres revenus imposables",
 		  "impôt . foyer fiscal . situation de famille",
 		  "impôt . méthode de calcul",
+		  "indépendant . IJSS",
+		  "indépendant . conjoint collaborateur",
+		  "indépendant . cotisations et contributions . cotisations . exonérations . pension invalidité",
+		  "indépendant . cotisations facultatives",
+		  "indépendant . revenus étrangers",
+		  "paramètres . impôt . revenu imposable",
 		  "situation personnelle . RSA",
 		  "situation personnelle . domiciliation fiscale à l'étranger",
 		  "établissement . commune . département",
