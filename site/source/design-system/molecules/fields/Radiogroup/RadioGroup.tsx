42a4c4a9b7fc75c1fa0c8173826154b0951e30c4
logic-fabric <lowic@logic-fabric.fr>
2025-11-21 10:20:05 +0100

    style(RadioGroup): add styles to new <RadioGroup /> component

--- site/source/design-system/molecules/fields/Radiogroup/RadioGroup.tsx
+++ site/source/design-system/molecules/fields/Radiogroup/RadioGroup.tsx
@@ -4,6 +4,9 @@ import {
 	RadioGroup as RARadioGroup,
 	type RadioGroupProps as RARadioGroupProps,
 } from 'react-aria-components'
+import { styled } from 'styled-components'
+
+import { fieldContainerStyles } from '../fieldsStyles'
 
 type RadioGroupProps = RARadioGroupProps & {
 	legend: string
@@ -12,14 +15,74 @@ type RadioGroupProps = RARadioGroupProps & {
 
 export function RadioGroup({ legend, options }: RadioGroupProps) {
 	return (
-		<RARadioGroup>
-			<RALabel>{legend}</RALabel>
+		<StyledRARadioGroup>
+			<StyledRALabel>{legend}</StyledRALabel>
 
 			{options.map((option) => (
-				<RARadio key={option} value={option}>
+				<StyledRARadio key={option} value={option}>
 					{option}
-				</RARadio>
+				</StyledRARadio>
 			))}
-		</RARadioGroup>
+		</StyledRARadioGroup>
 	)
 }
+
+const StyledRARadioGroup = styled(RARadioGroup)`
+	${fieldContainerStyles}
+
+	gap: 0
+`
+
+const StyledRALabel = styled(RALabel)`
+	margin-bottom: ${({ theme }) => theme.spacings.xs};
+`
+
+const StyledRARadio = styled(RARadio)`
+	position: relative;
+	display: flex;
+	align-items: center;
+	gap: ${({ theme }) => theme.spacings.xxs};
+
+	&::before,
+	&::after {
+		content: '';
+
+		border-radius: 50%;
+
+		cursor: pointer;
+
+		transition: all 200ms;
+	}
+
+	&::before {
+		padding: ${({ theme }) => theme.spacings.xxs};
+		border: ${({ theme }) => theme.spacings.md} solid white;
+
+		background: transparent;
+	}
+
+	&::after {
+		position: absolute;
+
+		left: ${({ theme }) => theme.spacings.sm};
+
+		width: ${({ theme }) => theme.spacings.md};
+		height: ${({ theme }) => theme.spacings.md};
+		border: ${({ theme }) => theme.spacings.xxxs} solid
+			${({ theme }) => theme.colors.extended.grey[600]};
+	}
+
+	&:hover::before {
+		border-color: ${({ theme }) => theme.colors.bases.primary[200]};
+
+		background: ${({ theme }) => theme.colors.bases.primary[200]};
+	}
+
+	&[data-selected='true']::before {
+		background: ${({ theme }) => theme.colors.bases.primary[700]};
+	}
+
+	&[data-selected='true']::after {
+		border-color: ${({ theme }) => theme.colors.bases.primary[700]};
+	}
+`
