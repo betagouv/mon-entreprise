a9c667908f866e50b5b5ab9b6e5953a439395e3d
Alice Dahan <lilice.dhn@gmail.com>
2025-11-21 17:40:48 +0100

    chore: étend le type DottedName aux règles de modele-ti

--- site/source/domaine/publicodes/DottedName.ts
+++ site/source/domaine/publicodes/DottedName.ts
@@ -1,4 +1,8 @@
 import { RègleModeleAssimiléSalarié } from 'modele-as'
 import { RègleModeleSocial } from 'modele-social'
+import { RègleModeleTravailleurIndépendant } from 'modele-ti'
 
-export type DottedName = RègleModeleSocial | RègleModeleAssimiléSalarié
+export type DottedName =
+	| RègleModeleSocial
+	| RègleModeleAssimiléSalarié
+	| RègleModeleTravailleurIndépendant
