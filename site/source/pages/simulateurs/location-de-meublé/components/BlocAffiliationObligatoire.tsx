512ed20c54f5f500cbe4b690f3e8ead37ac928da
Jalil Arfaoui <jalil@arfaoui.net>
2025-11-27 16:12:21 +0100

    refactor(cessation activité): Utilisation de `t` plutôt que `Trans`

--- site/source/pages/simulateurs/location-de-meublé/components/BlocAffiliationObligatoire.tsx
+++ site/source/pages/simulateurs/location-de-meublé/components/BlocAffiliationObligatoire.tsx
@@ -1,26 +1,27 @@
-import { Trans } from 'react-i18next'
+import { useTranslation } from 'react-i18next'
 
 import { Body, H3, Message, Spacing } from '@/design-system'
 
 import { ComparateurRégimesCards } from './ComparateurRégimesCards'
 
 export const BlocAffiliationObligatoire = () => {
+	const { t } = useTranslation()
+
 	return (
 		<>
 			<Spacing lg />
 			<Message type="info">
 				<H3>
-					<Trans i18nKey="pages.simulateurs.location-de-logement-meublé.affiliation.obligatoire.titre">
-						Votre activité est considérée comme professionnelle
-					</Trans>
+					{t(
+						'pages.simulateurs.location-de-logement-meublé.affiliation.obligatoire.titre',
+						'Votre activité est considérée comme professionnelle'
+					)}
 				</H3>
 				<Body>
-					<Trans i18nKey="pages.simulateurs.location-de-logement-meublé.affiliation.obligatoire.texte">
-						Vous devez obligatoirement déclarer vos revenus et des cotisations
-						sociales sont dues. Vous pouvez opter entre plusieurs régimes
-						d'affiliation : régime auto-entrepreneur, indépendant ou régime
-						général.
-					</Trans>
+					{t(
+						'pages.simulateurs.location-de-logement-meublé.affiliation.obligatoire.texte',
+						"Vous devez obligatoirement déclarer vos revenus et des cotisations sociales sont dues. Vous pouvez opter entre plusieurs régimes d'affiliation : régime auto-entrepreneur, indépendant ou régime général."
+					)}
 				</Body>
 			</Message>
 			<ComparateurRégimesCards />
