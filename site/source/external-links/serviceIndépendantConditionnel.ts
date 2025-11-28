f088efca2ba5e61d4f6a8964f71864b6e418c965
Alice Dahan <lilice.dhn@gmail.com>
2025-11-28 10:58:18 +0100

    refactor: supprime commentaire inutile

--- site/source/external-links/serviceIndépendantConditionnel.ts
+++ site/source/external-links/serviceIndépendantConditionnel.ts
@@ -1,6 +1,5 @@
 import { serviceIndépendant } from './serviceIndépendant'
 
-// TODO: gérer les traductions
 export const serviceIndépendantConditionnel = {
 	...serviceIndépendant,
 	associatedRule: 'entreprise . activité . libérale . réglementée = non',
