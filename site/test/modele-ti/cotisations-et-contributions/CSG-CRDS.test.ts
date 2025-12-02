3171d784b02d7db7a0daea3c2ed7e8e59abdb44c
Alice Dahan <lilice.dhn@gmail.com>
2025-12-02 14:21:35 +0100

    tests(modele-ti): ajout de tests sur les barèmes après réforme

---
+++ site/test/modele-ti/cotisations-et-contributions/CSG-CRDS.test.ts
@@ -0,0 +1,71 @@
+import rules from 'modele-ti'
+import Engine from 'publicodes'
+import { beforeEach, describe, expect, it } from 'vitest'
+
+const defaultSituation = {
+	'plafond sécurité sociale': '47100 €/an',
+	'entreprise . imposition': "'IR'",
+}
+
+describe('CSG-CRDS', () => {
+	let engine: Engine
+	beforeEach(() => {
+		engine = new Engine(rules)
+	})
+
+	describe('pour les artisans, commerçants et PLNR', () => {
+		it('applique un taux de 9,7% dans le cas général', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette CSG-CRDS':
+					'50000 €/an',
+			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . CSG-CRDS',
+				4850
+			)
+		})
+
+		it('applique un taux de 9,7% en début d’activité', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'entreprise . date de création': '31/01/2025',
+				'indépendant . cotisations et contributions . assiette CSG-CRDS':
+					'50000 €/an',
+			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . CSG-CRDS',
+				4850
+			)
+		})
+
+		it('applique un taux de 9,7% en cas d’année incomplète', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette CSG-CRDS':
+					'50000 €/an',
+				"entreprise . durée d'activité cette année": '250 jour',
+			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . CSG-CRDS',
+				4850
+			)
+		})
+
+		it('ne s’applique pas en cas de domiciliation fiscale à l’étranger', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette CSG-CRDS':
+					'50000 €/an',
+				"situation personnelle . domiciliation fiscale à l'étranger": 'oui',
+			})
+
+			expect(e).not.toBeApplicable(
+				'indépendant . cotisations et contributions . CSG-CRDS'
+			)
+		})
+	})
+})
