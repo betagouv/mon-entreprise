d0436ef700a47947b290374e898748896c5d23e0
Alice Dahan <lilice.dhn@gmail.com>
2025-12-05 10:33:10 +0100

    refactor: supprime let de useDocumentationIndexPath

--- site/source/hooks/useDocumentationIndexPath.ts
+++ site/source/hooks/useDocumentationIndexPath.ts
@@ -3,10 +3,10 @@ import { useSitePaths } from '@/sitePaths'
 
 export const useDocumentationPath = (nomModèle?: NomModèle) => {
 	const { absoluteSitePaths } = useSitePaths()
-	let path = absoluteSitePaths.documentation.index
+	const path = absoluteSitePaths.documentation.index
 
 	if (nomModèle && nomModèle !== 'modele-social') {
-		path += `/${nomModèle}`
+		return `${path}/${nomModèle}`
 	}
 
 	return path
