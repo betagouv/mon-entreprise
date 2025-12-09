d1ec2efe39c13613f05565701a72a39e8dca521c
Alice Dahan <lilice.dhn@gmail.com>
2025-12-09 14:04:37 +0100

    refactor(tests): améliore les tests unitaires des cotisations de modele-ti

--- site/test/modele-ti/cotisations-et-contributions/cotisations/maladie.test.ts
+++ site/test/modele-ti/cotisations-et-contributions/cotisations/maladie.test.ts
@@ -14,21 +14,143 @@ describe('Cotisation maladie', () => {
 	})
 
 	describe('pour les artisans, commerçants et PLNR', () => {
-		describe('en cas de domiciliation fiscale à l’étranger', () => {
-			it('applique un taux de 14,5%', () => {
-				const e = engine.setSituation({
-					...defaultSituation,
-					"situation personnelle . domiciliation fiscale à l'étranger": 'oui',
-					"entreprise . chiffre d'affaires": '13514 €/an', // assiette sociale de 10 000 €/an
-				})
-
-				expect(e).toEvaluate(
-					'indépendant . cotisations et contributions . cotisations . maladie',
-					1450
-				)
+		// Exemple issus de la doc Urssaf
+		it('applique un taux nul en cas d’assiette sociale inférieure à 20% du PASS', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette sociale':
+					'8949 €/an', // 19% du PASS
 			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
+				0
+			)
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
+				0
+			)
 		})
 
+		// Exemple issus de la doc Urssaf
+		it('applique un taux progressif compris entre 0% et 1,5% en cas d’assiette sociale comprise entre 20% et 40% du PASS', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette sociale':
+					'10000 €/an',
+			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
+				0.09
+			)
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
+				9
+			)
+		})
+
+		// Exemple issus de la doc Urssaf
+		it('applique un taux progressif compris entre 1,5% et 4% en cas d’assiette sociale comprise entre 40% et 60% du PASS', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette sociale':
+					'23000 €/an',
+			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
+				2.6
+			)
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
+				598
+			)
+		})
+
+		// Exemple issus de la doc Urssaf
+		it('applique un taux progressif compris entre 4% et 6,5% en cas d’assiette sociale comprise entre 60% et 110% du PASS', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette sociale':
+					'40000 €/an',
+			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
+				5.25
+			)
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
+				2100
+			)
+		})
+
+		// Exemple issus de la doc Urssaf
+		it('applique un taux progressif compris entre 6,5% et 7,7% en cas d’assiette sociale comprise entre 110% et 200% du PASS', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette sociale':
+					'60000 €/an',
+			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
+				6.73
+			)
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
+				4038
+			)
+		})
+
+		// Exemple issus de la doc Urssaf
+		it('applique un taux progressif compris entre 7,7% et 8,5% en cas d’assiette sociale comprise entre 200% et 300% du PASS', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette sociale':
+					'100000 €/an',
+			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
+				7.8
+			)
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
+				7800
+			)
+		})
+
+		// Exemple issus de la doc Urssaf
+		it('applique un taux 1 de 8,5% à 3 PASS et un taux 2 de 6,5% au reste de l’assiette sociale en cas d’assiette sociale supérieure à 300% du PASS', () => {
+			const e = engine.setSituation({
+				...defaultSituation,
+				'indépendant . cotisations et contributions . assiette sociale':
+					'150000 €/an',
+			})
+
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
+				8.5
+			)
+			// Tranche 1 :
+			// 3 PASS = 3 x 47 100 €/an = 141 300 €/an
+			// Taux 1 x 3 PASS = 8,5% x 141 300 €/an = 12 010,5 €/an
+
+			// Tranche 2 :
+			// assiette sociale - 3 PASS = 150 000 €/an - 141 300 €/an = 8 700 €/an
+			// Taux 2 x (assiette sociale - 3 PASS) = 6,5% x 8 700 €/an = 565,5 €/an
+
+			// Total :
+			// Tranche 1 + Tranche 2 = 12 010,5 €/an + 565,5 €/an = 12 576 €/an
+			expect(e).toEvaluate(
+				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
+				12576
+			)
+		})
+
+		// Exemples issus de la doc Urssaf
 		describe('en cas d’année incomplète', () => {
 			it('applique un taux nul en cas d’assiette sociale annualisée inférieure à 20% du PASS', () => {
 				const e = engine.setSituation({
@@ -135,7 +257,7 @@ describe('Cotisation maladie', () => {
 				expect(e).toEvaluate(
 					'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
 					386
-				) // devrait être 387, problème d'arrondi ?
+				)
 			})
 
 			it('applique un taux 1 de 8,5% à 3 PASS et un taux 2 de 6,5% au reste de l’assiette sociale en cas d’assiette sociale annualisée supérieure à 300% du PASS', () => {
@@ -172,133 +294,23 @@ describe('Cotisation maladie', () => {
 			})
 		})
 
-		it('applique un taux nul en cas d’assiette sociale inférieure à 20% du PASS', () => {
-			const e = engine.setSituation({
-				...defaultSituation,
-				'indépendant . cotisations et contributions . assiette sociale':
-					'8949 €/an', // 19% du PASS
+		describe('en cas de domiciliation fiscale à l’étranger', () => {
+			it('applique un taux de 14,5%', () => {
+				const e = engine.setSituation({
+					...defaultSituation,
+					"situation personnelle . domiciliation fiscale à l'étranger": 'oui',
+					"entreprise . chiffre d'affaires": '13514 €/an',
+				})
+
+				expect(e).toEvaluate(
+					'indépendant . cotisations et contributions . assiette sociale',
+					10000
+				)
+				expect(e).toEvaluate(
+					'indépendant . cotisations et contributions . cotisations . maladie',
+					1450
+				)
 			})
-
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
-				0
-			)
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
-				0
-			)
-		})
-
-		it('applique un taux progressif compris entre 0% et 1,5% en cas d’assiette sociale comprise entre 20% et 40% du PASS', () => {
-			const e = engine.setSituation({
-				...defaultSituation,
-				'indépendant . cotisations et contributions . assiette sociale':
-					'10000 €/an',
-			})
-
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
-				0.09
-			)
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
-				9
-			)
-		})
-
-		it('applique un taux progressif compris entre 1,5% et 4% en cas d’assiette sociale comprise entre 40% et 60% du PASS', () => {
-			const e = engine.setSituation({
-				...defaultSituation,
-				'indépendant . cotisations et contributions . assiette sociale':
-					'23000 €/an',
-			})
-
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
-				2.6
-			)
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
-				598
-			)
-		})
-
-		it('applique un taux progressif compris entre 4% et 6,5% en cas d’assiette sociale comprise entre 60% et 110% du PASS', () => {
-			const e = engine.setSituation({
-				...defaultSituation,
-				'indépendant . cotisations et contributions . assiette sociale':
-					'40000 €/an',
-			})
-
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
-				5.25
-			)
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
-				2100
-			)
-		})
-
-		it('applique un taux progressif compris entre 6,5% et 7,7% en cas d’assiette sociale comprise entre 110% et 200% du PASS', () => {
-			const e = engine.setSituation({
-				...defaultSituation,
-				'indépendant . cotisations et contributions . assiette sociale':
-					'60000 €/an',
-			})
-
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
-				6.73
-			)
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
-				4038
-			)
-		})
-
-		it('applique un taux progressif compris entre 7,7% et 8,5% en cas d’assiette sociale comprise entre 200% et 300% du PASS', () => {
-			const e = engine.setSituation({
-				...defaultSituation,
-				'indépendant . cotisations et contributions . assiette sociale':
-					'100000 €/an',
-			})
-
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
-				7.8
-			)
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
-				7800
-			)
-		})
-
-		it('applique un taux 1 de 8,5% à 3 PASS et un taux 2 de 6,5% au reste de l’assiette sociale en cas d’assiette sociale supérieure à 300% du PASS', () => {
-			const e = engine.setSituation({
-				...defaultSituation,
-				'indépendant . cotisations et contributions . assiette sociale':
-					'150000 €/an',
-			})
-
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité . taux 1',
-				8.5
-			)
-			// Tranche 1 :
-			// 3 PASS = 3 x 47 100 €/an = 141 300 €/an
-			// Taux 1 x 3 PASS = 8,5% x 141 300 €/an = 12 010,5 €/an
-
-			// Tranche 2 :
-			// assiette sociale - 3 PASS = 150 000 €/an - 141 300 €/an = 8 700 €/an
-			// Taux 2 x (assiette sociale - 3 PASS) = 6,5% x 8 700 €/an = 565,5 €/an
-
-			// Total :
-			// Tranche 1 + Tranche 2 = 12 010,5 €/an + 565,5 €/an = 12 576 €/an
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . cotisations . maladie . maladie-maternité',
-				12576
-			)
 		})
 	})
 
