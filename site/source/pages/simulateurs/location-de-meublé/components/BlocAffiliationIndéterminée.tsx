9701ad27ec6c2226e6df6fc61a8955d6245d37cf
Jalil Arfaoui <jalil@arfaoui.net>
2026-01-04 15:51:19 +0100

    fix(économie-collaborative): affiche un bloc dédié quand l'affiliation est indéterminée
    - Ajoute `estAffiliationObligatoire` qui retourne Either<boolean, RéponseManquante[]>
    - Crée `BlocAffiliationIndéterminée` pour lister les informations manquantes

---
+++ site/source/pages/simulateurs/location-de-meublé/components/BlocAffiliationIndéterminée.tsx
@@ -0,0 +1,46 @@
+import { useTranslation } from 'react-i18next'
+
+import { type RéponseManquante } from '@/contextes/économie-collaborative'
+import { Body, H3, Li, Message, Spacing, Ul } from '@/design-system'
+
+import { getLibelléInfoManquante } from '../getLibelléInfoManquante'
+import { ComparateurRégimesCards } from './ComparateurRégimesCards'
+
+export const BlocAffiliationIndéterminée = ({
+	réponsesManquantes,
+}: {
+	réponsesManquantes: RéponseManquante[]
+}) => {
+	const { t } = useTranslation()
+
+	return (
+		<>
+			<Spacing lg />
+			<Message type="secondary">
+				<H3>
+					{t(
+						'pages.simulateurs.location-de-logement-meublé.affiliation.indéterminée.titre',
+						"L'obligation d'affiliation dépend de votre situation"
+					)}
+				</H3>
+				<Body>
+					{t(
+						'pages.simulateurs.location-de-logement-meublé.affiliation.indéterminée.texte',
+						'Pour déterminer si vous devez vous affilier, nous avons besoin de connaître :'
+					)}
+				</Body>
+				<Ul
+					aria-label={t(
+						'pages.simulateurs.location-de-logement-meublé.affiliation.indéterminée.liste.aria-label',
+						'Informations manquantes'
+					)}
+				>
+					{réponsesManquantes.map((réponse) => (
+						<Li key={réponse}>{getLibelléInfoManquante(t, réponse)}</Li>
+					))}
+				</Ul>
+			</Message>
+			<ComparateurRégimesCards />
+		</>
+	)
+}
