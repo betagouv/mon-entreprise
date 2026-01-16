f12bcb3580d7dbe6b67f917e11741ce622a231d5
Alice Dahan <lilice.dhn@gmail.com>
2026-01-16 13:06:18 +0100

    chore(TI): supprime les avertissements réforme non implémentée

--- site/source/pages/simulateurs/profession-libérale/ProfessionLibérale.tsx
+++ site/source/pages/simulateurs/profession-libérale/ProfessionLibérale.tsx
@@ -1,6 +1,5 @@
 import { useTranslation } from 'react-i18next'
 
-import AvertissementRéformeAssietteNonImplémentée from '@/components/AvertissementRéformeAssietteNonImplémentée'
 import SimulateurWarning from '@/components/SimulateurWarning'
 import Simulation from '@/components/Simulation'
 import { YearSelectionBanner } from '@/components/Simulation/YearSelectionBanner'
@@ -22,30 +21,27 @@ export default function ProfessionLibérale() {
 				<SimulateurWarning
 					simulateur="profession-libérale"
 					informationsComplémentaires={
-						<>
-							<AvertissementRéformeAssietteNonImplémentée />
-							<Ul>
-								<DarkLi>
-									{t(
-										'pages.simulateurs.profession-libérale.warning.général',
-										'Ce simulateur est à destination des professions libérales en BNC. Il ne prend pas en compte les sociétés d’exercice libéral.'
-									)}
-								</DarkLi>
-								<DarkLi>
-									{t(
-										'pages.simulateurs.indépendant.warning.année-courante',
-										'Le montant calculé correspond aux cotisations de l’année {{ year }} (pour un revenu {{ year }}).',
-										{ year }
-									)}
-								</DarkLi>{' '}
-								<DarkLi>
-									{t(
-										'pages.simulateurs.profession-libérale.warning.cotisations-ordinales',
-										'Pour les professions réglementées, le simulateur ne calcule pas le montant des cotisations à l’ordre. Elles doivent être ajoutées manuellement dans la case « charges de fonctionnement ».'
-									)}
-								</DarkLi>
-							</Ul>
-						</>
+						<Ul>
+							<DarkLi>
+								{t(
+									'pages.simulateurs.profession-libérale.warning.général',
+									'Ce simulateur est à destination des professions libérales en BNC. Il ne prend pas en compte les sociétés d’exercice libéral.'
+								)}
+							</DarkLi>
+							<DarkLi>
+								{t(
+									'pages.simulateurs.indépendant.warning.année-courante',
+									'Le montant calculé correspond aux cotisations de l’année {{ year }} (pour un revenu {{ year }}).',
+									{ year }
+								)}
+							</DarkLi>{' '}
+							<DarkLi>
+								{t(
+									'pages.simulateurs.profession-libérale.warning.cotisations-ordinales',
+									'Pour les professions réglementées, le simulateur ne calcule pas le montant des cotisations à l’ordre. Elles doivent être ajoutées manuellement dans la case « charges de fonctionnement ».'
+								)}
+							</DarkLi>
+						</Ul>
 					}
 				/>
 				<IndépendantSimulationGoals />
