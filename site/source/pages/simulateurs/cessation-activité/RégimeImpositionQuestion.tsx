b8219e9c11d00816369234975494f223a726dc67
Jalil Arfaoui <jalil@arfaoui.net>
2026-01-28 23:43:46 +0100

    refactor(cessation-activité): utilise SimpleField et SwitchContainer

--- site/source/pages/simulateurs/cessation-activité/RégimeImpositionQuestion.tsx
+++ site/source/pages/simulateurs/cessation-activité/RégimeImpositionQuestion.tsx
@@ -1,45 +1,24 @@
-import { DottedName } from 'modele-social'
 import { useTranslation } from 'react-i18next'
-import { useDispatch } from 'react-redux'
-import { styled } from 'styled-components'
 
-import RuleInput from '@/components/conversation/RuleInput'
-import { H3 } from '@/design-system'
-import { ValeurPublicodes } from '@/domaine/engine/PublicodesAdapter'
-import { ajusteLaSituation } from '@/store/actions/actions'
+import {
+	RuleSwitchLabel,
+	SwitchContainer,
+} from '@/components/RéductionDeCotisations/réductionDeCotisations'
+import { SimpleField } from '@/pages/assistants/components/Fields'
 
 export const RégimeImpositionQuestion = () => {
 	const { t } = useTranslation()
-	const dispatch = useDispatch()
 
 	return (
-		<Container>
-			<StyledH3 as="h2">
-				{t(
+		<SwitchContainer isRule>
+			<SimpleField
+				dottedName="entreprise . imposition"
+				question={t(
 					'pages.simulateurs.cessation-activité.régime-imposition.question',
 					"Quel est votre régime d'imposition ?"
 				)}
-			</StyledH3>
-			<RuleInput
-				hideDefaultValue
-				dottedName="entreprise . imposition"
-				onChange={(imposition) => {
-					dispatch(
-						ajusteLaSituation({
-							'entreprise . imposition': imposition,
-						} as Record<DottedName, ValeurPublicodes>)
-					)
-				}}
+				labelStyle={RuleSwitchLabel}
 			/>
-		</Container>
+		</SwitchContainer>
 	)
 }
-
-const Container = styled.div`
-	margin-bottom: ${({ theme }) => theme.spacings.md};
-`
-
-const StyledH3 = styled(H3)`
-	margin-bottom: ${({ theme }) => theme.spacings.sm};
-	text-align: left;
-`
