d089b4b073ed7fec176dc1fb669fa9f9589e71c7
Jalil Arfaoui <jalil@arfaoui.net>
2026-01-28 09:46:06 +0100

    test(location-meublé): supprime TrackingProvider du TestProvider
    TrackingProvider fait des appels réseau vers tm.urssaf.fr qui sont
    bloqués par happy-dom et causent des erreurs dans les tests.

--- site/source/pages/simulateurs/location-de-meublé/test/helpers/TestProvider.tsx
+++ site/source/pages/simulateurs/location-de-meublé/test/helpers/TestProvider.tsx
@@ -4,7 +4,6 @@ import { I18nextProvider } from 'react-i18next'
 import { Provider as ReduxProvider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
 
-import { TrackingProvider } from '@/components/TrackingProvider'
 import { DesignSystemThemeProvider } from '@/design-system'
 import i18n from '@/locales/i18n'
 import { makeStore } from '@/store/store'
@@ -22,15 +21,13 @@ const testStore = makeStore()
 export const TestProvider = ({ children }: TestProviderProps) => {
 	return (
 		<HelmetProvider>
-			<TrackingProvider>
-				<I18nextProvider i18n={i18n}>
-					<ReduxProvider store={testStore}>
-						<DesignSystemThemeProvider>
-							<BrowserRouter>{children}</BrowserRouter>
-						</DesignSystemThemeProvider>
-					</ReduxProvider>
-				</I18nextProvider>
-			</TrackingProvider>
+			<I18nextProvider i18n={i18n}>
+				<ReduxProvider store={testStore}>
+					<DesignSystemThemeProvider>
+						<BrowserRouter>{children}</BrowserRouter>
+					</DesignSystemThemeProvider>
+				</ReduxProvider>
+			</I18nextProvider>
 		</HelmetProvider>
 	)
 }
