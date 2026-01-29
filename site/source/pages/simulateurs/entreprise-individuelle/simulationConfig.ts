2b4a6e68377ee9ad5ab88a5493dd0516086b83dc
Alice Dahan <lilice.dhn@gmail.com>
2026-01-29 13:27:40 +0100

    fix(TI): corrige la situation des simulateurs EI, EIRL et EURL

--- site/source/pages/simulateurs/entreprise-individuelle/simulationConfig.ts
+++ site/source/pages/simulateurs/entreprise-individuelle/simulationConfig.ts
@@ -5,6 +5,7 @@ import { configIndépendant } from '../indépendant/simulationConfig'
 export const configEntrepriseIndividuelle: SimulationConfig = {
 	...configIndépendant,
 	situation: {
+		...configIndépendant.situation,
 		'entreprise . EI': 'oui',
 	},
 }
