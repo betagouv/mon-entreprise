bbac880c88bbbda25862698977c57a519ecbe32b
Alice Dahan <lilice.dhn@gmail.com>
2026-01-29 14:16:36 +0100

    refactor(TI): simplifie la traduction des professions

--- site/source/pages/simulateurs/cipav/AvertissementCipav.tsx
+++ site/source/pages/simulateurs/cipav/AvertissementCipav.tsx
@@ -4,16 +4,10 @@ import { Avertissement } from '../profession-libérale/Avertissement'
 
 export const AvertissementCipav = () => {
 	const { t } = useTranslation()
-	// On est obligés d'utiliser un composant car nous devons par ailleurs utiliser
-	// <Trans> au lieu de t() pour ne pas escaper le / (cf AvertissementAvocat.tsx)
-	const Professionnelles = () => (
-		<>
-			{t(
-				'pages.simulateurs.cipav.professionnelles',
-				'professions réglementées Cipav'
-			)}
-		</>
+	const professionnelles = t(
+		'pages.simulateurs.cipav.professionnelles',
+		'professions réglementées relevant de la Cipav'
 	)
 
-	return <Avertissement Professionnelles={Professionnelles} />
+	return <Avertissement professionnelles={professionnelles} />
 }
