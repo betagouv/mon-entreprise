dbc19626caf77c26a41330235b36cb8268e4a5fa
Jalil Arfaoui <jalil@arfaoui.net>
2026-02-17 15:32:28 +0100

    refactor: supprime les directives 'use client' inutiles sans Next.js

--- site/source/lib/navigation/providers/ReactRouterNavigationProvider.tsx
+++ site/source/lib/navigation/providers/ReactRouterNavigationProvider.tsx
@@ -1,5 +1,3 @@
-'use client'
-
 import { ReactNode, useCallback, useEffect, useMemo, useRef } from 'react'
 import {
 	generatePath as rrGeneratePath,
--- site/source/lib/navigation/useNavigation.ts
+++ site/source/lib/navigation/useNavigation.ts
@@ -1,5 +1,3 @@
-'use client'
-
 import { useContext } from 'react'
 
 import { NavigationAPI } from './NavigationAPI'
