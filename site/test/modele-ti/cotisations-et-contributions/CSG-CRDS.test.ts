3150bc58fc243cca6cc6b937320c5d011215e033
Alice Dahan <lilice.dhn@gmail.com>
2026-03-09 11:39:06 +0100

    refactor: tests unitaires maladie

--- site/test/modele-ti/cotisations-et-contributions/CSG-CRDS.test.ts
+++ site/test/modele-ti/cotisations-et-contributions/CSG-CRDS.test.ts
@@ -2,10 +2,9 @@ import rules from 'modele-ti'
 import Engine from 'publicodes'
 import { beforeEach, describe, expect, it } from 'vitest'
 
-const defaultSituation = {
-	'plafond sécurité sociale': '47100 €/an',
-	'entreprise . imposition': "'IR'",
-}
+const CSG_CRDS = 'indépendant . cotisations et contributions . CSG-CRDS'
+
+const TAUX = 9.7 / 100
 
 describe('CSG-CRDS', () => {
 	let engine: Engine
@@ -16,92 +15,73 @@ describe('CSG-CRDS', () => {
 	describe('pour les artisans, commerçants et PLNR', () => {
 		it('applique un taux de 9,7% dans le cas général', () => {
 			const e = engine.setSituation({
-				...defaultSituation,
 				'indépendant . cotisations et contributions . assiette CSG-CRDS':
 					'50000 €/an',
 			})
 
-			const tauxDéductible = e.evaluate(
-				'indépendant . cotisations et contributions . CSG-CRDS . déductible . taux'
-			).nodeValue as number
+			const tauxDéductible = e.evaluate(`${CSG_CRDS} . déductible . taux`)
+				.nodeValue as number
 			const tauxNonDéductible = e.evaluate(
-				'indépendant . cotisations et contributions . CSG-CRDS . non déductible . taux'
+				`${CSG_CRDS} . non déductible . taux`
 			).nodeValue as number
-			expect(tauxDéductible + tauxNonDéductible).toEqual(9.7)
+			expect(tauxDéductible + tauxNonDéductible).toEqual(100 * TAUX)
 
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . CSG-CRDS',
-				4850
-			)
+			expect(e).toEvaluate(CSG_CRDS, Math.round(50_000 * TAUX))
 		})
 
 		it('applique un taux de 9,7% en cas d’année incomplète', () => {
 			const e = engine.setSituation({
-				...defaultSituation,
 				'indépendant . cotisations et contributions . assiette CSG-CRDS':
 					'50000 €/an',
 				"entreprise . durée d'activité cette année": '250 jour',
 			})
 
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . CSG-CRDS',
-				4850
-			)
+			expect(e).toEvaluate(CSG_CRDS, Math.round(50_000 * TAUX))
 		})
 
 		it('ne s’applique pas en cas de domiciliation fiscale à l’étranger', () => {
 			const e = engine.setSituation({
-				...defaultSituation,
 				'indépendant . cotisations et contributions . assiette CSG-CRDS':
 					'50000 €/an',
 				"situation personnelle . domiciliation fiscale à l'étranger": 'oui',
 			})
 
-			expect(e).not.toBeApplicable(
-				'indépendant . cotisations et contributions . CSG-CRDS'
-			)
+			expect(e).not.toBeApplicable(CSG_CRDS)
 		})
 	})
 
 	describe('pour les PLR', () => {
-		const defaultSituationPLR = {
-			...defaultSituation,
+		const defaultSituation = {
 			'entreprise . activité': "'libérale'",
 			'entreprise . activité . libérale . réglementée': 'oui',
 		}
 
 		it('applique un taux de 9,7%', () => {
 			const e = engine.setSituation({
-				...defaultSituationPLR,
+				...defaultSituation,
 				'indépendant . cotisations et contributions . assiette CSG-CRDS':
 					'50000 €/an',
 			})
 
-			const tauxDéductible = e.evaluate(
-				'indépendant . cotisations et contributions . CSG-CRDS . déductible . taux'
-			).nodeValue as number
+			const tauxDéductible = e.evaluate(`${CSG_CRDS} . déductible . taux`)
+				.nodeValue as number
 			const tauxNonDéductible = e.evaluate(
-				'indépendant . cotisations et contributions . CSG-CRDS . non déductible . taux'
+				`${CSG_CRDS} . non déductible . taux`
 			).nodeValue as number
-			expect(tauxDéductible + tauxNonDéductible).toEqual(9.7)
+			expect(tauxDéductible + tauxNonDéductible).toEqual(100 * TAUX)
 
-			expect(e).toEvaluate(
-				'indépendant . cotisations et contributions . CSG-CRDS',
-				4850
-			)
+			expect(e).toEvaluate(CSG_CRDS, Math.round(50_000 * TAUX))
 		})
 
 		it('ne s’applique pas en cas de domiciliation fiscale à l’étranger', () => {
 			const e = engine.setSituation({
-				...defaultSituationPLR,
+				...defaultSituation,
 				'indépendant . cotisations et contributions . assiette CSG-CRDS':
 					'50000 €/an',
 				"situation personnelle . domiciliation fiscale à l'étranger": 'oui',
 			})
 
-			expect(e).not.toBeApplicable(
-				'indépendant . cotisations et contributions . CSG-CRDS'
-			)
+			expect(e).not.toBeApplicable(CSG_CRDS)
 		})
 	})
 })
--- site/test/modele-ti/cotisations-et-contributions/exonérations/Acre.test.ts
+++ site/test/modele-ti/cotisations-et-contributions/exonérations/Acre.test.ts
@@ -5,7 +5,6 @@ import { beforeEach, describe, expect, it } from 'vitest'
 const COTISATIONS = 'indépendant . cotisations et contributions . cotisations'
 
 const defaultSituation = {
-	'entreprise . imposition': "'IR'",
 	'entreprise . date de création': '01/01/2026',
 }
 const defaultSituationAcre = {
