182f17edcfd624a951bcc2e33d58554947e1f356
Alice Dahan <lilice.dhn@gmail.com>
2026-03-17 11:14:10 +0100

    fix: supprime les boutons sous le simulateur location de meublé

--- site/source/contextes/économie-collaborative/index.ts
+++ site/source/contextes/économie-collaborative/index.ts
@@ -3,6 +3,7 @@ export { ÉconomieCollaborativeProvider } from './hooks/ÉconomieCollaborativeCo
 export { isCotisationsEnabled } from './featureToggles'
 export {
 	aRenseignéSesAutresRevenus,
+	simulationEstCommencée,
 	estSituationValide,
 	faitDeLaLocationCourteDurée,
 	faitDeLaLocationCourteEtLongueDurée,
