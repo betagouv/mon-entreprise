28b65c6814a66593fcefa5dd87e21a2f7ef613e8
Alice Dahan <lilice.dhn@gmail.com>
2026-03-17 10:43:31 +0100

    refactor: supprime domaine inutilisé

--- site/source/domaine/engine/SimpleRuleEvaluation.ts
+++
@@ -1,7 +0,0 @@
-import { PublicodesExpression } from 'publicodes'
-
-export type SimplePublicodesExpression =
-	| (PublicodesExpression & (string | number))
-	| undefined
-
-// export type SimpleRuleEvaluation = SimplePublicodesExpression | undefined | null
--- site/source/domaine/estObjectifExclusifDeLaSimulation.ts
+++
@@ -1,7 +0,0 @@
-import { DottedName } from '@/domaine/publicodes/DottedName'
-import { SimulationConfig } from '@/domaine/SimulationConfig'
-
-export const estObjectifExclusifDeLaSimulation =
-	(config: SimulationConfig) =>
-	(question: DottedName): boolean =>
-		config['objectifs exclusifs']?.includes(question) ?? false
