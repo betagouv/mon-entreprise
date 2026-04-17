5b1199feda3b54ea7b28c6e7f36f640c3cbaf4f3
Alice Dahan <lilice.dhn@gmail.com>
2026-04-17 17:43:00 +0200

    refactor(fiche de paie): factorise la section salaire net

--- site/source/components/simulationExplanation/FicheDePaie/SectionSalaireNet.tsx
+++ site/source/components/simulationExplanation/FicheDePaie/SectionSalaireNet.tsx
@@ -1,20 +1,47 @@
 import { ReactNode } from 'react'
 import { useTranslation } from 'react-i18next'
 
+import { H4 } from '@/design-system'
+
 import { Section } from './Section'
 
 type Props = {
-	children: ReactNode
+	montantNetSocial: ReactNode
+	remboursementsEtRéductions: ReactNode
+	netAvantImpôt: ReactNode
+	impôt: ReactNode
+	netAprèsImpôt: ReactNode
 }
 
-export const SectionSalaireNet = ({ children }: Props) => {
+export const SectionSalaireNet = ({
+	montantNetSocial,
+	remboursementsEtRéductions,
+	netAvantImpôt,
+	impôt,
+	netAprèsImpôt,
+}: Props) => {
 	const { t } = useTranslation()
 
 	return (
 		<Section
 			title={t('composants.fiche-de-paie.titres.salaire-net', 'Salaire net')}
 		>
-			<ul>{children}</ul>
+			<ul>
+				<li>{montantNetSocial}</li>
+				{remboursementsEtRéductions}
+				<li>{netAvantImpôt}</li>
+				<li>
+					<H4>
+						{t(
+							'composants.fiche-de-paie.salaire-net.impôt.titre',
+							'Impôt sur le revenu'
+						)}
+					</H4>
+					<ul>{impôt}</ul>
+				</li>
+
+				<li>{netAprèsImpôt}</li>
+			</ul>
 		</Section>
 	)
 }
