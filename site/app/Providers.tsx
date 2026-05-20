1095d33332e8bfb5b2a70ab5c375f0c33f6ca93e
Jalil Arfaoui <jalil@arfaoui.net>
2026-05-20 15:19:52 +0200

    chore(storage): déplace darkModeStorage de components/utils/ vers storage/ pour cohérence avec safeLocalStorage

--- site/app/Providers.tsx
+++ site/app/Providers.tsx
@@ -4,7 +4,7 @@ import { ReactNode } from 'react'
 import {
 	DARK_MODE_STORAGE_KEY,
 	parseDarkModeValue,
-} from '@/components/utils/darkModeStorage'
+} from '@/storage/darkModeStorage'
 
 import { ClientProviders } from './ClientProviders'
 
