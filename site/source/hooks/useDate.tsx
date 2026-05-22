7eef075af9cdf3014c8017b60848d0201ce76c78
Alice Dahan <lilice.dhn@gmail.com>
2026-05-22 16:58:07 +0200

    fix(cessation activité): corrige boucle infinie

--- site/source/hooks/useDate.tsx
+++ site/source/hooks/useDate.tsx
@@ -1,6 +1,6 @@
 import { useEngine } from '../utils/publicodes/EngineContext'
 
-export default function useDate() {
+export const useDate = () => {
 	const date = useEngine().evaluate('date')
 
 	return date.nodeValue as string
