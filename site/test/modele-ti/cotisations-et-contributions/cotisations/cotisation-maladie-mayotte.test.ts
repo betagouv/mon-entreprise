9a8d4183a40712e8cb540620336579169f4ff8c7
Alice Dahan <lilice.dhn@gmail.com>
2026-06-08 10:40:03 +0200

    feat(Mayotte): abattement 50% sur la contribution maladie

--- site/test/modele-ti/cotisations-et-contributions/cotisations/cotisation-maladie-mayotte.test.ts
+++ site/test/modele-ti/cotisations-et-contributions/cotisations/cotisation-maladie-mayotte.test.ts
@@ -20,19 +20,38 @@ describe('Cotisation additionnelle d’assurance maladie-maternité et autonomie
 		expect(engine).not.toBeApplicable(COTISATION)
 	})
 
-	it('applique un taux de 4,20% aux A/C/PLNR', () => {
-		const e = engine.setSituation(defaultSituation)
-
-		expect(e).toEvaluate(COTISATION, Math.round((60_000 * 4.2) / 100))
-	})
-
-	it('applique un taux de 4,20% aux PLR', () => {
+	it('applique un abattement de 50% à l’assiette sociale si elle est inférieure au Pass mahorais', () => {
 		const e = engine.setSituation({
 			...defaultSituation,
-			'entreprise . activité': "'libérale'",
-			'entreprise . activité . libérale . réglementée': 'oui',
+			'indépendant . cotisations et contributions . assiette sociale':
+				'35000 €/an',
 		})
 
-		expect(e).toEvaluate(COTISATION, Math.round((60_000 * 4.2) / 100))
+		expect(e).toEvaluate(`${COTISATION} . assiette`, 35_000 / 2)
+	})
+
+	it('applique un abattement valant 50% du Pass mahorais à l’assiette sociale si elle est supérieure au Pass mahorais', () => {
+		const e = engine.setSituation({
+			...defaultSituation,
+			'indépendant . cotisations et contributions . assiette sociale':
+				'40000 €/an',
+		})
+		const Pass = e.evaluate('plafond sécurité sociale . annuel')
+			.nodeValue as number
+
+		expect(e).toEvaluate(
+			`${COTISATION} . assiette`,
+			40_000 - Math.round(Pass / 2)
+		)
+	})
+
+	it('applique un taux de 4,20%', () => {
+		const e = engine.setSituation({
+			...defaultSituation,
+			'indépendant . cotisations et contributions . cotisations . maladie-maternité et autonomie . assiette':
+				'30000 €/an',
+		})
+
+		expect(e).toEvaluate(COTISATION, Math.round((30_000 * 4.2) / 100))
 	})
 })
