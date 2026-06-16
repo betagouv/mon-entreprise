467f30c0135cc751be28d089c60379a8eaf93ea0
Alice Dahan <lilice.dhn@gmail.com>
2026-06-16 11:18:05 +0200

    a11y(comparateur): gère le saut à la partie détails

--- site/source/components/Simulateur/BoutonDétail.tsx
+++ site/source/components/Simulateur/BoutonDétail.tsx
@@ -2,13 +2,16 @@ import { useTranslation } from 'react-i18next'
 
 import { ArrowDownIcon, Button } from '@/design-system'
 
+export const ÉLÉMENT_DÉTAILS_ID = 'simulation-détail'
+
 export const BoutonDétail = () => {
 	const { t } = useTranslation()
 
-	const scrollToDétails = () =>
-		document
-			.getElementById('simulation-détail')
-			?.scrollIntoView({ behavior: 'smooth' })
+	const scrollToDétails = () => {
+		const détails = document.getElementById(ÉLÉMENT_DÉTAILS_ID)
+		détails?.scrollIntoView({ behavior: 'smooth' })
+		détails?.focus()
+	}
 
 	return (
 		<Button light color="secondary" size="XS" onPress={scrollToDétails}>
