8455ad6b68d38ccf6bf06fe960710698ba73d165
Alice Dahan <lilice.dhn@gmail.com>
2026-06-17 13:35:32 +0200

    refactor: simplifie le code d'affichage des résultats de recherche

--- site/source/components/search/SimulatorHits.tsx
+++ site/source/components/search/SimulatorHits.tsx
@@ -13,7 +13,6 @@ import {
 } from '@/design-system'
 import { MergedSimulatorDataValues } from '@/hooks/useCurrentSimulatorData'
 import { useNavigationOrigin } from '@/hooks/useNavigationOrigin'
-import { useSitePaths } from '@/sitePaths'
 
 import { FromTop } from '../ui/animate'
 import { Highlight } from './Hightlight'
@@ -21,33 +20,27 @@ import { Highlight } from './Hightlight'
 type AlgoliaSimulatorHit = Hit<{
 	icône: string
 	title: string
+	path: MergedSimulatorDataValues['path']
 	pathId: MergedSimulatorDataValues['pathId']
+	tooltip?: MergedSimulatorDataValues['tooltip']
 }>
 
 type SimulatorHitsProps = {
 	hits: Array<AlgoliaSimulatorHit>
 }
 
-const SimulateurCardHit = ({
-	hit,
-	path,
-	tooltip,
-}: {
-	path: MergedSimulatorDataValues['path'] | '/'
-	tooltip?: MergedSimulatorDataValues['tooltip']
-	hit: AlgoliaSimulatorHit
-}) => {
+const SimulateurCardHit = ({ hit }: { hit: AlgoliaSimulatorHit }) => {
 	const [, setNavigationOrigin] = useNavigationOrigin()
 
 	return (
 		<StyledSmallCard
 			icon={<Emoji emoji={hit.icône} />}
-			to={{ pathname: path }}
+			to={{ pathname: hit.path ?? '/' }}
 			onPress={() => setNavigationOrigin({ fromSimulateurs: true })}
 			title={
 				<p>
 					<Highlight hit={hit} attribute="title" />{' '}
-					{tooltip && <InfoBulle description={tooltip} />}
+					{hit.tooltip && <InfoBulle description={hit.tooltip} />}
 				</p>
 			}
 		/>
@@ -61,51 +54,37 @@ const StyledSmallCard = styled(SmallCard)`
 export const SimulatorHits = connectHits<
 	{ hits: AlgoliaSimulatorHit[] },
 	AlgoliaSimulatorHit
->(({ hits }: SimulatorHitsProps) => {
-	const { absoluteSitePaths } = useSitePaths()
-
-	const getPath = (hit: AlgoliaSimulatorHit) =>
-		hit.pathId
-			.split('.')
-			.reduce<Record<string, unknown> | null>(
-				(acc, curr) =>
-					(acc && curr in acc && (acc[curr] as Record<string, unknown>)) ||
-					null,
-				absoluteSitePaths
-			) as MergedSimulatorDataValues['path'] | null
-
-	return (
-		<>
-			<H3 as="h2">
-				<Trans>Simulateurs</Trans>
-			</H3>
-			{!hits.length && (
-				<FromTop>
-					<SmallBody $grey>
-						<Trans>
-							Aucun résultat ne correspond à votre recherche. Essayez avec
-							d'autres mots-clés.
-						</Trans>
-					</SmallBody>
-				</FromTop>
+>(({ hits }: SimulatorHitsProps) => (
+	<>
+		<H3 as="h2">
+			<Trans>Simulateurs</Trans>
+		</H3>
+		{!hits.length && (
+			<FromTop>
+				<SmallBody $grey>
+					<Trans>
+						Aucun résultat ne correspond à votre recherche. Essayez avec
+						d'autres mots-clés.
+					</Trans>
+				</SmallBody>
+			</FromTop>
+		)}
+		<Grid container spacing={2} as="ul" style={{ padding: 0 }}>
+			{hits.map(
+				(hit) =>
+					hit.pathId && (
+						<Grid
+							item
+							key={hit.objectID}
+							xs={12}
+							lg={6}
+							as="li"
+							style={{ listStyle: 'none' }}
+						>
+							<SimulateurCardHit hit={hit} />
+						</Grid>
+					)
 			)}
-			<Grid container spacing={2} as="ul" style={{ padding: 0 }}>
-				{hits.map(
-					(hit) =>
-						hit.pathId && (
-							<Grid
-								item
-								key={hit.objectID}
-								xs={12}
-								lg={6}
-								as="li"
-								style={{ listStyle: 'none' }}
-							>
-								<SimulateurCardHit hit={hit} path={getPath(hit) ?? '/'} />
-							</Grid>
-						)
-				)}
-			</Grid>
-		</>
-	)
-})
+		</Grid>
+	</>
+))
