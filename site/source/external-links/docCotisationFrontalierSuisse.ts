dba82ea85e6aae381f68113c66dc0b9481f754a7
Jalil Arfaoui <jalil@arfaoui.net>
2026-06-29 16:50:32 +0200

    refactor(frontalier-suisse): liens externes en fichiers dédiés et sources Urssaf

---
+++ site/source/external-links/docCotisationFrontalierSuisse.ts
@@ -0,0 +1,9 @@
+import { URSSAF } from '@/utils/logos'
+
+export const docCotisationFrontalierSuisse = {
+	url: 'https://www.urssaf.fr/accueil/particulier/travailleur-frontalier-suisse/declarer-payer-cotisations.html#ancre-le-calcul-de-votre-cotisation',
+	title: 'Déclarer et payer votre cotisation maladie',
+	description:
+		'Le détail du calcul de la cotisation et les modalités de déclaration et de paiement.',
+	logo: URSSAF,
+}
