c557714603b87b0465d9d38fc39276d0bf868db6
Jalil Arfaoui <jalil@arfaoui.net>
2026-06-29 16:50:32 +0200

    refactor(frontalier-suisse): liens externes en fichiers dédiés et sources Urssaf

---
+++ site/source/external-links/docFrontalierSuisse.ts
@@ -0,0 +1,9 @@
+import { URSSAF } from '@/utils/logos'
+
+export const docFrontalierSuisse = {
+	url: 'https://www.urssaf.fr/accueil/particulier/travailleur-frontalier-suisse.html',
+	title: 'Travailleurs frontaliers en Suisse',
+	description:
+		"Affiliation, droit d'option et paiement de votre cotisation maladie auprès de l'Urssaf.",
+	logo: URSSAF,
+}
