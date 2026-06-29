1d30ba041f5788e48e53e90c18aa253aa0f5ef32
Jalil Arfaoui <jalil@arfaoui.net>
2026-06-29 22:52:23 +0200

    feat(frontalier-suisse): prend en compte la fin d'affiliation

---
+++ site/source/pages/simulateurs/cotisation-maladie-frontalier-suisse/questions/DateFinAffiliationQuestion.tsx
@@ -0,0 +1,39 @@
+import * as O from 'effect/Option'
+import { useCallback } from 'react'
+
+import { ComposantQuestion } from '@/components/Simulation/ComposantQuestion'
+import {
+	SituationFrontalierSuisse,
+	useFrontalierSuisse,
+} from '@/contextes/frontalier-suisse'
+import { DateField } from '@/design-system'
+
+export const DateFinAffiliationQuestion: ComposantQuestion<
+	SituationFrontalierSuisse
+> = () => {
+	const { situation, set } = useFrontalierSuisse()
+
+	const handleChange = useCallback(
+		(date: Date | undefined) => set.dateFinAffiliation(O.fromNullable(date)),
+		[set]
+	)
+
+	return (
+		<DateField
+			defaultSelected={O.getOrUndefined(situation.dateFinAffiliation)}
+			onChange={handleChange}
+		/>
+	)
+}
+
+DateFinAffiliationQuestion._tag = 'QuestionFournie'
+DateFinAffiliationQuestion.id = 'date-fin-affiliation'
+DateFinAffiliationQuestion.libellé = (t) =>
+	t(
+		'pages.simulateurs.cotisation-maladie-frontalier-suisse.questions.date-fin-affiliation.libellé',
+		'À quelle date votre affiliation prend-elle fin ?'
+	)
+DateFinAffiliationQuestion.applicable = (situation) =>
+	O.isSome(situation.dateAffiliation)
+DateFinAffiliationQuestion.répondue = (situation) =>
+	O.isSome(situation.dateFinAffiliation)
