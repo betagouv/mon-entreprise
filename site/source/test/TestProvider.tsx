1733a1be485014a4fbd4c82f7311fbff67f9dc2e
Jalil Arfaoui <jalil@arfaoui.net>
2026-07-27 17:19:28 +0200

    refactor(test): store Redux par-instance dans TestProvider pour isoler les tests

--- site/source/test/TestProvider.tsx
+++ site/source/test/TestProvider.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useMemo } from 'react'
+import { ReactNode, useMemo, useState } from 'react'
 import { HelmetProvider } from 'react-helmet-async'
 import { I18nextProvider } from 'react-i18next'
 import { Provider as ReduxProvider } from 'react-redux'
@@ -10,8 +10,6 @@ import { createI18nClient } from '@/locales/i18n-client'
 import { AvailableLang } from '@/locales/langue'
 import { makeStore } from '@/store/store'
 
-const testStore = makeStore()
-
 export const TestProvider = ({
 	children,
 	langue = 'fr',
@@ -20,11 +18,12 @@ export const TestProvider = ({
 	langue?: AvailableLang
 }) => {
 	const i18n = useMemo(() => createI18nClient(langue), [langue])
+	const [store] = useState(() => makeStore())
 
 	return (
 		<HelmetProvider>
 			<I18nextProvider i18n={i18n}>
-				<ReduxProvider store={testStore}>
+				<ReduxProvider store={store}>
 					<DesignSystemThemeProvider>
 						<BrowserRouter>
 							<ReactRouterNavigationProvider>
