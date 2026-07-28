18847d8c6e37dac9026ed942d26bf742cafd1bf3
logic-fabric <lowic@logic-fabric.fr>
2026-07-28 10:59:14 +0200

    feat(Next): add <PageHeader /> in home page

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -1,8 +1,9 @@
 'use client'
 
-import { useTranslation } from 'react-i18next'
+import { Trans, useTranslation } from 'react-i18next'
 
-import { Button, Container } from '@/design-system'
+import PageHeader from '@/components/PageHeader'
+import { Body, Button, Container, Intro, Strong } from '@/design-system'
 import { useDarkMode } from '@/hooks/useDarkMode'
 
 export default function Home() {
@@ -19,6 +20,26 @@ export default function Home() {
 					? t('app.basculerLight', 'Basculer en mode light')
 					: t('app.basculerDark', 'Basculer en mode dark')}
 			</Button>
+
+			<PageHeader
+				titre={t('landing.title', "L'assistant officiel des entrepreneurs")}
+				picture="/images/home-banner-illustration.svg"
+			>
+				<Intro $xxl>
+					<Trans i18nKey="landing.subtitle">
+						Des <Strong>assistants et simulateurs</Strong> pour obtenir des{' '}
+						<Strong>réponses personnalisées</Strong> à vos questions sur la{' '}
+						création et la gestion de votre entreprise.
+					</Trans>
+				</Intro>
+
+				<Body>
+					{t(
+						'pages.simulateurs.accueil.header',
+						'Tous les simulateurs sur ce site sont maintenus à jour avec les dernières évolutions législatives.'
+					)}
+				</Body>
+			</PageHeader>
 		</Container>
 	)
 }
