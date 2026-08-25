e193b4d2b2334e3d87f59fc75a5cd16771ec5dc5
Jalil Arfaoui <jalil@arfaoui.net>
2026-08-25 11:03:12 +0200

    refactor(Montant): supprime MontantPonctuel.moins devenue sans usage

--- site/source/domaine/MontantPonctuel.ts
+++ site/source/domaine/MontantPonctuel.ts
@@ -1,5 +1,3 @@
-import { dual } from 'effect/Function'
-
 import { montant, Montant } from './Montant'
 import { UnitéMonétairePonctuelle } from './Unites'
 
@@ -10,19 +8,3 @@ export const euros = (valeur: number): Montant<'€'> => montant(valeur, '€')
 export const eurosParTitreRestaurant = (
 	valeur: number
 ): Montant<'€/titre-restaurant'> => montant(valeur, '€/titre-restaurant')
-
-export const moins = dual<
-	<U extends UnitéMonétairePonctuelle>(
-		montantB: Montant<U>
-	) => (montantA: Montant<U>) => Montant<U>,
-	<U extends UnitéMonétairePonctuelle>(
-		montantA: Montant<U>,
-		montantB: Montant<U>
-	) => Montant<U>
->(
-	2,
-	<U extends UnitéMonétairePonctuelle>(
-		montantA: Montant<U>,
-		montantB: Montant<U>
-	): Montant<U> => montant(montantA.valeur - montantB.valeur, montantA.unité)
-)
