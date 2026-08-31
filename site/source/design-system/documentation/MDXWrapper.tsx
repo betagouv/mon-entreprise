90ff231e09679521a150a1bb7d0ac3980256d6ef
Alice Dahan <lilice.dhn@gmail.com>
2026-08-31 16:49:06 +0200

    refactor: crée un mapping partagé pour le rendering des composants en Markdown et MDX

--- site/source/design-system/documentation/MDXWrapper.tsx
+++ site/source/design-system/documentation/MDXWrapper.tsx
@@ -2,24 +2,7 @@ import { MDXProvider } from '@mdx-js/react'
 import { ReactNode } from 'react'
 import { styled } from 'styled-components'
 
-import ImgRenderer from '../markdown/ImgRenderer'
-import TextRenderer from '../markdown/TextRenderer'
-import {
-	Body,
-	Code,
-	H1,
-	H2,
-	H3,
-	H4,
-	H5,
-	H6,
-	Li,
-	Ol,
-	Pre,
-	Strong,
-	U,
-	Ul,
-} from '../typography'
+import { componentsRendering } from '../componentsRendering'
 
 const Blockquote = styled.blockquote`
 	margin: ${({ theme }) => `${theme.spacings.md} 0`};
@@ -32,32 +15,17 @@ const Blockquote = styled.blockquote`
 	font-style: italic;
 `
 
-export const components = {
-	h1: H1,
-	h2: H2,
-	h3: H3,
-	h4: H4,
-	h5: H5,
-	h6: H6,
-	p: Body,
-	strong: Strong,
-	u: U,
-	ul: Ul,
-	ol: Ol,
-	li: Li,
-	code: Code,
-	pre: Pre,
-	span: TextRenderer,
-	blockquote: Blockquote,
-	img: ImgRenderer,
-}
-
 type Props = {
 	children: ReactNode
 }
 
 export const MDXWrapper = ({ children }: Props) => (
-	<MDXProvider components={components}>
+	<MDXProvider
+		components={{
+			...componentsRendering,
+			blockquote: Blockquote,
+		}}
+	>
 		<MDXContent>{children}</MDXContent>
 	</MDXProvider>
 )
