528cb9a472b5748064e6d61d8ddcd271acdc360d
Alice Dahan <lilice.dhn@gmail.com>
2026-09-02 10:54:39 +0200

    fix(développeur): corrige la traduction de la page API

--- site/source/pages/integration/components/EtapesPourReproduireUnCalcul.tsx
+++ site/source/pages/integration/components/EtapesPourReproduireUnCalcul.tsx
@@ -14,12 +14,12 @@ export default function ÉtapesPourReproduireUnCalcul({ mode }: Props) {
 		<Ol>
 			<Li>
 				<Body>
-					<Trans i18nKey="pages.développeur.components.StepByStep.1">
+					<Trans i18nKey="pages.développeur.components.étapes-pour-reproduire-un-calcul.1">
 						<Strong>
 							Choisir le simulateur en fonction du calcul qui nous intéresse
 						</Strong>
 						<br />
-						Par exemple le{' '}
+						Par exemple, le{' '}
 						<Link to={useSitePaths().absoluteSitePaths.simulateurs.salarié}>
 							simulateur salarié
 						</Link>{' '}
@@ -29,20 +29,23 @@ export default function ÉtapesPourReproduireUnCalcul({ mode }: Props) {
 			</Li>
 			<Li>
 				<Body>
-					<Trans i18nKey="pages.développeur.components.StepByStep.2">
+					<Trans i18nKey="pages.développeur.components.étapes-pour-reproduire-un-calcul.2">
 						<Strong>
 							Effectuer une simulation avec les données que l'on souhaite
 							réutiliser
 						</Strong>
 						<br />
-						Par exemple un ou une{' '}
+						Par exemple, un ou une{' '}
 						<Link
 							to={{
 								pathname: useSitePaths().absoluteSitePaths.simulateurs.salarié,
 								search:
 									'salari%C3%A9+.+contrat=%27CDI%27&salari%C3%A9+.+contrat+.+salaire+brut=3400+%E2%82%AC%2Fmois&salari%C3%A9+.+contrat+.+statut+cadre=oui&salari%C3%A9+.+r%C3%A9mun%C3%A9ration+.+frais+professionnels+.+titres-restaurant=oui',
 							}}
-							aria-label="un cadre à 3400 € brut avec des titres-restaurants, accéder au simulateur salarié avec les données pré-remplies"
+							aria-label={t(
+								'pages.développeur.components.étapes-pour-reproduire-un-calcul.aria-label.exemple-simulation',
+								'un cadre à 3400 € brut avec des titres-restaurants, accéder au simulateur salarié avec les données pré-remplies'
+							)}
 						>
 							cadre à 3&nbsp;400&nbsp;€ brut avec des titres-restaurants
 						</Link>
@@ -52,21 +55,20 @@ export default function ÉtapesPourReproduireUnCalcul({ mode }: Props) {
 			</Li>
 			<Li>
 				<Body>
-					<Trans i18nKey="pages.développeur.components.StepByStep.3">
+					<Trans i18nKey="pages.développeur.components.étapes-pour-reproduire-un-calcul.3">
 						<Strong>
 							Aller sur la page de documentation de la donnée à calculer
 						</Strong>
 						<br />
-						Par exemple en cliquant sur «&nbsp;Salaire net&nbsp;» dans le
-						simulateur, ou en recherchant «&nbsp;Salaire net&nbsp;» dans la
-						recherche en haut à droite.
+						Par exemple, en cliquant sur «&nbsp;Salaire net&nbsp;» dans le
+						simulateur.
 					</Trans>
 				</Body>
 			</Li>
 			{mode === 'npm' && (
 				<Li>
 					<Body>
-						<Trans i18nKey="pages.développeur.components.StepByStep.4-npm">
+						<Trans i18nKey="pages.développeur.components.étapes-pour-reproduire-un-calcul.4-npm">
 							<Strong>
 								Copiez l'extrait de code personnalisé et intégrez-le dans votre
 								application
@@ -81,7 +83,7 @@ export default function ÉtapesPourReproduireUnCalcul({ mode }: Props) {
 			{mode === 'api' && (
 				<Li>
 					<Body>
-						<Trans i18nKey="pages.développeur.components.StepByStep.4-api">
+						<Trans i18nKey="pages.développeur.components.étapes-pour-reproduire-un-calcul.4-api">
 							<Strong>
 								Dans la section «&nbsp;Réutiliser ce calcul&nbsp;», vous
 								trouverez la requête API à copier-coller sous forme d'appel{' '}
@@ -96,7 +98,7 @@ export default function ÉtapesPourReproduireUnCalcul({ mode }: Props) {
 			)}
 			<Li>
 				<Body>
-					<Trans i18nKey="pages.développeur.components.StepByStep.5">
+					<Trans i18nKey="pages.développeur.components.étapes-pour-reproduire-un-calcul.5">
 						<Strong>
 							(facultatif) Modifiez les valeurs de la situation pour paramétrer
 							le calcul selon vos besoins
@@ -107,11 +109,11 @@ export default function ÉtapesPourReproduireUnCalcul({ mode }: Props) {
 						<Link
 							href="https://publi.codes/docs/manuel/principe-de-base"
 							aria-label={t(
-								'pages.développeur.bibliothèque.aria-label.publicodes.expression',
+								'pages.développeur.components.étapes-pour-reproduire-un-calcul.aria-label.expression-publicodes',
 								'expression ou objet publicodes, en savoir plus, nouvelle fenêtre'
 							)}
 						>
-							expression ou objet publicodes
+							expression ou objet Publicodes
 						</Link>
 						.
 					</Trans>
