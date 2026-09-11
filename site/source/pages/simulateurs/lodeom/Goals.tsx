22eecf4237b4446414e2ddd542d07f9bbd3b1717
Alice Dahan <lilice.dhn@gmail.com>
2026-09-11 11:31:32 +0200

    refactor(lodeom): extrait une fonction 'zoneAvecRépartitionEtRégularisation'

--- site/source/pages/simulateurs/lodeom/Goals.tsx
+++ site/source/pages/simulateurs/lodeom/Goals.tsx
@@ -14,7 +14,10 @@ import { DottedName } from '@/domaine/publicodes/DottedName'
 import { quantitéToNumber } from '@/domaine/Quantite'
 import { useBarèmeLodeom } from '@/hooks/useBarèmeLodeom'
 import useYear from '@/hooks/useYear'
-import { useZoneLodeom } from '@/hooks/useZoneLodeom'
+import {
+	useZoneLodeom,
+	zoneAvecRépartitionEtRégularisation,
+} from '@/hooks/useZoneLodeom'
 import EffectifSwitch from '@/pages/simulateurs/lodeom/components/EffectifSwitch'
 import RéductionMoisParMois from '@/pages/simulateurs/lodeom/components/RéductionMoisParMois'
 import RégularisationSwitch from '@/pages/simulateurs/lodeom/components/RégularisationSwitch'
@@ -48,7 +51,8 @@ export default function LodeomSimulationGoals() {
 
 	const currentZone = useZoneLodeom()
 	const currentBarème = useBarèmeLodeom()
-	const withRépartitionAndRégularisation = currentZone !== 'zone deux'
+	const withRépartitionAndRégularisation =
+		zoneAvecRépartitionEtRégularisation(currentZone)
 
 	const [lodeomMoisParMoisData, setData] = useState<MonthState[]>(
 		initialRéductionMoisParMois
@@ -167,7 +171,7 @@ export default function LodeomSimulationGoals() {
 				<>
 					<ZoneSwitch />
 					<BarèmeSwitch />
-					{currentZone !== 'zone deux' && (
+					{withRépartitionAndRégularisation && (
 						<>
 							<RégularisationSwitch
 								régularisationMethod={régularisationMethod}
