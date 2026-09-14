146579ba83d03eface4f7abdf06ae467ba073438
Alice Dahan <lilice.dhn@gmail.com>
2026-09-14 11:27:51 +0200

    style: modifie les couleurs de Tableau

--- site/source/design-system/documentation/Tableau/index.tsx
+++ site/source/design-system/documentation/Tableau/index.tsx
@@ -5,37 +5,48 @@ export const Tableau = styled.table`
 	border-collapse: collapse;
 	margin: ${({ theme }) => theme.spacings.lg} 0;
 
+	th,
+	td {
+		padding: ${({ theme }) => `${theme.spacings.sm} ${theme.spacings.md}`};
+	}
+
 	th {
 		background-color: ${({ theme }) =>
 			theme.darkMode
 				? theme.colors.bases.primary[800]
-				: theme.colors.bases.primary[100]};
+				: theme.colors.bases.primary[200]};
 		color: ${({ theme }) =>
 			theme.darkMode
 				? theme.colors.bases.primary[100]
 				: theme.colors.bases.primary[800]};
 		font-weight: 700;
-		padding: ${({ theme }) => `${theme.spacings.sm} ${theme.spacings.md}`};
-		border: 1px solid ${({ theme }) => theme.colors.bases.primary[300]};
+		border: 1px solid
+			${({ theme }) =>
+				theme.darkMode
+					? theme.colors.bases.primary[700]
+					: theme.colors.bases.primary[300]};
 		text-align: left;
 	}
 
 	td {
-		padding: ${({ theme }) => `${theme.spacings.sm} ${theme.spacings.md}`};
-		border: 1px solid ${({ theme }) => theme.colors.extended.grey[300]};
+		border: 1px solid
+			${({ theme }) =>
+				theme.darkMode
+					? theme.colors.extended.dark[500]
+					: theme.colors.extended.grey[400]};
 	}
 
 	tbody tr:nth-child(even) {
 		background-color: ${({ theme }) =>
 			theme.darkMode
 				? theme.colors.extended.dark[600]
-				: theme.colors.extended.grey[100]};
+				: theme.colors.extended.grey[300]};
 	}
 
 	tbody tr:hover {
 		background-color: ${({ theme }) =>
 			theme.darkMode
-				? theme.colors.extended.grey[600]
+				? theme.colors.extended.dark[500]
 				: theme.colors.bases.primary[100]};
 	}
 `
