52b23914cd7c18e8ac70a7a73ed07dd8ca09da5a
Alice Dahan <lilice.dhn@gmail.com>
2026-09-14 12:56:14 +0200

    fix(lodeom): réinitialise les données mois par mois au changement de zone

--- site/source/pages/simulateurs/lodeom/Goals.tsx
+++ site/source/pages/simulateurs/lodeom/Goals.tsx
@@ -71,6 +71,10 @@ export default function LodeomSimulationGoals() {
 		'salarié . cotisations . exonérations . lodeom . code régularisation'
 	).nodeValue as string
 
+	useEffect(() => {
+		setData(initialRéductionMoisParMois)
+	}, [currentZone])
+
 	const getNumberFromQuantitéPublicodes = (dottedName: DottedName) =>
 		pipe(
 			engine.evaluate(dottedName),
