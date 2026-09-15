f52e83ccfd9df27902bfa97f9e82cfad29cbab9c
Alice Dahan <lilice.dhn@gmail.com>
2026-09-15 11:42:19 +0200

    refactor(accueil): crée un composant QuiSommesNous

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -4,18 +4,10 @@ import { Trans, useTranslation } from 'react-i18next'
 import { styled } from 'styled-components'
 
 import PageHeader from '@/components/PageHeader'
+import { QuiSommesNous } from '@/components/QuiSommesNous'
 import { SimulateurCard } from '@/components/SimulateurCard'
 import { ForceThemeProvider } from '@/components/utils/DarkModeContext'
-import {
-	Body,
-	Container,
-	Grid,
-	H2,
-	H3,
-	Intro,
-	Link,
-	Strong,
-} from '@/design-system'
+import { Body, Container, Grid, H2, H3, Intro, Strong } from '@/design-system'
 import { useSimulatorsMetadata } from '@/hooks/useSimulatorsMetadata'
 import SearchOrCreate from '@/pages/_landing/SearchOrCreate'
 
@@ -222,71 +214,7 @@ export default function Home() {
 				<SearchOrCreate />
 			</ListeDesStatutsEtRechercheEntrepriseContainer>
 
-			<AboutContainer
-				backgroundColor={(theme) =>
-					theme.darkMode
-						? theme.colors.extended.dark[700]
-						: theme.colors.bases.primary[100]
-				}
-			>
-				<AboutGrid container columnSpacing={6}>
-					<DecorativeImageHiddenOnMobile item md={4} lg={3} xl={2}>
-						<img src="/images/home-about-decorative.svg" alt="" />
-					</DecorativeImageHiddenOnMobile>
-
-					<Grid item md={8} lg={9} xl={10}>
-						<Trans i18nKey="pages.landing.aboutUs">
-							<H2>Qui sommes-nous ?</H2>
-
-							<Body>
-								Nous sommes une{' '}
-								<Link
-									aria-label={t(
-										'aria-label.équipe',
-										'petite équipe, nouvelle fenêtre'
-									)}
-									href="https://beta.gouv.fr/startups/mon-entreprise.html#equipe"
-								>
-									petite équipe
-								</Link>{' '}
-								autonome et pluridisciplinaire au sein de l’
-								<Link
-									href="https://www.urssaf.fr"
-									aria-label={t(
-										'aria-label.urssaf',
-										'Urssaf, nouvelle fenêtre'
-									)}
-								>
-									Urssaf
-								</Link>
-								. Nous avons à cœur d’être au près de vos besoins afin
-								d’améliorer en permanence ce site conformément au{' '}
-								<Link
-									href="https://beta.gouv.fr/manifeste"
-									aria-label={t(
-										'aria-label.beta-gouv',
-										'manifeste beta.gouv.fr, nouvelle fenêtre'
-									)}
-								>
-									manifeste beta.gouv.fr
-								</Link>
-								.
-							</Body>
-
-							<Body>
-								Nous avons développé ce site pour accompagner les créateurs
-								d’entreprise dans le développement de leur activité.
-							</Body>
-
-							<Body>
-								Notre objectif est de lever toutes les incertitudes vis à vis de
-								l’administration afin que vous puissiez vous concentrer sur ce
-								qui compte : votre activité.
-							</Body>
-						</Trans>
-					</Grid>
-				</AboutGrid>
-			</AboutContainer>
+			<QuiSommesNous imgSrc="/images/home-about-decorative.svg" />
 		</>
 	)
 }
@@ -302,30 +230,10 @@ const ExplorerLesStatutsContainer = styled(Container)`
 const ListeDesStatutsEtRechercheEntrepriseContainer = styled(Container)`
 	padding-bottom: ${({ theme }) => theme.spacings.xl};
 `
-const AboutContainer = styled(Container)`
-	padding-bottom: ${({ theme }) => theme.spacings.xl};
-`
 
 const BlocListeDesStatuts = styled.div`
 	display: flex;
 	flex-direction: column;
 	row-gap: ${({ theme }) => theme.spacings.lg};
 	padding-bottom: ${({ theme }) => theme.spacings.lg};
-`
-
-const AboutGrid = styled(Grid)`
-	align-items: end;
-`
-
-const DecorativeImageHiddenOnMobile = styled(Grid)`
-	display: none;
-
-	img {
-		width: 100%;
-		padding-bottom: ${({ theme }) => theme.spacings.sm};
-	}
-
-	@media (min-width: ${({ theme }) => theme.breakpointsWidth.md}) {
-		display: block;
-	}
 `
