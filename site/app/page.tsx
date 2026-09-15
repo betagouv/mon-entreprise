7dddebdbebd06281842e9657b09c62143d46f079
Alice Dahan <lilice.dhn@gmail.com>
2026-09-15 10:40:38 +0200

    refactor(accueil): utilise des sections

--- site/app/page.tsx
+++ site/app/page.tsx
@@ -50,251 +50,269 @@ export default function Home() {
 				</PageHeader>
 			</Container>
 
-			<ContainerWithXxlPaddingBottom
-				forceTheme="dark"
-				backgroundColor={(theme) => theme.colors.bases.primary[600]}
-			>
-				<H2>
-					{t('pages.landing.section.explore-status', 'Explorer les statuts')}
-				</H2>
-
-				<Grid role="list" container spacing={4}>
-					<ForceThemeProvider forceTheme="default">
-						<SimulateurCard
-							role="listitem"
-							streched
-							{...simulators['choix-statut']}
-						/>
-
-						<SimulateurCard
-							role="listitem"
-							streched
-							{...simulators['comparaison-statuts']}
-						/>
-					</ForceThemeProvider>
-				</Grid>
-			</ContainerWithXxlPaddingBottom>
-
-			<Container>
-				<H2>{t('pages.landing.section.by-status', 'Par statut')}</H2>
-
-				<section>
-					<H3>
-						{t(
-							'pages.landing.sub-section.employees-and-employers',
-							'Travailleurs salariés et employeurs'
-						)}
-					</H3>
+			<section aria-labelledby="explorer-statuts">
+				<ContainerWithXxlPaddingBottom
+					forceTheme="dark"
+					backgroundColor={(theme) => theme.colors.bases.primary[600]}
+				>
+					<H2 id="explorer-statuts">
+						{t('pages.landing.section.explore-status', 'Explorer les statuts')}
+					</H2>
 
 					<Grid role="list" container spacing={4}>
-						<SimulateurCard
-							role="listitem"
-							niveauDeTitre="h4"
-							streched
-							{...simulators.salarié}
-						/>
+						<ForceThemeProvider forceTheme="default">
+							<SimulateurCard
+								role="listitem"
+								streched
+								{...simulators['choix-statut']}
+							/>
+
+							<SimulateurCard
+								role="listitem"
+								streched
+								{...simulators['comparaison-statuts']}
+							/>
+						</ForceThemeProvider>
 					</Grid>
-				</section>
+				</ContainerWithXxlPaddingBottom>
+			</section>
 
-				<section>
-					<H3>
+			<section aria-labelledby="par-statut">
+				<Container>
+					<H2 id="par-statut">
+						{t('pages.landing.section.by-status', 'Par statut')}
+					</H2>
+
+					<section aria-labelledby="salariees-et-employeuses">
+						<H3 id="salariees-et-employeuses">
+							{t(
+								'pages.landing.sub-section.employees-and-employers',
+								'Travailleurs salariés et employeurs'
+							)}
+						</H3>
+
+						<Grid role="list" container spacing={4}>
+							<SimulateurCard
+								role="listitem"
+								niveauDeTitre="h4"
+								streched
+								{...simulators.salarié}
+							/>
+						</Grid>
+					</section>
+
+					<section aria-labelledby="TI">
+						<H3 id="TI">
+							{t(
+								'pages.landing.sub-section.self-employed',
+								'Travailleurs indépendants'
+							)}
+						</H3>
+
+						<Grid role="list" container spacing={4}>
+							<SimulateurCard
+								role="listitem"
+								niveauDeTitre="h4"
+								darkerBackground
+								sansDescription
+								{...simulators['auto-entrepreneur']}
+							/>
+
+							<SimulateurCard
+								role="listitem"
+								niveauDeTitre="h4"
+								darkerBackground
+								sansDescription
+								{...simulators['entreprise-individuelle']}
+								précision={t(
+									'pages.landing.précision.ei',
+									'(hors auto-entrepreneur)'
+								)}
+							/>
+
+							<SimulateurCard
+								role="listitem"
+								niveauDeTitre="h4"
+								darkerBackground
+								sansDescription
+								{...simulators.eurl}
+							/>
+
+							<SimulateurCard
+								role="listitem"
+								niveauDeTitre="h4"
+								darkerBackground
+								sansDescription
+								{...simulators.sasu}
+							/>
+						</Grid>
+					</section>
+				</Container>
+			</section>
+
+			<section aria-labelledby="par-profession">
+				<Container>
+					<H2 id="par-profession">
+						{t('pages.landing.section.by-occupation', 'Par profession')}
+					</H2>
+
+					<Body>
 						{t(
-							'pages.landing.sub-section.self-employed',
-							'Travailleurs indépendants'
+							'pages.landing.sub-section.self-employed-description',
+							"Calculez vos cotisations et votre revenu net après impôt à partir du chiffre d'affaires et inversement"
 						)}
-					</H3>
+					</Body>
 
 					<Grid role="list" container spacing={4}>
 						<SimulateurCard
 							role="listitem"
-							niveauDeTitre="h4"
 							darkerBackground
 							sansDescription
-							{...simulators['auto-entrepreneur']}
+							{...simulators.artisan}
 						/>
 
 						<SimulateurCard
 							role="listitem"
-							niveauDeTitre="h4"
 							darkerBackground
 							sansDescription
-							{...simulators['entreprise-individuelle']}
+							{...simulators.commerçant}
+						/>
+
+						<SimulateurCard
+							role="listitem"
+							darkerBackground
+							sansDescription
+							{...simulators['profession-libérale']}
+						/>
+
+						<SimulateurCard
+							role="listitem"
+							darkerBackground
+							sansDescription
+							{...simulators['artiste-auteur']}
 							précision={t(
-								'pages.landing.précision.ei',
-								'(hors auto-entrepreneur)'
+								'pages.landing.précision.artiste-auteur',
+								'(hors intermittents du spectacle)'
 							)}
 						/>
+					</Grid>
+				</Container>
+			</section>
+
+			<section aria-labelledby="autres-outils">
+				<ContainerWithLgPaddingBottom>
+					<H2 id="autres-outils">
+						{t('pages.landing.section.other-tools', 'Autres outils')}
+					</H2>
+
+					<Grid role="list" container spacing={4}>
+						<SimulateurCard
+							role="listitem"
+							{...simulators['activité-partielle']}
+						/>
+
+						<SimulateurCard role="listitem" {...simulators.is} />
+
+						<SimulateurCard role="listitem" {...simulators.dividendes} />
 
 						<SimulateurCard
 							role="listitem"
-							niveauDeTitre="h4"
-							darkerBackground
-							sansDescription
-							{...simulators.eurl}
+							{...simulators['coût-création-entreprise']}
 						/>
 
 						<SimulateurCard
 							role="listitem"
-							niveauDeTitre="h4"
-							darkerBackground
-							sansDescription
-							{...simulators.sasu}
+							{...simulators['recherche-code-ape']}
+						/>
+
+						<SimulateurCard
+							role="listitem"
+							{...simulators['cessation-activité']}
+						/>
+
+						<SimulateurCard role="listitem" {...simulators.lodeom} />
+
+						<SimulateurCard
+							role="listitem"
+							{...simulators['location-de-logement-meublé']}
 						/>
 					</Grid>
-				</section>
-			</Container>
+				</ContainerWithLgPaddingBottom>
+			</section>
 
-			<Container>
-				<H2>{t('pages.landing.section.by-occupation', 'Par profession')}</H2>
+			<section aria-labelledby="recherche-entreprise">
+				<ContainerWithXxlPaddingBottom>
+					<SearchOrCreate id="recherche-entreprise" />
+				</ContainerWithXxlPaddingBottom>
+			</section>
 
-				<Body>
-					{t(
-						'pages.landing.sub-section.self-employed-description',
-						"Calculez vos cotisations et votre revenu net après impôt à partir du chiffre d'affaires et inversement"
-					)}
-				</Body>
+			<section aria-labelledby="qui-sommes-nous">
+				<ContainerWithLgPaddingBottom
+					backgroundColor={(theme) =>
+						theme.darkMode
+							? theme.colors.extended.dark[700]
+							: theme.colors.bases.primary[100]
+					}
+				>
+					<StyledAboutGrid container spacing={4}>
+						<StyledDecorativeImageHiddenOnMobile item xs={2} md={2}>
+							<img src="/images/home-about-decorative.svg" alt="" />
+						</StyledDecorativeImageHiddenOnMobile>
 
-				<Grid role="list" container spacing={4}>
-					<SimulateurCard
-						role="listitem"
-						darkerBackground
-						sansDescription
-						{...simulators.artisan}
-					/>
+						<Grid item xs={10} md={10}>
+							<Trans i18nKey="pages.landing.aboutUs">
+								<H2 id="qui-sommes-nous">Qui sommes-nous ?</H2>
 
-					<SimulateurCard
-						role="listitem"
-						darkerBackground
-						sansDescription
-						{...simulators.commerçant}
-					/>
+								<Body>
+									Nous sommes une{' '}
+									<Link
+										aria-label={t(
+											'aria-label.équipe',
+											'petite équipe, nouvelle fenêtre'
+										)}
+										href="https://beta.gouv.fr/startups/mon-entreprise.html#equipe"
+									>
+										petite équipe
+									</Link>{' '}
+									autonome et pluridisciplinaire au sein de l’
+									<Link
+										href="https://www.urssaf.fr"
+										aria-label={t(
+											'aria-label.urssaf',
+											'Urssaf, nouvelle fenêtre'
+										)}
+									>
+										Urssaf
+									</Link>
+									. Nous avons à cœur d’être au près de vos besoins afin
+									d’améliorer en permanence ce site conformément au{' '}
+									<Link
+										href="https://beta.gouv.fr/manifeste"
+										aria-label={t(
+											'aria-label.beta-gouv',
+											'manifeste beta.gouv.fr, nouvelle fenêtre'
+										)}
+									>
+										manifeste beta.gouv.fr
+									</Link>
+									.
+								</Body>
 
-					<SimulateurCard
-						role="listitem"
-						darkerBackground
-						sansDescription
-						{...simulators['profession-libérale']}
-					/>
+								<Body>
+									Nous avons développé ce site pour accompagner les créateurs
+									d’entreprise dans le développement de leur activité.
+								</Body>
 
-					<SimulateurCard
-						role="listitem"
-						darkerBackground
-						sansDescription
-						{...simulators['artiste-auteur']}
-						précision={t(
-							'pages.landing.précision.artiste-auteur',
-							'(hors intermittents du spectacle)'
-						)}
-					/>
-				</Grid>
-			</Container>
-
-			<ContainerWithLgPaddingBottom>
-				<H2>{t('pages.landing.section.other-tools', 'Autres outils')}</H2>
-
-				<Grid role="list" container spacing={4}>
-					<SimulateurCard
-						role="listitem"
-						{...simulators['activité-partielle']}
-					/>
-
-					<SimulateurCard role="listitem" {...simulators.is} />
-
-					<SimulateurCard role="listitem" {...simulators.dividendes} />
-
-					<SimulateurCard
-						role="listitem"
-						{...simulators['coût-création-entreprise']}
-					/>
-
-					<SimulateurCard
-						role="listitem"
-						{...simulators['recherche-code-ape']}
-					/>
-
-					<SimulateurCard
-						role="listitem"
-						{...simulators['cessation-activité']}
-					/>
-
-					<SimulateurCard role="listitem" {...simulators.lodeom} />
-
-					<SimulateurCard
-						role="listitem"
-						{...simulators['location-de-logement-meublé']}
-					/>
-				</Grid>
-			</ContainerWithLgPaddingBottom>
-
-			<ContainerWithXxlPaddingBottom>
-				<SearchOrCreate />
-			</ContainerWithXxlPaddingBottom>
-
-			<ContainerWithLgPaddingBottom
-				backgroundColor={(theme) =>
-					theme.darkMode
-						? theme.colors.extended.dark[700]
-						: theme.colors.bases.primary[100]
-				}
-			>
-				<StyledAboutGrid container spacing={4}>
-					<StyledDecorativeImageHiddenOnMobile item xs={2} md={2}>
-						<img src="/images/home-about-decorative.svg" alt="" />
-					</StyledDecorativeImageHiddenOnMobile>
-
-					<Grid item xs={10} md={10}>
-						<Trans i18nKey="pages.landing.aboutUs">
-							<H2>Qui sommes-nous ?</H2>
-
-							<Body>
-								Nous sommes une{' '}
-								<Link
-									aria-label={t(
-										'aria-label.équipe',
-										'petite équipe, nouvelle fenêtre'
-									)}
-									href="https://beta.gouv.fr/startups/mon-entreprise.html#equipe"
-								>
-									petite équipe
-								</Link>{' '}
-								autonome et pluridisciplinaire au sein de l’
-								<Link
-									href="https://www.urssaf.fr"
-									aria-label={t(
-										'aria-label.urssaf',
-										'Urssaf, nouvelle fenêtre'
-									)}
-								>
-									Urssaf
-								</Link>
-								. Nous avons à cœur d’être au près de vos besoins afin
-								d’améliorer en permanence ce site conformément au{' '}
-								<Link
-									href="https://beta.gouv.fr/manifeste"
-									aria-label={t(
-										'aria-label.beta-gouv',
-										'manifeste beta.gouv.fr, nouvelle fenêtre'
-									)}
-								>
-									manifeste beta.gouv.fr
-								</Link>
-								.
-							</Body>
-
-							<Body>
-								Nous avons développé ce site pour accompagner les créateurs
-								d’entreprise dans le développement de leur activité.
-							</Body>
-
-							<Body>
-								Notre objectif est de lever toutes les incertitudes vis à vis de
-								l’administration afin que vous puissiez vous concentrer sur ce
-								qui compte : votre activité.
-							</Body>
-						</Trans>
-					</Grid>
-				</StyledAboutGrid>
-			</ContainerWithLgPaddingBottom>
+								<Body>
+									Notre objectif est de lever toutes les incertitudes vis à vis
+									de l’administration afin que vous puissiez vous concentrer sur
+									ce qui compte : votre activité.
+								</Body>
+							</Trans>
+						</Grid>
+					</StyledAboutGrid>
+				</ContainerWithLgPaddingBottom>
+			</section>
 		</>
 	)
 }
