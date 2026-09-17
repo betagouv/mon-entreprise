71d2486b679f0edd2516ce4008dfe5e1ca72e3aa
Jalil Arfaoui <jalil@arfaoui.net>
2026-09-17 09:44:50 +0200

    refactor(documentation): clarifie les composants de références et leur vocabulaire

--- site/source/components/documentation/References/useReferencesAAfficher.test.ts
+++ site/source/components/documentation/References/useReferencesAAfficher.test.ts
@@ -1,6 +1,6 @@
 import { describe, expect, it } from 'vitest'
 
-import { référencesÀAfficher } from './references'
+import { référencesÀAfficher } from './useReferencesAAfficher'
 
 describe('référencesÀAfficher', () => {
 	const références = {
